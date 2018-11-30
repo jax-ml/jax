709cfe905da28a68afe480b1c047438775960aa1
Dougal Maclaurin <dougalm@google.com>
2018-11-30 16:17:54 -0500

    Set default TF log level to "1" to avoid reporting things like CPU frequency at import time. Also import jax.numpy in __init__.py because it has side effects that set up the infix operator overloading.

--- jax/__init__.py
+++ jax/__init__.py
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+os.environ.setdefault('TF_CPP_MIN_LOG_LEVEL', '1')
 from jax.api import *
+import jax.numpy as np  # side-effecting import sets up operator overloads
