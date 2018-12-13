8a02714a4a6e89f8dc23ebcbf0e9903a3012262d
Matthew Johnson <mattjj@google.com>
2018-12-12 20:31:25 -0800

    bump version number for pypi

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jax',
-    version='0.1.8',
+    version='0.1.9',
     description='Differentiate, compile, and transform Numpy code.',
     author='JAX team',
     author_email='jax-dev@google.com',
