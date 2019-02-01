9be87cae27d360541fb42745261ba1cc6f39b833
Peter Hawkins <phawkins@google.com>
2019-01-31 19:27:55 -0500

    Work around missing ReduceWindow cases in current Jaxlib release.

--- jax/numpy/lax_numpy.py
+++ jax/numpy/lax_numpy.py
@@ -917,7 +917,7 @@ def _make_cumulative_reduction(onp_reduction, window_reduce, init_val,
     window_dims = [1] * num_dims
     window_dims[axis] = a_shape[axis]
     return window_reduce(
-       a, window_dims, strides, xla_bridge.get_xla_client().PaddingType.VALID )
+       a, window_dims, strides, xla_bridge.get_xla_client().PaddingType.VALID)
 
   return cumulative_reduction
 
