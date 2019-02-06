9338d3d704d3869b3c98178d0095cfa8ae29f9a8
Peter Hawkins <phawkins@google.com>
2019-02-06 09:05:53 -0500

    Implement np.heaviside.

--- tests/lax_numpy_test.py
+++ tests/lax_numpy_test.py
@@ -120,6 +120,7 @@ JAX_COMPOUND_OP_RECORDS = [
               test_name="expm1_large"),
     op_record("expm1", 1, number_dtypes, all_shapes, jtu.rand_small_positive(), []),
     op_record("floor_divide", 2, number_dtypes, all_shapes, jtu.rand_nonzero(), ["rev"]),
+    op_record("heaviside", 2, default_dtypes, all_shapes, jtu.rand_default(), []),
     op_record("hypot", 2, default_dtypes, all_shapes, jtu.rand_default(), []),
     op_record("kron", 2, number_dtypes, nonempty_shapes, jtu.rand_default(), []),
     op_record("outer", 2, number_dtypes, all_shapes, jtu.rand_default(), []),
