6f2f0d98c4a830f37b520ef5b517cfdc2875c567
Peter Hawkins <phawkins@google.com>
2019-02-08 08:31:16 -0500

    Bump Jaxlib version to 0.1.7.

--- build/setup.py
+++ build/setup.py
@@ -20,7 +20,7 @@ binary_libs = [os.path.basename(f) for f in glob('jaxlib/*.so*')]
 
 setup(
     name='jaxlib',
-    version='0.1.6',
+    version='0.1.7',
     description='XLA library for JAX',
     author='JAX team',
     author_email='jax-dev@google.com',
