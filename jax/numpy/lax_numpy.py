9425fa812aa6a8e7f927855396299868c74c5f56
Matthew Johnson <mattjj@google.com>
2019-02-13 08:14:32 -0800

    add 'long' and 'complex' to pyval promotion logic

--- jax/numpy/lax_numpy.py
+++ jax/numpy/lax_numpy.py
@@ -146,7 +146,8 @@ def _promote_dtypes(*args):
   if len(args) < 2:
     return args
   else:
-    from_dtypes = (x if type(x) in (int, float) else _dtype(x) for x in args)
+    from_dtypes = (x if type(x) in (int, float, long, complex) else _dtype(x)
+                   for x in args)
     to_dtype = xla_bridge.canonicalize_dtype(result_type(*from_dtypes))
     return [lax.convert_element_type(x, to_dtype)
             if _dtype(x) != to_dtype else x for x in args]
