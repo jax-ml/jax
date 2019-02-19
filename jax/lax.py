81d43ee0832691d6e3a473028e19eba16bf527bc
Peter Hawkins <phawkins@google.com>
2019-02-19 11:59:37 -0500

    Use neg(t) rather than -t in sub_p transpose rule.

--- jax/lax.py
+++ jax/lax.py
@@ -1019,7 +1019,7 @@ ad.primitive_transposes[add_p] = _add_transpose
 
 def _sub_transpose(t, x, y):
   assert x is None and y is None  # computation must be linear, not affine
-  return [t, -t]
+  return [t, neg(t)]
 
 sub_p = standard_binop([_num, _num], 'sub')
 ad.defjvp(sub_p,
