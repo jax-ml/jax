1e0a9378f0a32c36009a05809348695e2c44dd2a
Matthew Johnson <mattjj@google.com>
2019-03-07 17:35:13 -0800

    fix typo

--- jax/lax.py
+++ jax/lax.py
@@ -3670,7 +3670,7 @@ def _while_loop_batching_rule(batched_args, batch_dims, aval_out, cond_jaxpr,
   # See https://github.com/google/jax/issues/441 for a discussion.
   # To batch a while_loop, we need to do some masking, since the elements of the
   # batch may run for different numbers of iterations. We perform that masking
-  # usnig lax.select, and keep the loop running so long as any of the batch
+  # using lax.select, and keep the loop running so long as any of the batch
   # elements need by effectively using an np.any(...) in the cond_fun.
   # The basic strategy here is to lift `cond_jaxpr` and `body_jaxpr` back into
   # traceable Python functions using `core.eval_jaxpr`. Then we can batch them
