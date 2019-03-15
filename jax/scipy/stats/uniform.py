6a53204653d56007399f0bee310b9fde0c4fea61
fehiepsi <fehiepsi@gmail.com>
2019-03-14 23:13:30 -0400

    no need to import onp

--- jax/scipy/stats/uniform.py
+++ jax/scipy/stats/uniform.py
@@ -16,7 +16,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as onp
 import scipy.stats as osp_stats
 
 from ... import lax
