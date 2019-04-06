31f38aa4b69539a38919da1e945c10d5c90f61a6
Matthew Johnson <mattjj@google.com>
2019-04-05 19:44:38 -0700

    call_initial_test passes!
    Co-authored-by: Dougal Maclaurin <dougalm@google.com>

--- jax/initial_style.py
+++ jax/initial_style.py
@@ -18,7 +18,8 @@ def pvals_with_zeros(zero_components, aval):
     return pe.PartialVal((aval, core.unit))
   elif isinstance(zero_components, ZeroTuple):
     avals, consts = unzip(map, pvals_with_zeros, zero_components, aval)
-    return pe.PartialVal((AbstractTuple(avals), core.JaxprTracerTuple(consts)))
+    return pe.PartialVal((core.AbstractTuple(avals),
+                          core.JaxprTracerTuple(consts)))
 
 def transpose_jaxpr(jaxpr, avals, tangent_components):
   assert False
@@ -42,25 +43,50 @@ def _call_initial_impl(consts, *args, **kwargs):
 
 def _call_initial_jvp(primals, tangents, jaxpr):
   avals = [aval for (aval, _) in map(_abstractify, primals)]
-  zeros = map(ad.get_zeros, tangents)
-  jaxpr_jvp, consts, zeros_out = ad.jvp_jaxpr(jaxpr, avals, zeros)
-  primal_out, tangent_out = call_initial_p.bind(core.pack(consts),
-                                                core.pack(primals),
-                                                core.pack(tangents),
-                                                jaxpr=jaxpr_jvp)
-  tangent_out_zeros = ad.put_zeros(ad.TangentTuple, zeros_out, tangent_out)
+  where_zeros = map(ad.get_zeros, tangents)
+  nonzero_tangents = ad.strip_zeros(core.unit, core.pack, where_zeros,
+                                    tangents)
+  jaxpr_jvp, consts, where_zeros_out = ad.jvp_jaxpr(jaxpr, avals, where_zeros)
+  primal_out, tangent_out = call_initial_p.bind(
+      core.pack(consts), core.pack(primals),
+      core.pack(nonzero_tangents), jaxpr=jaxpr_jvp)
+  tangent_out_zeros = ad.put_zeros(ad.TangentTuple, where_zeros_out,
+                                   tangent_out)
   return primal_out, tangent_out_zeros
+
+def is_const(x):
+  if x is None:
+    return True
+  elif type(x) is pe.JaxprTracerTuple:
+    return tuple(map(is_const, x))
+  elif isinstance(x, core.AbstractValue):
+    return False
+  else:
+    raise TypeError(type(x))
+
+def as_aval(pv, const):
+  if pv is None:
+    pv, _ = _abstractify(const)
+    return pv
+  elif type(pv) is pe.JaxprTracerTuple:
+    return map(as_aval, pv, const)
+  elif isinstance(pv, core.AbstractValue):
+    return pv
+  else:
+    raise TypeError((pv, const))
 
 def _call_initial_partial_eval(trace, *tracers, **kwargs):
   jaxpr = kwargs.pop('jaxpr')
   in_pvs, in_consts = unzip2([t.pval for t in tracers])
   first_components = map(is_const, in_pvs)
   avals = map(as_aval, in_pvs, in_consts)
-  jaxpr_1, jaxpr_2, aval_out, first_components_out = partial_eval_jaxpr(
+  jaxpr_1, jaxpr_2, out_pv, first_components_out = pe.partial_eval_jaxpr(
       jaxpr, avals, first_components)
-  call_initial_p.bind(*in_consts, jaxpr=jaxpr_2)
-  eqn = core.JaxprEqn(tracers, None, call_initial_p, (), False, dict(jaxpr=jaxpr_2))
-  return pe.JaxprTracer(self, PartialVal((aval_out, out_const)), eqn)
+  out_pv_const, consts = call_initial_p.bind(core.unit, *in_consts, jaxpr=jaxpr_1)
+  const_tracers = core.pack(map(trace.new_instantiated_const, consts))
+  eqn = core.JaxprEqn((const_tracers,) + tracers, None, call_initial_p, (), False,
+                      dict(jaxpr=jaxpr_2))
+  return pe.JaxprTracer(trace, pe.PartialVal((out_pv, out_pv_const)), eqn)
 
 
 def _call_initial_transpose():
