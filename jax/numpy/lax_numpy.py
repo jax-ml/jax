45b3c2fa1f23ab89a8a28c7ced60a5e4ebfee3bb
Matthew Johnson <mattjj@google.com>
2019-04-06 13:07:27 -0700

    tweak comments

--- jax/numpy/lax_numpy.py
+++ jax/numpy/lax_numpy.py
@@ -1264,14 +1264,14 @@ def arange(*args, **kwargs):
   if not args:
     raise TypeError("Required argument 'start' (pos 1) not found")  # same as numpy error
 
-  # if called like np.arange(N) we create a lazy lax._IotaConstant
+  # If called like np.arange(N), we create a lazy lax._IotaConstant.
   if len(args) == 1 and not kwargs:
     stop, = args
     dtype = dtype or _dtype(stop)
     if onp.issubdtype(dtype, onp.integer):
       return lax.iota(dtype, stop)  # avoids materializing
 
-  # fall back to instantiating an ndarray in host memory
+  # Fall back to instantiating an ndarray in host memory
   return onp.arange(*args, **kwargs)
 
 linspace = onp.linspace
