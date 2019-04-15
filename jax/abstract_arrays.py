aa5b036d6d69f10ce88d8745287c834054980d13
Matthew Johnson <mattjj@google.com>
2019-04-15 07:45:10 -0700

    misc python performance improvements

--- jax/abstract_arrays.py
+++ jax/abstract_arrays.py
@@ -44,13 +44,16 @@ class UnshapedArray(core.AbstractValue):
   array_abstraction_level = 3
 
   def __init__(self, dtype):
-    self.dtype = dtype
+    self.dtype = onp.dtype(xla_bridge.canonicalize_dtype(dtype))
 
   def __eq__(self, other):
     return type(self) is type(other) and self.dtype == other.dtype
 
   def __hash__(self):
-    return hash(str(self.dtype))
+    # can use hash(self.dtype) and rely on the fact that numpy reuses base dtype
+    # objects, e.g. `onp.zeros(3).dtype is onp.zeros(4).dtype`, or we can use
+    # the unique character code via hash(self.dtype.char)
+    return hash(self.dtype)
 
   def __repr__(self):
     return '{}({})'.format(self.__class__.__name__, self.str_short())
@@ -74,7 +77,7 @@ class UnshapedArray(core.AbstractValue):
       raise TypeError(other)
 
   def str_short(self):
-    return onp.dtype(self.dtype).name
+    return self.dtype.name
 
 
 class ShapedArray(UnshapedArray):
@@ -93,7 +96,10 @@ class ShapedArray(UnshapedArray):
             and self.dtype == other.dtype and self.shape == other.shape)
 
   def __hash__(self):
-    return hash((self.shape, str(self.dtype)))
+    # can use hash(self.dtype) and rely on the fact that numpy reuses base dtype
+    # objects, e.g. `onp.zeros(3).dtype is onp.zeros(4).dtype`, or we can use
+    # the unique character code via hash(self.dtype.char)
+    return hash((self.shape, self.dtype))
 
   def at_least_vspace(self):
     return self
@@ -107,9 +113,8 @@ class ShapedArray(UnshapedArray):
       raise TypeError(other)
 
   def str_short(self):
-    dtypestr = onp.dtype(self.dtype).name
     shapestr = ','.join(map(str, self.shape))
-    return '{}[{}]'.format(dtypestr, shapestr)
+    return '{}[{}]'.format(self.dtype.name, shapestr)
 
   def __len__(self):
     try:
