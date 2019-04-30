957782668a35d81bebc5769288919ff3748eedb1
Matthew Johnson <mattjj@google.com>
2019-04-30 11:52:33 -0700

    fix up other trace_to_subjaxpr call sites

--- jax/interpreters/partial_eval.py
+++ jax/interpreters/partial_eval.py
@@ -477,7 +477,7 @@ def eval_jaxpr_raw(jaxpr, consts, freevar_vals, *args):
 def compiled_call_impl(fun, *args):
   with new_master(JaxprTrace, True) as master:
     pvals = map(abstractify, args)
-    jaxpr, (pval, consts, env) = trace_to_subjaxpr(fun, master).call_wrapped(pvals)
+    jaxpr, (pval, consts, env) = trace_to_subjaxpr(fun, master, False).call_wrapped(pvals)
     jaxpr_ans = eval_jaxpr_raw(jaxpr, consts, env, *args)
     ans = merge_pvals(jaxpr_ans, pval)
     del master, pvals, pval, consts, env, jaxpr_ans, jaxpr
