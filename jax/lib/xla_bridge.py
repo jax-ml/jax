90a6f6ca21dd0a1c8ea4cee5d1e8b4816db74662
Matthew Johnson <mattjj@google.com>
2019-05-03 08:46:21 -0700

    xla_client.make_tuple gets backend

--- jax/lib/xla_bridge.py
+++ jax/lib/xla_bridge.py
@@ -187,7 +187,8 @@ def device_put_many(pyvals_and_devices):
     return [device_put(pyval, device) for (pyval, device) in pyvals_and_devices]
 
 def make_tuple(bufs, device_num=0):
-  return xla_client.Buffer.make_tuple(bufs, device=device_num)
+  return xla_client.Buffer.make_tuple(bufs, device=device_num,
+                                      backend=get_backend())
 
 
 Shape = xla_client.Shape        # pylint: disable=invalid-name
