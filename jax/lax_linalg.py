91a946d39c9ba1ae2884def085c427f51ad619d7
Sharad Vikram <sharad.vikram@gmail.com>
2019-05-04 15:05:11 -0700

    remove unnecessary hermetian; change ATOL for svd jvp x64 tests

--- jax/lax_linalg.py
+++ jax/lax_linalg.py
@@ -491,7 +491,7 @@ def svd_jvp_rule(primals, tangents, full_matrices, compute_uv):
   if m > n:
     dU = dU + (np.eye(m) - U.dot(Ut)).dot(dA).dot(V) / s_dim
   if n > m:
-    dV = dV + (np.eye(n) - V.dot(Vt)).dot(np.conj(dA).T).dot(U) / s_dim
+    dV = dV + (np.eye(n) - V.dot(Vt)).dot(dA.T).dot(U) / s_dim
   return core.pack((s, U, Vt)), core.pack((ds, dU, dV.T))
 
 def svd_cpu_translation_rule(c, operand, full_matrices, compute_uv):
