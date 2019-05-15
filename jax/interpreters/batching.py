140699ed7de017918f7049b35bae3e3f0ef4d080
Matthew Johnson <mattjj@google.com>
2019-05-15 07:25:03 -0700

    scan of vmap sketched out! cf. #716

--- jax/interpreters/batching.py
+++ jax/interpreters/batching.py
@@ -29,9 +29,9 @@ from ..core import Trace, Tracer, new_master, pack, AbstractTuple, JaxTuple
 from ..abstract_arrays import ShapedArray, make_shaped_array, array_types, raise_to_shaped
 from ..ad_util import add_jaxvals_p, zeros_like_p, zeros_like_jaxval
 from ..linear_util import transformation, transformation_with_aux, wrap_init
-from ..tree_util import register_pytree_node
 from ..util import unzip2, partial, safe_map
 from . import xla
+from . import partial_eval as pe
 
 map = safe_map
 
@@ -52,10 +52,10 @@ def batch_transform(size, in_dims, out_dim_dst, vals):
   with new_master(BatchTrace) as master:
     trace = BatchTrace(master, core.cur_sublevel())
     in_tracers = map(partial(BatchTracer, trace), vals, in_dims)
-    out_tracer = yield in_tracers, {}
-    out_tracer = trace.full_raise(out_tracer)
+    ans = yield in_tracers, {}
+    out_tracer = trace.full_raise(ans)
     out_val, out_dim = out_tracer.val, out_tracer.batch_dim
-    del master
+    del master, out_tracer
   yield moveaxis(size, out_dim_dst, out_dim, out_val)
 
 
@@ -75,6 +75,7 @@ class BatchTracer(Tracer):
   __slots__ = ['val', 'batch_dim']
 
   def __init__(self, trace, val, batch_dim):
+    assert core.skip_checks or type(batch_dim) in (int, tuple)
     self.trace = trace
     self.val = val
     self.batch_dim = batch_dim
@@ -365,3 +366,148 @@ def handle_scalar_broadcasting(nd, x, bdim):
     return x
   else:
     return x.reshape(x.shape + (1,) * (nd - x.ndim))
+
+
+def where_batched(bdim):
+  t = type(bdim)
+  if t is tuple:
+    return tuple(map(where_batched, bdim))
+  elif t in (int, type(None)):
+    return bdim is not None
+  else:
+    raise TypeError(t)
+
+def bools_to_bdims(bdim, batched_indicator_tree):
+  t = type(batched_indicator_tree)
+  if t is tuple:
+    return tuple(map(partial(bools_to_bdims, bdim), batched_indicator_tree))
+  elif t is bool:
+    return bdim if batched_indicator_tree else None
+  else:
+    raise TypeError(t)
+
+def instantiate_bdim(size, axis, instantiate, bdim, x):
+  """Instantiate or move a batch dimension to position `axis`.
+
+  Ensures that `x` is at least as high on the batched lattice as `instantiate`.
+
+  Args:
+    size: int, size of the axis to instantiate.
+    axis: int, where to instantiate or move the batch dimension.
+    instantiate: tuple-tree of booleans, where the tree structure is a prefix of
+      the tree structure in x, indicating whether to instantiate the batch
+      dimension at the corresponding subtree in x.
+    bdim: tuple-tree of ints or NoneTypes, with identical tree structure to
+      `instantaite`, indicating where the batch dimension exists in the
+      corresponding subtree of x.
+    x: JaxType value on which to instantiate or move batch dimensions.
+
+  Returns:
+    A new version of `x` with instantiated batch dimensions.
+  """
+  def _inst(instantiate, bdim, x):
+    if type(instantiate) is tuple:
+      assert type(bdim) is tuple  # instantiate at same granularity as bdim
+      return core.pack(map(_inst, instantiate, bdim, x))
+    elif type(instantiate) is bool:
+      if not instantiate:
+        if bdim is None:
+          return x
+        elif type(bdim) is int:
+          return moveaxis2(bdim, axis, x)
+        else:
+          raise TypeError(type(bdim))
+      else:
+        if bdim is None:
+          return broadcast2(size, axis, x)
+        elif type(bdim) is int:
+          return moveaxis2(bdim, axis, x)
+        else:
+          raise TypeError(type(bdim))
+    else:
+      raise TypeError(type(instantiate))
+
+  return _inst(instantiate, bdim, x)
+
+def moveaxis2(src, dst, x):
+  if src == dst:
+    return x
+  else:
+    return _moveaxis2(src, dst, x, get_aval(x))
+
+def _moveaxis2(src, dst, x, aval):
+  if type(aval) is JaxTuple:
+    return core.pack(map(partial(_moveaxis2, src, dst), x, aval))
+  else:
+    perm = [i for i in range(onp.ndim(x)) if i != src]
+    perm.insert(dst, src)
+    return x.transpose(perm)
+
+def broadcast2(size, axis, x):
+  return _broadcast2(size, axis, x, get_aval(x))
+
+def _broadcast2(size, axis, x, aval):
+  if type(aval) is JaxTuple:
+    return core.pack(map(partial(_broadcast2, size, axis), x, aval))
+  else:
+    # see comment at the top of this section
+    if isinstance(x, onp.ndarray) or onp.isscalar(x):
+      return onp.broadcast_to(x, (sz,) + onp.shape(x))
+    else:
+      return x.broadcast((sz,))  # should be a JAX arraylike
+
+def _promote_aval_rank(n, batched, aval):
+  assert isinstance(aval, core.AbstractValue)
+  if isinstance(aval, core.AbstractTuple):
+    t = type(batched)
+    if t is tuple:
+      return core.AbstractTuple(map(partial(_promote_aval_rank, n), batched, aval))
+    elif t is bool:
+      if batched:
+        return core.AbstractTuple(map(partial(_promote_aval_rank, n, batched), aval))
+      else:
+        return aval
+    else:
+      raise TypeError(t)
+  else:
+    if batched:
+      return ShapedArray((n,) + aval.shape, aval.dtype)
+    else:
+      return aval
+
+def batch_jaxpr(jaxpr, size, is_batched, instantiate):
+  f = wrap_init(core.jaxpr_as_fun(jaxpr))
+  f_batched, where_out_batched = batched_traceable(f, size, is_batched, instantiate)
+  in_avals = map(partial(_promote_aval_rank, size), is_batched, jaxpr.in_avals)
+  in_pvals = [pe.PartialVal((aval, core.unit)) for aval in in_avals]
+  jaxpr_out, pval_out, literals_out = pe.trace_to_jaxpr(f_batched, in_pvals,
+                                                        instantiate=True)
+  out_aval, _ = pval_out
+  jaxpr_out = core.TypedJaxpr(jaxpr_out, literals_out, in_avals, out_aval)
+  return jaxpr_out, where_out_batched()
+
+@transformation_with_aux
+def batched_traceable(size, is_batched, instantiate, *vals):
+  in_dims = bools_to_bdims(0, is_batched)
+  with new_master(BatchTrace) as master:
+    trace = BatchTrace(master, core.cur_sublevel())
+    in_tracers = map(partial(BatchTracer, trace), vals, in_dims)
+    ans = yield in_tracers, {}
+    out_tracer = trace.full_raise(ans)
+    out_val, out_dim = out_tracer.val, out_tracer.batch_dim
+    del master, out_tracer
+  out_val = instantiate_bdim(size, 0, instantiate, out_dim, out_val)
+  yield out_val, _binary_lattice_join(where_batched(out_dim), instantiate)
+
+def _binary_lattice_join(a, b):
+  t = (type(a), type(b))
+  if t == (tuple, tuple):
+    return tuple(map(_binary_lattice_join, a, b))
+  elif t == (tuple, bool):
+    return tuple(map(_binary_lattice_join, a, (b,) * len(a)))
+  elif t == (bool, tuple):
+    return tuple(map(_binary_lattice_join, (a,) * len(b), b))
+  elif t == (bool, bool):
+    return a or b
+  else:
+    raise TypeError((type(a), type(b)))
