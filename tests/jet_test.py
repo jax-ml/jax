38067d66c31970b312bd3923a0057d1c045efbae
jessebett <jessebett@gmail.com>
2019-08-13 14:15:30 -0400

    Incorrect at jvp based test

--- tests/jet_test.py
+++ tests/jet_test.py
@@ -1,31 +1,50 @@
+import numpy.random as npr
+
 from jax import jvp, jet
 import jax.numpy as np
 
-import numpy.random as npr
+npr.seed(1993)
 
+def jvps(f, primals, terms, n):
+  def j_0(f,v):
+    return lambda x: f(x)
+  def j_i(f,v):
+    return lambda x: jvp(f, (x, ), (v, ))[1]
 
-def jvps(f, x, v, n):
-    def j_i(f):
-        return lambda x: jvp(f,(x,),(v,))[1]
-    funcs = [f]
-    for i in range(n):
-        funcs.append(j_i(funcs[i]))
-    return [f(x) for f in funcs]
+  funcs = [f]
+  for i in range(n):
+    funcs.append(j_i(funcs[i],terms[i]))
+  return [f(primals) for f in funcs]
 
+def test_exp():
+  raise NotImplementedError
+
+def test_log():
+  raise NotImplementedError
+
+def test_tanh():
+  raise NotImplementedError
 
 def test_sin():
   x = 4.0
   vs = (1., 0., 0., 0.)
-  y, terms = jet(np.sin, (x,), [vs])
-  expected = jvps(np.sin, x, vs[0], 4)
+  y, terms = jet(np.sin, (x, ), [vs])
+  expected = jvps(np.sin, x, vs, 4)
   assert np.allclose(y, expected[0])
   assert all(map(np.allclose, terms, expected[1:]))
 
+def test_cos():
+  raise NotImplementedError
+
+def test_cos():
+  raise NotImplementedError
+
+## Test Combinations? 
 def test_sin_sin():
   x = 4.0
   vs = (1., 0., 0., 0.)
   f = lambda x: np.sin(np.sin(x))
-  y, terms = jet(f, (x,), [vs])
+  y, terms = jet(f, (x, ), [vs])
   expected = jvps(f, x, vs[0], 4)
   assert np.allclose(y, expected[0])
   assert all(map(np.allclose, terms, expected[1:]))
