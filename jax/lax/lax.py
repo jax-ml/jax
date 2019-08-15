932877dde69b907e251aedd2898efbae2053aa35
Peter Hawkins <phawkins@google.com>
2019-08-15 13:31:37 -0400

    Remove unnecessary reshape/concatenate in dynamic_slice_in_dim.

--- jax/lax/lax.py
+++ jax/lax/lax.py
@@ -1278,15 +1278,14 @@ def index_in_dim(operand, index, axis=0, keepdims=True):
 
 def dynamic_slice_in_dim(operand, start_index, slice_size, axis=0):
   """Convenience wrapper around dynamic_slice applying to one dimension."""
-  start_indices = [onp.array([0], dtype=_dtype(start_index))] * operand.ndim
+  start_indices = [0] * operand.ndim
   slice_sizes = list(operand.shape)
 
   axis = int(axis)
   axis_size = _const(start_index, operand.shape[axis])
-  start_indices[axis] = reshape(rem(start_index, axis_size), [1])
+  start_indices[axis] = rem(start_index, axis_size)
   slice_sizes[axis] = int(slice_size)
 
-  start_indices = concatenate(start_indices, 0)
   return dynamic_slice(operand, start_indices, slice_sizes)
 
 
