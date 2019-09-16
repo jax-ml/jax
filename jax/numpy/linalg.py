3e5f7869eaaeab1691f4b3c843c53f8d19bd0bab
David Pfau <pfau@google.com>
2019-09-16 21:27:55 +0100

    Fixed prefix for np.linalg.solve

--- jax/numpy/linalg.py
+++ jax/numpy/linalg.py
@@ -62,7 +62,7 @@ def svd(a, full_matrices=True, compute_uv=True):
 
 def _jvp_slogdet(g, ans, x):
   jvp_sign = np.zeros(x.shape[:-2])
-  jvp_logdet = np.trace(np.linalg.solve(x, g), axis1=-1, axis2=-2)
+  jvp_logdet = np.trace(solve(x, g), axis1=-1, axis2=-2)
   return jvp_sign, jvp_logdet
 
 
