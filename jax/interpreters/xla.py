51038457568380ef55d6d86cd54b55b853b8cf93
James Bradbury <jekbradbury@google.com>
2019-10-02 16:35:42 -0700

    unbreak debug_nans on jit

--- jax/interpreters/xla.py
+++ jax/interpreters/xla.py
@@ -171,12 +171,13 @@ def _execute_compiled_primitive(prim, compiled, backend, result_handler, *args):
   device_num, = compiled.DeviceOrdinals()
   input_bufs = [device_put(x, device_num, backend=backend) for x in args]
   out_buf = compiled.Execute(input_bufs)
-  if FLAGS.jax_debug_nans: check_nans(prim, out_buf)
+  if FLAGS.jax_debug_nans:
+    check_nans(prim, out_buf.destructure() if prim.multiple_results else out_buf)
   return result_handler(out_buf)
 
 def check_nans(prim, bufs):
   if prim.multiple_results:
-    for buf in bufs.destructure():
+    for buf in bufs:
       _check_nans(prim.name, buf.shape(), buf)
   else:
     _check_nans(prim.name, bufs.shape(), bufs)
