519356235618f3074b7870b36e64d192b4e48b7a
James Bradbury <jekbradbury@google.com>
2019-10-29 11:08:14 -0700

    split nesterov into its own optimizer

--- jax/experimental/optimizers.py
+++ jax/experimental/optimizers.py
@@ -200,12 +200,13 @@ def sgd(step_size):
   return init, update, get_params
 
 @optimizer
-def momentum(step_size, mass, nesterov=False):
-  """Construct optimizer triple for SGD with (optionally) Nesterov momentum.
+def momentum(step_size, mass):
+  """Construct optimizer triple for SGD with momentum.
 
   Args:
     step_size: positive scalar, or a callable representing a step size schedule
       that maps the iteration index to positive scalar.
+    mass: positive scalar representing the momentum coefficient.
 
   Returns:
     An (init_fun, update_fun, get_params) triple.
@@ -217,8 +218,34 @@ def momentum(step_size, mass, nesterov=False):
   def update(i, g, state):
     x, velocity = state
     velocity = mass * velocity + g
-    d = mass * velocity + g if nesterov else velocity
-    x = x - step_size(i) * d
+    x = x - step_size(i) * velocity
+    return x, velocity
+  def get_params(state):
+    x, _ = state
+    return x
+  return init, update, get_params
+
+
+@optimizer
+def nesterov(step_size, mass):
+  """Construct optimizer triple for SGD with Nesterov momentum.
+
+  Args:
+    step_size: positive scalar, or a callable representing a step size schedule
+      that maps the iteration index to positive scalar.
+    mass: positive scalar representing the momentum coefficient.
+
+  Returns:
+    An (init_fun, update_fun, get_params) triple.
+  """
+  step_size = make_schedule(step_size)
+  def init(x0):
+    v0 = np.zeros_like(x0)
+    return x0, v0
+  def update(i, g, state):
+    x, velocity = state
+    velocity = mass * velocity + g
+    x = x - step_size(i) * (mass * velocity + g)
     return x, velocity
   def get_params(state):
     x, _ = state
