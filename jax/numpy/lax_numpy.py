da6a474a63bea7d7d27f3ee112cff75be6693a74
Peter Hawkins <phawkins@google.com>
2019-11-26 22:47:03 -0500

    Simplify jax.numpy.tensordot by using lax.dot_general. (#1775)

--- jax/numpy/lax_numpy.py
+++ jax/numpy/lax_numpy.py
@@ -2161,38 +2161,35 @@ def vdot(a, b, precision=None):
 @_wraps(onp.tensordot, lax_description=_PRECISION_DOC)
 def tensordot(a, b, axes=2, precision=None):
   _check_arraylike("tensordot", a, b)
-  if not (ndim(a) >= 1 and ndim(b) >= 1):
+  a_ndim = ndim(a)
+  b_ndim = ndim(b)
+  if a_ndim < 1 or b_ndim < 1:
     msg = "tensordot requires a.ndim and b.dim to be at least 1, got {} and {}."
     raise TypeError(msg.format(ndim(a), ndim(b)))
 
+  a, b = _promote_dtypes(a, b)
   if type(axes) is int:
-    if axes == 0:
-      a, b = _promote_dtypes(a, b)
-      return lax.mul(lax.reshape(a, shape(a) + (1,) * ndim(b)),
-                     lax.reshape(b, (1,) * ndim(a) + shape(b)))
-    else:
-      a, b = _promote_dtypes(a, b)
-      a_reshape = lax.reshape(a, (_prod(a.shape[:-axes]), _prod(a.shape[-axes:])))
-      b_reshape = lax.reshape(b, (_prod(b.shape[:axes]), _prod(b.shape[axes:])))
-      out_reshape = lax.dot(a_reshape, b_reshape, precision=precision)
-      return lax.reshape(out_reshape, a.shape[:-axes] + b.shape[axes:])
+    if axes > _min(a_ndim, b_ndim):
+      msg = "Number of tensordot axes (axes {}) exceeds input ranks ({} and {})"
+      raise msg.format(axes, a.shape, b.shape)
+    contracting_dims = tuple(range(a_ndim - axes, a_ndim)), tuple(range(axes))
   elif type(axes) in (list, tuple) and len(axes) == 2:
     ax1, ax2 = axes
     if type(ax1) == type(ax2) == int:
-      a_transposed = moveaxis(a, ax1, -1) if ax1 != a.ndim - 1 else a
-      b_transposed = moveaxis(b, ax2, 0) if ax2 != 0 else b
-      return tensordot(a_transposed, b_transposed, 1, precision)
+      contracting_dims = ((_canonicalize_axis(ax1, a_ndim),),
+                          (_canonicalize_axis(ax2, b_ndim),))
     elif type(ax1) in (list, tuple) and type(ax2) in (list, tuple):
       if len(ax1) != len(ax2):
         msg = "tensordot requires axes lists to have equal length, got {} and {}."
         raise TypeError(msg.format(ax1, ax2))
-      num_axes = len(ax1)
-      a_transposed = moveaxis(a, ax1, tuple(range(a.ndim - num_axes, a.ndim)))
-      b_transposed = moveaxis(b, ax2, tuple(range(num_axes)))
-      return tensordot(a_transposed, b_transposed, num_axes, precision)
-  msg = ("tensordot axes argument must be an int, a pair of ints, or a pair of "
-         "lists/tuples of ints.")
-  raise TypeError(msg)
+      contracting_dims = (tuple(_canonicalize_axis(i, a_ndim) for i in ax1),
+                          tuple(_canonicalize_axis(i, b_ndim) for i in ax2))
+  else:
+    msg = ("tensordot axes argument must be an int, a pair of ints, or a pair "
+           "of lists/tuples of ints.")
+    raise TypeError(msg)
+  return lax.dot_general(a, b, (contracting_dims, ((), ())),
+                         precision=precision)
 
 
 @_wraps(onp.einsum, lax_description=_PRECISION_DOC)
