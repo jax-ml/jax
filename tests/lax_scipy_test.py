441ad4dbbdc5f38d9f61017621228d7dbf994a57
Peter Hawkins <phawkins@google.com>
2019-12-02 15:18:04 -0500

    Relax test tolerances for scipy test.

--- tests/lax_scipy_test.py
+++ tests/lax_scipy_test.py
@@ -129,7 +129,7 @@ class LaxBackedScipyTests(jtu.JaxTestCase):
     args = args_maker()
     self.assertAllClose(scipy_op(*args), lax_op(*args), atol=1e-3, rtol=1e-3,
                         check_dtypes=False)
-    self._CompileAndCheck(lax_op, args_maker, check_dtypes=True)
+    self._CompileAndCheck(lax_op, args_maker, check_dtypes=True, rtol=1e-5)
 
     if test_autodiff:
       jtu.check_grads(lax_op, args, order=1, atol=1e-3, rtol=3e-3, eps=1e-3)
