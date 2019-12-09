afb806f618451b1f39c4aa2a2fd402a9e9e1f9e0
Tuan Nguyen <anhtuan277@gmail.com>
2019-12-08 22:20:50 -0800

    Update sparse.py

--- jax/experimental/sparse.py
+++ jax/experimental/sparse.py
@@ -23,33 +23,37 @@ from .. import lax
 
 _T = lambda x: np.swapaxes(x, -1, -2)
 
-@_wraps(scipy.sparse.linalg.cg)
-def cg(A, b, x0=None, tol=1e-05, maxiter=None):
-  # solves for x where Ax = b  
-  N = np.shape(A)[0]
+def body_fun(matvec, p, x, r, k):
+  # inspired by https://en.wikipedia.org/wiki/Conjugate_gradient_method#Example_code_in_MATLAB_/_GNU_Octave
+  r_sq = np.matmul(_T(r), r)
+  Ap = matvec(p)
+  alpha = r_sq / np.matmul(_T(p), Ap)
+  x_new = x + alpha * p
+  r_new = r - alpha * Ap
+  r_sq_new = np.matmul(_T(r_new), r_new)
+  p_new = r_new + (r_sq_new / r_sq) * p
+  return matvec, p_new, x_new, r_new, k+1
+
+def _cg_solve(matvec, b, x0=None, tol=1e-05, maxiter=None):
+  N = np.shape(b)[0]
   if x0 is None:
     x_k = np.zeros(N)
   else:
     x_k = x0
     if not (np.shape(x0) == (N, 1) or np.shape(x0) == (N,)):
       raise ValueError('A and x have incompatible dimensions')
-  if maxiter is None:
-    maxiter = np.inf
-  r_k = b - np.matmul(A, x_k)
+    if maxiter is None:
+      maxiter = np.inf
+  r_k = b - matvec(x_k)
   p_k = r_k
   k = 0
-  A, p_k, x_k, r_k, k = lax.while_loop(
-    lambda r_k, k: (r_k < tol) & (k < maxiter), body_fun, (A, p_k, x_k, r_k, k)
-    )
+  matvec, p_k, x_k, r_k, k = lax.while_loop(
+    lambda r_k, k: (r_k < tol) & (k < maxiter), body_fun, (matvec, p_k, x_k, r_k, k)
+  )
   return x_k
 
-def body_fun(A, p, x, r, k):
-  # inspired by https://en.wikipedia.org/wiki/Conjugate_gradient_method#Example_code_in_MATLAB_/_GNU_Octave
-  r_sq = np.matmul(_T(r), r)
-  Ap = np.matmul(A, p)
-  alpha = r_sq / np.matmul(_T(p), Ap)
-  x_new = x + alpha * p
-  r_new = r - alpha * Ap
-  r_sq_new = np.matmul(_T(r_new), r_new)
-  p_new = r_new + (r_sq_new / r_sq) * p
-  return A, p_new, x_new, r_new, k+1
+@_wraps(scipy.sparse.linalg.cg)
+def cg(matvec, b):
+  # exposed as scipy.sparse.cg
+  return lax.custom_linear_solve(matvec, b, _cg_solve, symmetric=True)
+  
