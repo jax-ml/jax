f05c36d76dca4693795b4066e51324e9f2c30d07
Tuan Nguyen <anhtuan277@gmail.com>
2019-12-09 21:01:19 -0800

    add a test case for sparse pd matrix & add bigger dim

--- tests/lax_scipy_sparse_test.py
+++ tests/lax_scipy_sparse_test.py
@@ -18,7 +18,7 @@ from __future__ import print_function
 
 from functools import partial
 from absl.testing import parameterized
-
+from scipy.sparse import spdiags
 import numpy as onp
 import scipy.sparse.linalg as osp_sparse
 from jax import lax
@@ -28,7 +28,7 @@ from jax.experimental import sparse
 @parameterized.named_parameters(jtu.cases_from_list(
       {"testcase_name": "_{}D_{}".format(dim, onp.dtype(dtype).name),
        "dim": dim, "dtype": dtype}
-      for dim in [3, 5, 10]
+      for dim in [3, 7, 100, int(1e4)]
       for dtype in [onp.float32, onp.float64]))
 def test_linalg_sparse_cg(self, dim, dtype):
   def high_precision_dot(a, b):
@@ -42,8 +42,14 @@ def test_linalg_sparse_cg(self, dim, dtype):
   r = onp.random.RandomState(dim)
   square_mat = r.randn(dim, dim)
   a = onp.dot(square_mat, square_mat.T) + dim * onp.eye(dim)
+  diags = onp.array([0, -1, 2])
+  a_sparse = spdiags(a, diags, dim, dim).toarray()
   b = r.randn(dim)
   expected = osp_sparse.cg(a, b)
   actual = build_and_solve(a, b)
+  expected_sparse = osp_sparse.cg(a_sparse, b)
+  actual_sparse = build_and_solve(a_sparse, b)
   self.assertAllClose(expected, actual, atol=1e-5, check_dtypes=True)
+  self.assertAllClose(expected_sparse, actual_sparse, atol=1e-5, check_dtypes=True)
   jtu.check_grads(build_and_solve, (a, b), atol=1e-5, order=2, rtol=2e-3)
+  jtu.check_grads(build_and_solve, (a_sparse, b), atol=1e-5, order=2, rtol=2e-3)
