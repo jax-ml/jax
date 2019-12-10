85c93aafe4834bb637dd2fe36325dae557302683
Tuan Nguyen <anhtuan277@gmail.com>
2019-12-09 19:55:20 -0800

    Update lax_scipy_sparse_test.py

--- tests/lax_scipy_sparse_test.py
+++ tests/lax_scipy_sparse_test.py
@@ -16,29 +16,34 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from jax import lax
-from jax import numpy as np
-
-import unittest
-import numpy as onp
 from functools import partial
+from absl.testing import parameterized
+
+import numpy as onp
+import scipy.sparse.linalg as osp_sparse
+from jax import lax
 from jax import test_util as jtu
 from jax.experimental import sparse
-import scipy.sparse.linalg as osp_sparse
 
+@parameterized.named_parameters(jtu.cases_from_list(
+      {"testcase_name": "_{}D_{}".format(dim, onp.dtype(dtype).name),
+       "dim": dim, "dtype": dtype}
+      for dim in [1, 3, 5]
+      for dtype in [onp.float32, onp.float64]))
+def test_linalg_sparse_cg(self, dim, dtype):
+  def high_precision_dot(a, b):
+    return lax.dot(a, b, precision=lax.Precision.HIGHEST)
 
-def high_precision_dot(a, b):
-  return lax.dot(a, b, precision=lax.Precision.HIGHEST)
+  def build_and_solve(a, b):
+    # intentionally non-linear in a and b
+    matvec = partial(high_precision_dot, a)
+    return sparse.cg(matvec, b)
 
-def build_and_solve(a, b):
-  # intentionally non-linear in a and b
-  matvec = partial(high_precision_dot, np.exp(a))
-  return sparse.cg(matvec, np.cos(b))
-
-rng = onp.random.RandomState(0)
-a = rng.randn(2, 2)
-b = rng.randn(2)
-expected = osp_sparse.cg(np.exp(a), np.cos(b))
-actual = build_and_solve(a, b)
-self.assertAllClose(expected, actual, atol=1e-5, check_dtypes=True)
-jtu.check_grads(build_and_solve, (a, b), atol=1e-5, order=2, rtol=2e-3)
+  r = onp.random.RandomState(dim)
+  square_mat = r.randn(dim, dim)
+  a = onp.dot(square_mat, square_mat.T) + dim * onp.eye(dim)
+  b = r.randn(dim)
+  expected = osp_sparse.cg(a, b)
+  actual = build_and_solve(a, b)
+  self.assertAllClose(expected, actual, atol=1e-5, check_dtypes=True)
+  jtu.check_grads(build_and_solve, (a, b), atol=1e-5, order=2, rtol=2e-3)
