306eef5ba374121695db48d905c9ae349048fd28
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2020-01-03 15:08:33 -0500

    Issue1635 expm
    Implemented expm using Pade approximation based on tf.linalg.expm.

--- jax/scipy/linalg.py
+++ jax/scipy/linalg.py
@@ -26,7 +26,7 @@ from .. import lax_linalg
 from ..numpy.lax_numpy import _wraps
 from ..numpy import lax_numpy as np
 from ..numpy import linalg as np_linalg
-
+from ..api import custom_transforms, defvjp
 
 _T = lambda x: np.swapaxes(x, -1, -2)
 
@@ -264,3 +264,119 @@ def tril(m, k=0):
 @_wraps(scipy.linalg.triu)
 def triu(m, k=0):
   return np.triu(m, k)
+
+def expm(A):
+    """
+    Compute the matrix exponential using Pade approximation.
+    Based on TensorFlow's tf.linalg.expm
+
+    Args:
+    A : square matrix of type float32, float64, complex64, complex128
+    
+    Returns:
+    Matrix exponential of A , square matrix of same dimensions as A
+        
+
+    See: "The Scaling and Squaring Method for the Matrix
+    Exponential Revisited", Nicholas Higham, 2005.
+    
+    TODO: Write a version to exploit the sparsity of A
+    """
+    A = np.asarray(A)
+    if A.ndim != 2 or A.shape[0] != A.shape[1]:
+        raise ValueError('expected A to be a square matrix')
+    A_L1 = np_linalg.norm(A,1)
+    n_squarings = 0
+    def _nest_where(vals, cases):
+      assert len(vals) == len(cases) - 1
+      if len(vals) == 1:
+        return np.where(
+            np.less(A_L1, vals[0]), cases[0], cases[1])
+      else:
+        return np.where(
+            np.less(A_L1, vals[0]), cases[0],
+            _nest_where(vals[1:], cases[1:]))
+    if A.dtype == 'float64' or A.dtype == 'complex128':
+       U3,V3 = _pade3(A)
+       U5,V5 = _pade5(A)
+       U7,V7 = _pade7(A)
+       U9,V9 = _pade9(A)
+       maxnorm = 5.371920351148152
+       n_squarings = np.maximum(0, np.floor_divide(np.log2(A_L1 / maxnorm),1))
+       A = A / 2**n_squarings
+       U13,V13 = _pade13(A)
+       conds=np.array([1.495585217958292e-002, 2.539398330063230e-001, 9.504178996162932e-001, 2.097847961257068e+000])
+       U=_nest_where(conds,(U3,U5,U7,U9,U13))
+       V=_nest_where(conds,(V3,V5,V7,V9,V13))
+    elif A.dtype == 'float32' or A.dtype == 'complex64':
+        U3,V3 = _pade3(A)
+        U5,V5 = _pade5(A)
+        maxnorm = 3.925724783138660
+        n_squarings = np.maximum(0, np.floor_divide(np.log2(A_L1 / maxnorm),1))
+        A = A / 2**n_squarings
+        U7,V7 = _pade7(A)
+        conds=np.array([4.258730016922831e-001, 1.880152677804762e+000])
+        U=_nest_where(conds,(U3,U5,U7))
+        V=_nest_where(conds,(V3,V5,V7))
+    else:
+        raise TypeError("A.dtype={} is not supported.".format(A.dtype))
+    P = U + V  # p_m(A) : numerator
+    Q = -U + V # q_m(A) : denominator
+    R = np_linalg.solve(Q,P)
+    # squaring step to undo scaling
+    def my_body_fun(i,R):
+      return np.dot(R,R)
+    lower = np.zeros(1, dtype=n_squarings.dtype)
+    R = lax.fori_loop(lower[0],n_squarings,my_body_fun,R)
+    return R
+
+def _pade3(A):
+    b = (120., 60., 12., 1.)
+    ident = np.eye(*A.shape, dtype=A.dtype)
+    A2 = np.dot(A,A)
+    U = np.dot(A , (b[3]*A2 + b[1]*ident))
+    V = b[2]*A2 + b[0]*ident
+    return U,V
+
+def _pade5(A):
+    b = (30240., 15120., 3360., 420., 30., 1.)
+    ident = np.eye(*A.shape, dtype=A.dtype)
+    A2 = np.dot(A,A)
+    A4 = np.dot(A2,A2)
+    U = np.dot(A, b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U,V
+
+def _pade7(A):
+    b = (17297280., 8648640., 1995840., 277200., 25200., 1512., 56., 1.)
+    ident = np.eye(*A.shape, dtype=A.dtype)
+    A2 = np.dot(A,A)
+    A4 = np.dot(A2,A2)
+    A6 = np.dot(A4,A2)
+    U = np.dot(A, b[7]*A6 + b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = b[6]*A6 + b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U,V
+
+def _pade9(A):
+    b = (17643225600., 8821612800., 2075673600., 302702400., 30270240.,
+                2162160., 110880., 3960., 90., 1.)
+    ident = np.eye(*A.shape, dtype=A.dtype)
+    A2 = np.dot(A,A)
+    A4 = np.dot(A2,A2)
+    A6 = np.dot(A4,A2)
+    A8 = np.dot(A6,A2)
+    U = np.dot(A, b[9]*A8 + b[7]*A6 + b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = b[8]*A8 + b[6]*A6 + b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U,V
+
+def _pade13(A):
+    b = (64764752532480000., 32382376266240000., 7771770303897600.,
+    1187353796428800., 129060195264000., 10559470521600., 670442572800.,
+    33522128640., 1323241920., 40840800., 960960., 16380., 182., 1.)
+    ident = np.eye(*A.shape, dtype=A.dtype)
+    A2 = np.dot(A,A)
+    A4 = np.dot(A2,A2)
+    A6 = np.dot(A4,A2)
+    U = np.dot(A,np.dot(A6, b[13]*A6 + b[11]*A4 + b[9]*A2) + b[7]*A6 + b[5]*A4 + b[3]*A2 + b[1]*ident)
+    V = np.dot(A6, b[12]*A6 + b[10]*A4 + b[8]*A2) + b[6]*A6 + b[4]*A4 + b[2]*A2 + b[0]*ident
+    return U,V
