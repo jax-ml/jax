4b2a5a1f1bb4533f58929eb51eac3aa079a0be28
George Necula <necula@google.com>
2020-01-05 10:12:31 -0800

    Attempt a fix for PY2

--- tests/util_test.py
+++ tests/util_test.py
@@ -32,10 +32,12 @@ class UtilTest(jtu.JaxTestCase):
   def test_wrapped_fun_transforms(self):
     """Test a combination of transforms."""
 
-    def f(*args, factor=2):
+    def f(*args, **kwargs):
       """The function to be transformed.
       Scales the positional arguments by a factor.
       Takes only one keyword argument, the factor to scale by."""
+      factor = kwargs.pop('factor', 2)  # For PY2
+      assert not kwargs
       return tuple(a * factor for a in args)
 
     @lu.transformation_with_aux
