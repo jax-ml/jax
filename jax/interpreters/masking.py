d4a8bbb318914e54d94fd1e9254db663aa8981d7
Julius Kunze <juliuskunze@gmail.com>
2020-01-31 12:43:40 +0000

    Restore copyright, cleanup imports in masking.py

--- jax/interpreters/masking.py
+++ jax/interpreters/masking.py
@@ -1,11 +1,25 @@
-import string
-from collections import namedtuple, Counter
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from contextlib import contextmanager
+from collections import Counter, namedtuple
 from functools import partial
 from itertools import chain, product
 import operator as op
+import string
 
 import numpy as onp
-from contextlib import contextmanager
 
 from .. import abstract_arrays
 from .. import core
