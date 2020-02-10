bf99377bd57aeeef3c648340a400e1b44eda7340
Skye Wanderman-Milne <skyewm@google.com>
2020-02-09 16:48:50 -0800

    Add missing copyright notices.

--- jax/third_party/numpy/linalg.py
+++ jax/third_party/numpy/linalg.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as onp
 
 from jax.numpy import lax_numpy as np
@@ -82,7 +96,7 @@ def tensorsolve(a, b, axes=None):
       allaxes.insert(an, k)
 
     a = a.transpose(allaxes)
-  
+
   Q = a.shape[-(an - b.ndim):]
 
   prod = 1
@@ -91,8 +105,8 @@ def tensorsolve(a, b, axes=None):
 
   a = a.reshape(-1, prod)
   b = b.ravel()
-  
+
   res = np.asarray(la.solve(a, b))
   res = res.reshape(Q)
-  
+
   return res
