f710619aab5be522b0a1e0dda3246fe34db4303f
Peter Hawkins <phawkins@google.com>
2020-03-25 15:56:41 -0400

    Fix some missing cases of broadcasting in np.einsum.
    In particular, np.einsum allows one side of a batch or contracting dimension to have size 1 even if the other side has a non-1 size.
    
    Implement np.matmul in terms of np.einsum. This allows us to reuse einsum's logic for performing broadcasting without explicitly broadcasting the LHS and RHS together.

--- tests/lax_numpy_einsum_test.py
+++ tests/lax_numpy_einsum_test.py
@@ -315,6 +315,26 @@ class EinsumTest(jtu.JaxTestCase):
     self.assertAllClose(L, np.einsum('ntk,kd,dc->nc', S, W, V, optimize=path),
                         check_dtypes=False, rtol=rtol)
 
+  def test_contraction_broadcasting(self):
+    r = rng()
+    x = r.randn(3, 4, 5)
+    y = r.randn(3, 1, 6)
+    s = 'cij,cjk->cik'
+    self._check(s, x, y)
+
+  def test_batch_broadcasting(self):
+    r = rng()
+    x = r.randn(1, 4, 5)
+    y = r.randn(3, 5, 6)
+    s = 'cij,cjk->cik'
+    self._check(s, x, y)
+
+  def test_batch_and_contraction_broadcasting(self):
+    r = rng()
+    x = r.randn(1, 4, 5)
+    y = r.randn(3, 1, 6)
+    s = 'cij,cjk->cik'
+    self._check(s, x, y)
 
 if __name__ == '__main__':
   absltest.main()
