dd92a037131b85eaff80e13a517fb04781612be5
Stephan Hoyer <shoyer@google.com>
2020-04-09 10:18:07 -0700

    Docstring for test_util.check_grads (#2656)
    Fixes https://github.com/google/jax/issues/2648

--- jax/test_util.py
+++ jax/test_util.py
@@ -231,6 +231,24 @@ def check_vjp(f, f_vjp, args, atol=None, rtol=None, eps=EPS):
 
 def check_grads(f, args, order,
                 modes=["fwd", "rev"], atol=None, rtol=None, eps=None):
+  """Check gradients from automatic differentiation against finite differences.
+
+  Gradients are only checked in a single randomly chosen direction, which
+  ensures that the finite difference calculation does not become prohibitively
+  expensive even for large input/output spaces.
+
+  Args:
+    f: function to check at ``f(*args)``.
+    args: tuple of argument values.
+    order: forward and backwards gradients up to this order are checked.
+    modes: lists of gradient modes to check ('fwd' and/or 'rev').
+    atol: absolute tolerance for gradient equality.
+    rtol: relative tolerance for gradient equality.
+    eps: step size used for finite differences.
+
+  Raises:
+    AssertionError: if gradients do not match.
+  """
   args = tuple(args)
   eps = eps or EPS
 
