551db9576e6bf6c62ee224c0c1d2942552d8a52f
Mathis Gerdes <mathisgerdes@gmail.com>
2020-04-09 15:20:27 +0200

    Add more tests for np.roots function.
    - Include length 0 polynomial coefficients
    - Test strip_zeros=False argument
    - Test jit compiled version (only on cpu due to eigvals)
    - Confirm that adding leading zeros while skipping check
      for them results in nan's (expected behavior)

--- tests/polynomial_test.py
+++ tests/polynomial_test.py
@@ -19,7 +19,7 @@ from absl.testing import absltest
 from absl.testing import parameterized
 
 from jax import numpy as np
-from jax import test_util as jtu
+from jax import test_util as jtu, jit, partial
 
 from jax.config import config
 config.parse_flags_with_absl()
@@ -37,24 +37,103 @@ all_dtypes = real_dtypes + complex_dtypes
 class TestPolynomial(jtu.JaxTestCase):
 
   @parameterized.named_parameters(jtu.cases_from_list(
-    {"testcase_name": "dtype={}_len={}_leading={}_trailing={}".format(
-      jtu.format_shape_dtype_string((length,), dtype), length, leading, trailing),
-      "dtype": dtype, "length": length, "rng_factory": rng_factory, "leading": leading, "trailing": trailing}
-    for rng_factory in [jtu.rand_default]
+    {"testcase_name": "_dtype={}_leading={}_trailing={}".format(
+       jtu.format_shape_dtype_string((length+leading+trailing,), dtype),
+       leading, trailing),
+     "dtype": dtype, "rng_factory": rng_factory, "length": length,
+     "leading": leading, "trailing": trailing}
     for dtype in all_dtypes
-    for length in [3, 9, 10, 17]
-    for leading in [1, 2, 3, 5, 7, 10]
-    for trailing in [1, 2, 3, 5, 7, 10]))
-  def testRoots(self, length, dtype, rng_factory, leading, trailing):
+    for rng_factory in [jtu.rand_default]
+    for length in [0, 3, 9, 10, 17]
+    for leading in [0, 1, 2, 3, 5, 7, 10]
+    for trailing in [0, 1, 2, 3, 5, 7, 10]))
+  def testRoots(self, dtype, rng_factory, length, leading, trailing):
     rng = rng_factory()
+
     def args_maker():
       p = rng((length,), dtype)
-      return (np.concatenate([np.zeros(leading, p.dtype), p, np.zeros(trailing, p.dtype)]),)
+      return np.concatenate(
+        [np.zeros(leading, p.dtype), p, np.zeros(trailing, p.dtype)]),
 
     # order may differ (np.sort doesn't deal with complex numbers)
     np_fn = lambda arg: onp.sort(np.roots(arg))
     onp_fn = lambda arg: onp.sort(onp.roots(arg))
     self._CheckAgainstNumpy(onp_fn, np_fn, args_maker, check_dtypes=False)
+
+  @parameterized.named_parameters(jtu.cases_from_list(
+    {"testcase_name": "_dtype={}_trailing={}".format(
+        jtu.format_shape_dtype_string((length+trailing,), dtype), trailing),
+     "dtype": dtype, "rng_factory": rng_factory, "length": length,
+     "trailing": trailing}
+    for dtype in all_dtypes
+    for rng_factory in [jtu.rand_default]
+    for length in [0, 1, 3, 10]
+    for trailing in [0, 1, 3, 7]))
+  def testRootsNostrip(self, length, dtype, rng_factory, trailing):
+    rng = rng_factory()
+
+    def args_maker():
+      p = rng((length,), dtype)
+      if length != 0:
+        return np.concatenate([p, np.zeros(trailing, p.dtype)]),
+      else:
+        # adding trailing would make input invalid (start with zeros)
+        return p,
+
+    # order may differ (np.sort doesn't deal with complex numbers)
+    np_fn = lambda arg: onp.sort(np.roots(arg, strip_zeros=False))
+    onp_fn = lambda arg: onp.sort(onp.roots(arg))
+    self._CheckAgainstNumpy(onp_fn, np_fn, args_maker, check_dtypes=False)
+
+  @parameterized.named_parameters(jtu.cases_from_list(
+    {"testcase_name": "_dtype={}_trailing={}".format(
+      jtu.format_shape_dtype_string((length + trailing,), dtype), trailing),
+      "dtype": dtype, "rng_factory": rng_factory, "length": length,
+      "trailing": trailing}
+    for dtype in all_dtypes
+    for rng_factory in [jtu.rand_default]
+    for length in [0, 1, 3, 10]
+    for trailing in [0, 1, 3, 7]))
+  # TODO: enable when there is an eigendecomposition implementation
+  # for GPU/TPU.
+  @jtu.skip_on_devices("gpu", "tpu")
+  def testRootsJit(self, length, dtype, rng_factory, trailing):
+    rng = rng_factory()
+
+    def args_maker():
+      p = rng((length,), dtype)
+      if length != 0:
+        return np.concatenate([p, np.zeros(trailing, p.dtype)]),
+      else:
+        # adding trailing would make input invalid (start with zeros)
+        return p,
+
+    # order may differ (np.sort doesn't deal with complex numbers)
+    roots_compiled = jit(partial(np.roots, strip_zeros=False))
+    np_fn = lambda arg: onp.sort(roots_compiled(arg))
+    onp_fn = lambda arg: onp.sort(onp.roots(arg))
+    self._CheckAgainstNumpy(onp_fn, np_fn, args_maker, check_dtypes=False)
+
+  @parameterized.named_parameters(jtu.cases_from_list(
+    {"testcase_name": "_dtype={}_zeros={}_nonzeros={}".format(
+        jtu.format_shape_dtype_string((zeros+nonzeros,), dtype),
+        zeros, nonzeros),
+     "zeros": zeros, "nonzeros": nonzeros, "dtype": dtype,
+     "rng_factory": rng_factory}
+    for dtype in all_dtypes
+    for rng_factory in [jtu.rand_default]
+    for zeros in [1, 2, 5]
+    for nonzeros in [0, 1, 3]))
+  def testRootsInvalid(self, zeros, nonzeros, dtype, rng_factory):
+    rng = rng_factory()
+
+    # The polynomial coefficients here start with zero and would have to
+    # be stripped before computing eigenvalues of the companion matrix.
+    # Setting strip_zeros=False skips this check,
+    # allowing jit transformation but yielding nan's for these inputs.
+    p = np.concatenate([np.zeros(zeros, dtype), rng((nonzeros,), dtype)])
+
+    self.assertTrue(np.any(np.isnan(np.roots(p, strip_zeros=False))))
 
 
 if __name__ == "__main__":
