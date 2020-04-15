814bc1c8bf82915594714841f5bd2f92c38edc22
Joschka Braun <joschka.braun@gmail.com>
2020-04-15 15:51:03 -0400

    Added testing for numpy.ediff1d implementation

--- tests/lax_numpy_test.py
+++ tests/lax_numpy_test.py
@@ -579,6 +579,22 @@ class LaxBackedNumpyTests(jtu.JaxTestCase):
                             check_dtypes=jtu.PYTHON_SCALAR_SHAPE not in shapes)
     self._CompileAndCheck(jnp_op, args_maker, check_dtypes=True)
 
+  @parameterized.named_parameters(jtu.cases_from_list(
+      {"testcase_name": "_{}_{}_{}".format(jtu.format_shape_dtype_string(shape, dtype),
+       "None" if end_dtype is None else onp.dtype(end_dtype).name,
+       "None" if begin_dtype is None else onp.dtype(begin_dtype).name,),
+       "shape": shape, "dtype": dtype, "end_dtype": end_dtype,
+       "begin_dtype": begin_dtype, "rng": jtu.rand_default()}
+      for dtype in number_dtypes
+      for end_dtype in [None] + number_dtypes
+      for begin_dtype in [None] + number_dtypes
+      for shape in all_shapes))
+  def testEDiff1d(self, shape, dtype, end_dtype, begin_dtype, rng):
+    args_maker = lambda: [rng(shape, dtype), (None if end_dtype is None else rng(shape, end_dtype)), (None if begin_dtype is None else rng(shape, begin_dtype))]
+    onp_fun = lambda x, to_end, to_begin: onp.ediff1d(x, to_end, to_begin)
+    jnp_fun = lambda x, to_end, to_begin: jnp.ediff1d(x, to_end, to_begin)
+    self._CheckAgainstNumpy(onp_fun, jnp_fun, args_maker, check_dtypes=True)
+
   @parameterized.named_parameters(itertools.chain.from_iterable(
       jtu.cases_from_list(
         {"testcase_name": "{}_inshape={}_axis={}_dtype={}_keepdims={}".format(
