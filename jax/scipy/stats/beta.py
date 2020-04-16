7a6a60ca1c0fbb93a682e54777351f5637709903
Ziyad Edher <ziyad.edher@gmail.com>
2020-04-16 13:51:42 -0400

    Implement frozen Beta distributions

--- jax/scipy/stats/beta.py
+++ jax/scipy/stats/beta.py
@@ -12,27 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import scipy.stats as osp_stats
 
-from ... import lax
-from ...numpy.lax_numpy import (_promote_args_inexact, _constant_like, _wraps,
-                                where, inf, logical_or)
-from ..special import betaln
+from jax import lax
+from jax.numpy import lax_numpy as jnp
+from jax.scipy import special
+from jax.scipy.stats._freezing import Freezer, _required
 
 
-@_wraps(osp_stats.beta.logpdf, update_doc=False)
+freeze = Freezer(__name__.split(".")[-1], a=_required, b=_required, loc=0, scale=1)
+
+
+@freeze.wrap
+@jnp._wraps(osp_stats.beta.logpdf, update_doc=False)
 def logpdf(x, a, b, loc=0, scale=1):
-  x, a, b, loc, scale = _promote_args_inexact("beta.logpdf", x, a, b, loc, scale)
-  one = _constant_like(x, 1)
-  shape_term = lax.neg(betaln(a, b))
+  x, a, b, loc, scale = jnp._promote_args_inexact("beta.logpdf", x, a, b, loc, scale)
+  one = jnp._constant_like(x, 1)
+  shape_term = lax.neg(special.betaln(a, b))
   y = lax.div(lax.sub(x, loc), scale)
   log_linear_term = lax.add(lax.mul(lax.sub(a, one), lax.log(y)),
                             lax.mul(lax.sub(b, one), lax.log1p(lax.neg(y))))
   log_probs = lax.sub(lax.add(shape_term, log_linear_term), lax.log(scale))
-  return where(logical_or(lax.gt(x, lax.add(loc, scale)),
-                          lax.lt(x, loc)), -inf, log_probs)
+  return jnp.where(jsp.logical_or(lax.gt(x, lax.add(loc, scale)),
+                                  lax.lt(x, loc)), -jnp.inf, log_probs)
 
-@_wraps(osp_stats.beta.pdf, update_doc=False)
+
+@freeze.wrap
+@jnp._wraps(osp_stats.beta.pdf, update_doc=False)
 def pdf(x, a, b, loc=0, scale=1):
   return lax.exp(logpdf(x, a, b, loc, scale))
-
