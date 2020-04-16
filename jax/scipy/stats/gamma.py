34fbeff2764bf6293dda3f688b0dae2b93df0d1e
Ziyad Edher <ziyad.edher@gmail.com>
2020-04-16 13:53:03 -0400

    Implement frozen Gamma distributions

--- jax/scipy/stats/gamma.py
+++ jax/scipy/stats/gamma.py
@@ -12,24 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import scipy.stats as osp_stats
 
-from ... import lax
-from ...numpy.lax_numpy import (_promote_args_inexact, _constant_like, _wraps,
-                                where, inf)
-from ..special import gammaln
+from jax import lax
+from jax.numpy import lax_numpy as jnp
+from jax.scipy import special
+from jax.scipy.stats._freezing import Freezer, _required
 
 
-@_wraps(osp_stats.gamma.logpdf, update_doc=False)
+freeze = Freezer(__name__.split(".")[-1], a=_required, loc=0, scale=1)
+
+
+@freeze.wrap
+@jnp._wraps(osp_stats.gamma.logpdf, update_doc=False)
 def logpdf(x, a, loc=0, scale=1):
-  x, a, loc, scale = _promote_args_inexact("gamma.logpdf", x, a, loc, scale)
-  one = _constant_like(x, 1)
+  x, a, loc, scale = jnp._promote_args_inexact("gamma.logpdf", x, a, loc, scale)
+  one = jnp._constant_like(x, 1)
   y = lax.div(lax.sub(x, loc), scale)
   log_linear_term = lax.sub(lax.mul(lax.sub(a, one), lax.log(y)), y)
-  shape_terms = lax.add(gammaln(a), lax.log(scale))
+  shape_terms = lax.add(special.gammaln(a), lax.log(scale))
   log_probs = lax.sub(log_linear_term, shape_terms)
-  return where(lax.lt(x, loc), -inf, log_probs)
+  return jnp.where(lax.lt(x, loc), -jnp.inf, log_probs)
 
-@_wraps(osp_stats.gamma.pdf, update_doc=False)
+
+@freeze.wrap
+@jnp._wraps(osp_stats.gamma.pdf, update_doc=False)
 def pdf(x, a, loc=0, scale=1):
   return lax.exp(logpdf(x, a, loc, scale))
