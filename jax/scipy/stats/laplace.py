9e50a9abc0fec6a9a4f4550648dfcfa75b28d1fe
Ziyad Edher <ziyad.edher@gmail.com>
2020-04-16 13:53:15 -0400

    Implement frozen Laplace distributions

--- jax/scipy/stats/laplace.py
+++ jax/scipy/stats/laplace.py
@@ -12,29 +12,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import scipy.stats as osp_stats
 
-from ... import lax
-from ...numpy.lax_numpy import _promote_args_inexact, _constant_like, _wraps
+from jax import lax
+from jax.numpy import lax_numpy as jnp
+from jax.scipy.stats._freezing import Freezer
 
 
-@_wraps(osp_stats.laplace.logpdf, update_doc=False)
+freeze = Freezer(__name__.split(".")[-1], loc=0, scale=1)
+
+
+@freeze.wrap
+@jnp._wraps(osp_stats.laplace.logpdf, update_doc=False)
 def logpdf(x, loc=0, scale=1):
-  x, loc, scale = _promote_args_inexact("laplace.logpdf", x, loc, scale)
-  two = _constant_like(x, 2)
+  x, loc, scale = jnp._promote_args_inexact("laplace.logpdf", x, loc, scale)
+  two = jnp._constant_like(x, 2)
   linear_term = lax.div(lax.abs(lax.sub(x, loc)), scale)
   return lax.neg(lax.add(linear_term, lax.log(lax.mul(two, scale))))
 
-@_wraps(osp_stats.laplace.pdf, update_doc=False)
+
+@freeze.wrap
+@jnp._wraps(osp_stats.laplace.pdf, update_doc=False)
 def pdf(x, loc=0, scale=1):
   return lax.exp(logpdf(x, loc, scale))
 
-@_wraps(osp_stats.laplace.cdf, update_doc=False)
+
+@freeze.wrap
+@jnp._wraps(osp_stats.laplace.cdf, update_doc=False)
 def cdf(x, loc=0, scale=1):
-  x, loc, scale = _promote_args_inexact("laplace.cdf", x, loc, scale)
-  half = _constant_like(x, 0.5)
-  one = _constant_like(x, 1)
-  zero = _constant_like(x, 0)
+  x, loc, scale = jnp._promote_args_inexact("laplace.cdf", x, loc, scale)
+  half = jnp._constant_like(x, 0.5)
+  one = jnp._constant_like(x, 1)
+  zero = jnp._constant_like(x, 0)
   diff = lax.div(lax.sub(x, loc), scale)
   return lax.select(lax.le(diff, zero),
                     lax.mul(half, lax.exp(diff)),
