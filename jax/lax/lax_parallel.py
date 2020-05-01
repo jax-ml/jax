6a55d0a0b609b966bcbbb948f9c72d6b34aac603
James Bradbury <jekbradbury@gmail.com>
2020-05-01 03:55:27 -0700

    fix psum transpose rule

--- jax/lax/lax_parallel.py
+++ jax/lax/lax_parallel.py
@@ -343,7 +343,7 @@ pxla.split_axis_rules[psum_p] = \
     partial(_allreduce_split_axis_rule, psum_p, lax._reduce_sum)
 xla.parallel_translations[psum_p] = _psum_translation_rule
 pxla.parallel_pure_rules[psum_p] = lambda *args, shape: (x * prod(shape) for x in args)
-ad.deflinear(psum_p, lambda *ts, axis_name, axis_index_groups: psum(
+ad.deflinear(psum_p, lambda ts, axis_name, axis_index_groups: psum_p.bind(
     *ts, axis_name=axis_name, axis_index_groups=axis_index_groups))
 pxla.multi_host_supported_collectives.add(psum_p)
 
