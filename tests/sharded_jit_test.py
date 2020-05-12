888c9c77b3daee21b2fb2ce20657acc57da52b7a
Skye Wanderman-Milne <skyewm@google.com>
2020-05-12 14:46:37 -0700

    Implement pmap of sharded_jit

--- tests/sharded_jit_test.py
+++ tests/sharded_jit_test.py
@@ -21,10 +21,13 @@ from unittest import SkipTest
 
 import numpy as np
 from absl.testing import absltest
+from absl.testing import parameterized
 
 import jax
-from jax import vjp
+from jax import pmap, vjp
+from jax import lax
 from jax import test_util as jtu
+from jax import tree_util
 from jax.interpreters import pxla
 from jax.interpreters.sharded_jit import sharded_jit, with_sharding_constraint
 from jax.interpreters.sharded_jit import PartitionSpec as P
@@ -228,6 +231,187 @@ class ShardedJitTestNoTpu(jtu.JaxTestCase):
     self.assertIn("sharding={devices=[2,1]0,1}", hlo.as_hlo_text())
     # Annotation from sharded_jit
     self.assertIn("sharding={replicated}", hlo.as_hlo_text())
+
+class PmapOfShardedJitTest(jtu.JaxTestCase):
+
+  def setUp(self):
+    if jtu.device_under_test() != "tpu":
+      raise SkipTest
+
+  # TODO(skye): make a similar version for ShardedJitTest and run the same tests
+  def _runTest(self, f, in_partitions, out_partitions, dtype=np.float32):
+    """Compares pmap(sharded_jit(f, ...)) to pmap(f)"""
+    shape = (2, 4, 4)
+    num_shards = shape[0] * np.prod(in_partitions[0])
+    if num_shards > jax.local_device_count():
+      raise SkipTest("requires %d devices" % num_shards)
+
+    x = np.arange(np.prod(shape, dtype=dtype)).reshape(shape)
+    y = x + 1
+    result = pmap(
+        sharded_jit(f, in_parts=in_partitions, out_parts=out_partitions))(x, y)
+    expected = pmap(f)(x, y)
+    self.assertAllClose(result, expected, check_dtypes=False)
+
+    flat_result = tree_util.tree_flatten(result)[0]
+    for r in flat_result:
+      self.assertTrue(isinstance(r, pxla.ShardedDeviceArray))
+      self.assertEqual(len(r.device_buffers), num_shards)
+
+  @parameterized.named_parameters({
+      "testcase_name":
+          "_in_parts={}_out_parts={}".format(in_partitions,
+                                             out_partitions).replace(" ", ""),
+      "in_partitions":
+          in_partitions,
+      "out_partitions":
+          out_partitions
+  } for in_partitions in [
+      (P(2, 1), P(2, 1)),
+      (P(2, 1), P(1, 2)),
+      (P(2, 2), P(2, 2)),
+      (P(4, 1), P(2, 2)),
+  ] for out_partitions in [in_partitions[0], None])
+  def testBasic(self, in_partitions, out_partitions):
+
+    def f(x, y):
+      return lax.dot(x, y)
+
+    self._runTest(f, in_partitions, out_partitions)
+
+  @parameterized.named_parameters({
+      "testcase_name":
+          "_in_parts={}_out_parts={}".format(in_partitions,
+                                             out_partitions).replace(" ", ""),
+      "in_partitions":
+          in_partitions,
+      "out_partitions":
+          out_partitions
+  } for in_partitions in [
+      (P(2, 1), P(2, 1)),
+      (P(2, 1), P(1, 2)),
+      (P(4, 1), P(2, 2))
+  ] for out_partitions in [(in_partitions[1], in_partitions[0], None),
+                           (None, None, None)])
+  def testMultipleOutputs(self, in_partitions, out_partitions):
+
+    def f(x, y):
+      a = lax.dot(x, y)
+      # TODO(skye): use these more interesting outputs once returning constants
+      # works
+      # return a, a + 1, 3
+      return a, a + x, x + y
+
+    self._runTest(f, in_partitions, out_partitions)
+
+  @parameterized.named_parameters({
+      "testcase_name":
+          "_in_parts={}_out_parts={}".format(in_partitions,
+                                             out_partitions).replace(" ", ""),
+      "in_partitions":
+          in_partitions,
+      "out_partitions":
+          out_partitions
+  } for in_partitions in [
+      (P(2, 1), P(2, 1)),
+      (P(2, 1), P(1, 2)),
+      (P(4, 1), P(2, 2))
+  ] for out_partitions in [in_partitions[0], None])
+  def testArrayConstants(self, in_partitions, out_partitions):
+
+    def f(x, y):
+      a = lax.dot(x, y)
+      b = a + jnp.ones(a.shape)
+      c = b + jnp.ones(a.shape[0])
+      return c
+
+    self._runTest(f, in_partitions, out_partitions)
+
+  def testPyTreeArgs(self):
+    if jax.local_device_count() < 4:
+      raise SkipTest("requires 4 devices")
+
+    def f(a, b, c):
+      a1, a2 = a
+      c1, (c2, c3) = c
+      return a1 + a2 + b + c1 + c2 + c3
+
+    def _make_arg(*shape):
+      return np.arange(np.prod(shape)).reshape(shape)
+
+    a = (_make_arg(2, 4, 4), _make_arg(2))
+    b = _make_arg(2, 4, 4)
+    c = (_make_arg(2), (_make_arg(2, 4, 4), _make_arg(2, 4, 4)))
+
+    in_parts = (None, P(2, 1), (None, P(2, 1)))
+    out_parts = P(2, 1)
+
+    result = pmap(sharded_jit(f, in_parts=in_parts, out_parts=out_parts))(a, b, c)
+    expected = pmap(f)(a, b, c)
+
+    self.assertAllClose(result, expected, check_dtypes=False)
+    self.assertTrue(isinstance(result, pxla.ShardedDeviceArray))
+    self.assertEqual(len(result.device_buffers), 4)
+
+  def testPyTreeOutputs(self):
+    if jax.local_device_count() < 4:
+      raise SkipTest("requires 4 devices")
+
+    def f(x):
+      return x + 1, ((x + 2, x + 3), x + 4)
+
+    shape = (2, 4, 4)
+    x = np.arange(np.prod(shape)).reshape(shape)
+    in_parts = (P(2, 1),)
+    out_parts = (P(2, 1), ((P(1, 2), None), P(2, 1)))
+
+    result = pmap(sharded_jit(f, in_parts=in_parts, out_parts=out_parts))(x)
+    expected = pmap(f)(x)
+
+    self.assertAllClose(result, expected, check_dtypes=False)
+
+  def testManyArgs(self):
+    if jax.local_device_count() < 4:
+      raise SkipTest("requires 4 devices")
+
+    num_args = 200
+
+    def f(*args):
+      return jnp.sum(args)
+
+    shape = (2, 4, 4)
+    args = [np.arange(np.prod(shape)).reshape(shape)] * num_args
+    in_partitions = (P(2, 1),) * num_args
+    out_partitions = None
+    result = pmap(sharded_jit(
+        f, in_parts=in_partitions, out_parts=out_partitions))(*args)
+    expected = pmap(f)(*args)
+
+    self.assertAllClose(result, expected, check_dtypes=False)
+    self.assertTrue(isinstance(result, pxla.ShardedDeviceArray))
+    self.assertEqual(len(result.device_buffers), 4)
+
+  def testShardingConstraint(self):
+    if jax.local_device_count() < 4:
+      raise SkipTest("requires 4 devices")
+
+    @partial(sharded_jit, in_parts=None, out_parts=None)
+    def f(x):
+      y = jnp.dot(x, x)
+      y = with_sharding_constraint(y, P(2,1))
+      return y * 2
+
+    def expected_f(x):
+      return jnp.dot(x, x) * 2
+
+    shape = (2, 8, 8)
+    x = np.arange(np.prod(shape)).reshape(shape)
+    result = pmap(f)(x)
+    expected = pmap(expected_f)(x)
+
+    self.assertAllClose(result, expected, check_dtypes=False)
+    self.assertIsInstance(result, pxla.ShardedDeviceArray)
+    self.assertLen(result.device_buffers, 4)
 
 
 if __name__ == "__main__":
