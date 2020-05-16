daa27ce99f0a16f8d15f7e9ca5814f88779f8277
joaogui <joaoguilhermearujo@gmail.com>
2020-05-16 11:11:01 -0300

    allows tuples as input to reductions

--- jax/numpy/lax_numpy.py
+++ jax/numpy/lax_numpy.py
@@ -1520,8 +1520,10 @@ def _make_reduction(np_fun, op, init_val, preproc=None, bool_op=None,
     if out is not None:
       raise ValueError("reduction does not support the `out` argument.")
 
+    if isinstance(a, tuple):
+      a = asarray(a)
     if not isinstance(a, ndarray):
-      raise TypeError(f"Reductions only work on ndarrays, input is of type {type(a)}")
+      raise TypeError(f"Reductions only work on ndarrays and tuples, input is of type {type(a)}")
     a = preproc(a) if preproc else a
     dims = _reduction_dims(a, axis)
     result_dtype = dtype or _dtype(np_fun(np.ones((), dtype=_dtype(a))))
