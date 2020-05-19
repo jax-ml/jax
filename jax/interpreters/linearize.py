1214b395b6ea010c174ec585263fa4c78213e34b
Adam Paszke <apaszke@google.com>
2020-05-19 15:28:53 +0000

    Add support for staging out the primal computation

--- jax/interpreters/linearize.py
+++ jax/interpreters/linearize.py
@@ -2,12 +2,12 @@ from collections import namedtuple
 
 from jax import core
 from jax import ad_util
-from jax.util import unzip2, toposort, safe_map, safe_zip
+from jax.util import unzip2, unzip3, toposort, safe_map, safe_zip
 from jax.linear_util import wrap_init
 
 from . import ad
 from . import partial_eval as pe
-from ..core import new_jaxpr_eqn
+from ..core import new_jaxpr_eqn, TypedJaxpr
 from .. import numpy as jnp
 
 
@@ -16,7 +16,8 @@ zip = safe_zip
 
 
 # TODO: Use weakrefs (for out_tracers?)?
-SubJaxpr = namedtuple('SubJaxpr', ['id', 'in_tracers', 'out_tracers', 'jaxpr'])
+NonLinearSubJaxpr = namedtuple('NonLinearSubJaxpr', ['id', 'in_tracers', 'out_tracers', 'jaxpr'])
+SubJaxpr = namedtuple('SubJaxpr', ['id', 'in_nonlin_tracers', 'in_lin_tracers', 'out_tracers', 'jaxpr']) # Some arguments have to be non-linear!
 LambdaBinding = namedtuple('LambdaBinding', [])
 Zeros = namedtuple('Zeros', ['shape'])
 ConstVar = namedtuple('ConstVar', ['val'])
@@ -25,30 +26,48 @@ ConstVar = namedtuple('ConstVar', ['val'])
 def lin(fun, *primals):
   with core.new_master(LinTrace) as master:
     trace = LinTrace(master, core.cur_sublevel())
-    in_tracers = [LinTracer(trace, p, LambdaBinding()) for p in primals]
+    in_tracers = [LinTracer(trace, p, None, LambdaBinding()) for p in primals]
+    out = fun.call_wrapped(*in_tracers)
+    out_tracer = trace.full_raise(out)
+    return tracers_to_jaxpr(in_tracers, [out_tracer])
+
+def rematlin(fun, *primals):
+  with core.new_master(LinTrace) as master:
+    trace = LinTrace(master, core.cur_sublevel())
+    in_tracers = [LinTracer(trace, p, ConstVar(p), LambdaBinding()) for p in primals]
     out = fun.call_wrapped(*in_tracers)
     out_tracer = trace.full_raise(out)
     return tracers_to_jaxpr(in_tracers, [out_tracer])
 
 
 class LinTracer(core.Tracer):
-  __slots__ = ['primal', 'recipe']
+  __slots__ = ['primal', 'primal_recipe', 'linear_recipe', '_aval']
 
-  def __init__(self, trace, primal, recipe):
+  def __init__(self, trace, primal, primal_recipe, linear_recipe):
     self._trace = trace
     self.primal = primal
-    self.recipe = recipe  # Recipe for the linearized program
+    self._aval = None
+    self.primal_recipe = primal_recipe
+    self.linear_recipe = linear_recipe  # Recipe for the linearized version of the primal
+
+  def __repr__(self):
+    return 'Linearized<{}:{}>'.format(self.aval, self._trace)
 
   @property
   def aval(self):
-    return core.get_aval(self.primal)
+    if self._aval is not None:
+      return self._aval
+    else:
+      return core.get_aval(self.primal)
 
   @property
   def parents(self):
-    if isinstance(self.recipe, SubJaxpr):
-      return self.recipe.in_tracers
-    else:
-      return []
+    parents = []
+    if isinstance(self.linear_recipe, SubJaxpr):
+      parents += self.linear_recipe.in_lin_tracers + self.linear_recipe.in_nonlin_tracers
+    if isinstance(self.primal_recipe, NonLinearSubJaxpr):
+      parents += self.primal_recipe.in_tracers
+    return parents
 
   def full_lower(self):
     return self
@@ -56,7 +75,7 @@ class LinTracer(core.Tracer):
 
 class LinTrace(core.Trace):
   def pure(self, val):
-    return LinTracer(self, val, Zeros(jnp.shape(val)))
+    return LinTracer(self, val, None, Zeros(jnp.shape(val)))
 
   lift = pure
 
@@ -65,39 +84,105 @@ class LinTrace(core.Trace):
 
   def instantiate_const(self, val):
     # TODO: Should we keep the val in the primal, or should it be None?
-    return LinTracer(self, None, ConstVar(val))
+    return LinTracer(self, None, None, ConstVar(val))
 
   def process_primitive(self, primitive, tracers, params):
-    primals_in, recipes_in = unzip2((t.primal, t.recipe) for t in tracers)
-    jvp_rule = ad.primitive_jvps[primitive]
-    primal_out, jaxpr, consts = unzip_jvp_rule(jvp_rule, primals_in, params)
+    primals_in, primal_recipes_in, _ = unzip3((t.primal, t.primal_recipe, t.linear_recipe) for t in tracers)
+
+    primal_pvals_in = [pe.PartialVal.unknown(core.get_aval(p)) if recipe is not None else pe.PartialVal.known(p)
+                       for p, recipe in zip(primals_in, primal_recipes_in)]
+    tangent_pvals_in = [pe.PartialVal.unknown(core.get_aval(p).at_least_vspace())
+                        for p in primals_in]
+    flat_jvp_rule = get_flat_jvp_rule(primitive, params)
+    jaxpr, pvals_out, consts = pe.trace_to_jaxpr(flat_jvp_rule, primal_pvals_in + tangent_pvals_in)
+
+    num_args = len(primals_in)
+    num_outputs = len(pvals_out) // 2
+
+    typed_jaxpr = TypedJaxpr(jaxpr,
+                             consts,
+                             map(core.raise_to_shaped, map(core.get_aval, primals_in)) * 2,
+                             [p.get_aval() for p in pvals_out])
+    typed_primal_jaxpr, typed_linear_jaxpr, out_unknowns = pe.partial_eval_jaxpr(
+      typed_jaxpr,
+      unknowns=([False] * num_args + [True] * num_args),
+      instantiate=False,
+      trace_type=None)
+    assert out_unknowns == [False] * num_outputs + [True] * num_outputs
+    primal_jaxpr = typed_primal_jaxpr.jaxpr
+    linear_jaxpr = typed_linear_jaxpr.jaxpr
+
+    # Primal jaxpr outputs only primals and possibly residuals
+    primal_jaxpr.invars = primal_jaxpr.invars[:num_args]
+    primal_jaxpr.outvars = primal_jaxpr.outvars[:num_outputs] + primal_jaxpr.outvars[num_outputs * 2:]
+    residual_avals = map(core.raise_to_shaped, typed_primal_jaxpr.out_avals[num_outputs * 2:])
+    # Linear jaxpr only takes in tangents and residulas, and only returns the tangents
+    linear_jaxpr.invars = linear_jaxpr.invars[num_args:]
+    linear_jaxpr.outvars = linear_jaxpr.outvars[num_outputs:]
+
+    # Wrap results back in tracers
+    primal_tracers = [LinTracer(self, None, None, None) for _ in range(num_outputs)]
+    residual_tracers = [LinTracer(self, None, None, None) for _ in primal_jaxpr.outvars[num_outputs:]]
     const_tracers = map(self.instantiate_const, consts)
-    tracer_out = LinTracer(self, primal_out, None)
-    recipe_out = SubJaxpr(object(), [*const_tracers, *tracers], [tracer_out], jaxpr)
-    tracer_out.recipe = recipe_out
-    return tracer_out
+
+    primal_recipe = NonLinearSubJaxpr(object(),
+                                      [*const_tracers, *tracers],
+                                      [*primal_tracers, *residual_tracers],
+                                      primal_jaxpr)
+    linear_recipe = SubJaxpr(object(),
+                             residual_tracers,
+                             tracers,
+                             primal_tracers,
+                             linear_jaxpr)
+
+    primal_pvals_out, tangent_pvals_out = split(pvals_out, parts=2)
+    for pt, pval in zip(primal_tracers, primal_pvals_out):
+      if pval.is_known():
+        pt.primal = pval.get_known()
+      else:
+        pt.primal = pval.get_aval().val  # TODO: This is very ConcreteArray centric...
+        pt.primal_recipe = primal_recipe
+      pt.linear_recipe = linear_recipe
+    for rt, aval in zip(residual_tracers, residual_avals):
+      rt.primal_recipe = primal_recipe
+      rt._aval = aval
+
+    assert not primitive.multiple_results
+    return primal_tracers[0]
 
 
-def unzip_jvp_rule(jvp_rule, primals_in, params):
+def get_flat_jvp_rule(primitive, params):
+  jvp_rule = ad.primitive_jvps[primitive]
+
   @wrap_init
-  def linearized(*tangents_in):
+  def flat_jvp_rule(*inputs):
+    primals_in, tangents_in = split(inputs, parts=2)
     primal_out, tangent_out = jvp_rule(primals_in, tangents_in, **params)
-    tangent_out = ad.instantiate_zeros(primal_out, tangent_out)
-    return primal_out, tangent_out
+    # TODO: Why do we need to instantiate zeros here?
+    if primitive.multiple_results:
+      tangent_out = [ad.instantiate_zeros(p, t) for p, t in zip(primal_out, tangent_out)]
+      return primal_out + tangent_out
+    else:
+      tangent_out = ad.instantiate_zeros(primal_out, tangent_out)
+      return primal_out, tangent_out
 
-  tangent_pvals_in = [pe.PartialVal.unknown(core.get_aval(p).at_least_vspace())
-                      for p in primals_in]
-  jaxpr, pvals_out, consts = pe.trace_to_jaxpr(linearized, tangent_pvals_in)
-  primal_pval_out, tangent_pval_out = pvals_out
-  assert primal_pval_out.is_known()
-  primal_out = primal_pval_out.get_known()
-  assert len(jaxpr.outvars) == 2 and jaxpr.outvars[0] is core.unitvar
-  jaxpr.outvars = jaxpr.outvars[1:]
-  return primal_out, jaxpr, consts
+  return flat_jvp_rule
+
+
+
+def unzip_jvp_rule(jvp_rule, primals_in, primal_recipes_in, params):
+  return primals_out, primal_recipies_out, jaxpr, consts
 
 
 def tracers_to_jaxpr(in_tracers, out_tracers):
   newvar = core.gensym('')
+
+  t_to_linvar = {}
+  def getlinvar(t):
+    var = t_to_linvar.get(id(t))
+    if var is None:
+      var = t_to_linvar[id(t)] = newvar(t.aval)
+    return var
 
   t_to_var = {}
   def getvar(t):
@@ -115,26 +200,48 @@ def tracers_to_jaxpr(in_tracers, out_tracers):
     return var
 
   sorted_tracers = toposort(out_tracers)
-  invars = map(getvar, in_tracers)
+  invars = map(getlinvar, in_tracers)
   eqns = []
   consts = {}
   processed_eqn_ids = set()
   for t in sorted_tracers:
-    recipe = t.recipe
+    # TODO: Make sure to emit nonlinear recipes too!
+    recipe = t.linear_recipe
     if isinstance(recipe, SubJaxpr):
-      assert recipe.id not in processed_eqn_ids
-      processed_eqn_ids.add(recipe.id)
-      eqns.append(
-        new_jaxpr_eqn(map(getvar, recipe.in_tracers), map(getvar, recipe.out_tracers),
-                      core.call_p, dict(call_jaxpr=recipe.jaxpr)))
+      if recipe.id not in processed_eqn_ids:
+        processed_eqn_ids.add(recipe.id)
+        eqns.append(
+          new_jaxpr_eqn(map(getlinvar, recipe.in_lin_tracers) + map(getvar, recipe.in_nonlin_tracers),
+                        map(getlinvar, recipe.out_tracers),
+                        core.call_p,
+                        dict(call_jaxpr=recipe.jaxpr)))
     elif isinstance(recipe, LambdaBinding):
       pass
     elif isinstance(recipe, Zeros):
       eqns.append(
-        new_jaxpr_eqn([Literal(0.)], getvar(t),
+        new_jaxpr_eqn([Literal(0.)], getlinvar(t),
                       lax.broadcast_in_dim_p,
                       dict(shape=recipe.shape,
                            broadcast_dimensions=range(len(recipe.shape)))))
+    elif isinstance(recipe, ConstVar):
+      v = t_to_var[id(t)] = getconstvar(recipe.val)
+      consts[v] = recipe.val
+    elif recipe is None:  # Residual tracers in remat don't have a linear recipe
+      pass
+    else:
+      raise TypeError(recipe)
+
+    recipe = t.primal_recipe
+    if recipe is None:  # Nonlinear recipes are not used outside of remat
+      pass
+    elif isinstance(recipe, NonLinearSubJaxpr):
+      if recipe.id not in processed_eqn_ids:
+        processed_eqn_ids.add(recipe.id)
+        eqns.append(
+          new_jaxpr_eqn(map(getvar, recipe.in_tracers),
+                        map(getvar, recipe.out_tracers),
+                        core.call_p,
+                        dict(call_jaxpr=recipe.jaxpr)))
     elif isinstance(recipe, ConstVar):
       v = t_to_var[id(t)] = getconstvar(recipe.val)
       consts[v] = recipe.val
@@ -142,6 +249,12 @@ def tracers_to_jaxpr(in_tracers, out_tracers):
       raise TypeError(recipe)
 
   constvars, constvals = unzip2(consts.items())
-  jaxpr = core.Jaxpr((), (*constvars, *invars), map(getvar, out_tracers), eqns)
+  jaxpr = core.Jaxpr(constvars, invars, map(getlinvar, out_tracers), eqns)
   core.skip_checks or core.check_jaxpr(jaxpr)
   return jaxpr, constvals
+
+
+def split(l, parts):
+  assert len(l) % parts == 0
+  chunk = len(l) // parts
+  return [l[i:i + chunk] for i in range(0, len(l), chunk)]
--- lin.py
+++ lin.py
@@ -5,6 +5,15 @@ import jax.numpy as np
 def f(x, y):
   return -np.sin(x) * y
 
+def f(x, y):
+  a = np.sin(x)
+  # print(a)
+  b = -a
+  # print(b)
+  c = b * y
+  # print(c)
+  return c
+
 ### reference
 
 from jax import make_jaxpr, jvp
@@ -13,8 +22,11 @@ def lin2(f, *args):
   jaxpr = make_jaxpr(lambda *args_dot: jvp(f, args, args_dot)[1])(*args)
   print(jaxpr)
 
-jaxpr = lin2(f, 3., 4.)
+x, y = np.array([3.]), np.array([4.])
+# x, y = 3., 4.
+jaxpr = lin2(f, x, y)
 
 ### test
 
-print(lin.lin(lu.wrap_init(f), 3., 4.))
+print(lin.lin(lu.wrap_init(f), x, y))
+print(lin.rematlin(lu.wrap_init(f), x, y))
