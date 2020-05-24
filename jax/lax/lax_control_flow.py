afadb12b643f4bf5b95a839ed158cffb9e2e5641
George Necula <gcnecula@gmail.com>
2020-05-24 10:50:07 +0300

    Improved tapping support for while: tap inside cond, vmap of while (#3195)
    * Improved tapping support for while: tap inside cond, vmap of while
    
    * Fix float64->float32 in tests

--- jax/lax/lax_control_flow.py
+++ jax/lax/lax_control_flow.py
@@ -343,9 +343,13 @@ def _pred_bcast_select(c, pred, x, y):
   x_shape = c.get_shape(x).dimensions()
   y_shape = c.get_shape(y).dimensions()
   assert x_shape == y_shape
-  assert pred_shape == x_shape[:len(pred_shape)] == y_shape[:len(pred_shape)]
-  bcast_pred = xops.BroadcastInDim(pred, x_shape, list(range(len(pred_shape))))
-  return xops.Select(bcast_pred, x, y)
+  if not c.get_shape(x).is_array() and not c.get_shape(y).is_array():
+    # Two tokens
+    return xops.AfterAll(c, [x, y])
+  else:
+    assert pred_shape == x_shape[:len(pred_shape)] == y_shape[:len(pred_shape)]
+    bcast_pred = xops.BroadcastInDim(pred, x_shape, list(range(len(pred_shape))))
+    return xops.Select(bcast_pred, x, y)
 
 def _while_loop_batching_rule(args, dims, cond_nconsts, cond_jaxpr,
                               body_nconsts, body_jaxpr):
