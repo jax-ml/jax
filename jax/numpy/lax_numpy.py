fd72a8a364caccf2fab2fb00ac5ad16713985e8f
Stephan Hoyer <shoyer@google.com>
2020-05-29 13:29:14 -0700

    Support static valued scalar arrays in expand_dims/squeeze (#3250)
    * Support static valued scalar arrays in expand_dims/squeeze
    
    * Fix reduction_dims

--- jax/numpy/lax_numpy.py
+++ jax/numpy/lax_numpy.py
@@ -1145,19 +1145,16 @@ def squeeze(a, axis: Union[int, Tuple[int, ...]] = None):
   if axis is None:
     a_shape = shape(a)
     axis = tuple(i for i, d in enumerate(a_shape) if d == 1)
-  elif isinstance(axis, int):
+  elif not isinstance(axis, tuple):
     axis = (axis,)
   return lax.squeeze(a, axis)
 
 
 @_wraps(np.expand_dims)
 def expand_dims(a, axis: Union[int, Tuple[int, ...]]):
-  # TODO(https://github.com/google/jax/issues/3243)
-  try:
-    axis = (int(axis),)  # type: ignore[arg-type]
-  except TypeError:
-    pass
-  return lax.expand_dims(a, axis)  # type: ignore[arg-type]
+  if not isinstance(axis, tuple):
+    axis = (axis,)
+  return lax.expand_dims(a, axis)
 
 
 @_wraps(np.swapaxes)
@@ -1541,7 +1538,7 @@ def _make_reduction(np_fun, op, init_val, preproc=None, bool_op=None,
 
 def _reduction_dims(a, axis):
   if axis is None:
-    return np.arange(ndim(a))
+    return tuple(range(ndim(a)))
   elif isinstance(axis, (np.ndarray, tuple, list)):
     return tuple(_canonicalize_axis(x, ndim(a)) for x in axis)
   elif isinstance(axis, int):
