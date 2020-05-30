18d1e81e496db427074a22a80906c0f5d6c833ba
Stephan Hoyer <shoyer@google.com>
2020-05-29 17:36:29 -0700

    Fix copyright date

--- tests/flattree_test.py
+++ tests/flattree_test.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 from functools import partial
 import itertools as it
