38bfcee753893ced209c9829f5675fded39b1911
George Necula <gcnecula@gmail.com>
2020-06-02 14:41:36 +0300

    Refactoring of jax_to_tf tests: (#3262)
    * Moved control-flow tests into their own file
    * Added a helper module tf_test_util, with a helper function ConvertAndCompare
    * Used self.assertAllClose instead of numpy.testing.assert_all_close because
      the former iterates over lists and tuples (and is standard in other JAX tests)
    * Used @parameterized.named_parameters for parameterized tests, for nicer test
     names.

--- jax/experimental/jax_to_tf/tests/savedmodel_test.py
+++ jax/experimental/jax_to_tf/tests/savedmodel_test.py
@@ -13,28 +13,22 @@
 # limitations under the License.
 
 import os
-import unittest
 
-import numpy as np
 from absl.testing import absltest
-from absl.testing import parameterized
-import jax
-from jax import test_util as jtu
-import jax.numpy as jnp
 
 import jax
-import jax.lax as lax
 import jax.numpy as jnp
 import numpy as np
-import tensorflow as tf
+import tensorflow as tf  # type: ignore[import]
 
 from jax.experimental import jax_to_tf
+from jax.experimental.jax_to_tf.tests import tf_test_util
 
 from jax.config import config
 config.parse_flags_with_absl()
 
 
-class SavedModelTest(jtu.JaxTestCase):
+class SavedModelTest(tf_test_util.JaxToTfTestCase):
 
   def testSavedModel(self):
     f_jax = jax.jit(lambda x: jnp.sin(jnp.cos(x)))
@@ -42,12 +36,13 @@ class SavedModelTest(jtu.JaxTestCase):
     model.f = tf.function(jax_to_tf.convert(f_jax),
                           input_signature=[tf.TensorSpec([], tf.float32)])
     x = np.array(0.7)
-    np.testing.assert_allclose(model.f(x), f_jax(x))
+    self.assertAllClose(model.f(x), f_jax(x))
     # Roundtrip through saved model on disk.
     model_dir = os.path.join(absltest.get_default_test_tmpdir(), str(id(model)))
     tf.saved_model.save(model, model_dir)
     restored_model = tf.saved_model.load(model_dir)
-    np.testing.assert_allclose(restored_model.f(x), f_jax(x))
+    self.assertAllClose(restored_model.f(x), f_jax(x))
+
 
 if __name__ == "__main__":
   absltest.main()
