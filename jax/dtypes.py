c7a021e8336fba62fefd367ef532c21284b5975d
Tom Hennigan <tomhennigan@google.com>
2020-06-11 20:48:05 +0100

    Improve error message when passing an invalid dtype. (#3405)
    I spotted this when debugging an issue like:
    
        self.assertEqual(x_jax, x_tf, check_dtypes=True)
    
    The fix here is of course to use `x_tf.numpy()`, but it was not clear where the
    error was from originally.

--- jax/dtypes.py
+++ jax/dtypes.py
@@ -79,7 +79,10 @@ def canonicalize_dtype(dtype):
   """Convert from a dtype to a canonical dtype based on FLAGS.jax_enable_x64."""
   if isinstance(dtype, str) and dtype == "bfloat16":
     dtype = bfloat16
-  dtype = np.dtype(dtype)
+  try:
+    dtype = np.dtype(dtype)
+  except TypeError as e:
+    raise TypeError(f'dtype {dtype!r} not understood') from e
 
   if FLAGS.jax_enable_x64:
     return dtype
