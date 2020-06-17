1bceb68fdacef5bb90f9e9a995ebf910f6556d91
Skye Wanderman-Milne <skyewm@google.com>
2020-06-17 16:28:53 -0700

    Set minimum sphinx version

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ release = ''
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '2.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
