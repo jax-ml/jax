ff6f0160da0d7da0a8f1fde9fd6955c0fa0ec8ad
Matthew Johnson <mattjj@google.com>
2020-06-22 17:18:48 -0700

    remove the lower_fun default multiple_results=True

--- jax/api.py
+++ jax/api.py
@@ -1853,7 +1853,7 @@ def custom_transforms(fun):
   fun_p.def_abstract_eval(fun_abstract_eval)
 
   def fun_translation(c, *xla_args, **params):
-    return xla.lower_fun(fun_impl)(c, *xla_args, **params)
+    return xla.lower_fun(fun_impl, multiple_results=True)(c, *xla_args, **params)
   xla.translations[fun_p] = fun_translation
 
   return CustomTransformsFunction(fun, fun_p)
