Correct type of sign when b is None in logsumexp

jax/scipy/special.py
@@ -111,7 +111,7 @@ def logsumexp(a, axis=None, b=None, keepdims=False, return_sign=False):
   if b is None:
     out = lax.add(lax.log(lax.reduce(lax.exp(lax.sub(a, amax_singletons)),
                                      _constant_like(a, 0), lax.add, dims)), amax)
-    sign = jnp.where(jnp.isnan(out), np.nan, 1.0)
+    sign = jnp.where(jnp.isnan(out), np.nan, 1.0).astype(out)
   else:
     sumexp = lax.reduce(lax.mul(lax.exp(lax.sub(a, amax_singletons)), b),
                         _constant_like(a, 0), lax.add, dims)