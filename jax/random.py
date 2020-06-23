2f7108f78ba4d7fd12a8ad6232685d0d10b28c01
Matthew Johnson <mattjj@google.com>
2020-06-22 17:50:33 -0700

    remove the lower_fun default multiple_results=True (#3524)

--- jax/random.py
+++ jax/random.py
@@ -216,9 +216,11 @@ threefry2x32_p.def_impl(partial(xla.apply_primitive, threefry2x32_p))
 threefry2x32_p.def_abstract_eval(_threefry2x32_abstract_eval)
 batching.defbroadcasting(threefry2x32_p)
 xla.translations[threefry2x32_p] = xla.lower_fun(
-    partial(_threefry2x32_lowering, use_rolled_loops=False))
+    partial(_threefry2x32_lowering, use_rolled_loops=False),
+    multiple_results=True)
 xla.backend_specific_translations['cpu'][threefry2x32_p] = xla.lower_fun(
-    partial(_threefry2x32_lowering, use_rolled_loops=True))
+    partial(_threefry2x32_lowering, use_rolled_loops=True),
+    multiple_results=True)
 if cuda_prng:
   xla.backend_specific_translations['gpu'][threefry2x32_p] = \
       _threefry2x32_gpu_translation_rule
