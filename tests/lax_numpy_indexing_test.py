6ef7f34d0b5406c1a47b6dea1d6ea4207c1c9fa4
Jake VanderPlas <jakevdp@google.com>
2020-07-08 12:46:22 -0700

    fix duplicate test name issue

--- tests/lax_numpy_indexing_test.py
+++ tests/lax_numpy_indexing_test.py
@@ -906,9 +906,9 @@ class IndexedUpdateTest(jtu.JaxTestCase):
     self._CompileAndCheck(jax_fn, args_maker)
 
   @parameterized.named_parameters(jtu.cases_from_list({
-      "testcase_name": "{}_inshape={}_indexer={}_update={}_op={}".format(
+      "testcase_name": "{}_inshape={}_indexer={}_update={}_op={}_sugared={}".format(
           name, jtu.format_shape_dtype_string(shape, dtype), indexer,
-          jtu.format_shape_dtype_string(update_shape, update_dtype), op.name),
+          jtu.format_shape_dtype_string(update_shape, update_dtype), op.name, sugared),
        "shape": shape, "dtype": dtype, "rng_factory": rng_factory, "indexer": indexer,
        "update_shape": update_shape, "update_dtype": update_dtype,
        "op": op, "sugared": sugared
