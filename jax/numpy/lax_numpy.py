3ba01ad66bef11ddf59950e2baa6a4d570047b48
Alex Dragan <35031007+aldragan0@users.noreply.github.com>
2020-07-13 23:23:33 +0300

    Improve np.intersect1d

--- jax/numpy/lax_numpy.py
+++ jax/numpy/lax_numpy.py
@@ -1249,12 +1249,8 @@ def _intersect1d_sorted_mask(ar1, ar2, return_indices=False):
     Helper function for intersect1d which is jit-able
     """
     ar = concatenate((ar1, ar2))
-
-    if return_indices:
-      indices = argsort(ar)
-      aux = ar[indices]
-    else:
-      aux = sort(ar)
+    iota = lax.broadcasted_iota(np.int64, shape(ar), dimension=0)
+    aux, indices = lax.sort_key_val(ar, iota)
 
     mask = aux[1:] == aux[:-1]
     if return_indices:
