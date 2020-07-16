deb39aad24bc8e7e8cfcad9dd0319601f06e960e
David Pfau <pfau@google.com>
2020-07-16 16:53:38 +0100

    Add grad_type flag to det (still some issues w/vjp through lax.scan)

--- jax/lib/python3.7/site-packages/numpy/f2py/__main__.py
+++
@@ -1,4 +0,0 @@
-# See http://cens.ioc.ee/projects/f2py2e/
-from numpy.f2py.f2py2e import main
-
-main()
--- jax/lib/python3.7/site-packages/numpy/f2py/__version__.py
+++
@@ -1,8 +0,0 @@
-major = 2
-
-try:
-    from __svn_version__ import version
-    version_info = (major, version)
-    version = '%s_%s' % version_info
-except (ImportError, ValueError):
-    version = str(major)
