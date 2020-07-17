165f31ef28ec6ddf3ea199f7198516f20a7ac1f6
Peter Hawkins <phawkins@google.com>
2020-07-17 08:38:33 -0400

    Also test for transpose in dot vmap test.

--- tests/batching_test.py
+++ tests/batching_test.py
@@ -295,6 +295,7 @@ class BatchingTest(jtu.JaxTestCase):
     f = vmap(partial(jnp.einsum, 'ij,j->i'), (None, 0))
     jaxpr = make_jaxpr(f)(jnp.zeros((1000, 1000)), jnp.zeros((1000, 1000)))
     assert "broadcast" not in str(jaxpr)
+    assert "transpose" not in str(jaxpr)
 
   def testPad(self):
     R = np.random.RandomState(0).randn
