0379d70debdbc73367e55fe7b67e6409b965253d
Stephan Hoyer <shoyer@google.com>
2020-07-20 15:05:50 -0700

    improve docs

--- jax/scipy/optimize/_minimize.py
+++ jax/scipy/optimize/_minimize.py
@@ -36,17 +36,31 @@ def minimize(
     fun: Callable,
     x0: jnp.ndarray,
     args: Tuple = (),
-    method: Optional[str] = None,
     *,
+    method: Optional[str] = None,
     tol: Optional[float] = None,
     options: Optional[Mapping[str, Any]] = None,
 ) -> OptimizeResults:
   """Minimization of scalar function of one or more variables.
 
+  This API for this function matches SciPy with some minor deviations:
+  - Gradients of ``fun`` are calculated automatically using JAX's autodiff
+    support when required.
+  - The ``method`` argument is required. You must specify a solver.
+  - Various optional arguments in the SciPy interface have not yet been
+    implemented.
+  - Optimization results may differ from SciPy due to differences in the line
+    search implementation.
+
+  ``minimize`` supports ``jit`` compilation. It does not yet support
+   differentiation or arguments in the form of multi-dimensional arrays, but
+   support for both is planned.
+
   Args:
     fun: the objective function to be minimized, ``fun(x, *args) -> float``,
       where ``x`` is an 1-D array with shape ``(n,)`` and ``args`` is a tuple
       of the fixed parameters needed to completely specify the function.
+      ``fun`` must support differentiation.
     x0: initial guess. Array of real elements of size ``(n,)``, where 'n' is
       the number of independent variables.
     args: extra arguments passed to the objective function.
