7a2ced1b6f732c928ff1e9aaf0fa11ae9cc84142
Jamie Townsend <jamiehntownsend@gmail.com>
2020-07-28 13:19:12 +0000

    Fix flaky generated_fun_test.py test

--- tests/generated_fun_test.py
+++ tests/generated_fun_test.py
@@ -186,7 +186,7 @@ def inner_prod(xs, ys):
   return sum(jnp.sum(x * y) for x, y in xys)
 
 def jvp_fd(fun, args, tangents):
-  EPS = 1e-4
+  EPS = 1e-3
   def eval_eps(eps):
     return fun(*[x if t is None else x + eps * t
                  for x, t in zip(args, tangents)])
