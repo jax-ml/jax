a2e746ff3d28504f8709238829d72f90829f80fc
John Aslanides <jaslanides@google.com>
2020-07-31 14:47:42 +0100

    Catch invalid (negative) in/out axes in vmap.

--- jax/api.py
+++ jax/api.py
@@ -877,6 +877,12 @@ def vmap(fun: Callable, in_axes=0, out_axes=0) -> Callable:
     msg = ("vmap out_axes must be an int, None, or (nested) container with "
            "those types as leaves, but got {}.")
     raise TypeError(msg.format(out_axes))
+  if not all(l >= 0 for l in tree_leaves(in_axes) if isinstance(l, int)):
+    msg = "vmap in_axes leaves must be non-negative integers or None, but got {}."
+    raise TypeError(msg.format(in_axes))
+  if not all(l >= 0 for l in tree_leaves(out_axes) if isinstance(l, int)):
+    msg = "vmap in_axes leaves must be non-negative integers or None, but got {}."
+    raise TypeError(msg.format(out_axes))
 
   @wraps(fun, docstr=docstr)
   def batched_fun(*args):
