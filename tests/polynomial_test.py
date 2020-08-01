4d99caa2229c563e9d9b5796eb0aa78729ffab9d
Julius Kunze <juliuskunze@gmail.com>
2020-08-01 17:37:31 +0200

    Fix polynomial tests (#3933)

--- tests/polynomial_test.py
+++ tests/polynomial_test.py
@@ -58,10 +58,9 @@ class TestPolynomial(jtu.JaxTestCase):
       return jnp.concatenate(
         [jnp.zeros(leading, p.dtype), p, jnp.zeros(trailing, p.dtype)]),
 
-    # order may differ (jnp.sort doesn't deal with complex numbers)
-    np_fn = lambda arg: np.sort(jnp.roots(arg))
+    jnp_fn = lambda arg: jnp.sort(jnp.roots(arg))
     np_fn = lambda arg: np.sort(np.roots(arg))
-    self._CheckAgainstNumpy(np_fn, np_fn, args_maker, check_dtypes=False,
+    self._CheckAgainstNumpy(np_fn, jnp_fn, args_maker, check_dtypes=False,
                             tol=3e-6)
 
   @parameterized.named_parameters(jtu.cases_from_list(
@@ -84,10 +83,9 @@ class TestPolynomial(jtu.JaxTestCase):
         # adding trailing would make input invalid (start with zeros)
         return p,
 
-    # order may differ (jnp.sort doesn't deal with complex numbers)
-    np_fn = lambda arg: np.sort(jnp.roots(arg, strip_zeros=False))
+    jnp_fn = lambda arg: jnp.sort(jnp.roots(arg, strip_zeros=False))
     np_fn = lambda arg: np.sort(np.roots(arg))
-    self._CheckAgainstNumpy(np_fn, np_fn, args_maker,
+    self._CheckAgainstNumpy(np_fn, jnp_fn, args_maker,
                             check_dtypes=False, tol=1e-6)
 
   @parameterized.named_parameters(jtu.cases_from_list(
@@ -113,13 +111,12 @@ class TestPolynomial(jtu.JaxTestCase):
         # adding trailing would make input invalid (start with zeros)
         return p,
 
-    # order may differ (jnp.sort doesn't deal with complex numbers)
     roots_compiled = jit(partial(jnp.roots, strip_zeros=False))
-    np_fn = lambda arg: np.sort(roots_compiled(arg))
+    jnp_fn = lambda arg: jnp.sort(roots_compiled(arg))
     np_fn = lambda arg: np.sort(np.roots(arg))
     # Using strip_zeros=False makes the algorithm less efficient
     # and leads to slightly different values compared ot numpy
-    self._CheckAgainstNumpy(np_fn, np_fn, args_maker,
+    self._CheckAgainstNumpy(np_fn, jnp_fn, args_maker,
                             check_dtypes=False, tol=1e-6)
 
   @parameterized.named_parameters(jtu.cases_from_list(
