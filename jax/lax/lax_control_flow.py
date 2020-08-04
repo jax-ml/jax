c24a9b964e1230c4a30b18084b1de16ef42faa70
Jake VanderPlas <jakevdp@google.com>
2020-08-04 12:45:16 -0700

    Cleanup: update license copyrights to 2020

--- jax/lax/lax_control_flow.py
+++ jax/lax/lax_control_flow.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2019 Google LLC
+# Copyright  2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
