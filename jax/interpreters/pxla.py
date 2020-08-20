911fcb1c312508e6d277967b79ddd1f8cbb2ca90
Matthew Johnson <mattjj@google.com>
2020-08-20 13:04:44 -0700

    deflake

--- jax/interpreters/pxla.py
+++ jax/interpreters/pxla.py
@@ -1242,7 +1242,8 @@ def _soft_pmap_callable(fun, axis_name, axis_size, mapped_invars, *avals):
   compile_options = xb.get_compile_options(
           num_replicas=num_devices, num_partitions=1, device_assignment=None)
   compile_options.tuple_arguments = tuple_args
-  compiled = xla.backend_compile(xb.get_backend(None), built, compile_options)
+  backend = xb.get_backend(None)
+  compiled = xla.backend_compile(backend, built, compile_options)
 
   input_specs = [
       ShardingSpec(shards_per_axis=(num_devices,) + (1,) * (aval.ndim - 1),
--- jax/interpreters/xla.py
+++ jax/interpreters/xla.py
@@ -34,7 +34,7 @@ from ..abstract_arrays import (ConcreteArray, ShapedArray, AbstractToken,
                                abstract_token)
 from ..core import Literal, pp_eqn_compact
 from ..pprint_util import pp
-from ..util import (partial, partialmethod, cache, prod, unzip2, memoize,
+from ..util import (partial, partialmethod, cache, prod, unzip2,
                     extend_name_stack, wrap_name, safe_zip, safe_map)
 from ..lib import xla_bridge as xb
 from ..lib import xla_client as xc
