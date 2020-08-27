1133e742276f7d44636c53b06173712df7b2e5e2
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2020-08-27 18:21:58 +0200

    Implement a proper shape checking rule for gather.
    The implementation is based on the corresponding shape inference
    code in `tensorflow/compiler/xla/service/shape_inference.cc`. The
    tests added in `tests/lax_test.py` are similarly mirroring the
    corresponding tests in tensorflow, with slight adaptations for the
    particular setting of JAX. Fixes google/jax#2826, and in principle
    fixes google/jax#4154 and google/jax#3905.

--- jax/lax/lax.py
+++ jax/lax/lax.py
@@ -3831,15 +3831,137 @@ def _gather_dtype_rule(operand, start_indices, **kwargs):
 
 def _gather_shape_rule(operand, start_indices, *, dimension_numbers,
                        slice_sizes):
-  if len(operand.shape) != len(slice_sizes):
-    msg = ("slice_sizes must have rank equal to the gather operand; "
-          "operand.shape={}, slice_sizes={}".format(operand.shape, slice_sizes))
-    raise ValueError(msg)
-  result_rank = len(dimension_numbers.offset_dims) + start_indices.ndim - 1
-  start_indices_shape = iter(start_indices.shape[:-1])
-  slice_sizes = iter(np.delete(slice_sizes, dimension_numbers.collapsed_slice_dims))
-  return tuple(next(slice_sizes) if i in dimension_numbers.offset_dims
-               else next(start_indices_shape) for i in range(result_rank))
+  """Validates the well-formedness of the arguments to Gather.
+
+  The code implements the checks based on the detailed operation semantics of
+  XLA's `Gather <https://www.tensorflow.org/xla/operation_semantics#gather>`_
+  operator and following the outline of the implementation of
+  ShapeInference::InferGatherShape in TensorFlow.
+  """
+  rank = lambda arr: len(arr.shape)
+
+  def _is_sorted(dims, name):
+    for i in range(1, len(dims)):
+      if dims[i] < dims[i - 1]:
+        raise TypeError(f"{name} in gather op must be sorted; got {dims}")
+
+  def _sorted_dims_in_range(dims, rank, name):
+    if len(dims) == 0:
+      return
+    invalid_dim = None
+    if dims[0] < 0:
+      invalid_dim = dims[0]
+    elif dims[-1] >= rank:
+      invalid_dim = dims[-1]
+    if invalid_dim:
+      raise TypeError(f"Invalid {name} set in gather op; valid range is "
+                      f"[0, {rank}); got: {invalid_dim}.")
+
+  def _no_duplicate_dims(dims, name):
+    if len(set(dims)) != len(dims):
+      raise TypeError(f"{name} in gather op must not repeat; got: {dims}.")
+
+  offset_dims = dimension_numbers.offset_dims
+  collapsed_slice_dims = dimension_numbers.collapsed_slice_dims
+  start_index_map = dimension_numbers.start_index_map
+
+  # Note: in JAX, index_vector_dim is always computed as below, cf. the
+  # documentation of the GatherDimensionNumbers class.
+  index_vector_dim = rank(start_indices) - 1
+
+  # This case should never happen in JAX, due to the implicit construction of
+  # index_vector_dim, but is included for completeness.
+  if rank(start_indices) < index_vector_dim or index_vector_dim < 0:
+    raise TypeError(f"Gather index leaf dimension must be within [0, rank("
+                    f"start_indices) + 1). rank(start_indices) is "
+                    f"{rank(start_indices)} and gather index leaf dimension is "
+                    f"{index_vector_dim}.")
+
+  expanded_start_indices_shape = list(start_indices.shape)
+
+  # This case should never happen in JAX, due to the implicit construction of
+  # index_vector_dim, but is included for completeness.
+  if len(expanded_start_indices_shape) == index_vector_dim:
+    expanded_start_indices_shape.append(1)
+
+  # Start ValidateGatherDimensions
+  # In the error messages output by XLA, "offset_dims" is called "Output window
+  # dimensions" in error messages. For consistency's sake, our error messages
+  # stick to "offset_dims".
+  _is_sorted(offset_dims, "offset_dims")
+  _no_duplicate_dims(offset_dims, "offset_dims")
+
+  output_offset_dim_count = len(offset_dims)
+  output_shape_rank = len(offset_dims) + rank(start_indices) - 1
+
+  for i in range(output_offset_dim_count):
+    offset_dim = offset_dims[i]
+    if offset_dim < 0 or offset_dim >= output_shape_rank:
+      raise TypeError(f"Offset dimension {i} in gather op is out of bounds; "
+                      f"got {offset_dim}, but should have been in "
+                      f"[0, {output_shape_rank})")
+
+  if len(start_index_map) != start_indices.shape[index_vector_dim]:
+    raise TypeError(f"Gather op has {len(start_index_map)} elements in "
+                    f"start_index_map and the bound of dimension "
+                    f"index_vector_dim={index_vector_dim} of start_indices is "
+                    f"{start_indices.shape[index_vector_dim]}. These two "
+                    f"numbers must be equal.")
+
+  for i in range(len(start_index_map)):
+    operand_dim_for_start_index_i = start_index_map[i]
+    if (operand_dim_for_start_index_i < 0 or
+        operand_dim_for_start_index_i >= rank(operand)):
+      raise TypeError(f"Invalid start_index_map; domain is "
+                      f"[0, {rank(operand)}), got: "
+                      f"{i}->{operand_dim_for_start_index_i}.")
+
+  _no_duplicate_dims(start_index_map, "start_index_map")
+
+  # _is_sorted and _sorted_dims_in_range are checked in the opposite order
+  # compared to the XLA implementation. In cases when the input is not sorted
+  # AND there are problematic collapsed_slice_dims, the error message will thus
+  # be different.
+  _is_sorted(collapsed_slice_dims, "collapsed_slice_dims")
+  _sorted_dims_in_range(collapsed_slice_dims, rank(operand),
+                        "collapsed_slice_dims")
+  _no_duplicate_dims(collapsed_slice_dims, "collapsed_slice_dims")
+  # End ValidateGatherDimensions
+
+  if rank(operand) != len(slice_sizes):
+    raise TypeError(f"Gather op must have one slice size for every input "
+                    f"dimension; got: len(slice_sizes)={len(slice_sizes)}, "
+                    f"input_shape.rank={rank(operand)}")
+
+  if len(slice_sizes) != len(offset_dims) + len(collapsed_slice_dims):
+    raise TypeError(f"All components of the offset index in a gather op must "
+                    f"either be a offset dimension or explicitly collapsed; "
+                    f"got len(slice_sizes)={len(slice_sizes)}, "
+                    f"output_slice_sizes={offset_dims}, collapsed_slice_dims="
+                    f"{collapsed_slice_dims}.")
+
+  for i in range(len(slice_sizes)):
+    slice_size = slice_sizes[i]
+    corresponding_input_size = operand.shape[i]
+
+    if slice_size < 0 or slice_size > corresponding_input_size:
+      raise TypeError(f"Slice size at index {i} in gather op is out of range, "
+                      f"must be within [0, {corresponding_input_size + 1}), "
+                      f"got {slice_size}.")
+
+  for i in range(len(collapsed_slice_dims)):
+    bound = slice_sizes[collapsed_slice_dims[i]]
+    if bound > 1:
+      raise TypeError(f"Gather op can only collapse slice dims with bound 1 "
+                      f"or 0, but bound is {bound} for index "
+                      f"{collapsed_slice_dims[i]} at position {i}.")
+
+  expanded_start_indices_shape.pop(index_vector_dim)
+  start_indices_shape = iter(expanded_start_indices_shape)
+
+  slice_sizes = iter(np.delete(slice_sizes, collapsed_slice_dims))
+  return tuple(next(slice_sizes) if i in offset_dims
+               else next(start_indices_shape) for i in range(output_shape_rank))
 
 def _gather_translation_rule(c, operand, start_indices, *, dimension_numbers,
                              slice_sizes):
