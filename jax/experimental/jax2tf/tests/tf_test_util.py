ab365f79fe30f79a0c16ba8743b79a70710447cb
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2020-09-07 09:43:07 +0200

    Cleanup: fix docstring for 80 char boundary.

--- jax/experimental/jax2tf/tests/tf_test_util.py
+++ jax/experimental/jax2tf/tests/tf_test_util.py
@@ -85,11 +85,12 @@ class JaxToTfTestCase(jtu.JaxTestCase):
       custom_assert: a function that will be called
         `custom_assert(result_jax, result_tf)` to assert equality of the
         results. Use this function when JAX and TF produce different results.
-        This function is only used for "eager" and "graph" modes by default, not for
-        the "compiled" mode, because in that case we expect the results to be equal.
-      always_custom_assert: if True, custom_assert is also called in "compiled" mode.
-        This is useful in cases where JAX and TF produce different but equally valid
-        results.
+        This function is only used for "eager" and "graph" modes by default, not
+        for the "compiled" mode, because in that case we expect the results to
+        be equal.
+      always_custom_assert: if True, custom_assert is also called in "compiled"
+        mode. This is useful in cases where JAX and TF produce different but
+        equally valid results.
       expect_tf_exceptions: if True, there may be exceptions in some evaluation
         modes; when there is no exception the result should be the same
         as in JAX.
