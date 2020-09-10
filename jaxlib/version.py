fc39332c4fb1e7d824603a59f1b5176949c59e85
Skye Wanderman-Milne <skyewm@google.com>
2020-09-10 15:53:04 -0700

    Clarify when jaxlib version should be bumped. (#4250)

--- jaxlib/version.py
+++ jaxlib/version.py
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This should only be increased prior to building + releasing a new jaxlib.
 __version__ = "0.1.55"
