d6af9215c9937a0cc69ac04594a31dc9b80a1460
James Bradbury <jekbradbury@google.com>
2020-09-14 12:09:32 -0700

    Partially revert #4277
    @romanngg says this is the only part of the change that broke internal code.

--- jax/experimental/stax.py
+++ jax/experimental/stax.py
@@ -49,8 +49,7 @@ def Dense(out_dim, W_init=glorot_normal(), b_init=normal()):
   def init_fun(rng, input_shape):
     output_shape = input_shape[:-1] + (out_dim,)
     k1, k2 = random.split(rng)
-    b_shape = (1,) * (len(output_shape) - 1) + (out_dim,)
-    W, b = W_init(k1, (input_shape[-1], out_dim)), b_init(k2, b_shape)
+    W, b = W_init(k1, (input_shape[-1], out_dim)), b_init(k2, (out_dim,))
     return output_shape, (W, b)
   def apply_fun(params, inputs, **kwargs):
     W, b = params
