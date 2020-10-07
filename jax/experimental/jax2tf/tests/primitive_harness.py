6a4fdc97608eeed11c045b833a75c107ada9cc37
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2020-10-07 15:55:23 +0200

    [jax2tf] Fix min_max and add_mul harnesses.
    Some of the array parameters were wrapped in StaticArg(), which
    resulted in the function not being actually converted through
    jax2tf.
    
    One oddity is that the test now requires "always_custom_assert" to
    be set to True to run properly, implying that even in compiled
    mode, the results produced by jax and jax2tf are inconsistent.

--- jax/experimental/jax2tf/tests/primitive_harness.py
+++ jax/experimental/jax2tf/tests/primitive_harness.py
@@ -240,7 +240,7 @@ lax_add_mul = tuple(
 ) + tuple(
   Harness(f"fun={f_jax.__name__}_bounds_{jtu.dtype_str(dtype)}",
           f_jax,
-          [StaticArg(lhs), StaticArg(rhs)],
+          [lhs, rhs],
           f_jax=f_jax,
           dtype=dtype)
   for f_jax in [lax.add, lax.mul]
@@ -265,7 +265,7 @@ lax_min_max = tuple(
 ) + tuple(
   Harness(f"fun={f_jax.__name__}_inf_nan_{jtu.dtype_str(dtype)}_{lhs[0]}_{rhs[0]}",
           f_jax,
-          [StaticArg(lhs), StaticArg(rhs)],
+          [lhs, rhs],
           f_jax=f_jax,
           dtype=dtype)
   for f_jax in [lax.min, lax.max]
