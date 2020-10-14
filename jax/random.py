1d4c53cef91e438237f855a2dac2febd0e2c5da8
Peter Hawkins <phawkins@google.com>
2020-10-14 14:33:56 -0400

    Fix CUDA launch error when generating an empty PRNG array.

--- jax/random.py
+++ jax/random.py
@@ -213,6 +213,10 @@ def _threefry2x32_gpu_translation_rule(c, k1, k2, x1, x2):
       c.get_shape(k1).dimensions(), c.get_shape(k2).dimensions(),
       c.get_shape(x1).dimensions(), c.get_shape(x2).dimensions())
   rank = len(shape)
+  if 0 in shape:
+    zeros = xla_client.ops.Broadcast(
+        xla_bridge.constant(c, np.array(0, np.uint32)), shape)
+    return xla_client.ops.Tuple(c, [zeros, zeros])
   def _broadcast(x):
     ndims = c.get_shape(x).rank()
     return xla_client.ops.BroadcastInDim(x, shape,
