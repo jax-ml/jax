Fix incorrect slice gradient when a slice has zero size and striding.

jax/lax/lax.py
@@ -3683,13 +3683,17 @@ def _slice_transpose_rule(t, operand, *, start_indices, limit_indices, strides):
     pads = zip(start_indices, np.subtract(operand_shape, limit_indices),
                (0,) * len(start_indices))
   else:
-    real_limits = np.add(np.add(start_indices, 1),
-                          np.multiply(np.subtract(t.shape, 1), strides))
+    real_limits = np.add(
+      start_indices,
+      np.where(np.array(t.shape) == 0, 0,
+               np.add(1, np.multiply(np.subtract(t.shape, 1), strides))))
     pads = safe_zip(start_indices, np.subtract(operand_shape, real_limits),
                     np.subtract(strides, 1))
   result = pad(t, _const(t, 0), pads)
-  assert result.shape == operand_shape
+  assert result.shape == operand_shape, (
+    f"result.shape={result.shape} operand_shape={operand_shape}")
   return [result]
+
 
 def _slice_batching_rule(batched_args, batch_dims, *, start_indices,
                          limit_indices, strides):