c876efcc2c10911361c8420c80cbe4f359a759dc
Peter Hawkins <phawkins@google.com>
2020-10-17 11:37:55 -0400

    Move implementation of jax.image to jax._src.image.

--- jax/_src/image/__init__.py
+++ jax/_src/image/__init__.py
@@ -11,12 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Common functions for neural network libraries."""
-
-# flake8: noqa: F401
-from jax._src.image.scale import (
-  resize,
-  ResizeMethod,
-  scale_and_translate,
-)
