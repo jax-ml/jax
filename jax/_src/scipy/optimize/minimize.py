b5ae3433b92bcc4cef1a4c32148c7a9f8d1ef515
joshuaalbert <45720Physics>
2020-10-23 14:17:33 +0200

    * de-lint

--- jax/_src/scipy/optimize/minimize.py
+++ jax/_src/scipy/optimize/minimize.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from functools import partial
 from typing import Any, Callable, Mapping, Optional, Tuple, Union
 
 from .bfgs import minimize_bfgs
