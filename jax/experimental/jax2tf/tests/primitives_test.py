85ca4ed19528f2c2abdcb5dfb82a3f154cbb9bf2
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2020-11-03 15:39:14 +0100

    [jax2tf] Fix and trim FFT conversion test.

--- jax/experimental/jax2tf/tests/primitives_test.py
+++ jax/experimental/jax2tf/tests/primitives_test.py
@@ -170,15 +170,8 @@ class JaxPrimitiveTest(tf_test_util.JaxToTfTestCase):
                                   "only 1D FFT is currently supported."):
         harness.dyn_fun(*harness.dyn_args_maker(self.rng()))
     else:
-      tol = None
-      if jtu.device_under_test() in ("cpu", "gpu"):
-        if harness.params["dtype"] in jtu.dtypes.boolean:
-          tol = 0.01
-        else:
-          tol = 1e-3
       self.ConvertAndCompare(harness.dyn_fun,
-                             *harness.dyn_args_maker(self.rng()),
-                             atol=tol, rtol=tol)
+                             *harness.dyn_args_maker(self.rng()))
 
   @primitive_harness.parameterized(primitive_harness.lax_linalg_cholesky)
   def test_cholesky(self, harness: primitive_harness.Harness):
