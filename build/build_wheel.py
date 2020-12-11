c23edb805eff038f07c5b0a6edebdf5c5774a645
Peter Hawkins <phawkins@google.com>
2020-12-11 06:44:45 -0800

    Include a LICENSE.txt in jaxlib wheels.
    PiperOrigin-RevId: 346988918

--- build/build_wheel.py
+++ build/build_wheel.py
@@ -130,8 +130,10 @@ def prepare_wheel(sources_path):
   copy_to_jaxlib = functools.partial(copy_file, dst_dir=jaxlib_dir)
 
   verify_mac_libraries_dont_reference_chkstack()
+  copy_to_jaxlib(r.Rlocation("__main__/build/LICENSE.txt"))
   copy_file(r.Rlocation("__main__/jaxlib/setup.py"), dst_dir=sources_path)
-  copy_to_jaxlib(r.Rlocation("__main__/jaxlib/init.py"), dst_filename="__init__.py")
+  copy_to_jaxlib(r.Rlocation("__main__/jaxlib/init.py"),
+                 dst_filename="__init__.py")
   copy_to_jaxlib(r.Rlocation("__main__/jaxlib/lapack.so"))
   copy_to_jaxlib(r.Rlocation("__main__/jaxlib/_pocketfft.so"))
   copy_to_jaxlib(r.Rlocation("__main__/jaxlib/pocketfft_flatbuffers_py_generated.py"))
