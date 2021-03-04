a0d5a6cfec17f5dc4585d59986d11ac846d7a931
Skye Wanderman-Milne <skyewm@google.com>
2021-03-04 14:18:55 -0800

    Update suggested `pip install` command in build_wheel.py
    Adding `--force-reinstall --no-deps` makes pip always install the new jaxlib, even if a jaxlib with the same version is already installed. This change will save me many seconds of typing in the future.

--- build/build_wheel.py
+++ build/build_wheel.py
@@ -187,7 +187,7 @@ def build_wheel(sources_path, output_path):
     output_file = os.path.join(output_path, os.path.basename(wheel))
     sys.stderr.write(f"Output wheel: {output_file}\n\n")
     sys.stderr.write("To install the newly-built jaxlib wheel, run:\n")
-    sys.stderr.write(f"  pip install {output_file}\n\n")
+    sys.stderr.write(f"  pip install --force-reinstall --no-deps {output_file}\n\n")
     shutil.copy(wheel, output_path)
 
 
