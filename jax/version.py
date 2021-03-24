c4a099093cd2bf40e891c8253cfd703d6ecbb5c1
Matthew Johnson <mattjj@google.com>
2021-03-23 19:33:04 -0700

    update version and changelog for pypi

--- jax/version.py
+++ jax/version.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.2.10"
+__version__ = "0.2.11"
 _minimum_jaxlib_version = "0.1.62"
