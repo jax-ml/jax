BUG: ensure that explicit conversion to uint64 does not overflow

jax/dtypes.py
@@ -129,16 +129,14 @@ def _scalar_type_to_dtype(typ: type, value: Any = None):
   return dtype
 
 
-def coerce_to_array(x):
+def coerce_to_array(x, dtype=None):
   """Coerces a scalar or NumPy array to an np.array.
 
   Handles Python scalar type promotion according to JAX's rules, not NumPy's
   rules.
   """
-  if type(x) in python_scalar_dtypes:
+  if dtype is None and type(x) in python_scalar_dtypes:
     dtype = _scalar_type_to_dtype(type(x), x)
-  else:
-    dtype = None
   return np.asarray(x, dtype)
 
 iinfo = np.iinfo