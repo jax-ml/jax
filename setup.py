55b421ff36db3a43a8dbb7cccc0073ac0b6ed69d
mariosasko <mariosasko777@gmail.com>
2021-06-10 16:06:11 +0200

    Specify zip_safe for mypy

--- setup.py
+++ setup.py
@@ -57,5 +57,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-    ]
+    ],
+    zip_safe=False,
 )
