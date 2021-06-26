a50c2732b6938942ce72e1e51ab430ddb4cb8499
George Karpenkov <cheshire@google.com>
2021-06-25 17:46:28 -0700

    Reland bf16 gemm: rollback of rollback
    PiperOrigin-RevId: 381579643

--- tests/lax_numpy_test.py
+++ tests/lax_numpy_test.py
@@ -2426,7 +2426,8 @@ class LaxBackedNumpyTests(jtu.JaxTestCase):
     args_maker = lambda: [rng(yshape, dtype), rng(xshape, dtype) if xshape is not None else None]
     np_fun = partial(np.trapz, dx=dx, axis=axis)
     jnp_fun = partial(jnp.trapz, dx=dx, axis=axis)
-    tol = jtu.tolerance(dtype, {np.float64: 1e-12})
+    tol = jtu.tolerance(dtype, {np.float64: 1e-12,
+                                dtypes.bfloat16: 4e-2})
     self._CheckAgainstNumpy(np_fun, jnp_fun, args_maker, tol=tol,
                             check_dtypes=False)
     self._CompileAndCheck(jnp_fun, args_maker, atol=tol, rtol=tol,
