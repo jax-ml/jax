5badea53ae2491b3c57374c7d0a832abc509c039
Jake VanderPlas <jakevdp@google.com>
2021-07-09 12:42:39 -0700

    setup.py: add Python 3.9 classifier

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     zip_safe=False,
 )
