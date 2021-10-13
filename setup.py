0072c32546074f7b568c9dfdc4962c66d819af94
Skye Wanderman-Milne <skyewm@google.com>
2021-10-12 17:37:29 -0700

    Update CHANGELOG and verson numbers for jaxlib 0.1.72 release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 # The following should be updated with each new jaxlib release.
-_current_jaxlib_version = '0.1.71'
+_current_jaxlib_version = '0.1.72'
 _available_cuda_versions = ['102', '111']
 
 _dct = {}
@@ -24,7 +24,7 @@ with open('jax/version.py') as f:
 __version__ = _dct['__version__']
 _minimum_jaxlib_version = _dct['_minimum_jaxlib_version']
 
-_libtpu_version = '0.1.dev20210809'
+_libtpu_version = '0.1.dev20211012'
 
 setup(
     name='jax',
