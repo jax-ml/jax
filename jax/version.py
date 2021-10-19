f66985de25ea3a61dae8ce748207e9fe134df514
Yash Katariya <yashkatariya@google.com>
2021-10-19 09:20:31 -0700

    Update jax version number after release
    PiperOrigin-RevId: 404281188

--- jax/version.py
+++ jax/version.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.2.24"
+__version__ = "0.2.25"
 _minimum_jaxlib_version = "0.1.69"
