a62355c84f2d90e12d8d330be42ec1351031b349
yashkatariya <yashkatariya@google.com>
2022-02-03 14:00:14 -0800

    Furo

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,7 @@ napolean_use_rtype = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
