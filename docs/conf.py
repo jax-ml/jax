86648b6fe8ae822870eab3d8ed46f17739b385bc
yashkatariya <yashkatariya@google.com>
2022-02-03 15:40:07 -0800

    book

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,7 @@ napolean_use_rtype = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = 'sphinx_book_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
