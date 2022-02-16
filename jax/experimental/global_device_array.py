94aade035a5fdeb2d3ed6f1744fcf1fa16240b8c
Yash Katariya <yashkatariya@google.com>
2022-02-16 08:51:55 -0800

    Set the aval inside _create_local_shards iteration. Since we are iterating over device buffers there, why pay the cost twice!
    PiperOrigin-RevId: 429058996

--- jax/experimental/global_device_array.py
+++ jax/experimental/global_device_array.py
@@ -337,17 +337,13 @@ class GlobalDeviceArray:
     for db in self._device_buffers:
       device = db.device()
       index, rid = global_indices_rid[device]
+      if db.aval is None:
+        db.aval = core.ShapedArray(db.shape, db.dtype)
       out.append(Shard(device, index, rid, db))
     return out
 
-
   @pxla.maybe_cached_property
   def local_shards(self) -> Sequence[Shard]:
-    for s in self._local_shards:
-      # Ignore the type because mypy thinks data is None but local_shards
-      # cannot have data=None which is checked in `_create_local_shards`.
-      if s.data.aval is None:  # type: ignore
-        s.data.aval = core.ShapedArray(s.data.shape, s.data.dtype)  # type: ignore
     return self._local_shards
 
   @pxla.maybe_cached_property
