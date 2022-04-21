c1261ccd27010b4e3c8442ff7c69cae5459a4f4c
Aart Bik <ajcbik@google.com>
2022-04-21 11:48:16 -0700

    Adds a wrapper to sparse tensor dialect, as part of an an initial prototype of an alternate JAX compilation path that emits the MLIR MHLO/CHLO dialects instead of classic XLA HLO together with sparse tensor types.
    PiperOrigin-RevId: 443438043

--- jax/_src/lib/mlir/dialects/__init__.py
+++ jax/_src/lib/mlir/dialects/__init__.py
@@ -17,3 +17,9 @@ import jaxlib.mlir.dialects.builtin as builtin
 import jaxlib.mlir.dialects.chlo as chlo
 import jaxlib.mlir.dialects.mhlo as mhlo
 import jaxlib.mlir.dialects.func as func
+try:
+  import jaxlib.mlir.dialects.sparse_tensor as sparse_tensor
+except (ModuleNotFoundError, ImportError):
+  # TODO(ajcbik,phawkins): make this unconditional when jaxlib > 0.3.7
+  #                        is the minimum version.
+  pass
