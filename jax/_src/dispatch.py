fb370b86ffee19cbd78cffa87e0492aefa34969d
Aart Bik <ajcbik@google.com>
2022-04-21 16:33:52 -0700

    Adds a ability to pass computation directly as module to backend
    PiperOrigin-RevId: 443512012

--- jax/_src/dispatch.py
+++ jax/_src/dispatch.py
@@ -670,7 +670,11 @@ def compile_or_get_cached(backend, computation, compile_options):
   if isinstance(computation, ir.Module):
     sym_name = computation.operation.attributes['sym_name']
     module_name = ir.StringAttr(sym_name).value
-    computation = mlir.module_to_string(computation)
+    # Convert ir.Module to str representation (the default), unless the
+    # back-end expliclity flags the ability to handle a module directly
+    # (avoiding the overhead of back and forth conversions)
+    if getattr(backend, "needs_str_ir", True):
+      computation = mlir.module_to_string(computation)
   else:
     module_name = computation.name()
 
