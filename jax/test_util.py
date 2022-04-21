d9508304e48c34be4452c4a8bc771aa03468e68b
Jake VanderPlas <jakevdp@google.com>
2022-04-21 12:12:40 -0700

    Deprecate remaining functionality in jax.test_util

--- jax/test_util.py
+++ jax/test_util.py
@@ -12,30 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# flake8: noqa: F401
-from jax._src.public_test_util import (
+from jax._src.public_test_util import (  # noqa: F401
   check_grads as check_grads,
   check_jvp as check_jvp,
   check_vjp as check_vjp,
 )
 
-# Conditional imports of private test utilities; these require their own BUILD target.
-# TODO(jakevdp): remove these imports once downstream dependencies are cleaned.
+# TODO(jakevdp): remove everything below once downstream callers are fixed.
+
+# Unconditionally import private test_util because it contains flag definitions.
+# In bazel, jax._src.test_util requires its own BUILD target so it may not be present.
+# pytype: disable=import-error
 try:
-  from jax._src.test_util import (  # pytype: disable=import-error
-    cases_from_list,
-    check_close,
-    check_eq,
-    device_under_test,
-    format_shape_dtype_string,
-    rand_uniform,
-    skip_on_devices,
-    with_config as with_config,
-    xla_bridge,
-    _default_tolerance,
-    DeprecatedJaxTestCase as JaxTestCase,
-    DeprecatedJaxTestLoader as JaxTestLoader,
-    DeprecatedBufferDonationTestCase as BufferDonationTestCase,
-  )
+  import jax._src.test_util as _private_test_util
 except ImportError:
   pass
+else:
+  del _private_test_util
+
+# Use module-level getattr to add warnings to imports of deprecated names.
+# pylint: disable=import-outside-toplevel
+def __getattr__(attr):
+  try:
+    from jax._src import test_util
+  except ImportError:
+    raise AttributeError(f"module {__name__} has no attribute {attr}")
+  if attr in ['cases_from_list', 'check_close', 'check_eq', 'device_under_test',
+              'format_shape_dtype_string', 'rand_uniform', 'skip_on_devices',
+              'with_config', 'xla_bridge', '_default_tolerance']:
+    import warnings
+    warnings.warn(f"jax.test_util.{attr} is deprecated and will soon be removed.", FutureWarning)
+    return getattr(test_util, attr)
+  elif attr in ['JaxTestCase', 'JaxTestLoader', 'BufferDonationTestCase']:
+    # Do the TestCase imports separately, since they were previously deprecated via a different
+    # mechanism & we don't want to annoy projects who may have temporarily filtered a specific warning.
+    return getattr(test_util, 'Deprecated' + attr)
+  else:
+    raise AttributeError(f"module {__name__} has no attribute {attr}")
