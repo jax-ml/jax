beb420bd5d4bb4dec9e73a970ef994cd3db752f7
Peter Hawkins <phawkins@google.com>
2022-04-25 09:15:25 -0700

    Revert: https://github.com/google/jax/pull/10221 (2nd revert)
    Prefer jnp.tile over concatenate.
    
    jnp.tile generates a jaxpr like the following:
    ```
    { lambda ; a:i32[720192]. let
        b:i32[1,720192] = reshape[dimensions=None new_sizes=(1, 720192)] a
        c:i32[720192] = squeeze[dimensions=(0,)] b
        d:i32[2,720192] = broadcast_in_dim[
          broadcast_dimensions=(1,)
          shape=(2, 720192)
        ] c
        e:i32[1440384] = reshape[dimensions=None new_sizes=(1440384,)] d
      in (e,) }
    ```
    
    whereas lax.concatenate generates the following jaxpr:
    ```
    { lambda ; a:i32[720192]. let
        b:i32[1440384] = concatenate[dimension=0] a a
      in (b,) }
    ```
    
    It seems the TPU compiler isn't doing as good a job with laying out memory for the formulation with `jnp.tile`. `reshape` in particular can be difficult for it to handle well, and it's best to avoid it when possible.
    
    Since the benefit was marginal (a simpler jaxpr... but is it? Really?) and the cost is real (a user's model broke), we should revert this change.
    
    PiperOrigin-RevId: 444287005

--- jax/_src/numpy/lax_numpy.py
+++ jax/_src/numpy/lax_numpy.py
@@ -3278,9 +3278,7 @@ def _roll(a, shift, axis):
                   np.broadcast_to(axis, b_shape)):
     i = _canonicalize_axis(i, a_ndim)
     x = remainder(x, (a_shape[i] or 1))
-    reps = [1] * a_ndim
-    reps[i] = 2
-    a = tile(a, reps)
+    a = lax.concatenate((a, a), i)
     a = lax.dynamic_slice_in_dim(a, a_shape[i] - x, a_shape[i], axis=i)
   return a
 
