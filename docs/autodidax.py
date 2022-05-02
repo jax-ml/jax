1134ff5871325c26bbfe1aed4a9c2054c4765abd
Jake VanderPlas <jakevdp@google.com>
2022-05-02 11:22:31 -0700

    CI: update jupytext to 0.13.8

--- docs/autodidax.py
+++ docs/autodidax.py
@@ -20,7 +20,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.6
+#       jupytext_version: 1.13.8
 #   kernelspec:
 #     display_name: Python 3
 #     name: python3
