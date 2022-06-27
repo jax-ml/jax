1e29b7b7626ad8757084e7959e6cfd6e5dd4286e
Peter Hawkins <phawkins@google.com>
2022-06-27 09:21:54 -0400

    Update CHANGELOG.md and setup.py for 0.3.14 release.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ _available_cuda_versions = ['11']
 _default_cuda_version = '11'
 _available_cudnn_versions = ['82', '805']
 _default_cudnn_version = '82'
-_libtpu_version = '0.1.dev20220621'
+_libtpu_version = '0.1.dev20220627'
 
 _dct = {}
 with open('jax/version.py') as f:
