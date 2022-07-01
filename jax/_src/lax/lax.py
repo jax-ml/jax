8e0af98f6f3d11be6fe8b72dcf0198b6bc2eb34a
Anish Tondwalkar <atondwal@google.com>
2022-07-01 14:56:35 -0700

    mhlo.rng op with distribution attr
    Aligns with the XLA kRng which takes a distribution as an attribute
    instead of having separate ops for each distribution.
    
    PiperOrigin-RevId: 458549234

--- jax/_src/lax/lax.py
+++ jax/_src/lax/lax.py
@@ -4243,7 +4243,11 @@ def _rng_uniform_lowering(ctx, a, b, *, shape):
   aval_out, = ctx.avals_out
   shape, = mlir.ir_constants(np.array(aval_out.shape, np.int64),
                              canonicalize_types=False)
-  return mhlo.RngUniformOp(a, b, shape).results
+  if jax._src.lib.mlir_api_version <= 22:
+    return mhlo.RngUniformOp(a, b, shape).results
+  else:
+    return mhlo.RngOp(a, b, shape,
+                      mhlo.RngDistributionAttr.get('UNIFORM')).results
 
 mlir.register_lowering(rng_uniform_p, _rng_uniform_lowering)
 
