eeb9b5f1f651498c28a5e49e92949292245ea3c2
Jake VanderPlas <jakevdp@google.com>
2022-08-15 15:32:45 -0700

    pre-commit hook: update flake8, mypy, & jupytext

--- docs/autodidax.py
+++ docs/autodidax.py
@@ -20,7 +20,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.8
+#       jupytext_version: 1.14.1
 #   kernelspec:
 #     display_name: Python 3
 #     name: python3
