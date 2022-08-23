31242cd9450ae09b33d24b2c10485c5379f4c753
Sudhakar <sudhakars@nvidia.com>
2022-08-23 09:29:11 -0700

    combine gpu multiprocess tests and use pytest with `forked` option to run each test in a separate subprocess to WAR multiple `jax.distributed.init` within a single test file/process

--- tests/multiprocess_gpu_test.py
+++ tests/multiprocess_gpu_test.py
@@ -24,6 +24,7 @@ from absl.testing import parameterized
 import jax
 from jax.config import config
 from jax._src import distributed
+import jax.numpy as jnp
 import jax._src.lib
 from jax._src import test_util as jtu
 
@@ -145,6 +146,47 @@ class MultiProcessGpuTest(jtu.JaxTestCase):
       out, _ = proc.communicate()
       self.assertEqual(proc.returncode, 0)
       self.assertEqual(out, f'{num_gpus_per_task},{num_gpus}')
+
+@unittest.skipIf(
+    os.environ.get("SLURM_JOB_NUM_NODES", None) != "2",
+    "Slurm environment with at least two nodes needed!")
+class MultiNodeGpuTest(jtu.JaxTestCase):
+
+  def test_gpu_multi_node_initialize_and_psum(self):
+
+    # Hookup the ENV vars expected to be set already in the SLURM environment
+    nodelist = os.environ.get("SLURM_STEP_NODELIST", None)
+    if nodelist is not None:
+      coordinator_address = nodelist.split('[')[0] + \
+                            nodelist.split('[')[1].split(',')[0]
+    num_tasks = os.environ.get("SLURM_NPROCS", None)
+    taskid = os.environ.get("SLURM_PROCID", None)
+    localid = os.environ.get("SLURM_LOCALID", None)
+
+    # fixing port since it needs to be the same for all the processes
+    port = "54321"
+
+    print(f"coord addr:port : {coordinator_address}:{port}\nTotal tasks: "
+          f"{num_tasks}\ntask id: {taskid}\nlocal id: {localid}")
+
+    self.assertEqual(
+        coordinator_address is None or num_tasks is None or taskid is None,
+        False)
+
+    jax.distributed.initialize(coordinator_address=f'{coordinator_address}:{port}',
+                               num_processes=int(num_tasks),
+                               process_id=int(taskid))
+
+    print(f"Total devices: {jax.device_count()}, Total tasks: {int(num_tasks)}, "
+          f"Devices per task: {jax.local_device_count()}")
+
+    self.assertEqual(jax.device_count(),
+                     int(num_tasks) * jax.local_device_count())
+
+    x = jnp.ones(jax.local_device_count())
+    y = jax.pmap(lambda x: jax.lax.psum(x, "i"), axis_name="i")(x)
+    self.assertEqual(y[0], jax.device_count())
+    print(y)
 
 if __name__ == "__main__":
   absltest.main(testLoader=jtu.JaxTestLoader())
