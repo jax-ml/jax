bd7770618c9e958adcf9cf731f6b8902f6c829de
Wonhyeong Seo <wonhseo@kakao.com>
2022-08-23 23:28:48 +0900

    copy of https://github.com/harryjulian/jax/pull/1

--- jax/_src/scipy/stats/multinomial.py
+++ jax/_src/scipy/stats/multinomial.py
@@ -1,6 +1,20 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import scipy.stats as osp_stats
 from jax import lax
-from jax._src.lax.lax import _const as _lax_const
 from jax._src.numpy import lax_numpy as jnp
 from jax._src.numpy.lax_numpy import _promote_args_inexact
 from jax._src.numpy.util import _wraps
@@ -10,13 +24,11 @@ from jax._src.scipy.special import gammaln, xlogy
 def logpmf(x, n, p):
   """JAX implementation of scipy.stats.multinomial.logpmf."""
   x, p = _promote_args_inexact("multinomial.logpmf", x, p)
-  one = _lax_const(x, 1)
-  nplusone, xplusone = jnp.add(n, one), jnp.add(x, one)
-  logprobs = gammaln(nplusone) + jnp.sum(xlogy(x, p) - gammaln(xplusone), axis=-1)
+  logprobs = gammaln(n + 1) + jnp.sum(xlogy(x, p) - gammaln(x + 1), axis=-1)
   return jnp.where(jnp.equal(jnp.sum(x), n), logprobs, -jnp.inf)
 
 @_wraps(osp_stats.multinomial.pmf, update_doc=False)
 def pmf(x, n, p):
   """JAX implementation of scipy.stats.multinomial.pmf."""
   x, p = _promote_args_inexact("multinomial.pmf", x, p)
-  return jnp.exp(logpmf(x, n, p))
+  return lax.exp(logpmf(x, n, p))
