afe74b471030608c0e66124a0859940547a8a294
Jake VanderPlas <jakevdp@google.com>
2022-10-10 16:54:29 -0700

    [typing] add type annotations to jax.scipy.linalg

--- jax/_src/third_party/scipy/linalg.py
+++ jax/_src/third_party/scipy/linalg.py
@@ -1,3 +1,5 @@
+from typing import Callable, Tuple
+
 import scipy.linalg
 
 from jax import jit, lax
@@ -5,9 +7,11 @@ from jax._src.numpy import lax_numpy as jnp
 from jax._src.numpy.linalg import norm
 from jax._src.numpy.util import _wraps
 from jax._src.scipy.linalg import rsf2csf, schur
+from jax._src.typing import ArrayLike, Array
+
 
 @jit
-def _algorithm_11_1_1(F, T):
+def _algorithm_11_1_1(F: Array, T: Array) -> Tuple[Array, Array]:
   # Algorithm 11.1.1 from Golub and Van Loan "Matrix Computations"
   N = T.shape[0]
   minden = jnp.abs(T[0, 0])
@@ -46,7 +50,7 @@ will be printed if the error in the array output is estimated to be large.
 """
 
 @_wraps(scipy.linalg.funm, lax_description=_FUNM_LAX_DESCRIPTION)
-def funm(A, func, disp=True):
+def funm(A: ArrayLike, func: Callable[[Array], Array], disp: bool = True) -> Tuple[Array, Array]:
   A = jnp.asarray(A)
   if A.ndim != 2 or A.shape[0] != A.shape[1]:
     raise ValueError('expected square array_like input')
