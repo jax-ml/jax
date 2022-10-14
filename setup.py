fb72c38e1967cf221ac3e384f6a64ea97bf5d8bb
Peter Hawkins <phawkins@google.com>
2022-10-14 14:56:07 +0000

    Add Python 3.11 as a compatible Python version.

--- setup.py
+++ setup.py
@@ -108,6 +108,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     zip_safe=False,
 )
