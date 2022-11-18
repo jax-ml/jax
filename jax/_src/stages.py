7a3dbcf94e7f7f14532dc8f6779302827e92b3e1
jax authors <no-reply@google.com>
2022-11-18 04:18:11 -0800

    Change params to _params to avoid clashes with downstream users.
    PiperOrigin-RevId: 489441483

--- jax/_src/stages.py
+++ jax/_src/stages.py
@@ -458,22 +458,22 @@ class Compiled(Stage):
     return tree_util.tree_unflatten(self.out_tree, shardings_flat)  # pytype: disable=attribute-error
 
   @staticmethod
-  def call(params, *args, **kwargs):
+  def call(_params, *args, **kwargs):
     if jax.config.jax_dynamic_shapes:
       raise NotImplementedError
-    if params.no_kwargs and kwargs:
+    if _params.no_kwargs and kwargs:
       kws = ', '.join(kwargs.keys())
       raise NotImplementedError(
           "function was compiled by a transformation that does not support "
           f"keyword arguments, but called with keyword arguments: {kws}")
     args_flat, in_tree = tree_util.tree_flatten((args, kwargs))
-    if in_tree != params.in_tree:
+    if in_tree != _params.in_tree:
       # TODO(frostig): provide more info about the source function
       # and transformation
       raise TypeError(
-          f"function compiled for {params.in_tree}, called with {in_tree}")
+          f"function compiled for {_params.in_tree}, called with {in_tree}")
     try:
-      out_flat = params.executable.call(*args_flat)
+      out_flat = _params.executable.call(*args_flat)
     except TypeError as e:
       # We can't transform ahead-of-time compiled calls, since we've
       # lowered and compiled for a fixed function signature, and JAX
@@ -491,7 +491,7 @@ class Compiled(Stage):
               f"Tracer type {type(arg)}.") from e
       else:
         raise
-    outs = tree_util.tree_unflatten(params.out_tree, out_flat)
+    outs = tree_util.tree_unflatten(_params.out_tree, out_flat)
     return outs, out_flat
 
   def __call__(self, *args, **kwargs):
