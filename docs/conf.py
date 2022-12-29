406b5525a416e674cac72c8f6ac07a6ca00691cc
Jake VanderPlas <jakevdp@google.com>
2022-12-29 15:37:54 -0800

    DOC: fix sphinx version for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ main_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
