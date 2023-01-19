17720fc53338844a7b8a98a5915b263f7b31ede3
Anlun Xu <anlunx@google.com>
2023-01-19 15:01:55 -0800

    JAX test debug
    PiperOrigin-RevId: 503274973

--- jax/_src/dispatch.py
+++ jax/_src/dispatch.py
@@ -1060,11 +1060,9 @@ def compile_or_get_cached(backend, computation: ir.Module, compile_options,
 
   # Persistent compilation cache only implemented on TPU.
   # TODO(skye): add warning when initializing cache on unsupported default platform
-  supported_platforms = ["tpu"]
+  supported_platforms = ["tpu", "gpu"]
   # GPU caching can be enabled if JitRt is enabled.
   # TODO(b/232263664): Remove check when JitRt is enabled by default.
-  if "--xla_gpu_enable_xla_runtime_executable=true" in os.environ.get("XLA_FLAGS", ""):
-    supported_platforms.append("gpu")
   # (b/233850967) CPU caching can be enabled if XLA Runtime is enabled.
   if "--xla_cpu_use_xla_runtime=true" in os.environ.get("XLA_FLAGS", ""):
     supported_platforms.append("cpu")
--- jax/experimental/compilation_cache/compilation_cache.py
+++ jax/experimental/compilation_cache/compilation_cache.py
@@ -56,6 +56,7 @@ def get_executable(xla_computation, compile_options,
   xla_executable_serialized = _cache.get(cache_key)
   if not xla_executable_serialized:
     return None
+  print(xla_executable_serialized)
   xla_executable_deserialized = backend.deserialize_executable(
       xla_executable_serialized,
       compile_options)
