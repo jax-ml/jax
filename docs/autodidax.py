20b55a119ebfbc87f698647dd32255a3b145f49a
Jake VanderPlas <jakevdp@google.com>
2023-01-23 14:42:03 -0800

    CI: update jupytext version

--- docs/autodidax.py
+++ docs/autodidax.py
@@ -20,7 +20,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.1
+#       jupytext_version: 1.14.4
 #   kernelspec:
 #     display_name: Python 3
 #     name: python3
