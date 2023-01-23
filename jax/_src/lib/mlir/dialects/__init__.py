94af71a24c1225ae6c39d9a1d9b7c0144b93af4d
Jake VanderPlas <jakevdp@google.com>
2023-01-23 15:59:33 -0800

    CI: fix mypy jaxlib version

--- jax/_src/lib/mlir/dialects/__init__.py
+++ jax/_src/lib/mlir/dialects/__init__.py
@@ -28,4 +28,4 @@ use_stablehlo = xla_client.mlir_api_version >= 42
 if use_stablehlo:
   import jaxlib.mlir.dialects.stablehlo as hlo
 else:
-  import jaxlib.mlir.dialects.mhlo as hlo
+  import jaxlib.mlir.dialects.mhlo as hlo  # type: ignore[no-redef]
