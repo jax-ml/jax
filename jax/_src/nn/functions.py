98b552895ba7023acde8773062375e77851d936f
George Necula <gcnecula@gmail.com>
2023-04-12 13:27:17 +0300

    [shape_poly] Add shape polymorphism support for jax.nn.one_hot

--- jax/_src/nn/functions.py
+++ jax/_src/nn/functions.py
@@ -379,9 +379,10 @@ def normalize(x: Array,
 @partial(jax.jit, static_argnames=("num_classes", "dtype", "axis"))
 def _one_hot(x: Array, num_classes: int, *,
              dtype: Any, axis: Union[int, AxisName]) -> Array:
-  num_classes = core.concrete_or_error(
-      int, num_classes,
-      "The error arose in jax.nn.one_hot argument `num_classes`.")
+  if not core.is_special_dim_size(num_classes):
+    num_classes = core.concrete_or_error(
+        int, num_classes,
+        "The error arose in jax.nn.one_hot argument `num_classes`.")
   dtype = dtypes.canonicalize_dtype(dtype)
   x = jnp.asarray(x)
   try:
@@ -425,9 +426,10 @@ def one_hot(x: Array, num_classes: int, *,
     axis: the axis or axes along which the function should be
       computed.
   """
-  num_classes = core.concrete_or_error(
-      int, num_classes,
-      "The error arose in jax.nn.one_hot argument `num_classes`.")
+  if not core.is_special_dim_size(num_classes):
+    num_classes = core.concrete_or_error(
+        int, num_classes,
+        "The error arose in jax.nn.one_hot argument `num_classes`.")
   return _one_hot(x, num_classes, dtype=dtype, axis=axis)
 
 
