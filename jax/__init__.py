e0d2736e378fe8f2e5b427658419328df8c20cec
Matthew Johnson <mattjj@google.com>
2023-04-19 18:11:35 -0700

    add custom_jvp for jax.nn.softmax
    This avoids saving the jnp.exp(...) value.

--- jax/__init__.py
+++ jax/__init__.py
@@ -51,6 +51,7 @@ from jax._src.config import (
   check_tracer_leaks as check_tracer_leaks,
   checking_leaks as checking_leaks,
   enable_custom_prng as enable_custom_prng,
+  softmax_custom_jvp as softmax_custom_jvp,
   enable_custom_vjp_by_custom_transpose as enable_custom_vjp_by_custom_transpose,
   debug_nans as debug_nans,
   debug_infs as debug_infs,
