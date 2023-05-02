1e8ee5266460636e2809e274814e1b9f9742c669
Yash Katariya <yashkatariya@google.com>
2023-05-02 15:32:57 -0700

    Copybara import of the project:
    --
    57af5360a1ca1356dbf7760c76e241f7134ef6dd by Jake VanderPlas <jakevdp@google.com>:
    
    [Rollback] Update required Python version to 3.9
    
    PiperOrigin-RevId: 528905991

--- build/build.py
+++ build/build.py
@@ -74,8 +74,8 @@ def get_python_version(python_bin_path):
   return major, minor
 
 def check_python_version(python_version):
-  if python_version < (3, 9):
-    print("ERROR: JAX requires Python 3.9 or newer, found ", python_version)
+  if python_version < (3, 8):
+    print("ERROR: JAX requires Python 3.8 or newer, found ", python_version)
     sys.exit(-1)
 
 
--- tests/dynamic_api_test.py
+++ tests/dynamic_api_test.py
@@ -14,6 +14,7 @@
 
 from functools import partial
 import re
+import sys
 import unittest
 import numpy as np
 
@@ -33,6 +34,8 @@ from jax._src import test_util as jtu
 config.parse_flags_with_absl()
 FLAGS = config.FLAGS
 
+
+python_version = (sys.version_info[0], sys.version_info[1])
 
 @jtu.with_config(jax_dynamic_shapes=True, jax_numpy_rank_promotion="allow")
 class DynamicShapeStagingTest(jtu.JaxTestCase):
