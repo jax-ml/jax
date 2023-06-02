592833e02ad65ff5bc69341c38ddd4ab34005eff
Jake VanderPlas <jakevdp@google.com>
2023-06-02 03:57:30 -0700

    Change uses of np.product to np.prod
    product is deprecated as of numpy 1.25.0.

--- jax/_src/third_party/numpy/linalg.py
+++ jax/_src/third_party/numpy/linalg.py
@@ -7,7 +7,7 @@ from jax._src.numpy.util import check_arraylike, _wraps
 
 def _isEmpty2d(arr):
   # check size first for efficiency
-  return arr.size == 0 and np.product(arr.shape[-2:]) == 0
+  return arr.size == 0 and np.prod(arr.shape[-2:]) == 0
 
 
 def _assertNoEmpty2d(*arrays):
