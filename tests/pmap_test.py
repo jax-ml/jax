1ad0a11897db066298a249f726638cb748c2fb24
Roy Frostig <frostig@google.com>
2023-07-10 18:28:50 -0700

    AOT: better error messages on call signature mismatch
    Also update error example in AOT docs.

--- tests/pmap_test.py
+++ tests/pmap_test.py
@@ -242,9 +242,9 @@ class PythonPmapTest(jtu.JaxTestCase):
     f_exe = f.lower(x_f32).compile()
     self.assertRaisesRegex(
         TypeError,
-        r"Computation was compiled for different input types and called with "
-        r"different types. Here are the 1 mismatches:\n"
-        r"Compiled with.*float32.*and called with.*int32.*for arg x",
+        r"Argument types differ .*"
+        r"The mismatches are:\n"
+        r"Argument 'x' compiled with.*float32.*and called with.*int32.*",
         lambda: f_exe(x_i32))
 
   def testLowerCompileMultiArg(self):
