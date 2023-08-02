391d45fe49ab7a39864a0d5f7353c5fdd1eb0985
Laurent El Shafey <shafey@google.com>
2023-08-01 22:41:01 -0700

    [Pallas] Import ops in jax/experimental/pallas/ops/__init__.py
    PiperOrigin-RevId: 553031447

--- jax/experimental/pallas/ops/__init__.py
+++ jax/experimental/pallas/ops/__init__.py
@@ -11,3 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+from jax.experimental.pallas.ops import attention
+from jax.experimental.pallas.ops import layer_norm
+from jax.experimental.pallas.ops import rms_norm
+from jax.experimental.pallas.ops import softmax
