f10e3338d6fc969e87b0b94d4332e43659133c3b
Yash Katariya <yashkatariya@google.com>
2023-10-18 13:25:12 -0700

    Start release for jax 0.4.19
    PiperOrigin-RevId: 574575158

--- setup.py
+++ setup.py
@@ -22,13 +22,13 @@ from setuptools import setup, find_packages
 
 project_name = 'jax'
 
-_current_jaxlib_version = '0.4.18'
+_current_jaxlib_version = '0.4.19'
 # The following should be updated with each new jaxlib release.
 _latest_jaxlib_version_on_pypi = '0.4.18'
 _available_cuda11_cudnn_versions = ['86']
 _default_cuda11_cudnn_version = '86'
 _default_cuda12_cudnn_version = '89'
-_libtpu_version = '0.1.dev20231006'
+_libtpu_version = '0.1.dev20231018'
 
 def load_version_module(pkg_path):
   spec = importlib.util.spec_from_file_location(
