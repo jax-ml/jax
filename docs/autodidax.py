389eb97a7c57ddc3333343a1eff8d0588787caa4
Jake VanderPlas <jakevdp@google.com>
2023-10-30 09:12:24 -0700

    CI: update pre-commit hooks to latest version

--- docs/autodidax.py
+++ docs/autodidax.py
@@ -20,7 +20,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.7
+#       jupytext_version: 1.15.2
 #   kernelspec:
 #     display_name: Python 3
 #     name: python3
