ed9a4c293948c2a209daccc34eb30bcd2828c084
Roy Frostig <frostig@google.com>
2023-10-31 13:42:45 -0700

    add `jax.threefry_partitionable` context manager

--- jax/__init__.py
+++ jax/__init__.py
@@ -67,6 +67,7 @@ from jax._src.config import (
   numpy_rank_promotion as numpy_rank_promotion,
   jax2tf_associative_scan_reductions as jax2tf_associative_scan_reductions,
   legacy_prng_key as legacy_prng_key,
+  threefry_partitionable as threefry_partitionable,
   transfer_guard as transfer_guard,
   transfer_guard_host_to_device as transfer_guard_host_to_device,
   transfer_guard_device_to_device as transfer_guard_device_to_device,
