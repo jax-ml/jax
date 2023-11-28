86e99a9e2cf9de63127448bbce0a6187bf78aeef
George Necula <necula@google.com>
2023-11-28 02:32:36 -0800

    Disable test failing in jaxlib GPU build
    PiperOrigin-RevId: 585902789

--- tests/lax_test.py
+++ tests/lax_test.py
@@ -154,6 +154,7 @@ class LaxTest(jtu.JaxTestCase):
     to_dtype=jtu.dtypes.all_floating + jtu.dtypes.all_integer + jtu.dtypes.all_unsigned,
     shape = [(), (2,), (2, 3)]
   )
+  @jtu.skip_on_devices("gpu")  # TODO(b/313567948): Test fails on GPU jaxlib build
   def testBitcastConvertType(self, from_dtype, to_dtype, shape):
     rng = jtu.rand_default(self.rng())
     itemsize_in = np.dtype(from_dtype).itemsize
