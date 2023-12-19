67d5c3bdea4cb7014ca63ebd97eacc7a5e8f71aa
Peter Hawkins <phawkins@google.com>
2023-12-19 13:05:26 -0800

    [JAX:GPU] Add a test that verifies that the XLA_PYTHON_CLIENT_PREALLOCATE environment variable is parsed correctly.
    Fixes https://github.com/google/jax/issues/19035
    
    PiperOrigin-RevId: 592322040

---
+++ tests/gpu_memory_flags_test.py
@@ -0,0 +1,60 @@
+# Copyright 2023 The JAX Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import unittest
+
+from absl.testing import absltest
+import jax
+from jax._src import config
+from jax._src import test_util as jtu
+from jax._src.lib import xla_extension_version
+
+config.parse_flags_with_absl()
+
+
+class GpuMemoryAllocationTest(absltest.TestCase):
+
+  # This test must be run in its own subprocess.
+  @unittest.skipIf(
+      "PYTEST_CURRENT_TEST" in os.environ,
+      "Test must run in an isolated process",
+  )
+  @unittest.skipIf(
+      "XLA_PYTHON_CLIENT_ALLOCATOR" in os.environ,
+      "Test does not work if the python client allocator has been overriden",
+  )
+  @unittest.skipIf(xla_extension_version < 225, "jaxlib version too old")
+  def test_gpu_memory_allocation(self):
+    falsey_values = ("0", "False", "false")
+    preallocate = (
+        os.environ.get("XLA_PYTHON_CLIENT_PREALLOCATE", "1")
+        not in falsey_values
+    )
+    device = jax.devices()[0]
+    mem_stats = device.memory_stats()
+    self.assertEqual(mem_stats["pool_bytes"], 0)
+    x = jax.lax.add(1, 2)
+
+    mem_stats = device.memory_stats()
+    if preallocate:
+      self.assertEqual(mem_stats["pool_bytes"], mem_stats["bytes_limit"])
+    else:
+      self.assertLessEqual(
+          mem_stats["pool_bytes"], mem_stats["bytes_limit"] // 2
+      )
+
+
+if __name__ == "__main__":
+  absltest.main(testLoader=jtu.JaxTestLoader())
