145cbb55d86eb4ee17ed7fc798703a34e8d6cc9a
Cjkkkk <ske@nvidia.com>
2024-01-03 14:13:55 -0800

    add license

--- jax/_src/cudnn/fused_attention_stableHLO_test.py
+++ jax/_src/cudnn/fused_attention_stableHLO_test.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The JAX Authors.
+# Copyright 2023 The JAX Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from functools import partial
 from absl.testing import absltest
 from typing import Any, Optional
