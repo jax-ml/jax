47f5375e8c997e1a2ac519a6d3e3203e3aa171b1
Yash Katariya <yashkatariya@google.com>
2024-02-29 22:57:01 -0800

    Correctly set the version number
    PiperOrigin-RevId: 611713955

--- jax/_src/interpreters/pxla.py
+++ jax/_src/interpreters/pxla.py
@@ -2043,7 +2043,7 @@ def lower_sharding_computation(
       any(not is_unspecified(o) for o in out_shardings))
 
   gs = GSPMDSharding.get_replicated(device_assignment)
-  if xla_extension_version < 240 or hasattr(backend, "compile_replicated"):
+  if xla_extension_version < 239 or hasattr(backend, "compile_replicated"):
     in_shardings = tuple(gs if is_unspecified(i) else i for i in in_shardings)
 
   da_object = _create_da_object(tuple(device_assignment))
@@ -2644,7 +2644,7 @@ def _cached_compilation(computation, name, mesh, spmd_lowering,
         get_logical_mesh_ids(list(mesh.shape.values()))
         .reshape(-1))
   compile_options.parameter_is_tupled_arguments = tuple_args
-  if xla_extension_version >= 240:
+  if xla_extension_version >= 239:
     opts.allow_spmd_sharding_propagation_to_parameters = list(allow_prop_to_inputs)
   opts.allow_spmd_sharding_propagation_to_output = list(allow_prop_to_outputs)
 
@@ -2865,7 +2865,7 @@ class UnloadedMeshExecutable:
     else:
       if pmap_nreps == 1:
         assert mesh is None
-        if xla_extension_version >= 240:
+        if xla_extension_version >= 239:
           in_shardings = _maybe_get_and_check_in_shardings(
               xla_executable, in_shardings, tuple(da), global_in_avals,
               len(ordered_effects))
