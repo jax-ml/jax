49831826a4d207a97f5e2253b7105803c9fe653a
rajasekharporeddy <rajasekharp@google.com>
2024-03-20 11:47:59 +0530

    Update tests for hilbert function

--- tests/linalg_test.py
+++ tests/linalg_test.py
@@ -2089,8 +2089,11 @@ class LaxLinalgTest(jtu.JaxTestCase):
     n=[0, 1, 5, 10, 20],
     )
   def testHilbert(self, n):
-    args_maker = lambda: [n]
-    self._CheckAgainstNumpy(osp.linalg.hilbert, jsp.linalg.hilbert, args_maker)
+    args_maker = lambda: []
+    osp_fun = partial(osp.linalg.hilbert, n=n)
+    jsp_fun = partial(jsp.linalg.hilbert, n=n)
+    self._CheckAgainstNumpy(osp_fun, jsp_fun, args_maker)
+    self._CompileAndCheck(jsp_fun, args_maker)
 
 if __name__ == "__main__":
   absltest.main(testLoader=jtu.JaxTestLoader())
