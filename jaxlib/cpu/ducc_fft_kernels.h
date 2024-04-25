6bfbb4593a42fced91ba50de47271af425c74c20
George Necula <necula@google.com>
2024-04-25 00:28:19 -0700

    Remove old ducc_fft custom call.
    Starting in June 2023 we have switched the CPU lowering for FFT to use
    the new custom call dynamic_ducc_fft. We are now out of the backwards
    compatibility window and we remove the old ducc_fft.
    
    We need to keep dynamic_ducc_fft a little bit longer (May 2024).
    
    PiperOrigin-RevId: 627981921

--- jaxlib/cpu/ducc_fft_kernels.h
+++ jaxlib/cpu/ducc_fft_kernels.h
@@ -20,10 +20,9 @@ limitations under the License.
 
 namespace jax {
 
-// TODO(b/287702203): this must be kept until EOY 2023 for backwards
-// of serialized functions using fft.
-void DuccFft(void* out, void** in, XlaCustomCallStatus*);
 
+// TODO(b/311175955): this must be kept until May 2024 for backwards
+// of serialized functions using fft.
 void DynamicDuccFft(void* out, void** in, XlaCustomCallStatus*);
 
 }  // namespace jax
