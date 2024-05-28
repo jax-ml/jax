21727ecaa74655d8acac064b04bd3a8f3230c0b6
Andrey Portnoy <aportnoy@nvidia.com>
2024-05-28 17:28:06 -0400

    Implement custom_vjp for foo

--- docs/cuda-custom-call/foo.cu
+++ docs/cuda-custom-call/foo.cu
@@ -2,34 +2,47 @@
 #include <xla/ffi/api/api.h>
 #include <xla/ffi/api/ffi.h>
 
+namespace ffi = xla::ffi;
+
+//----------------------------------------------------------------------------//
+//                            Forward pass                                    //
+//----------------------------------------------------------------------------//
+
+
 // c = a * (b+1)
 // This strawman operation works well for demo purposes because:
 // 1. it's simple enough to be quickly understood,
 // 2. it's complex enough to require intermediate outputs in grad computation,
 //    like many operations in practice do, and
 // 3. it does not have a built-in implementation in JAX.
-__global__ void FooKernel(const float *a, const float *b, float *c, size_t n) {
+__global__ void FooFwdKernel(const float *a,
+                             const float *b,
+                             float *c,
+                             float *b_plus_1,  // intermediate output b+1
+                             size_t n) {
   size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
   const size_t grid_stride = blockDim.x * gridDim.x;
-  for (size_t i = tid; i < n; i += grid_stride)
-    c[i] = a[i] * (b[i] + 1.0f);
+  for (size_t i = tid; i < n; i += grid_stride) {
+    b_plus_1[i] = b[i] + 1.0f;
+    c[i] = a[i] * b_plus_1[i];
+  }
 }
 
-namespace ffi = xla::ffi;
-
 // XLA FFI binding wrapper that launches the kernel
-extern "C" XLA_FFI_Error *Foo(XLA_FFI_CallFrame *call_frame) {
+extern "C" XLA_FFI_Error *FooFwd(XLA_FFI_CallFrame *call_frame) {
   static const auto *kImpl =
       ffi::Ffi::Bind()
-          .Ctx<ffi::PlatformStream<cudaStream_t>>()
-          .Arg<ffi::Buffer<ffi::DataType::F32>>()
-          .Arg<ffi::Buffer<ffi::DataType::F32>>()
-          .Ret<ffi::Buffer<ffi::DataType::F32>>()
+          .Ctx<ffi::PlatformStream<cudaStream_t>>()  // stream
+          .Arg<ffi::Buffer<ffi::DataType::F32>>()    // a
+          .Arg<ffi::Buffer<ffi::DataType::F32>>()    // b
+          .Ret<ffi::Buffer<ffi::DataType::F32>>()    // c
+          .Ret<ffi::Buffer<ffi::DataType::F32>>()    // b_plus_1
           .Attr<size_t>("n")
           .To([](cudaStream_t stream,
                  ffi::Buffer<ffi::DataType::F32> a,
                  ffi::Buffer<ffi::DataType::F32> b,
                  ffi::Result<ffi::Buffer<ffi::DataType::F32>> c,
+                 ffi::Result<ffi::Buffer<ffi::DataType::F32>> b_plus_1,
                  size_t n) -> ffi::Error {
             // Host function wrapper that launches the kernel with hardcoded
             // grid/block size. Note, it uses types from XLA FFI. The return
@@ -40,12 +53,71 @@ extern "C" XLA_FFI_Error *Foo(XLA_FFI_CallFrame *call_frame) {
             const int block_dim = 128;
             const int grid_dim = 1;
             // Note how we access regular Buffer data vs Result Buffer data:
-            FooKernel<<<grid_dim, block_dim, /*shared_mem=*/0, stream>>>(
-                a.data, b.data, c->data, n);
+            FooFwdKernel<<<grid_dim, block_dim, /*shared_mem=*/0, stream>>>(
+                a.data, b.data, c->data, b_plus_1->data, n);
             // Check for launch time errors. Note that this function may also
             // return error codes from previous, asynchronous launches. This
             // means that an error status returned here could have been caused
             // by a different kernel previously launched by XLA.
+            cudaError_t last_error = cudaGetLastError();
+            if (last_error != cudaSuccess) {
+              return ffi::Error(XLA_FFI_Error_Code_INTERNAL,
+                                std::string("CUDA error: ") +
+                                cudaGetErrorString(last_error));
+            }
+            return ffi::Error::Success();
+      }).release();
+  return kImpl->Call(call_frame);
+}
+
+//----------------------------------------------------------------------------//
+//                            Backward pass                                   //
+//----------------------------------------------------------------------------//
+
+// compute da = dc * (b+1), and
+//         db = dc * a
+__global__ void FooBwdKernel(const float *c_grad,    // incoming gradient wrt c
+                             const float *a,         // original input a
+                             const float *b_plus_1,  // intermediate output b+1
+                             float *a_grad,          // outgoing gradient wrt a
+                             float *b_grad,          // outgoing gradient wrt b
+                             size_t n) {
+  size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+  const size_t grid_stride = blockDim.x * gridDim.x;
+  for (size_t i = tid; i < n; i += grid_stride) {
+    // In practice on GPUs b_plus_1 can be recomputed for practically free
+    // instead of storing it out and reusing, so the reuse here is a bit
+    // contrived. We do it to demonstrate residual/intermediate output passing
+    // between the forward and the backward pass which becomes useful when
+    // recomputation is more expensive than reuse.
+    a_grad[i] = c_grad[i] * b_plus_1[i];
+    b_grad[i] = c_grad[i] * a[i];
+  }
+}
+
+
+extern "C" XLA_FFI_Error *FooBwd(XLA_FFI_CallFrame *call_frame) {
+  static const auto *kImpl =
+      ffi::Ffi::Bind()
+          .Ctx<ffi::PlatformStream<cudaStream_t>>()  // stream
+          .Arg<ffi::Buffer<ffi::DataType::F32>>()    // c_grad
+          .Arg<ffi::Buffer<ffi::DataType::F32>>()    // a
+          .Arg<ffi::Buffer<ffi::DataType::F32>>()    // b_plus_1
+          .Ret<ffi::Buffer<ffi::DataType::F32>>()    // a_grad
+          .Ret<ffi::Buffer<ffi::DataType::F32>>()    // b_grad
+          .Attr<size_t>("n")
+          .To([](cudaStream_t stream,
+                 ffi::Buffer<ffi::DataType::F32> c_grad,
+                 ffi::Buffer<ffi::DataType::F32> a,
+                 ffi::Result<ffi::Buffer<ffi::DataType::F32>> b_plus_1,
+                 ffi::Result<ffi::Buffer<ffi::DataType::F32>> a_grad,
+                 ffi::Result<ffi::Buffer<ffi::DataType::F32>> b_grad,
+                 size_t n) -> ffi::Error {
+            const int block_dim = 128;
+            const int grid_dim = 1;
+            FooBwdKernel<<<grid_dim, block_dim, /*shared_mem=*/0, stream>>>(
+                c_grad.data, a.data, b_plus_1->data, a_grad->data, b_grad->data,
+                n);
             cudaError_t last_error = cudaGetLastError();
             if (last_error != cudaSuccess) {
               return ffi::Error(XLA_FFI_Error_Code_INTERNAL,
--- docs/cuda-custom-call/foo.py
+++ docs/cuda-custom-call/foo.py
@@ -20,40 +20,49 @@ XLA_CUSTOM_CALL_API_VERSION = 1
 # version 4 accepts MLIR dictionaries in backend_config
 STABLEHLO_CUSTOM_CALL_API_VERSION = 4
 
-# this string is how we identify the kernel to XLA:
+# this string is how we identify kernels to XLA:
 # - first we register a pointer to the kernel with XLA under this name
 # - then we "tell" JAX to emit StableHLO specifying this name to XLA
-XLA_CUSTOM_CALL_TARGET = "foo"
+XLA_CUSTOM_CALL_TARGET_FWD = "foo-fwd"
+XLA_CUSTOM_CALL_TARGET_BWD = "foo-bwd"
 
-# independently, the corresponding JAX primitive must also be named,
-# it can have a name different from the XLA target
-JAX_PRIMITIVE = "foo"
+# independently, corresponding JAX primitive must also be named,
+# names can be different from XLA targets, here they are the same
+JAX_PRIMITIVE_FWD = "foo-fwd"
+JAX_PRIMITIVE_BWD = "foo-bwd"
 
+# paths in current directory need to be qualified with "./"
 SHARED_LIBRARY = "./foo.so"
-
 
 library = ctypes.cdll.LoadLibrary(SHARED_LIBRARY)
 
+
+#-----------------------------------------------------------------------------#
+#                              Forward pass                                   #
+#-----------------------------------------------------------------------------#
+
 # register the XLA FFI binding pointer with XLA
 xla_client.register_custom_call_target(
-    name=XLA_CUSTOM_CALL_TARGET,
-    fn=jax.ffi.build_capsule(library.Foo),
+    name=XLA_CUSTOM_CALL_TARGET_FWD,
+    fn=jax.ffi.build_capsule(library.FooFwd),
     platform=XLA_PLATFORM,
     api_version=XLA_CUSTOM_CALL_API_VERSION
 )
 
-foo_p = jax.core.Primitive(JAX_PRIMITIVE)
-def foo(a, b):
-    return foo_p.bind(a, b)
 
-def _foo_abstract_eval(a, b):
+# our forward primitive will also return the intermediate output b+1
+# so it can be reused in the backward pass computation
+def _foo_fwd_abstract_eval(a, b):
     assert a.shape == b.shape
     assert a.dtype == b.dtype
-    return jax.core.ShapedArray(a.shape, a.dtype)
+    shaped_array = jax.core.ShapedArray(a.shape, a.dtype)
+    return (
+        shaped_array,  # output c
+        shaped_array,  # intermediate output b+1
+    )
 
-foo_p.def_abstract_eval(_foo_abstract_eval)
 
-def _foo_lowering(ctx, a, b):
+def _foo_fwd_lowering(ctx, a, b):
     typ = ir.RankedTensorType(a.type)
     shape = typ.shape
     u64 = ir.IntegerType.get_unsigned(64)
@@ -61,19 +70,112 @@ def _foo_lowering(ctx, a, b):
     # default XLA layout that just means row major
     layout = range(len(shape)-1, -1, -1)
     return custom_call(
-        XLA_CUSTOM_CALL_TARGET,
+        XLA_CUSTOM_CALL_TARGET_FWD,
         api_version=STABLEHLO_CUSTOM_CALL_API_VERSION,
-        result_types=[typ],  # same type as inputs
+        result_types=[typ, typ],  # c, b_plus_1, same types as inputs
         operands=[a,b],
         backend_config=dict(n=ir.IntegerAttr.get(u64, n)),
-        operand_layouts=[layout, layout],
-        result_layouts=[layout],
+        operand_layouts=[layout, layout],  # a, b
+        result_layouts=[layout, layout],   # c, b_plus_1
     ).results
 
-mlir.register_lowering(foo_p, _foo_lowering, platform=JAX_PLATFORM)
+
+# construct a new JAX primitive
+foo_fwd_p = jax.core.Primitive(JAX_PRIMITIVE_FWD)
+# register the abstract evaluation rule for the forward primitive
+foo_fwd_p.def_abstract_eval(_foo_fwd_abstract_eval)
+foo_fwd_p.multiple_results = True
+mlir.register_lowering(foo_fwd_p, _foo_fwd_lowering, platform=JAX_PLATFORM)
+
+#-----------------------------------------------------------------------------#
+#                              Backward pass                                  #
+#-----------------------------------------------------------------------------#
+
+# register the XLA FFI binding pointer with XLA
+xla_client.register_custom_call_target(
+    name=XLA_CUSTOM_CALL_TARGET_BWD,
+    fn=jax.ffi.build_capsule(library.FooBwd),
+    platform=XLA_PLATFORM,
+    api_version=XLA_CUSTOM_CALL_API_VERSION
+)
+
+
+def _foo_bwd_abstract_eval(c_grad, a, b_plus_1):
+    assert c_grad.shape == a.shape
+    assert a.shape == b_plus_1.shape
+    assert c_grad.dtype == a.dtype
+    assert a.dtype == b_plus_1.dtype
+
+    shaped_array = jax.core.ShapedArray(a.shape, a.dtype)
+    return (
+        shaped_array,  # a_grad
+        shaped_array,  # b_grad
+    )
+
+
+def _foo_bwd_lowering(ctx, c_grad, a, b_plus_1):
+    typ = ir.RankedTensorType(a.type)
+    shape = typ.shape
+    u64 = ir.IntegerType.get_unsigned(64)
+    n = np.prod(shape)
+    # default XLA layout that just means row major
+    layout = range(len(shape)-1, -1, -1)
+    return custom_call(
+        XLA_CUSTOM_CALL_TARGET_BWD,
+        api_version=STABLEHLO_CUSTOM_CALL_API_VERSION,
+        result_types=[typ, typ],  # a_grad, b_grad
+        operands=[c_grad, a, b_plus_1],
+        backend_config=dict(n=ir.IntegerAttr.get(u64, n)),
+        operand_layouts=[layout, layout, layout],  # c_grad, a, b_plus_1
+        result_layouts=[layout, layout],   # a_grad, b_grad
+    ).results
+
+# construct a new JAX primitive
+foo_bwd_p = jax.core.Primitive(JAX_PRIMITIVE_BWD)
+# register the abstract evaluation rule for the forward primitive
+foo_bwd_p.def_abstract_eval(_foo_bwd_abstract_eval)
+foo_bwd_p.multiple_results = True
+mlir.register_lowering(foo_bwd_p, _foo_bwd_lowering, platform=JAX_PLATFORM)
+
+
+#-----------------------------------------------------------------------------#
+#                              User facing API                                #
+#-----------------------------------------------------------------------------#
+
+def foo_fwd(a, b):
+    c, b_plus_1 = foo_fwd_p.bind(a, b)
+    return c, (a, b_plus_1)
+
+def foo_bwd(res, c_grad):
+    a, b_plus_1 = res
+    # needs this conversion from list to tuple (why is a list produced in the
+    # first place?), otherwise get this error:
+    #   TypeError: Custom VJP bwd rule must produce an output with the same
+    #   container (pytree) structure as the args tuple of the primal function,
+    #   and in particular must produce a tuple of length equal to the number of
+    #   arguments to the primal function, but got bwd output structure
+    #   PyTreeDef([*, *]) for primal input structure PyTreeDef((*, *)).
+    return tuple(foo_bwd_p.bind(c_grad, a, b_plus_1))
+
+@jax.custom_vjp
+def foo(a, b):
+    c, _ = foo_fwd(a, b)
+    return c
+
+foo.defvjp(foo_fwd, foo_bwd)
+
+#-----------------------------------------------------------------------------#
+#                                    Test                                     #
+#-----------------------------------------------------------------------------#
 
 a = 2. * jnp.ones((2,3))
 b = 3. * jnp.ones((2,3))
 assert (jax.jit(foo)(a, b) == (2*(3+1))).all()
 
-#print(jax.grad(foo)(a, b))
+def loss(a, b):
+    return jnp.sum(foo(a, b))
+
+da, db = jax.jit(jax.grad(loss, argnums=(0,1)))(a, b)
+
+assert (da == (b+1)).all()
+assert (db == a).all()
