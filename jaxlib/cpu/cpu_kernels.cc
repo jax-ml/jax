swe-train/google__jax

Port JAX LAPACK custom calls to XLA's FFI (API v4)
- Upgrade CPU custom calls to FFI.
- Fix the Triangular Solve kernel to work for the batched case. (Previously it delegated to `hlo.triangular_solve`.)
- Fix the array shape bug in the Tridiagonal Reduction kernel (insufficient allocation, shape off by one).
- Correct and extend the `_lapack` module stub to not produce warnings.
- Make Schur Decomposition return a fixed number of values.
- (WiP) Update the implementation of Schur form to consider the `select` parameter.

PiperOrigin-RevId: 620111786

jaxlib/cpu/cpu_kernels.cc
@@ -15,98 +15,61 @@ limitations under the License.
 
 // This file is not used by JAX itself, but exists to assist with running
 // JAX-generated HLO code from outside of JAX.
-#include <complex>
-#include "jaxlib/cpu/ducc_fft_kernels.h"
-#include "jaxlib/cpu/lapack_kernels.h"
-#include "xla/service/custom_call_target_registry.h"
+
+#include "jaxlib/cpu/cpu_kernels.h"
+
+#define JAX_CPU_REGISTER_HANDLER(name) \
+  XLA_FFI_REGISTER_HANDLER(XLA_FFI_GetApi(), #name, "Host", name);
 
 namespace jax {
 namespace {
 
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("blas_strsm", Trsm<float>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("blas_dtrsm", Trsm<double>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("blas_ctrsm",
-                                         Trsm<std::complex<float>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("blas_ztrsm",
-                                         Trsm<std::complex<double>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_sgetrf", Getrf<float>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_dgetrf", Getrf<double>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_cgetrf",
-                                         Getrf<std::complex<float>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_zgetrf",
-                                         Getrf<std::complex<double>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_sgeqrf", Geqrf<float>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_dgeqrf", Geqrf<double>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_cgeqrf",
-                                         Geqrf<std::complex<float>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_zgeqrf",
-                                         Geqrf<std::complex<double>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_sorgqr", Orgqr<float>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_dorgqr", Orgqr<double>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_cungqr",
-                                         Orgqr<std::complex<float>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_zungqr",
-                                         Orgqr<std::complex<double>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_spotrf", Potrf<float>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_dpotrf", Potrf<double>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_cpotrf",
-                                         Potrf<std::complex<float>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_zpotrf",
-                                         Potrf<std::complex<double>>::Kernel,
-                                         "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_sgesdd",
-                                         RealGesdd<float>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_dgesdd",
-                                         RealGesdd<double>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
-    "lapack_cgesdd", ComplexGesdd<std::complex<float>>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
-    "lapack_zgesdd", ComplexGesdd<std::complex<double>>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_ssyevd",
-                                         RealSyevd<float>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_dsyevd",
-                                         RealSyevd<double>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
-    "lapack_cheevd", ComplexHeevd<std::complex<float>>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
-    "lapack_zheevd", ComplexHeevd<std::complex<double>>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_sgeev",
-                                         RealGeev<float>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_dgeev",
-                                         RealGeev<double>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
-    "lapack_cgeev", ComplexGeev<std::complex<float>>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
-    "lapack_zgeev", ComplexGeev<std::complex<double>>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_sgees",
-                                         RealGees<float>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("lapack_dgees",
-                                         RealGees<double>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
-    "lapack_cgees", ComplexGees<std::complex<float>>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
-    "lapack_zgees", ComplexGees<std::complex<double>>::Kernel, "Host");
-XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
-    "dynamic_ducc_fft", DynamicDuccFft, "Host");
+JAX_CPU_REGISTER_HANDLER(blas_strsm);
+JAX_CPU_REGISTER_HANDLER(blas_dtrsm);
+JAX_CPU_REGISTER_HANDLER(blas_ctrsm);
+JAX_CPU_REGISTER_HANDLER(blas_ztrsm);
+JAX_CPU_REGISTER_HANDLER(lapack_sgetrf);
+JAX_CPU_REGISTER_HANDLER(lapack_dgetrf);
+JAX_CPU_REGISTER_HANDLER(lapack_cgetrf);
+JAX_CPU_REGISTER_HANDLER(lapack_zgetrf);
+JAX_CPU_REGISTER_HANDLER(lapack_sgeqrf);
+JAX_CPU_REGISTER_HANDLER(lapack_dgeqrf);
+JAX_CPU_REGISTER_HANDLER(lapack_cgeqrf);
+JAX_CPU_REGISTER_HANDLER(lapack_zgeqrf);
+JAX_CPU_REGISTER_HANDLER(lapack_sorgqr);
+JAX_CPU_REGISTER_HANDLER(lapack_dorgqr);
+JAX_CPU_REGISTER_HANDLER(lapack_cungqr);
+JAX_CPU_REGISTER_HANDLER(lapack_zungqr);
+JAX_CPU_REGISTER_HANDLER(lapack_spotrf);
+JAX_CPU_REGISTER_HANDLER(lapack_dpotrf);
+JAX_CPU_REGISTER_HANDLER(lapack_cpotrf);
+JAX_CPU_REGISTER_HANDLER(lapack_zpotrf);
+JAX_CPU_REGISTER_HANDLER(lapack_sgesdd);
+JAX_CPU_REGISTER_HANDLER(lapack_dgesdd);
+JAX_CPU_REGISTER_HANDLER(lapack_cgesdd);
+JAX_CPU_REGISTER_HANDLER(lapack_zgesdd);
+JAX_CPU_REGISTER_HANDLER(lapack_ssyevd);
+JAX_CPU_REGISTER_HANDLER(lapack_dsyevd);
+JAX_CPU_REGISTER_HANDLER(lapack_cheevd);
+JAX_CPU_REGISTER_HANDLER(lapack_zheevd);
+JAX_CPU_REGISTER_HANDLER(lapack_sgeev);
+JAX_CPU_REGISTER_HANDLER(lapack_dgeev);
+JAX_CPU_REGISTER_HANDLER(lapack_cgeev);
+JAX_CPU_REGISTER_HANDLER(lapack_zgeev);
+JAX_CPU_REGISTER_HANDLER(lapack_sgees);
+JAX_CPU_REGISTER_HANDLER(lapack_dgees);
+JAX_CPU_REGISTER_HANDLER(lapack_cgees);
+JAX_CPU_REGISTER_HANDLER(lapack_zgees);
+JAX_CPU_REGISTER_HANDLER(lapack_sgehrd);
+JAX_CPU_REGISTER_HANDLER(lapack_dgehrd);
+JAX_CPU_REGISTER_HANDLER(lapack_cgehrd);
+JAX_CPU_REGISTER_HANDLER(lapack_zgehrd);
+JAX_CPU_REGISTER_HANDLER(lapack_ssytrd);
+JAX_CPU_REGISTER_HANDLER(lapack_dsytrd);
+JAX_CPU_REGISTER_HANDLER(lapack_chetrd);
+JAX_CPU_REGISTER_HANDLER(lapack_zhetrd);
+
+#undef JAX_CPU_REGISTER_HANDLER
 
 }  // namespace
 }  // namespace jax

jax-ml/jax

jaxlib/cpu/cpu_kernels.cc
@@ -15,12 +15,17 @@ limitations under the License.
 
 // This file is not used by JAX itself, but exists to assist with running
 // JAX-generated HLO code from outside of JAX.
-#include <complex>
-#include "jaxlib/cpu/lapack_kernels.h"
+
+#include "jaxlib/cpu/lapack.h"
 #include "xla/service/custom_call_target_registry.h"
+
+#define JAX_CPU_REGISTER_HANDLER(name) \
+  XLA_FFI_REGISTER_HANDLER(XLA_FFI_GetApi(), #name, "Host", name);
 
 namespace jax {
 namespace {
+
+// Old-style kernels
 
 XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM("blas_strsm", Trsm<float>::Kernel,
                                          "Host");
@@ -104,6 +109,55 @@ XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
     "lapack_cgees", ComplexGees<std::complex<float>>::Kernel, "Host");
 XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
     "lapack_zgees", ComplexGees<std::complex<double>>::Kernel, "Host");
+
+// FFI Kernels
+
+JAX_CPU_REGISTER_HANDLER(blas_strsm_ffi);
+JAX_CPU_REGISTER_HANDLER(blas_dtrsm_ffi);
+JAX_CPU_REGISTER_HANDLER(blas_ctrsm_ffi);
+JAX_CPU_REGISTER_HANDLER(blas_ztrsm_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_sgetrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dgetrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_cgetrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zgetrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_sgeqrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dgeqrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_cgeqrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zgeqrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_sorgqr_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dorgqr_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_cungqr_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zungqr_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_spotrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dpotrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_cpotrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zpotrf_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_sgesdd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dgesdd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_cgesdd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zgesdd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_ssyevd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dsyevd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_cheevd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zheevd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_sgeev_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dgeev_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_cgeev_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zgeev_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_sgees_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dgees_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_cgees_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zgees_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_sgehrd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dgehrd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_cgehrd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zgehrd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_ssytrd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_dsytrd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_chetrd_ffi);
+JAX_CPU_REGISTER_HANDLER(lapack_zhetrd_ffi);
+
+#undef JAX_CPU_REGISTER_HANDLER
 
 }  // namespace
 }  // namespace jax