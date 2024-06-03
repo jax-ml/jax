79002c61f6e70540b0aeb1b062636622c8b8e1d2
Andrey Portnoy <aportnoy@nvidia.com>
2024-06-03 15:48:29 -0400

    Move build_capsule to jax.ffi

--- docs/cuda-custom-call/mul.py
+++ docs/cuda-custom-call/mul.py
@@ -22,21 +22,10 @@ STABLEHLO_CUSTOM_CALL_API_VERSION = 4
 
 mulso = ctypes.cdll.LoadLibrary('./mul.so')
 
-# I don't know why this more elaborate setup is needed, and simply
-#   capsule = ctypes.pythonapi.PyCapsule_New(mulso.Mul)
-# doesn't work: capsule degenerates to a Python int.
-# https://stackoverflow.com/questions/65056619/converting-ctypes-c-void-p-to-pycapsule
-def build_capsule(funcptr):
-    PyCapsule_Destructor = ctypes.CFUNCTYPE(None, ctypes.py_object)
-    PyCapsule_New = ctypes.pythonapi.PyCapsule_New
-    PyCapsule_New.restype = ctypes.py_object
-    PyCapsule_New.argtypes = (ctypes.c_void_p, ctypes.c_char_p, PyCapsule_Destructor)
-    return PyCapsule_New(funcptr, None, PyCapsule_Destructor(0))
-
 # register the XLA FFI binding pointer with XLA under the name "mul"
 xla_client.register_custom_call_target(
     name="mul",
-    fn=build_capsule(mulso.Mul),
+    fn=jax.ffi.build_capsule(mulso.Mul),
     platform=XLA_PLATFORM,
     api_version=XLA_CUSTOM_CALL_API_VERSION
 )
--- jax/_src/extend/ffi.py
+++ jax/_src/extend/ffi.py
@@ -15,9 +15,48 @@
 from __future__ import annotations
 
 import os
+import ctypes
 
 from jax._src.lib import jaxlib
 
+# Use Python stable C API function to construct a PyCapsule.
+# From ctypes.pythonapi documentation:
+#
+#  ctypes.pythonapi
+#    An instance of PyDLL that exposes Python C API functions as attributes.
+#    Note that all these functions are assumed to return C int, which is of
+#    course not always the truth, so you have to assign the correct restype
+#    attribute to use these functions.
+#
+# Taken from: https://docs.python.org/3/library/ctypes.html.
+#
+# More context here: https://stackoverflow.com/questions/24377845/ctype-why-specify-argtypes.
+#
+# Following this advice we annotate argument and return types of PyCapsule_New
+# before we call it. Example here:
+# https://stackoverflow.com/questions/65056619/converting-ctypes-c-void-p-to-pycapsule
+def build_capsule(funcptr):
+  """Construct a PyCapsule out of a ctypes function pointer.
+
+  A typical use for this is registering custom call targets with XLA:
+
+    import ctypes
+    import jax
+    from jax.lib import xla_client
+
+    fooso = ctypes.cdll.LoadLibrary('./foo.so')
+    xla_client.register_custom_call_target(
+      name="bar",
+      fn=jax.ffi.build_capsule(fooso.bar),
+      platform=PLATFORM,
+      api_version=API_VERSION
+    )
+  """
+  PyCapsule_Destructor = ctypes.CFUNCTYPE(None, ctypes.py_object)
+  PyCapsule_New = ctypes.pythonapi.PyCapsule_New
+  PyCapsule_New.restype = ctypes.py_object
+  PyCapsule_New.argtypes = (ctypes.c_void_p, ctypes.c_char_p, PyCapsule_Destructor)
+  return PyCapsule_New(funcptr, None, PyCapsule_Destructor(0))
 
 def include_dir() -> str:
   """Get the path to the directory containing header files bundled with jaxlib"""
