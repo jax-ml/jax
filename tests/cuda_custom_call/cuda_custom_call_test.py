2d58c55be70f2e27de37a299c9348024502c5ec4
Andrey Portnoy <aportnoy@nvidia.com>
2024-06-04 14:10:48 -0400

    Format using yapf
    yapf -i --style='{based_on_style: google, indent_width: 2}' ...

--- tests/cuda_custom_call/cuda_custom_call_test.py
+++ tests/cuda_custom_call/cuda_custom_call_test.py
@@ -22,7 +22,6 @@ from jax._src import test_util as jtu
 config.parse_flags_with_absl()
 # end test boilerplate
 
-
 # XLA needs uppercase, "cuda" isn't recognized
 XLA_PLATFORM = "CUDA"
 
@@ -51,48 +50,45 @@ SHARED_LIBRARY = "tests/cuda_custom_call/libfoo.so"
 
 library = ctypes.cdll.LoadLibrary(SHARED_LIBRARY)
 
-
 #-----------------------------------------------------------------------------#
 #                              Forward pass                                   #
 #-----------------------------------------------------------------------------#
 
 # register the XLA FFI binding pointer with XLA
-xla_client.register_custom_call_target(
-    name=XLA_CUSTOM_CALL_TARGET_FWD,
-    fn=ffi.pycapsule(library.FooFwd),
-    platform=XLA_PLATFORM,
-    api_version=XLA_CUSTOM_CALL_API_VERSION
-)
+xla_client.register_custom_call_target(name=XLA_CUSTOM_CALL_TARGET_FWD,
+                                       fn=ffi.pycapsule(library.FooFwd),
+                                       platform=XLA_PLATFORM,
+                                       api_version=XLA_CUSTOM_CALL_API_VERSION)
 
 
 # our forward primitive will also return the intermediate output b+1
 # so it can be reused in the backward pass computation
 def _foo_fwd_abstract_eval(a, b):
-    assert a.shape == b.shape
-    assert a.dtype == b.dtype
-    shaped_array = jax.core.ShapedArray(a.shape, a.dtype)
-    return (
-        shaped_array,  # output c
-        shaped_array,  # intermediate output b+1
-    )
+  assert a.shape == b.shape
+  assert a.dtype == b.dtype
+  shaped_array = jax.core.ShapedArray(a.shape, a.dtype)
+  return (
+      shaped_array,  # output c
+      shaped_array,  # intermediate output b+1
+  )
 
 
 def _foo_fwd_lowering(ctx, a, b):
-    typ = ir.RankedTensorType(a.type)
-    shape = typ.shape
-    u64 = ir.IntegerType.get_unsigned(64)
-    n = np.prod(shape)
-    # default XLA layout that just means row major
-    layout = range(len(shape)-1, -1, -1)
-    return custom_call(
-        XLA_CUSTOM_CALL_TARGET_FWD,
-        api_version=STABLEHLO_CUSTOM_CALL_API_VERSION,
-        result_types=[typ, typ],  # c, b_plus_1, same types as inputs
-        operands=[a,b],
-        backend_config=dict(n=ir.IntegerAttr.get(u64, n)),
-        operand_layouts=[layout, layout],  # a, b
-        result_layouts=[layout, layout],   # c, b_plus_1
-    ).results
+  typ = ir.RankedTensorType(a.type)
+  shape = typ.shape
+  u64 = ir.IntegerType.get_unsigned(64)
+  n = np.prod(shape)
+  # default XLA layout that just means row major
+  layout = range(len(shape) - 1, -1, -1)
+  return custom_call(
+      XLA_CUSTOM_CALL_TARGET_FWD,
+      api_version=STABLEHLO_CUSTOM_CALL_API_VERSION,
+      result_types=[typ, typ],  # c, b_plus_1, same types as inputs
+      operands=[a, b],
+      backend_config=dict(n=ir.IntegerAttr.get(u64, n)),
+      operand_layouts=[layout, layout],  # a, b
+      result_layouts=[layout, layout],  # c, b_plus_1
+  ).results
 
 
 # construct a new JAX primitive
@@ -107,43 +103,42 @@ mlir.register_lowering(foo_fwd_p, _foo_fwd_lowering, platform=JAX_PLATFORM)
 #-----------------------------------------------------------------------------#
 
 # register the XLA FFI binding pointer with XLA
-xla_client.register_custom_call_target(
-    name=XLA_CUSTOM_CALL_TARGET_BWD,
-    fn=ffi.pycapsule(library.FooBwd),
-    platform=XLA_PLATFORM,
-    api_version=XLA_CUSTOM_CALL_API_VERSION
-)
+xla_client.register_custom_call_target(name=XLA_CUSTOM_CALL_TARGET_BWD,
+                                       fn=ffi.pycapsule(library.FooBwd),
+                                       platform=XLA_PLATFORM,
+                                       api_version=XLA_CUSTOM_CALL_API_VERSION)
 
 
 def _foo_bwd_abstract_eval(c_grad, a, b_plus_1):
-    assert c_grad.shape == a.shape
-    assert a.shape == b_plus_1.shape
-    assert c_grad.dtype == a.dtype
-    assert a.dtype == b_plus_1.dtype
+  assert c_grad.shape == a.shape
+  assert a.shape == b_plus_1.shape
+  assert c_grad.dtype == a.dtype
+  assert a.dtype == b_plus_1.dtype
 
-    shaped_array = jax.core.ShapedArray(a.shape, a.dtype)
-    return (
-        shaped_array,  # a_grad
-        shaped_array,  # b_grad
-    )
+  shaped_array = jax.core.ShapedArray(a.shape, a.dtype)
+  return (
+      shaped_array,  # a_grad
+      shaped_array,  # b_grad
+  )
 
 
 def _foo_bwd_lowering(ctx, c_grad, a, b_plus_1):
-    typ = ir.RankedTensorType(a.type)
-    shape = typ.shape
-    u64 = ir.IntegerType.get_unsigned(64)
-    n = np.prod(shape)
-    # default XLA layout that just means row major
-    layout = range(len(shape)-1, -1, -1)
-    return custom_call(
-        XLA_CUSTOM_CALL_TARGET_BWD,
-        api_version=STABLEHLO_CUSTOM_CALL_API_VERSION,
-        result_types=[typ, typ],  # a_grad, b_grad
-        operands=[c_grad, a, b_plus_1],
-        backend_config=dict(n=ir.IntegerAttr.get(u64, n)),
-        operand_layouts=[layout, layout, layout],  # c_grad, a, b_plus_1
-        result_layouts=[layout, layout],   # a_grad, b_grad
-    ).results
+  typ = ir.RankedTensorType(a.type)
+  shape = typ.shape
+  u64 = ir.IntegerType.get_unsigned(64)
+  n = np.prod(shape)
+  # default XLA layout that just means row major
+  layout = range(len(shape) - 1, -1, -1)
+  return custom_call(
+      XLA_CUSTOM_CALL_TARGET_BWD,
+      api_version=STABLEHLO_CUSTOM_CALL_API_VERSION,
+      result_types=[typ, typ],  # a_grad, b_grad
+      operands=[c_grad, a, b_plus_1],
+      backend_config=dict(n=ir.IntegerAttr.get(u64, n)),
+      operand_layouts=[layout, layout, layout],  # c_grad, a, b_plus_1
+      result_layouts=[layout, layout],  # a_grad, b_grad
+  ).results
+
 
 # construct a new JAX primitive
 foo_bwd_p = jax.core.Primitive(JAX_PRIMITIVE_BWD)
@@ -152,23 +147,26 @@ foo_bwd_p.def_abstract_eval(_foo_bwd_abstract_eval)
 foo_bwd_p.multiple_results = True
 mlir.register_lowering(foo_bwd_p, _foo_bwd_lowering, platform=JAX_PLATFORM)
 
-
 #-----------------------------------------------------------------------------#
 #                              User facing API                                #
 #-----------------------------------------------------------------------------#
 
+
 def foo_fwd(a, b):
-    c, b_plus_1 = foo_fwd_p.bind(a, b)
-    return c, (a, b_plus_1)
+  c, b_plus_1 = foo_fwd_p.bind(a, b)
+  return c, (a, b_plus_1)
+
 
 def foo_bwd(res, c_grad):
-    a, b_plus_1 = res
-    return foo_bwd_p.bind(c_grad, a, b_plus_1)
+  a, b_plus_1 = res
+  return foo_bwd_p.bind(c_grad, a, b_plus_1)
+
 
 @jax.custom_vjp
 def foo(a, b):
-    c, _ = foo_fwd(a, b)
-    return c
+  c, _ = foo_fwd(a, b)
+  return c
+
 
 foo.defvjp(foo_fwd, foo_bwd)
 
@@ -176,42 +174,47 @@ foo.defvjp(foo_fwd, foo_bwd)
 #                                    Test                                     #
 #-----------------------------------------------------------------------------#
 
+
 class CustomCallTest(jtu.JaxTestCase):
-    def test_fwd_interpretable(self):
-        shape = (2, 3)
-        a = 2. * jnp.ones(shape)
-        b = 3. * jnp.ones(shape)
-        observed = jax.jit(foo)(a, b)
-        expected = (2.*(3.+1.))
-        self.assertArraysEqual(observed, expected)
 
-    def test_bwd_interpretable(self):
-        shape = (2, 3)
-        a = 2. * jnp.ones(shape)
-        b = 3. * jnp.ones(shape)
-        def loss(a, b):
-            return jnp.sum(foo(a, b))
-        da_observed, db_observed = jax.jit(jax.grad(loss, argnums=(0,1)))(a, b)
-        da_expected = b+1
-        db_expected = a
-        self.assertArraysEqual(da_observed, da_expected)
-        self.assertArraysEqual(db_observed, db_expected)
+  def test_fwd_interpretable(self):
+    shape = (2, 3)
+    a = 2. * jnp.ones(shape)
+    b = 3. * jnp.ones(shape)
+    observed = jax.jit(foo)(a, b)
+    expected = (2. * (3. + 1.))
+    self.assertArraysEqual(observed, expected)
 
-    def test_fwd_random(self):
-        shape = (2, 3)
-        akey, bkey = jax.random.split(jax.random.key(0))
-        a = jax.random.normal(key=akey, shape=shape)
-        b = jax.random.normal(key=bkey, shape=shape)
-        observed = jax.jit(foo)(a, b)
-        expected = a * (b+1)
-        self.assertArraysEqual(observed, expected)
+  def test_bwd_interpretable(self):
+    shape = (2, 3)
+    a = 2. * jnp.ones(shape)
+    b = 3. * jnp.ones(shape)
 
-    def test_bwd_random(self):
-        shape = (2, 3)
-        akey, bkey = jax.random.split(jax.random.key(0))
-        a = jax.random.normal(key=akey, shape=shape)
-        b = jax.random.normal(key=bkey, shape=shape)
-        jtu.check_grads(f=jax.jit(foo), args=(a,b), order=1, modes=("rev",))
+    def loss(a, b):
+      return jnp.sum(foo(a, b))
+
+    da_observed, db_observed = jax.jit(jax.grad(loss, argnums=(0, 1)))(a, b)
+    da_expected = b + 1
+    db_expected = a
+    self.assertArraysEqual(da_observed, da_expected)
+    self.assertArraysEqual(db_observed, db_expected)
+
+  def test_fwd_random(self):
+    shape = (2, 3)
+    akey, bkey = jax.random.split(jax.random.key(0))
+    a = jax.random.normal(key=akey, shape=shape)
+    b = jax.random.normal(key=bkey, shape=shape)
+    observed = jax.jit(foo)(a, b)
+    expected = a * (b + 1)
+    self.assertArraysEqual(observed, expected)
+
+  def test_bwd_random(self):
+    shape = (2, 3)
+    akey, bkey = jax.random.split(jax.random.key(0))
+    a = jax.random.normal(key=akey, shape=shape)
+    b = jax.random.normal(key=bkey, shape=shape)
+    jtu.check_grads(f=jax.jit(foo), args=(a, b), order=1, modes=("rev",))
+
 
 if __name__ == "__main__":
   absltest.main(testLoader=jtu.JaxTestLoader())
