7c059d4630eb33a96d13c9e60079cffd5a5f7493
George Necula <gcnecula@gmail.com>
2024-07-07 17:10:51 +0300

    [pallas] Document the indexing_mode=Unblocked()
    In the process discovered that the padding in the interpreter
    mode was with 0s. I changed it to NaN/minint to match the
    padding for the blocked mode.

--- jax/_src/pallas/pallas_call.py
+++ jax/_src/pallas/pallas_call.py
@@ -213,7 +213,8 @@ def _pallas_call_impl_interpret(
       if padding is not None and any(p != (0, 0) for p in padding):
         if input_output_aliases:
           raise NotImplementedError("Padding with aliasing not supported.")
-        x = lax.pad(x, jnp.zeros((), x.dtype), [(*p, 0) for p in padding])
+        pad_value = uninitialized_value(shape=(), dtype=x.dtype)
+        x = lax.pad(x, pad_value, [(*p, 0) for p in padding])
     carry.append(x)
 
   block_shapes_without_mapped_dims = [
