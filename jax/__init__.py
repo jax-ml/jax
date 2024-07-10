876d22f11d273a34b5796470b02ac31bb382a1fe
Michael Hudgins <michaelhudgins@google.com>
2024-07-10 11:26:51 -0700

    Testing CI post clang 18 update
    PiperOrigin-RevId: 651092839

--- jax/__init__.py
+++ jax/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# DO_NOT_SUBMIT
 # Set default C++ logging level before any logging happens.
 import os as _os
 _os.environ.setdefault('TF_CPP_MIN_LOG_LEVEL', '1')
