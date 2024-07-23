81cb1addfd36db9c662913868d4e8165bd00b4ae
Christos Perivolaropoulos <cperivol@google.com>
2024-07-23 02:11:39 -0700

    [MosaicGPU] Add a __repr__ to FragmentedArray so pallas:mosaic_gpu errors are more readable.
    PiperOrigin-RevId: 655082250

--- jax/experimental/mosaic/gpu/fragmented_array.py
+++ jax/experimental/mosaic/gpu/fragmented_array.py
@@ -166,6 +166,11 @@ class FragmentedArray:
       case _:
         raise NotImplementedError
 
+  def __repr__(self):
+    return (
+        f"FragmentedArray(layout={self.layout}, shape={self.shape})"
+    )
+
   @classmethod
   def load_strided(cls, ref: ir.Value):
     if not ir.MemRefType.isinstance(ref.type):
