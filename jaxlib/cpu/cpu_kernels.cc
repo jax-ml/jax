8ebe9e25014605379b68eb888848ca7c2fc230b5
jax authors <google-ml-automation@google.com>
2024-07-29 04:18:42 -0700

    Disable duplicate registration of LAPACK/BLAS FFI handlers on CPU.
    PiperOrigin-RevId: 657148572

--- jaxlib/cpu/cpu_kernels.cc
+++ jaxlib/cpu/cpu_kernels.cc
@@ -118,30 +118,30 @@ XLA_REGISTER_CUSTOM_CALL_TARGET_WITH_SYM(
 
 // FFI Kernels
 
-JAX_CPU_REGISTER_HANDLER(blas_strsm_ffi);
-JAX_CPU_REGISTER_HANDLER(blas_dtrsm_ffi);
-JAX_CPU_REGISTER_HANDLER(blas_ctrsm_ffi);
-JAX_CPU_REGISTER_HANDLER(blas_ztrsm_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_sgetrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_dgetrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_cgetrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_zgetrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_sgeqrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_dgeqrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_cgeqrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_zgeqrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_sorgqr_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_dorgqr_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_cungqr_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_zungqr_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_spotrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_dpotrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_cpotrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_zpotrf_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_sgesdd_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_dgesdd_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_cgesdd_ffi);
-JAX_CPU_REGISTER_HANDLER(lapack_zgesdd_ffi);
+// JAX_CPU_REGISTER_HANDLER(blas_strsm_ffi);
+// JAX_CPU_REGISTER_HANDLER(blas_dtrsm_ffi);
+// JAX_CPU_REGISTER_HANDLER(blas_ctrsm_ffi);
+// JAX_CPU_REGISTER_HANDLER(blas_ztrsm_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_sgetrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_dgetrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_cgetrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_zgetrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_sgeqrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_dgeqrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_cgeqrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_zgeqrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_sorgqr_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_dorgqr_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_cungqr_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_zungqr_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_spotrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_dpotrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_cpotrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_zpotrf_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_sgesdd_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_dgesdd_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_cgesdd_ffi);
+// JAX_CPU_REGISTER_HANDLER(lapack_zgesdd_ffi);
 
 #undef JAX_CPU_REGISTER_HANDLER
 
