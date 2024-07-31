5c9bb612a775ca23d311eef1aeac03dfe0828a62
jax authors <google-ml-automation@google.com>
2024-07-31 01:10:52 -0700

    mesh_utils: allow meshes that do not include device at (0, 0, 0).
    This is required to allow the use of subslices: e.g., the two halves
    of a TPU slice.  One of them will not include the device at
    coordinates (0, 0, 0).
    
    E.g., assume we have a TPU v4 1x2x1 slice.
    
    BEFORE THIS CL, if we call _get_physical_tpu_mesh() (an auxiliary for
    the public create_device_mesh()) with
    
    jax_devices=[device(0,TPU_DEVICE,coords=[0,0,0,0],vtask=0,slice=0,default_mem=device,mem_spaces=3)]
    
    we get the expected result
    
    [[[device(0,TPU_DEVICE,coords=[0,0,0,0],vtask=0,slice=0,default_mem=device,mem_spaces=3)]]]
    
    However, if we call it with
    
    jax_devices=[device(1,TPU_DEVICE,coords=[0,1,0,0],vtask=0,slice=0,default_mem=device,mem_spaces=3)]
    
    we get the wrong mesh
    
    [[[None]
      [device(1,TPU_DEVICE,coords=[0,1,0,0],vtask=0,slice=0,default_mem=device,mem_spaces=3)]]]
    
    That's because the code before this CL assumed the the incoming
    jax_devices are arranged in a cuboid that starts at (0, 0, 0).  When
    working with subslices (e.g., half of a TPU slice) that is not always
    the case.
    
    AFTER THIS CL, the second case will return
    [[[device(1,TPU_DEVICE,coords=[0,1,0,0],vtask=0,slice=0,default_mem=device,mem_spaces=3)]]]
    
    For each dimension from the TPU coordinates, this CL computes the min
    / max; we expect the provided devices to fill the [min, max] interval
    (in that dimension).  By requesting this for each dimension, we
    request that the set of provided devices constitute a cuboid, but,
    unlike before this CL, that cuboid does not need to include (0, 0, 0):
    it can be "translated", which allows e.g., both half-slices of a big
    slice.
    
    PiperOrigin-RevId: 657902201

--- jax/experimental/mesh_utils.py
+++ jax/experimental/mesh_utils.py
@@ -575,7 +575,12 @@ def _get_physical_tpu_mesh(jax_devices: Sequence[Any]) -> np.ndarray:
 
   Args:
     jax_devices: A list of JAX devices in a TPU slice in process-tiled z, y, x,
-      core order, e.g. from jax.devices().
+      core order, e.g. from jax.devices(). The coordinates of these devices
+      should constitute a cuboid with no holes; e.g., the coordinates can be
+      {(1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1)} (a 1x2x2 cuboid); passing
+      only 3 of these devices would result in a "hole" in that cuboid, which is
+      an error.  As in our example, the cuboid is not required to include the
+      point (0, 0, 0).
 
   Returns:
     A np.ndarray of JAX devices with shape [global_x, global_y, global_z]. On
@@ -583,24 +588,57 @@ def _get_physical_tpu_mesh(jax_devices: Sequence[Any]) -> np.ndarray:
   """
   device_kind = jax_devices[0].device_kind
   device_coords = [d.coords for d in jax_devices]
-  dims = tuple(d + 1 for d in max(device_coords))
+  coord_size = len(device_coords[0])
+  # Position-wise max and min coordinates:
+  max_coords = tuple(
+      max(dc[i] for dc in device_coords) for i in range(coord_size)
+  )
+  min_coords = tuple(
+      min(dc[i] for dc in device_coords) for i in range(coord_size)
+  )
+  dims = tuple(h - l + 1 for (h, l) in zip(max_coords, min_coords))
+
+  max_cores_per_chip = max(d.core_on_chip for d in jax_devices)
+  min_cores_per_chip = min(d.core_on_chip for d in jax_devices)
+  cores_per_chip = max_cores_per_chip - min_cores_per_chip + 1
+
   assert len(dims) == 3, dims
+  assert (
+      len(jax_devices) == np.prod(dims) * cores_per_chip
+  ), f'{jax_devices=} {dims=} {cores_per_chip=}'
+
   if device_kind in (_TPU_V2, _TPU_V3):
-    cores_per_chip = max(d.core_on_chip for d in jax_devices) + 1
     out = np.empty(dims[:2] + (cores_per_chip,), dtype=object)
-    for coords, d in zip(device_coords, jax_devices):
+    for d in jax_devices:
+      coords = d.coords
       assert coords[2] == 0, d
-      out[coords[0], coords[1], d.core_on_chip] = d
+      out[
+          coords[0] - min_coords[0],
+          coords[1] - min_coords[1],
+          d.core_on_chip - min_cores_per_chip,
+      ] = d
   else:
     out = np.empty(dims, dtype=object)
-    for coords, d in zip(device_coords, jax_devices):
+    for d in jax_devices:
+      coords = d.coords
       if d.core_on_chip != 0:
         raise AssertionError(
             'Creating meshes for TPU >v3 requires one device per chip'
             f' ("megacore" mode). Got device id {d.core_on_chip} for a device'
             f' of kind {device_kind}: {d}.'
         )
-      out[coords[0], coords[1], coords[2]] = d
+      out[
+          coords[0] - min_coords[0],
+          coords[1] - min_coords[1],
+          coords[2] - min_coords[2],
+      ] = d
+
+  # Check there is no "hole" in the mesh we constructed.
+  if (out == None).any():  # pylint: disable=singleton-comparison
+    raise AssertionError(
+        'Constructed mesh contains a "hole"; probable cause: coordinates '
+        f'of jax_devices are not a contiguous cuboid: {jax_devices}'
+    )
   return out
 
 
