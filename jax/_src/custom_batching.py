1d425b2d30fec2d60d579d7f1bc17952db889a8d
Dan Foreman-Mackey <danfm@google.com>
2024-08-05 17:51:47 +0100

    Small tweaks to custom_vmap UI.
    I'm working on some extensions to `custom_vmap` and came across these
    small UI improvements (I think!). This includes the two changes:
    
    1. A weakening of the `kwargs` check to be consistent with the one in
       `custom_vjp`/`custom_jvp`, and
    2. An improved error message when `def_vmap` isn't called.

--- jax/_src/custom_batching.py
+++ jax/_src/custom_batching.py
@@ -27,7 +27,7 @@ from jax._src import source_info_util
 from jax._src import traceback_util
 from jax._src import tree_util
 from jax._src import util
-from jax._src.api_util import flatten_fun_nokwargs
+from jax._src.api_util import flatten_fun_nokwargs, resolve_kwargs
 from jax._src.interpreters import ad
 from jax._src.interpreters import batching
 from jax._src.interpreters.batching import not_mapped
@@ -64,7 +64,12 @@ class custom_vmap:
 
   @traceback_util.api_boundary
   def __call__(self, *args, **kwargs):
-    assert not kwargs
+    fun_name = getattr(self.fun, "__name__", str(self.fun))
+    if not self.vmap_rule:
+      raise AttributeError(
+          f"No batching rule defined for custom_vmap function {fun_name} "
+          "using def_vmap.")
+    args = resolve_kwargs(self.fun, args, kwargs)
     args_flat, in_tree = tree_flatten(args)
     flat_fun, out_tree = flatten_fun_nokwargs(lu.wrap_init(self.fun), in_tree)
     in_avals = [core.raise_to_shaped(core.get_aval(x)) for x in args_flat]
