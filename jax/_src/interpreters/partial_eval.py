34424731868948d79f31aea1e3ce989405a0431e
Dougal <d.maclaurin@gmail.com>
2024-08-14 11:09:54 -0400

    Reset name stack when tracing out a jaxpr

--- jax/_src/interpreters/partial_eval.py
+++ jax/_src/interpreters/partial_eval.py
@@ -2237,7 +2237,7 @@ def trace_to_jaxpr_dynamic(
   frame.debug_info = debug_info
 
   trace = DynamicJaxprTrace(frame)
-  with core.new_trace(trace):
+  with core.new_trace(trace), source_info_util.reset_name_stack():
     in_tracers = _input_type_to_tracers(trace.new_arg, in_avals)
     in_tracers = [t for t, keep in zip(in_tracers, keep_inputs) if keep]
     with core.set_current_trace(trace):
@@ -2256,7 +2256,7 @@ def trace_to_jaxpr_dynamic2(
   ) -> tuple[Jaxpr, OutputType, list[Any]]:
 
   trace = DynamicJaxprTrace(JaxprStackFrame())
-  with core.new_trace(trace):
+  with core.new_trace(trace), source_info_util.reset_name_stack():
     trace.frame.debug_info = debug_info
     in_avals, keep_inputs = unzip2(fun.in_type)
     in_tracers = _input_type_to_tracers(trace.new_arg, in_avals)
