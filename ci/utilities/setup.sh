f4434b29e472276f42bc52009b398700996ed1bb
Nitin Srinivasan <srnitin@google.com>
2024-08-15 05:28:37 +0000

    fix checking if variable is empty...part2

--- ci/utilities/setup.sh
+++ ci/utilities/setup.sh
@@ -29,7 +29,7 @@ set -exo pipefail -o history -o allexport
 # Tempoary way to source build configs. In the final version of the CL, these
 # will be moved to the Kokoro build configs and the jobs will automatically
 # source before running the script.
-if [[ -z "$BUILD_CONFIG_FILE" ]]; then
+if [[ -n "$BUILD_CONFIG_FILE" ]]; then
   echo "Please set a config file to BUILD_CONFIG_FILE"
   exit 1
 fi
@@ -37,7 +37,7 @@ source "$BUILD_CONFIG_FILE"
 
 # Decide whether to use the release tag. JAX CI jobs build from the main
 # branch by default. 
-if [[ -z "$JAXCI_RELEASE_TAG" ]]; then
+if [[ -n "$JAXCI_RELEASE_TAG" ]]; then
   git checkout tags/"$JAXCI_RELEASE_TAG"
 fi
 
