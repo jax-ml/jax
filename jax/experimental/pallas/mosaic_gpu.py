bbcc3eef3c1fedda3c0eef48c8bd49fd34a313c9
Adam Paszke <apaszke@google.com>
2024-10-18 10:31:00 -0700

    [Pallas:MGPU] Fix the implementation of WGMMA with transposed RHS
    It's not enough that we have the physical transpose between the order
    of tiled dimensions, we also need the user to explicitly transpose the
    logical dimensions. This fixes a shape error that was previously hidden
    because the RHS was square.
    
    PiperOrigin-RevId: 687350270

--- jax/experimental/pallas/mosaic_gpu.py
+++ jax/experimental/pallas/mosaic_gpu.py
@@ -24,6 +24,7 @@ from jax._src.pallas.mosaic_gpu.core import GPUMemorySpace as GPUMemorySpace
 from jax._src.pallas.mosaic_gpu.core import GPUMesh as GPUMesh
 from jax._src.pallas.mosaic_gpu.core import SwizzleTransform as SwizzleTransform
 from jax._src.pallas.mosaic_gpu.core import TilingTransform as TilingTransform
+from jax._src.pallas.mosaic_gpu.core import transpose_ref as transpose_ref
 from jax._src.pallas.mosaic_gpu.core import TransposeTransform as TransposeTransform
 from jax._src.pallas.mosaic_gpu.core import WGMMAAccumulatorRef as ACC  # noqa: F401
 from jax._src.pallas.mosaic_gpu.core import WGMMAAccumulatorRef as WGMMAAccumulatorRef
