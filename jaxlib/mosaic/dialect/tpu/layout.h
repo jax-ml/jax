6ee727e8fa3459d93dc5106b2e7ea355d702d34a
Tomás Longeri <tlongeri@google.com>
2024-12-06 20:42:13 -0800

    [Mosaic:TPU] Retilings with replicated sublanes on source and increasing tile size
    This is a generalization of the (1, 128) -> (8, 128) 32-bit replicated retiling
    
    PiperOrigin-RevId: 703704106

--- jaxlib/mosaic/dialect/tpu/layout.h
+++ jaxlib/mosaic/dialect/tpu/layout.h
@@ -252,6 +252,14 @@ class VectorLayout {
 
   int8_t bitwidth() const { return bitwidth_; }
   const LayoutOffsets &offsets() const { return offsets_; }
+  const LayoutOffsets getCanonicalOffsets(
+      const ArrayRef<int64_t> shape,
+      const std::array<int64_t, 2> target_shape) const {
+    const std::array<int64_t, 2> tiled_ishape = getImplicitTiledDims(shape, 1);
+    return {
+        tiling_[0] == 1 && tiled_ishape[0] == 1 ? std::nullopt : offsets_[0],
+        offsets_[1]};
+  }
   const std::array<int64_t, 2> &tiling() const { return tiling_; }
   ImplicitDim implicit_dim() const { return implicit_dim_; }
   int packing() const { return 32 / bitwidth_; }
