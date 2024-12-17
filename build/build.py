44171bd49c05ac87765dabf513fd0db16f7f4cde
Nitin Srinivasan <srnitin@google.com>
2024-12-17 17:29:57 +0000

    fix syntax error

--- build/build.py
+++ build/build.py
@@ -456,17 +456,17 @@ async def main():
     wheel_build_command_base.append(f"--repo_env=BAZEL_COMPILER=\"{clang_path}\"")
 
     # Disable clang extention that rejects unknown arguments.
-    wheel_build_command.append("--copt=-Wunused-arguments")
+    wheel_build_command_base.append("--copt=-Wunused-arguments")
     # Error on struct/class mismatches, since this causes link failures on
     # Windows.
-    wheel_build_command.append("--copt=-Werror=mismatched-tags")
+    wheel_build_command_base.append("--copt=-Werror=mismatched-tags")
     if clang_major_version >= 16:
       # Disable clang extention that rejects type definitions within offsetof
       # on newer clang versions. This was added in clang-16 by
       # https://reviews.llvm.org/D133574.
       # Can be removed once upb is updated, since a type definition is used
       # within the offset of in the current version of ubp.
-      wheel_build_command.append("--copt=-Wno-gnu-offsetof-extensions")
+      wheel_build_command_base.append("--copt=-Wno-gnu-offsetof-extensions")
   else:
     logging.debug("Use Clang: False")
 
