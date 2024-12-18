cca9afa28f3099ad998792c3eb9329e66555ec83
Yash Katariya <yashkatariya@google.com>
2024-12-17 16:42:42 -0800

    Delete enable_memories code in C++ since that flag is always True and cannot be turned off now.
    PiperOrigin-RevId: 707298305

--- jax/_src/config.py
+++ jax/_src/config.py
@@ -214,7 +214,6 @@ def trace_context():
           sharding_in_types.value,
           use_direct_linearize.value,
           softmax_custom_jvp.value,
-          enable_memories.value,
           disable_jit.value,
           debug_key_reuse.value,
           jax_xla_profile_version.value,
@@ -972,18 +971,10 @@ pmap_shmap_merge = bool_state(
     upgrade=True,
     help='If True, pmap and shard_map API will be merged.')
 
-def _update_jax_memories_global(val):
-  jax_jit.global_state().enable_memories = val
-
-def _update_jax_memories_thread_local(val):
-  jax_jit.thread_local_state().enable_memories = val
-
 enable_memories = bool_state(
     'jax_enable_memories',
     default=True,
     upgrade=True,
-    update_global_hook=_update_jax_memories_global,
-    update_thread_local_hook=_update_jax_memories_thread_local,
     help=("If True, will allow fetching memory kinds available on executable "
           "and annotate Shardings with it."))
 
