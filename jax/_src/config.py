5a068da699507d2e282632d1a15b918446dfb750
Yash Katariya <yashkatariya@google.com>
2025-01-17 22:12:24 -0800

    Remove memories flag now that JAX 0.5.0 has been released since it always defaults to True.
    PiperOrigin-RevId: 716908015

--- jax/_src/config.py
+++ jax/_src/config.py
@@ -974,22 +974,6 @@ pmap_shmap_merge = bool_state(
     upgrade=True,
     help='If True, pmap and shard_map API will be merged.')
 
-# Remove after next JAX release on Jan 15, 2025.
-if hasattr(jax_jit.global_state(), 'enable_memories'):
-  def _update_jax_memories_global(val):
-    jax_jit.global_state().enable_memories = val
-
-  def _update_jax_memories_thread_local(val):
-    jax_jit.thread_local_state().enable_memories = val
-
-  enable_memories = bool_state(
-      'jax_enable_memories',
-      default=True,
-      upgrade=True,
-      update_global_hook=_update_jax_memories_global,
-      update_thread_local_hook=_update_jax_memories_thread_local,
-      help=("If True, will allow fetching memory kinds available on executable "
-            "and annotate Shardings with it."))
 
 spmd_mode = enum_state(
     name='jax_spmd_mode',
