a7a885aa5ab5dfd308f5658807a75d1405c3063b
Peter Hawkins <phawkins@google.com>
2025-01-27 09:14:38 -0800

    Disable a Pallas ops test that fails on TPU v6e.
    PiperOrigin-RevId: 720194737

--- tests/pallas/ops_test.py
+++ tests/pallas/ops_test.py
@@ -951,6 +951,8 @@ class OpsTest(PallasBaseTest):
       # TODO(apaszke): Remove after 12 weeks have passed.
       if not jtu.if_cloud_tpu_at_least(2024, 12, 19):
         self.skipTest("Requires libtpu built at least on 2024-12-19")
+      if fn == jnp.exp2 and dtype == "bfloat16":
+        self.skipTest("TODO(b/392601160): test fails in CI")
 
     if (
         jtu.test_device_matches(["gpu"])
