d4758b6d5e74a80991b5ac4d540d992bebb71998
Tzu-Wei Sung <twsung@google.com>
2025-01-30 09:49:03 -0800

    [Mosaic][NFC] Factor out xla-array related utils in a separate file.
    Also added tests.
    
    PiperOrigin-RevId: 721424194

--- jaxlib/mosaic/dialect/tpu/transforms/apply_vector_layout.cc
+++ jaxlib/mosaic/dialect/tpu/transforms/apply_vector_layout.cc
@@ -60,6 +60,7 @@
 #include "mlir/include/mlir/IR/Builders.h"
 #include "mlir/include/mlir/IR/ImplicitLocOpBuilder.h"
 #include "mlir/include/mlir/IR/OperationSupport.h"
+#include "jaxlib/mosaic/dialect/tpu/array_util.h"
 #include "jaxlib/mosaic/dialect/tpu/layout.h"
 #include "jaxlib/mosaic/dialect/tpu/tpu_dialect.h"
 #include "jaxlib/mosaic/dialect/tpu/transforms/apply_vector_layout_extensions.h"
@@ -181,36 +182,6 @@ SmallVector<xla::Array<Value>> split(const xla::Array<Value> &vregs, int axis) {
   return chunks;
 };
 
-template <typename T>
-ArrayRef<T> XlaArrayToFlatArrayRef(xla::Array<T> xla_array) {
-  return ArrayRef<T>(xla_array.data(), xla_array.num_elements());
-}
-
-template <typename T, typename Range>
-xla::Array<T> XlaArrayFromShapeAndValues(ArrayRef<int64_t> sizes, Range vals) {
-  // TODO(tlongeri): is there no way to avoid default initialization in the
-  // constructor?
-  xla::Array<T> arr(sizes);
-  arr.SetValues(vals);
-  return arr;
-}
-
-bool incrementSliceIndex(const MutableArrayRef<int64_t> idx,
-                         const absl::Span<const int64_t> starts,
-                         const absl::Span<const int64_t> limits) {
-  const int64_t nd = idx.size();
-  CHECK_EQ(nd, starts.size());
-  CHECK_EQ(nd, limits.size());
-  for (int64_t i = nd - 1; i >= 0; --i) {
-    ++idx[i];
-    if (idx[i] < limits[i]) {
-      return true;
-    }
-    idx[i] = starts[i];
-  }
-  return false;
-}
-
 bool incrementIndex(const MutableArrayRef<int64_t> idx,
                     const absl::Span<const int64_t> limits) {
   const int64_t nd = idx.size();
@@ -223,58 +194,6 @@ bool incrementIndex(const MutableArrayRef<int64_t> idx,
     idx[i] = 0;
   }
   return false;
-}
-
-bool sliceIsEmpty(const absl::Span<const int64_t> starts,
-                  const absl::Span<const int64_t> limits) {
-  for (auto [s, l] : llvm::zip_equal(starts, limits)) {
-    CHECK_LE(s, l);
-    if (s == l) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// An alternative to xla::Array::UpdateSlice that takes a single value
-template <typename T>
-void updateSlice(xla::Array<T> &arr, const T &value,
-                 const absl::Span<const int64_t> starts,
-                 const absl::Span<const int64_t> limits) {
-  if (sliceIsEmpty(starts, limits)) {
-    return;
-  }
-  SmallVector<int64_t> idx(toArrayRef(starts));
-  do {
-    arr(idx) = value;
-  } while (incrementSliceIndex(idx, starts, limits));
-}
-
-// An alternative to xla::Array::UpdateSlice that takes a range of data
-template <typename T, typename Range>
-void updateSliceFromRange(xla::Array<T> &arr, Range data,
-                          const absl::Span<const int64_t> starts,
-                          const absl::Span<const int64_t> limits) {
-  if (sliceIsEmpty(starts, limits)) {
-    return;
-  }
-  SmallVector<int64_t> idx(toArrayRef(starts));
-  auto in_bounds = [&] {
-    for (int64_t i = 0; i < idx.size(); ++i) {
-      if (idx[i] >= arr.dim(i)) {
-        return false;
-      }
-    }
-    return true;
-  };
-  auto data_it = data.begin();
-  do {
-    if (in_bounds()) {
-      arr(idx) = *data_it;
-    }
-    ++data_it;
-  } while (incrementSliceIndex(idx, starts, limits));
-  CHECK(data_it == data.end());
 }
 
 FailureOr<int64_t> getIntConst(Value v, bool silent = false) {
