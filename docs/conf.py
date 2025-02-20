380fb0a5a08088e99fe7387813f31ab2b7da556c
Jake VanderPlas <jakevdp@google.com>
2025-02-20 13:14:59 -0800

    CI: skip execution of convolutions.ipynb
    This is failing on readthedocs with a dead kernel error

--- docs/conf.py
+++ docs/conf.py
@@ -222,6 +222,8 @@ nb_execution_excludepatterns = [
     # TODO(jakevdp): enable execution on the following if possible:
     'notebooks/Distributed_arrays_and_automatic_parallelization.*',
     'notebooks/autodiff_remat.*',
+    # Fails on readthedocs with Kernel Died
+    'notebooks/convolutions.ipynb',
     # Requires accelerators
     'pallas/quickstart.*',
     'pallas/tpu/pipelining.*',
