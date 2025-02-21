655267609b5589fda8358ae7aaf2eb832036407a
jax authors <google-ml-automation@google.com>
2025-02-21 03:28:11 -0800

    Reverts 9ad9c3d3c2b6b5c3ea736af9b4d8c595537de93c
    PiperOrigin-RevId: 729462063

--- jax/experimental/jax2tf/call_tf.py
+++ jax/experimental/jax2tf/call_tf.py
@@ -591,8 +591,7 @@ def _call_tf_lowering(
   call = func_dialect.CallOp(callee_result_types,
                              ir.FlatSymbolRefAttr.get(fn),
                              tuple(args_op) + captured_ops)
-  if result_shape.is_tuple() and xla_client.mlir_api_version < 58:
-    # In API version 58, the results are always flattened.
+  if result_shape.is_tuple():
     flat_results = [hlo.get_tuple_element(call, mlir.i32_attr(i))
                     for i in range(len(result_shapes))]
   else:
