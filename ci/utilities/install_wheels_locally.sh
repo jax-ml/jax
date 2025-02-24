4b4f2f9cb94cb43a3ff33bde4fb5a9c063b3e5da
Nitin Srinivasan <srnitin@google.com>
2025-02-24 10:13:00 -0800

    Use `uv` to install Python packages
    PiperOrigin-RevId: 730499307

--- ci/utilities/install_wheels_locally.sh
+++ ci/utilities/install_wheels_locally.sh
@@ -27,15 +27,21 @@ fi
 echo "Installing the following wheels:"
 echo "${WHEELS[@]}"
 
+# Install `uv` if it's not already installed. `uv` is much faster than pip for
+# installing Python packages.
+if ! command -v uv >/dev/null 2>&1; then
+  pip install uv~=0.5.30
+fi
+
 # On Windows, convert MSYS Linux-like paths to Windows paths.
 if [[ $(uname -s) =~ "MSYS_NT" ]]; then
-  "$JAXCI_PYTHON" -m pip install $(cygpath -w "${WHEELS[@]}")
+  "$JAXCI_PYTHON" -m uv pip install $(cygpath -w "${WHEELS[@]}")
 else
-  "$JAXCI_PYTHON" -m pip install "${WHEELS[@]}"
+  "$JAXCI_PYTHON" -m uv pip install "${WHEELS[@]}"
 fi
 
 if [[ "$JAXCI_INSTALL_JAX_CURRENT_COMMIT" == "1" ]]; then
   echo "Installing the JAX package in editable mode at the current commit..."
   # Install JAX package at the current commit.
-  "$JAXCI_PYTHON" -m pip install -U -e .
+  "$JAXCI_PYTHON" -m uv pip install -U -e .
 fi
