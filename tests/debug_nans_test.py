Fix shard_map debug_nan leakage of manual out_avals in the impl rules of jit i.e. impl rule of jit saw a manual out_aval which is not expected. This is a band-aid for now with a TODO to do a proper fix
PiperOrigin-RevId: 730499532

tests/debug_nans_test.py
@@ -155,8 +155,7 @@ class DebugNaNsTest(jtu.JaxTestCase):
     _, f_vjp = jax.vjp(shmap_f, jnp.zeros([1]))
 
     with self.assertRaisesRegex(
-        FloatingPointError,
-        r"invalid value \(nan\) encountered in mul\nWhen differentiating"):
+        FloatingPointError, r"Invalid value \(nan\) encountered"):
       ans, = f_vjp(jnp.ones([1]))
       ans.block_until_ready()
 