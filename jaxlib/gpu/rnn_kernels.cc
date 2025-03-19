ee74c289ac02b0f0f07ce8819bef4b2f97207d4b
Peter Hawkins <phawkins@google.com>
2025-03-19 10:03:57 -0700

    Move //jaxlib:handle_pool to //jaxlib/gpu:handle_pool.
    This is a GPU-specific target.
    
    PiperOrigin-RevId: 738441625

--- jaxlib/gpu/rnn_kernels.cc
+++ jaxlib/gpu/rnn_kernels.cc
@@ -23,7 +23,7 @@ limitations under the License.
 #include "absl/strings/str_format.h"
 #include "jaxlib/gpu/ffi_wrapper.h"
 #include "jaxlib/gpu/gpu_kernel_helpers.h"
-#include "jaxlib/handle_pool.h"
+#include "jaxlib/gpu/handle_pool.h"
 #include "jaxlib/kernel_helpers.h"
 #include "xla/service/custom_call_status.h"
 
