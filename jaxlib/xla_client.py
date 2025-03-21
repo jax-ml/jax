4410ccc7a1abdbfd59491c49fca60188c3826dd3
Peter Hawkins <phawkins@google.com>
2025-03-21 13:26:09 -0700

    Remove symlinking of xla_client.py.
    Use a stub instead. Symlinking led to confusing behaviors because Python may believe there are two copies of the module.
    
    PiperOrigin-RevId: 739280690

---
+++ jaxlib/xla_client.py
@@ -0,0 +1,18 @@
+# Copyright 2025 The JAX Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from jaxlib.xla.xla_client import *  # noqa: F403
+from jaxlib.xla.xla_client import _version  # noqa: F401
+from jaxlib.xla.xla_client import _xla  # noqa: F401
