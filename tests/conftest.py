13d88b634087f4f0d45c5ba95db81337af92d558
charleshofer <Charles.Hofer@amd.com>
2025-03-24 11:26:31 -0500

    Add back raw totals in JSON reports (#281)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,8 @@
-import os
 import pytest
 
-
-INCLUDE_SKIPS = os.getenv("ROCM_TEST_INCLUDE_SKIPS", default=False)
 
 @pytest.hookimpl(optionalhook=True)
 def pytest_json_modifyreport(json_report):
     """Get rid of skipped tests in reporting. We only care about xfails."""
-    if (not INCLUDE_SKIPS
-            and "summary" in json_report
-            and "total" in json_report["summary"]):
+    if "summary" in json_report and "total" in json_report["summary"]:
         json_report["summary"]["unskipped_total"] = json_report["summary"]["total"] - json_report["summary"].get("skipped", 0)
-        del json_report["summary"]["total"]
