8bd956d96a6979bcead917d7d0f8593203888cfe
jax authors <google-ml-automation@google.com>
2025-03-27 03:02:36 -0700

    [Pallas] Skip reads/writes from/to slices of kernel input/output buffers when the slices do not change between iterations of the grid loop that interprets kernels on CPU.
    PiperOrigin-RevId: 741082349

--- tests/pallas/tpu_pallas_interpret_test.py
+++ tests/pallas/tpu_pallas_interpret_test.py
@@ -18,24 +18,48 @@ To work around https://github.com/jax-ml/jax/issues/25671 , this file
 contains only tests that do not use shard_map.
 """
 
-from absl.testing import absltest
-from absl.testing import parameterized
 import functools
 
+from absl.testing import absltest
+from absl.testing import parameterized
 import jax
 from jax._src import test_util as jtu
 import jax._src.pallas.mosaic.interpret as mosaic_interpret
 from jax.experimental import pallas as pl
 from jax.experimental.pallas import tpu as pltpu
 import jax.numpy as jnp
-
 import numpy as np
 
 
 jax.config.parse_flags_with_absl()
 
 
+class CountStoreCallbacksContext(object):
+  """Wraps the I/O callback `store` into a callback that counts the number of calls to `store`."""
+
+  def __init__(self):
+    self._num_stores = 0
+    self._saved = mosaic_interpret.store
+
+  def __enter__(self):
+    def _store_callback(self, *args, **kwargs):
+      self._num_stores += 1
+      return self._saved(*args, **kwargs)
+
+    mosaic_interpret.store = functools.partial(_store_callback, self)
+    return self
+
+  def __exit__(self, ty, value, traceback):
+    del ty, value, traceback
+    mosaic_interpret.store = self._saved
+
+  @property
+  def num_stores(self):
+    return self._num_stores
+
+
 class InterpretTest(jtu.JaxTestCase):
+
   def setUp(self):
     super().setUp()
     self.num_devices = jax.device_count()
@@ -50,17 +74,18 @@ class InterpretTest(jtu.JaxTestCase):
     @jax.jit
     def matmul(x: jax.Array, y: jax.Array):
       return pl.pallas_call(
-        matmul_kernel,
-        out_shape=jax.ShapeDtypeStruct((x.shape[0], y.shape[1]), x.dtype),
-        grid=(2, 2),
-        in_specs=[
-            pl.BlockSpec((x.shape[0] // 2, x.shape[1]), lambda i, j: (i, 0)),
-            pl.BlockSpec((y.shape[0], y.shape[1] // 2), lambda i, j: (0, j))
-        ],
-        out_specs=pl.BlockSpec(
-            (x.shape[0] // 2, y.shape[1] // 2), lambda i, j: (i, j),
-        ),
-        interpret=mosaic_interpret.TPUInterpretParams(),
+          matmul_kernel,
+          out_shape=jax.ShapeDtypeStruct((x.shape[0], y.shape[1]), x.dtype),
+          grid=(2, 2),
+          in_specs=[
+              pl.BlockSpec((x.shape[0] // 2, x.shape[1]), lambda i, j: (i, 0)),
+              pl.BlockSpec((y.shape[0], y.shape[1] // 2), lambda i, j: (0, j)),
+          ],
+          out_specs=pl.BlockSpec(
+              (x.shape[0] // 2, y.shape[1] // 2),
+              lambda i, j: (i, j),
+          ),
+          interpret=mosaic_interpret.TPUInterpretParams(),
       )(x, y)
 
     k1, k2 = jax.random.split(jax.random.key(0))
@@ -79,9 +104,11 @@ class InterpretTest(jtu.JaxTestCase):
       grid_spec = pltpu.PrefetchScalarGridSpec(
           num_scalar_prefetch=1,
           grid=(1, 1),
-          in_specs=[pl.BlockSpec(
-              sizes,
-              lambda i, j, block_idx: (block_idx[0], block_idx[1]))],
+          in_specs=[
+              pl.BlockSpec(
+                  sizes, lambda i, j, block_idx: (block_idx[0], block_idx[1])
+              )
+          ],
           out_specs=pl.BlockSpec(sizes, lambda *_: (0, 0)),
       )
 
@@ -96,17 +123,21 @@ class InterpretTest(jtu.JaxTestCase):
 
     shape = (512, 512)
     x = jnp.reshape(jnp.arange(np.prod(shape), dtype=jnp.int32), shape)
-    result = block_dynamic_slice(x, starts=jnp.array([128, 256]), sizes=(128, 128))
-    ref = jax.lax.dynamic_slice(x, start_indices=(128, 256), slice_sizes=(128, 128))
+    result = block_dynamic_slice(
+        x, starts=jnp.array([128, 256]), sizes=(128, 128)
+    )
+    ref = jax.lax.dynamic_slice(
+        x, start_indices=(128, 256), slice_sizes=(128, 128)
+    )
     diff = jnp.max(jnp.abs(result - ref))
     np.testing.assert_allclose(result, ref)
 
   def test_dynamic_grid_and_aliasing(self):
-    self.skipTest('Broken pending fix to extra reads/writes of inputs/outputs')
     def kernel(s_ref, x_ref, o_ref):
       o_ref[...] = x_ref[...] + s_ref[0].astype(x_ref.dtype)
 
     iters = jax.random.randint(jax.random.key(0), (), 10, 20, dtype=jnp.int32)
+
     @jax.jit
     def f(s, x):
       return pl.pallas_call(
@@ -119,11 +150,11 @@ class InterpretTest(jtu.JaxTestCase):
           ],
           out_specs=pl.BlockSpec(x.shape, lambda i: (0, 0)),
           input_output_aliases={1: 0},
-          interpret=mosaic_interpret.TPUInterpretParams()
+          interpret=mosaic_interpret.TPUInterpretParams(),
       )(s, x)
 
     s = jnp.array([1], dtype=jnp.int32)
-    x = jnp.arange(32 * 128.).reshape((32, 128))
+    x = jnp.arange(32 * 128.0).reshape((32, 128))
     y = f(s, x)
     # NOTE: No matter how many times the kernel body is run, the kernel input
     # buffer will only be written once by the pallas_call machinery, just
@@ -136,6 +167,7 @@ class InterpretTest(jtu.JaxTestCase):
       @pl.when((pl.program_id(0) == 0) & (pl.program_id(1) == 0))
       def _():
         s_ref[0] = jnp.int32(0)
+
       s = s_ref[0]
       s_ref[0] = s + 1
       o_ref[:] = x_ref[:] + s.astype(x_ref.dtype)
@@ -149,7 +181,8 @@ class InterpretTest(jtu.JaxTestCase):
             pl.BlockSpec(block_shape=(8, 128), index_map=lambda i, j: (i, j)),
         ],
         out_specs=pl.BlockSpec(
-            block_shape=(8, 128), index_map=lambda i, j: (j, i)),
+            block_shape=(8, 128), index_map=lambda i, j: (j, i)
+        ),
         scratch_shapes=(pltpu.SMEM((1,), jnp.int32),),
         input_output_aliases={0: 0},
         interpret=mosaic_interpret.TPUInterpretParams(),
@@ -184,7 +217,8 @@ class InterpretTest(jtu.JaxTestCase):
       copy.wait()
 
     x = jnp.zeros((8, 128), jnp.float32)
-    y = pl.pallas_call(kernel_without_race,
+    y = pl.pallas_call(
+        kernel_without_race,
         out_shape=jax.ShapeDtypeStruct(x.shape, x.dtype),
         in_specs=[pl.BlockSpec(memory_space=pltpu.TPUMemorySpace.ANY)],
         scratch_shapes=[
@@ -192,12 +226,14 @@ class InterpretTest(jtu.JaxTestCase):
             pltpu.SemaphoreType.DMA,
         ],
         interpret=mosaic_interpret.TPUInterpretParams(
-            detect_races=True, dma_execution_mode=dma_execution_mode),
+            detect_races=True, dma_execution_mode=dma_execution_mode
+        ),
     )(x).block_until_ready()
     self.assertFalse(mosaic_interpret.races.races_found)
     np.testing.assert_allclose(y, x + 1.0)
 
-    pl.pallas_call(kernel_with_race,
+    pl.pallas_call(
+        kernel_with_race,
         out_shape=jax.ShapeDtypeStruct(x.shape, x.dtype),
         in_specs=[pl.BlockSpec(memory_space=pltpu.TPUMemorySpace.ANY)],
         scratch_shapes=[
@@ -205,7 +241,8 @@ class InterpretTest(jtu.JaxTestCase):
             pltpu.SemaphoreType.DMA,
         ],
         interpret=mosaic_interpret.TPUInterpretParams(
-            detect_races=True, dma_execution_mode=dma_execution_mode),
+            detect_races=True, dma_execution_mode=dma_execution_mode
+        ),
     )(x).block_until_ready()
     self.assertTrue(mosaic_interpret.races.races_found)
 
@@ -228,8 +265,8 @@ class InterpretTest(jtu.JaxTestCase):
     z = jax.jit(matmul)(x, y)
     np.testing.assert_array_equal(z, jnp.full_like(z, jnp.inf))
 
-    lowered = jax.jit(matmul).lower(x, y).as_text(dialect="stablehlo")
-    self.assertNotIn("dot_general", lowered)
+    lowered = jax.jit(matmul).lower(x, y).as_text(dialect='stablehlo')
+    self.assertNotIn('dot_general', lowered)
 
   @parameterized.parameters('nan', 'zero')
   def test_uninitialized_memory(self, uninitialized_memory):
@@ -250,7 +287,8 @@ class InterpretTest(jtu.JaxTestCase):
             pltpu.VMEM((8, 128), jnp.int16),
         ],
         interpret=mosaic_interpret.TPUInterpretParams(
-            uninitialized_memory=uninitialized_memory),
+            uninitialized_memory=uninitialized_memory
+        ),
     )()
     if uninitialized_memory == 'nan':
       self.assertTrue(jnp.isnan(x).all())
@@ -261,6 +299,33 @@ class InterpretTest(jtu.JaxTestCase):
       np.testing.assert_equal(np.array(y), 0)
       np.testing.assert_equal(np.array(z), 0)
 
+  def test_correct_number_of_stores(self):
+    def kernel(x_ref, s_ref, o_ref):
+      s = s_ref[0]
+      x_ref[:] += jax.lax.full_like(x_ref, s)
+      s_ref[0] = s + 1
+      o_ref[:] = x_ref[:]
 
-if __name__ == "__main__":
+    def kernel_call(x, s):
+      return pl.pallas_call(
+          kernel,
+          out_shape=jax.ShapeDtypeStruct((16, 256), jnp.float32),
+          grid=(2, 2),
+          in_specs=[
+              pl.BlockSpec((8, 256), lambda i, j: (i, 0)),
+              pl.BlockSpec(memory_space=pltpu.SMEM),
+          ],
+          out_specs=pl.BlockSpec((8, 256), lambda i, j: (i, 0)),
+          interpret=mosaic_interpret.TPUInterpretParams(),
+      )(x, s)
+
+    with CountStoreCallbacksContext() as store_callbacks_counter:
+      result = jax.jit(kernel_call)(
+          jnp.zeros((16, 256), jnp.float32), jnp.zeros((1,), jnp.int32)
+      )
+      np.testing.assert_allclose(result[::8, ::256], [[1.0], [5.0]])
+      self.assertEqual(store_callbacks_counter.num_stores, 5)
+
+
+if __name__ == '__main__':
   absltest.main(testLoader=jtu.JaxTestLoader())
