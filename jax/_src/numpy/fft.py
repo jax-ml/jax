8d2e9a853a2b7c86012479cc133ed864bd1d3ada
Jon Barron <barron@google.com>
2025-04-21 09:23:36 -0700

    Simplify (and potentially accelerate) fftfreq(), as a single vectorized op instead of multiple .at[] calls.
    PiperOrigin-RevId: 749818535

--- jax/_src/numpy/fft.py
+++ jax/_src/numpy/fft.py
@@ -1186,21 +1186,8 @@ def fftfreq(n: int, d: ArrayLike = 1.0, *, dtype: DTypeLike | None = None,
           "The d argument of jax.numpy.fft.fftfreq only takes a single value. "
           "Got d = %s." % list(d))
 
-  k = jnp.zeros(n, dtype=dtype, device=device)
-  if n % 2 == 0:
-    # k[0: n // 2 - 1] = jnp.arange(0, n // 2 - 1)
-    k = k.at[0: n // 2].set(jnp.arange(0, n // 2, dtype=dtype))
-
-    # k[n // 2:] = jnp.arange(-n // 2, -1)
-    k = k.at[n // 2:].set(jnp.arange(-n // 2, 0, dtype=dtype))
-
-  else:
-    # k[0: (n - 1) // 2] = jnp.arange(0, (n - 1) // 2)
-    k = k.at[0: (n - 1) // 2 + 1].set(jnp.arange(0, (n - 1) // 2 + 1, dtype=dtype))
-
-    # k[(n - 1) // 2 + 1:] = jnp.arange(-(n - 1) // 2, -1)
-    k = k.at[(n - 1) // 2 + 1:].set(jnp.arange(-(n - 1) // 2, 0, dtype=dtype))
-
+  i = jnp.arange(n, dtype=dtype, device=device)
+  k = ((i + n//2) % n - n//2)
   return k / jnp.array(d * n, dtype=dtype, device=device)
 
 
