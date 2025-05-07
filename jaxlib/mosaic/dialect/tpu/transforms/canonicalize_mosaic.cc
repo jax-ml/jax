21c86ee87060c7d921b48afcd55ca2eebac632a3
Jaswanth Sreeram <jsreeram@google.com>
2025-05-07 11:26:27 -0700

    Fold transposes feeding the RHS of a matmul into a change in the dot dimension numbers.
    PiperOrigin-RevId: 755935066

--- jaxlib/mosaic/dialect/tpu/transforms/canonicalize_mosaic.cc
+++ jaxlib/mosaic/dialect/tpu/transforms/canonicalize_mosaic.cc
@@ -205,6 +205,37 @@ LogicalResult tpu_matmul_rule(const CanonicalizeContext &ctx,
     }
   }
 
+  // Attempt to canonicalize matmul(x, transpose(y)) to a matmul with the
+  // dimension numbers changed which will later be lowered into a more efficient
+  // operation that fuses the transpose into the matmul.
+  auto transpose_op =
+      dyn_cast_if_present<vector::TransposeOp>(rhs.getDefiningOp());
+  auto dimension_numbers = op.getDimensionNumbers();
+  if (transpose_op && transpose_op->hasOneUse() &&
+      dimension_numbers->getRhsContractingDims().size() == 1 &&
+      dimension_numbers->getRhsNonContractingDims().size() == 1) {
+    auto rhs_non_contracting_dim =
+        dimension_numbers->getRhsNonContractingDims()[0];
+    auto rhs_contracting_dim = dimension_numbers->getRhsContractingDims()[0];
+    auto permutation = transpose_op.getPermutation();
+    if (permutation[rhs_contracting_dim] == rhs_non_contracting_dim &&
+        permutation[rhs_non_contracting_dim] == rhs_contracting_dim &&
+        std::all_of(dimension_numbers->getRhsBatchDims().begin(),
+                    dimension_numbers->getRhsBatchDims().end(),
+                    [&](long batch_dim) {
+                      return permutation[batch_dim] == batch_dim;
+                    })) {
+      if (auto transpose_op_vector_operand =
+              dyn_cast<TypedValue<VectorType>>(transpose_op.getOperand())) {
+        // The transpose is DCE'ed away at a later point.
+        rhs = transpose_op_vector_operand;
+        transpose_rhs = !transpose_rhs;
+      } else {
+        return op->emitOpError("Unexpected operand type for transpose op.");
+      }
+    }
+  }
+
   auto dot_dim_matmul = [&](auto lhs, auto rhs, auto acc) {
     auto precision_attr = op.getPrecisionAttr();
 
