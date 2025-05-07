74938be8456050fec5032682f1463ea36983e1de
Dan Foreman-Mackey <danfm@google.com>
2025-05-07 14:03:14 -0400

    Consolidate initial/final style custom_vjp primitives into one.

--- jax/custom_derivatives.py
+++ jax/custom_derivatives.py
@@ -26,7 +26,6 @@ from jax._src.custom_derivatives import (
   custom_jvp_call_jaxpr_p as custom_jvp_call_jaxpr_p,
   custom_vjp as custom_vjp,
   custom_vjp_call_p as custom_vjp_call_p,
-  custom_vjp_call_jaxpr_p as custom_vjp_call_jaxpr_p,
   custom_vjp_primal_tree_values as custom_vjp_primal_tree_values,
   CustomVJPPrimal as CustomVJPPrimal,
   linear_call as linear_call,
