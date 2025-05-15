27c0e582dcc3bae2145bd64fe24caacd880d66b0
Sergei Lebedev <slebedev@google.com>
2025-05-15 00:54:31 -0700

    [pallas] Do not emit verbose lowering errors by default
    The errors are too verbose and mostly not very useful.
    
    PiperOrigin-RevId: 759025165

--- jax/_src/pallas/pallas_call.py
+++ jax/_src/pallas/pallas_call.py
@@ -1235,7 +1235,7 @@ _PALLAS_USE_MOSAIC_GPU = config.bool_state(
 
 _PALLAS_VERBOSE_ERRORS = config.bool_flag(
     "jax_pallas_verbose_errors",
-    default=config.bool_env("JAX_PALLAS_VERBOSE_ERRORS", True),
+    default=config.bool_env("JAX_PALLAS_VERBOSE_ERRORS", False),
     help=(
         "If True, print verbose error messages for Pallas kernels."
     ),
