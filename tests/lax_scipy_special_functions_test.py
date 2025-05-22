570287021a296806a0377d1dafceb165974d298b
Dan Foreman-Mackey <danfm@google.com>
2025-05-22 09:35:00 -0700

    Reduce input ranges tested for hyp2f1.
    The `z` input to this function only supports input in the range 0 <= z < 1, so most of the tested samples were explicitly `inf`. The other parameters can take arbitrary values, but the dynamic range required explodes for values larger than ~1, so the old tests were quite numerically unstable. We could test some specific carefully chosen larger values, but I don't think this brings much benefit.
    
    PiperOrigin-RevId: 762000716

--- tests/lax_scipy_special_functions_test.py
+++ tests/lax_scipy_special_functions_test.py
@@ -159,7 +159,7 @@ JAX_SPECIAL_FUNCTION_RECORDS = [
     ),
     op_record(
         "hyp2f1", 4, float_dtypes,
-        functools.partial(jtu.rand_uniform, low=0.5, high=30), False
+        functools.partial(jtu.rand_uniform, low=0.1, high=0.9), False
     ),
     op_record("log_softmax", 1, float_dtypes, jtu.rand_default, True),
     op_record("softmax", 1, float_dtypes, jtu.rand_default, True),
