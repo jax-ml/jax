c1c0c0fa27f52de19ddd5112580c8da2f711e1e8
Dimitar (Mitko) Asenov <dasenov@google.com>
2025-05-23 06:20:38 -0700

    [Mosaic GPU] Pass the right number of immediate values in the wgmma inline asm.
    Before this change, in the int32 case, we pass two extra immediate args compared to the number of parameters in the ASM string. Running tests with `-UNDEBUG` detects the error.
    
    I think this likely broke with the special-casing of `int32` in cl/761489756.
    
    I've added an assert that should prevent mismatches in the future.
    
    PiperOrigin-RevId: 762394530

--- jax/experimental/mosaic/gpu/wgmma.py
+++ jax/experimental/mosaic/gpu/wgmma.py
@@ -226,11 +226,18 @@ def wgmma_m64(
     return llvm.ConstantOp(i32, ir.IntegerAttr.get(i32, x)).result
 
   use_out = scale_a = scale_b = lc(1)
-  imms = [use_out, scale_a, scale_b]
+  if out_ty == i32:
+    imms = [use_out]
+  else:
+    imms = [use_out, scale_a, scale_b]
+
   if supports_transpose and a_transpose is not None:
     imms += [lc(int(a_transpose)), lc(int(b_transpose))]
   elif supports_transpose:
     imms += [lc(int(b_transpose))]
+
+  assert len(imms) == num_imm_regs + 1  # +1 for the use_out_reg in setp.ne.b32
+
   if acc.ndim != 10 or acc.shape[0] != 1 or math.prod(acc.shape[2:]) != 2:
     raise ValueError(acc.shape)
   acc_struct_type = ir.Type.parse(
