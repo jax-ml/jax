12966b5f578e78f5f80fda3bc877441767738584
Tom Natan <tomnatan@google.com>
2025-05-23 07:25:14 -0700

    Fix sharding rule in jax test
    `ValueError: Sharding rule has 1 operands, but the operation has 2 operands`
    
    PiperOrigin-RevId: 762412744

--- tests/cache_key_test.py
+++ tests/cache_key_test.py
@@ -181,7 +181,7 @@ class CacheKeyTest(jtu.JaxTestCase):
     _cp_add.def_partition(
       infer_sharding_from_operands=_infer_sharding_from_operands,
       partition=_partition,
-      sharding_rule='i i -> i')
+      sharding_rule='..., ... -> ...')
 
     devices = np.asarray(jax.devices())
     with Mesh(devices, ('x',)) as m:
