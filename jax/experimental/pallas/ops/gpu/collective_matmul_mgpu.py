f9da4ac4fcf9650385305051a940003b567f0105
Zhonglin Han <hanzlfs@users.noreply.github.com>
2025-05-28 13:51:53 -0700

    Update collective_matmul_mgpu.py

--- jax/experimental/pallas/ops/gpu/collective_matmul_mgpu.py
+++ jax/experimental/pallas/ops/gpu/collective_matmul_mgpu.py
@@ -42,6 +42,7 @@ def all_gather_lhs_matmul(
     block_n: int,
     block_k: int,
     max_concurrent_steps: int,
+    dtype: jnp.dtype = jnp.float16,
 ) -> jax.Array:
   if (num_devices := jax.device_count()) != jax.process_count():
     raise ValueError("The kernel only supports one device per process")
@@ -121,7 +122,7 @@ def all_gather_lhs_matmul(
         @functools.partial(
             pl.run_scoped,
             acc_ref=plgpu.ACC((block_m, block_n)),
-            out_smem=plgpu.SMEM((block_m, block_n), jnp.float16, transforms=transforms),
+            out_smem=plgpu.SMEM((block_m, block_n), dtype, transforms=transforms),
         )
         def _(acc_ref, out_smem):
           pl.semaphore_wait(capacity_sem)
@@ -173,8 +174,8 @@ def all_gather_lhs_matmul(
 
   result, _ = plgpu.kernel(
       kernel_body,
-      out_shape=[jax.ShapeDtypeStruct((axis_size * m_shard, n_shard), jnp.float16),
-                  jax.ShapeDtypeStruct((num_sms, 2, block_m, k), jnp.float16)],
+      out_shape=[jax.ShapeDtypeStruct((axis_size * m_shard, n_shard), dtype),
+                  jax.ShapeDtypeStruct((num_sms, 2, block_m, k), dtype)],
       scratch_shapes=[
           plgpu.SemaphoreType.REGULAR, plgpu.SemaphoreType.REGULAR,
       ],
