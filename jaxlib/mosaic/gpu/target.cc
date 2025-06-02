bf37ab2510c51a71e4a1061d0289204f56fb36bb
jax authors <google-ml-automation@google.com>
2025-06-02 02:18:11 -0700

    Automated Code Change
    PiperOrigin-RevId: 766096798

--- jaxlib/mosaic/gpu/target.cc
+++ jaxlib/mosaic/gpu/target.cc
@@ -16,7 +16,6 @@ limitations under the License.
 
 #include <memory>
 #include <string>
-#include <utility>
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
@@ -25,6 +24,8 @@ limitations under the License.
 #include "absl/strings/strip.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
+#include "absl/strings/string_view.h"
+#include "absl/strings/strip.h"
 #include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/MC/TargetRegistry.h"
 
