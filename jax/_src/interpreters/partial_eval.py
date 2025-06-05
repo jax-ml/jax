bcd52e839c677cd256d9f9c3461a19a66bf1369e
Matthew Johnson <mattjj@google.com>
2025-06-05 11:14:54 -0700

    skip pytype on slow file
    PiperOrigin-RevId: 767687785

--- jax/_src/interpreters/partial_eval.py
+++ jax/_src/interpreters/partial_eval.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# pytype: skip-file
 from __future__ import annotations
 
 from collections import namedtuple
