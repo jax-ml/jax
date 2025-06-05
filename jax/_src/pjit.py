9f7e802980fdaed415f0513e1f0ab331cb646b91
Emily Fertig <emilyaf@google.com>
2025-06-04 17:20:25 -0700

    [Rollback] Roll-forward with fix and test: prototype of cross-host device transfers for TFRT TPU.
    Reverts 8d8cc2bca67fc75718b73337c9ce19d6b77065e9
    
    PiperOrigin-RevId: 767348122

--- jax/_src/pjit.py
+++ jax/_src/pjit.py
@@ -414,8 +414,10 @@ def _parse_jit_arguments(fun: Callable, *, in_shardings: Any,
   if backend is not None or device is not None:
     warnings.warn(
         'backend and device argument on jit is deprecated. You can use'
-        ' `jax.device_put(..., jax.local_devices("cpu")[0])` on the inputs to'
-        ' the jitted function to get the same behavior.', DeprecationWarning)
+        ' `jax.device_put(..., jax.local_devices(backend="cpu")[0])` on the'
+        ' inputs to the jitted function to get the same behavior.',
+        DeprecationWarning,
+    )
     if device is not None and backend is not None:
       raise ValueError("can't specify both a device and a backend for jit, "
                        f"got {device=} and {backend=}")
