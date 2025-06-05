a8ce17c9c745ed0f6be46cac6492c474859db5ad
Dimitar (Mitko) Asenov <dasenov@google.com>
2025-06-05 07:31:57 -0700

    [Mosaic GPU] Handle `None` transforms in `swizzle_and_transforms_from_transforms_attr`
    PiperOrigin-RevId: 767598481

--- jax/experimental/mosaic/gpu/transform_inference.py
+++ jax/experimental/mosaic/gpu/transform_inference.py
@@ -375,16 +375,6 @@ def _infer_memref_cast_transforms(
   return [transforms], [transforms]
 
 
-def _should_have_transforms(op: ir.OpView) -> bool:
-  """Returns 'True' if the operation should be assigned in/out transforms."""
-  return any(
-      map(
-          inference_utils.is_transformable_smem_memref,
-          itertools.chain(op.operands, op.results),
-      )
-  )
-
-
 def infer_transforms(module: ir.Module):
   """Infers transforms for the given module.
 
@@ -398,7 +388,7 @@ def infer_transforms(module: ir.Module):
   annotate the same memref.
   """
   def inference_step(op: ir.Operation):
-    if not _should_have_transforms(op):
+    if not inference_utils.should_have_transforms(op):
       return
     elif inference_rule := _transform_inference_rules.get(op.OPERATION_NAME, None):  # pytype: disable=attribute-error
       pass
