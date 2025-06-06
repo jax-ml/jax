a22873cce24252fccdbf43ed7ab4966a2859869d
Mythereus <136450574+MythicArrow@users.noreply.github.com>
2025-06-06 12:15:50 +0200

    Updated tests according to the new differentiation rules
    For the new diff type see the previous commit

--- tests/nn_test.py
+++ tests/nn_test.py
@@ -475,14 +475,20 @@ class NNFunctionsTest(jtu.JaxTestCase):
      expected = jnp.where(x >= 0, x, a * x) 
       
      y = nn.prelu(x, a) 
-     self.assertAllClose(y, expected,check_dtypes=False) 
+     self.assertTrue(jnp.allclose(y, expected, rtol=1e-6, atol=1e-6))
   
      y_scalar = nn.prelu(x, 0.1) 
-     self.assertAllClose(y_scalar, expected,check_dtypes=False) 
+     self.assertTrue(jnp.allclose(y_scalar, expected, rtol=1e-6, atol=1e-6))
     
-  def testPreluGrad(x, a): 
-     return jnp.sum(nn.prelu(x, a)) 
-     check_grads(testprelugrad, (x, a), order=1) 
+  def testPreluGrad(self):
+    x = jnp.array([[1.0, -1.0, 0.0, -2.0]], dtype=jnp.float32)
+        a = jnp.array([0.1], dtype=jnp.float32)
+
+        def sum(x, a):
+            return jnp.sum(nn.prelu(x, a))
+
+        check_grads(sum, (x, a), order=1, modes=["fwd", "rev"])
+    
       
   def testSoftplusValue(self):
     val = nn.softplus(89.)
