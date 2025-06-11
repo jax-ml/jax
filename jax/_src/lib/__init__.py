004b54807f2c4d303515f81c5c483d864790d018
jax authors <google-ml-automation@google.com>
2025-06-11 13:40:36 -0700

    Add alternative location of `CUDA_ROOT` for Bazel build/tests with hermetic CUDA.
    PiperOrigin-RevId: 770300064

--- jax/_src/lib/__init__.py
+++ jax/_src/lib/__init__.py
@@ -172,9 +172,21 @@ def _cuda_path() -> str | None:
 
     return str(cuda_nvcc_path)
 
+  def _try_bazel_runfiles() -> str | None:
+    """Try to get the path to the cuda installation in bazel runfiles."""
+    python_runfiles = os.environ.get('PYTHON_RUNFILES')
+    if not python_runfiles:
+      return None
+    cuda_nvcc_root = os.path.join(python_runfiles, 'cuda_nvcc')
+    if os.path.exists(cuda_nvcc_root):
+      return cuda_nvcc_root
+    return None
+
   if (path := _try_cuda_root_environment_variable()) is not None:
     return path
   elif (path := _try_cuda_nvcc_import()) is not None:
+    return path
+  elif (path := _try_bazel_runfiles()) is not None:
     return path
 
   return None
