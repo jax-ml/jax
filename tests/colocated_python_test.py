19f34a06e54dbe10d140828112a373498533c0d0
Hyeontaek Lim <hyeontaek@google.com>
2025-06-17 16:08:21 -0700

    [JAX] Remove sleeping from colocated Python execution tests
    Using `time.sleep()` and elapsed time measurement for validating
    sequential/concurrent executions seems unreliable in some test environment, and
    they also lengthen the test time of successful cases. This change replaces the
    time-based tests with synchronization-based tests so that no time delay or
    elapsed time measurement is needed.
    
    PiperOrigin-RevId: 772661473

--- tests/colocated_python_test.py
+++ tests/colocated_python_test.py
@@ -16,7 +16,6 @@ import base64
 import struct
 import tempfile
 import threading
-import time
 
 from absl.testing import absltest
 from absl.testing import parameterized
@@ -234,75 +233,101 @@ class ColocatedPythonTest(jtu.JaxTestCase):
     cpu_devices = colocated_python.colocated_cpu_devices(jax.local_devices())
     x = np.array(1)
     x = jax.device_put(x, cpu_devices[0])
-    # Make sure that this input array is ready for use by the colocated Python
-    # function and does not disrupt elapsed time measurement.
-    jax.block_until_ready(x)
 
     @colocated_python.colocated_python
-    def sleep(x: jax.Array) -> jax.Array:
-      time.sleep(5)
+    def func0(x: jax.Array) -> jax.Array:
+      colocated_python._testing_global_state = 100
       return x
 
-    # Specify out_specs_fn so that all executions are asynchronously dispatched.
-    sleep = sleep.specialize(out_specs_fn=lambda x: x)
+    @colocated_python.colocated_python
+    def func1(x: jax.Array) -> jax.Array:
+      assert "_testing_global_state" in colocated_python.__dict__
+      assert colocated_python._testing_global_state == 100
+      colocated_python._testing_global_state += 1
+      return x
 
-    def sleep_twice_and_wait(x: jax.Array) -> None:
-      _ = sleep(x)
-      jax.block_until_ready(sleep(x))
+    @colocated_python.colocated_python
+    def func2(x: jax.Array) -> jax.Array:
+      assert "_testing_global_state" in colocated_python.__dict__
+      assert colocated_python._testing_global_state == 101
+      return x
 
-    start_time = time.time()
+    @colocated_python.colocated_python
+    def cleanup():
+      if "_testing_global_state" in colocated_python.__dict__:
+        del colocated_python._testing_global_state
 
-    # Two executions of `sleep` within `sleep_twice_and_wait` should run
-    # sequentially.
-    if on_main_thread:
-      sleep_twice_and_wait(x)
-    else:
-      t = threading.Thread(target=sleep_twice_and_wait, args=(x,))
-      t.start()
-      t.join()
+    # Specify out_specs_fn so that their executions are asynchronously
+    # dispatched.
+    func0 = func0.specialize(out_specs_fn=lambda x: x)
+    func1 = func1.specialize(out_specs_fn=lambda x: x)
+    func2 = func2.specialize(out_specs_fn=lambda x: x)
 
-    elapsed_time = time.time() - start_time
+    # cleanup needs specialization because they do not have input arguments.
+    cleanup = cleanup.specialize(devices=cpu_devices[:1])
 
-    # If sequential execution did not happen, elapsed time typically will be
-    # around 5 seconds.
-    self.assertGreaterEqual(elapsed_time, 10)
+    def calls(x: jax.Array) -> None:
+      # No explicit blocking before making the next call.
+      func0(x)
+      func1(x)
+      jax.block_until_ready(func2(x))
+
+    try:
+      # Executions in `calls` should run sequentially.
+      if on_main_thread:
+        calls(x)
+      else:
+        t = threading.Thread(target=calls, args=(x,))
+        t.start()
+        t.join()
+      # Executions should succeed without an error.
+    finally:
+      cleanup()
 
   def test_concurrent_execution(self):
     cpu_devices = colocated_python.colocated_cpu_devices(jax.local_devices())
     x = np.array(1)
     x = jax.device_put(x, cpu_devices[0])
-    # Make sure that this input array is ready for use by the colocated Python
-    # function and does not disrupt elapsed time measurement.
-    jax.block_until_ready(x)
 
     @colocated_python.colocated_python
-    def sleep(x: jax.Array) -> jax.Array:
-      time.sleep(5)
+    def init(x: jax.Array) -> jax.Array:
+      colocated_python._testing_global_state = threading.Barrier(3)
       return x
 
-    # Specify out_specs_fn so that all executions are asynchronously dispatched.
-    sleep = sleep.specialize(out_specs_fn=lambda x: x)
+    @colocated_python.colocated_python
+    def func(x: jax.Array) -> jax.Array:
+      assert "_testing_global_state" in colocated_python.__dict__
+      colocated_python._testing_global_state.wait(timeout=5)
+      return x
 
-    def sleep_and_wait(x: jax.Array) -> None:
-      jax.block_until_ready(sleep(x))
+    @colocated_python.colocated_python
+    def cleanup():
+      if "_testing_global_state" in colocated_python.__dict__:
+        del colocated_python._testing_global_state
 
-    start_time = time.time()
+    # Specify out_specs_fn so that their executions are asynchronously
+    # dispatched.
+    func = func.specialize(out_specs_fn=lambda x: x)
 
-    # All three executions of `sleep_and_wait` should run concurrently.
-    t1 = threading.Thread(target=sleep_and_wait, args=(x,))
-    t2 = threading.Thread(target=sleep_and_wait, args=(x,))
-    t1.start()
-    t2.start()
-    sleep_and_wait(x)
-    t1.join()
-    t2.join()
+    # cleanup needs specialization because they do not have input arguments.
+    cleanup = cleanup.specialize(devices=cpu_devices[:1])
 
-    elapsed_time = time.time() - start_time
+    try:
+      jax.block_until_ready(init(x))
 
-    self.assertGreaterEqual(elapsed_time, 5)
-    # If concurrent execution did not happen, elapsed time typically will be
-    # around 15 seconds.
-    self.assertLess(elapsed_time, 10)
+      # All func calls should run concurrently and enter/exit the barrier.
+      t1 = threading.Thread(target=func, args=(x,))
+      t2 = threading.Thread(target=func, args=(x,))
+      t3 = threading.Thread(target=func, args=(x,))
+      t1.start()
+      t2.start()
+      t3.start()
+      t1.join()
+      t2.join()
+      t3.join()
+      # Executions should succeed without a deadlock.
+    finally:
+      cleanup()
 
   def test_inputs_with_different_device_orders(self):
     cpu_devices = colocated_python.colocated_cpu_devices(jax.local_devices())[:2]
