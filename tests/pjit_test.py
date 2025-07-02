jax-ml/jax

commit d241f6d6e9c486bc0eae22b035511afa38897d93

Add out_sharding to `jnp.arange`
PiperOrigin-RevId: 778253718

jax/_src/numpy/lax_numpy.py
@@ -31,7 +31,7 @@ from functools import partial
 import math
 import operator
 import os
-from typing import (Any, IO, Literal, Protocol, TypeVar, Union, overload)
+from typing import Any, IO, Literal, Protocol, TypeVar, Union, overload
 import warnings
 
 import numpy as np
@@ -5882,7 +5882,8 @@ def identity(n: DimSize, dtype: DTypeLike | None = None) -> Array:
 @export
 def arange(start: ArrayLike | DimSize, stop: ArrayLike | DimSize | None = None,
            step: ArrayLike | None = None, dtype: DTypeLike | None = None,
-           *, device: xc.Device | Sharding | None = None) -> Array:
+           *, device: xc.Device | Sharding | None = None,
+           out_sharding: NamedSharding | P | None = None) -> Array:
   """Create an array of evenly-spaced values.
 
   JAX implementation of :func:`numpy.arange`, implemented in terms of
@@ -5909,6 +5910,10 @@ def arange(start: ArrayLike | DimSize, stop: ArrayLike | DimSize | None = None,
       be determined via type promotion of `start`, `stop`, and `step`.
     device: (optional) :class:`~jax.Device` or :class:`~jax.sharding.Sharding`
       to which the created array will be committed.
+    out_sharding: (optional) :class:`~jax.NamedSharding` or :class:`~jax.P` to
+      which the created array will be committed. Prefer using the `out_sharding`,
+      argument if using explicit sharding
+      (https://docs.jax.dev/en/latest/notebooks/explicit-sharding.html)
 
   Returns:
     Array of evenly-spaced values from ``start`` to ``stop``, separated by ``step``.
@@ -5951,16 +5956,19 @@ def arange(start: ArrayLike | DimSize, stop: ArrayLike | DimSize | None = None,
     - :func:`jax.numpy.linspace`: generate a fixed number of evenly-spaced values.
     - :func:`jax.lax.iota`: directly generate integer sequences in XLA.
   """
-  # TODO(vfdev-5): optimize putting the array directly on the device specified
-  # instead of putting it on default device and then on the specific device
-  output = _arange(start, stop=stop, step=step, dtype=dtype)
-  if device is not None:
-    return api.device_put(output, device=device)
-  return output
+  sharding = util.choose_device_or_out_sharding(
+      device, out_sharding, 'jnp.arange')
+  if sharding is None or isinstance(sharding, NamedSharding):
+    return _arange(start, stop=stop, step=step, dtype=dtype,
+                   out_sharding=sharding)
+  else:
+    output = _arange(start, stop=stop, step=step, dtype=dtype)
+    return api.device_put(output, sharding)
 
 
 def _arange(start: ArrayLike | DimSize, stop: ArrayLike | DimSize | None = None,
-            step: ArrayLike | None = None, dtype: DTypeLike | None = None) -> Array:
+            step: ArrayLike | None = None, dtype: DTypeLike | None = None,
+            out_sharding: NamedSharding | None = None) -> Array:
   dtypes.check_user_dtype_supported(dtype, "arange")
   if not config.dynamic_shapes.value:
     util.check_arraylike("arange", start)
@@ -5993,11 +6001,13 @@ def _arange(start: ArrayLike | DimSize, stop: ArrayLike | DimSize | None = None,
         not dtypes.issubdtype(start_dtype, dtypes.extended)):
       ceil_ = ufuncs.ceil if isinstance(start, core.Tracer) else np.ceil
       start = ceil_(start).astype(int)
-    return lax.iota(dtype, start)  # type: ignore[arg-type]
+    return lax.broadcasted_iota(dtype, (start,), 0, out_sharding=out_sharding)  # type: ignore[arg-type]
   else:
     if step is None and start == 0 and stop is not None:
-      return lax.iota(dtype, np.ceil(stop).astype(int))
-    return array(np.arange(start, stop=stop, step=step, dtype=dtype))
+      return lax.broadcasted_iota(dtype, (np.ceil(stop).astype(int),), 0,
+                                  out_sharding=out_sharding)
+    return array(np.arange(start, stop=stop, step=step, dtype=dtype),
+                 device=out_sharding)
 
 
 def _arange_dynamic(

tests/pjit_test.py
@@ -8110,6 +8110,35 @@ class ShardingInTypesTest(jtu.JaxTestCase):
     out = jax.jit(jax.vmap(f))(xs)
     self.assertEqual(out.sharding, NamedSharding(mesh, P(('x', 'y'), None)))
 
+  @jtu.with_explicit_mesh((2,), 'x')
+  def test_jnp_arange_out_sharding(self, mesh):
+    s = NamedSharding(mesh, P('x'))
+
+    out = jnp.arange(8, dtype=jnp.float32, out_sharding=s)
+    self.assertEqual(out.sharding, s)
+    self.assertArraysEqual(out, np.arange(8, dtype=np.float32))
+
+    out = jnp.arange(8, dtype=jnp.float32, out_sharding=P('x'))
+    self.assertEqual(out.sharding, s)
+    self.assertArraysEqual(out, np.arange(8, dtype=np.float32))
+
+    out = jnp.arange(start=8, stop=16, dtype=jnp.float32, out_sharding=P('x'))
+    self.assertEqual(out.sharding, s)
+    self.assertArraysEqual(out, np.arange(start=8, stop=16, dtype=np.float32))
+
+    out = jnp.arange(start=8, stop=16, step=2, dtype=jnp.float32,
+                     out_sharding=P('x'))
+    self.assertEqual(out.sharding, s)
+    self.assertArraysEqual(out, np.arange(start=8, stop=16, step=2,
+                                          dtype=np.float32))
+
+    @jax.jit
+    def f():
+      return jnp.arange(8, dtype=np.float32, out_sharding=P('x'))
+    out = f()
+    self.assertEqual(out.sharding, s)
+    self.assertArraysEqual(out, np.arange(8, dtype=np.float32))
+
 
 @jtu.pytest_mark_if_available('multiaccelerator')
 class PJitErrorTest(jtu.JaxTestCase):

jax/_src/numpy/util.py
@@ -325,7 +325,7 @@ def normalize_device_to_sharding(device: xc.Device | Sharding | None) -> Shardin
 
 def choose_device_or_out_sharding(device: xc.Device | Sharding | None,
                                   out_sharding: NamedSharding | P | None,
-                                  name: str):
+                                  name: str) -> Sharding | NamedSharding | None:
   if device is not None and out_sharding is not None:
     raise ValueError(
         f"Only one of `device` or `out_sharding` can be set. Got {device=} and"

jax/numpy/__init__.pyi
@@ -135,6 +135,7 @@ def arange(
     step: ArrayLike | None = ...,
     dtype: DTypeLike | None = ..., *,
     device: _Device | _Sharding | None = ...,
+    out_sharding: NamedSharding | P | None = ...,
 ) -> Array: ...
 def arccos(x: ArrayLike, /) -> Array: ...
 def arccosh(x: ArrayLike, /) -> Array: ...

commit 778a95b4a34e7595502c07c6a7a9b05bf4aba275

Fix a bug introduced in https://github.com/jax-ml/jax/pull/29910. Only take the `out_sharding` path in `_arange` if the sharding is not concrete.
PiperOrigin-RevId: 778297212

jax/_src/numpy/lax_numpy.py
@@ -5911,8 +5911,8 @@ def arange(start: ArrayLike | DimSize, stop: ArrayLike | DimSize | None = None,
     device: (optional) :class:`~jax.Device` or :class:`~jax.sharding.Sharding`
       to which the created array will be committed.
     out_sharding: (optional) :class:`~jax.NamedSharding` or :class:`~jax.P` to
-      which the created array will be committed. Prefer using the `out_sharding`,
-      argument if using explicit sharding
+      which the created array will be committed. Use `out_sharding` argument,
+      if using explicit sharding
       (https://docs.jax.dev/en/latest/notebooks/explicit-sharding.html)
 
   Returns:
@@ -5958,7 +5958,8 @@ def arange(start: ArrayLike | DimSize, stop: ArrayLike | DimSize | None = None,
   """
   sharding = util.choose_device_or_out_sharding(
       device, out_sharding, 'jnp.arange')
-  if sharding is None or isinstance(sharding, NamedSharding):
+  if sharding is None or not sharding._is_concrete:
+    assert sharding is None or isinstance(sharding, NamedSharding)
     return _arange(start, stop=stop, step=step, dtype=dtype,
                    out_sharding=sharding)
   else:

tests/pjit_test.py
@@ -3767,6 +3767,11 @@ class ArrayPjitTest(jtu.JaxTestCase):
     self.assertEqual(out2.devices(), {jax.devices()[0]})
     self.assertArraysEqual(out2, np_inp)
 
+  def test_jnp_arange_concrete_sharding(self):
+    mesh = jtu.create_mesh((2,), 'x')
+    out = jnp.arange(8, device=NamedSharding(mesh, P('x')))
+    self.assertEqual(out.sharding, NamedSharding(mesh, P('x')))
+
   def test_jit_submhlo_cached(self):
     @jax.jit
     def nest(x):