5fd02cfc89e86fc62768abed46ba2da3092a40a7
Benjamin Chetioui <bchetioui@google.com>
2025-07-18 01:40:39 -0700

    [Pallas/Mosaic GPU] Add a deviceless export test for Pallas/Mosaic GPU.
    PiperOrigin-RevId: 784490639

--- tests/pallas/export_pallas_test.py
+++ tests/pallas/export_pallas_test.py
@@ -14,13 +14,16 @@
 
 """Test exporting Pallas kernels."""
 
+import contextlib
 import sys
 
 from absl.testing import absltest
 import jax
 from jax import export
 from jax._src import test_util as jtu
+from jax._src.pallas import pallas_call as pallas_call_lib
 from jax.experimental import pallas as pl
+
 import numpy as np
 try:
   from jax._src.lib import triton
@@ -31,7 +34,7 @@ except ImportError:
 jax.config.parse_flags_with_absl()
 
 
-class ExportTest(jtu.JaxTestCase):
+class ExportTestWithTriton(jtu.JaxTestCase):
 
   def setUp(self):
     if sys.platform == "win32":
@@ -39,10 +42,12 @@ class ExportTest(jtu.JaxTestCase):
 
     super().setUp()
 
+  def _check_cuda_export(self, exp):
+    self.assertRegex(
+        exp.mlir_module(),
+        r"stablehlo.custom_call @__gpu\$xla\.gpu\.triton.+name\s*=\s*\"my_custom_kernel_name\"")
+
   def test_cross_platform(self):
-    # TODO(apaszke): Remove after 12 weeks have passed.
-    if not jtu.if_cloud_tpu_at_least(2024, 12, 19):
-      self.skipTest("Requires libtpu built after 2024-12-19")
     def add_vectors_kernel(x_ref, y_ref, o_ref):
       x, y = x_ref[...], y_ref[...]
       o_ref[...] = x + y
@@ -83,9 +88,21 @@ class ExportTest(jtu.JaxTestCase):
           exp.mlir_module(),
           r"stablehlo.custom_call @tpu_custom_call.+kernel_name\s*=\s*\"my_custom_kernel_name\"")
     if "cuda" in platforms:
-      self.assertRegex(
-          exp.mlir_module(),
-          r"stablehlo.custom_call @__gpu\$xla\.gpu\.triton.+name\s*=\s*\"my_custom_kernel_name\"")
+      self._check_cuda_export(exp)
+
+
+class ExportTestWithMosaicGpu(ExportTestWithTriton):
+
+  def setUp(self):
+    context_stack = contextlib.ExitStack()
+    context_stack.enter_context(pallas_call_lib._PALLAS_USE_MOSAIC_GPU(True))
+    self.addCleanup(context_stack.close)
+    super().setUp()
+
+  def _check_cuda_export(self, exp):
+    self.assertRegex(
+        exp.mlir_module(),
+        r"stablehlo.custom_call @mosaic_gpu.+name\s*=\s*\\22my_custom_kernel_name\\22")
 
 
 if __name__ == '__main__':
