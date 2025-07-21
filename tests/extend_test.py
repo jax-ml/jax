e577ef39a4d526910749fe63b0225b814a9a7deb
Daniel Suo <dsuo@google.com>
2025-07-21 11:56:32 -0700

    [jex] Add `proto` <-> `xla_client.HloSharding` and `proto -> `xla_client.OpSharding` APIs to jex.
    Part of work to finalize `xla_client.HloSharding` and `xla_client.OpSharding` deprecations ahead of the JAX 0.7.0 release.
    
    PiperOrigin-RevId: 785521424

--- tests/extend_test.py
+++ tests/extend_test.py
@@ -25,6 +25,7 @@ from jax._src import prng
 from jax._src import test_util as jtu
 from jax._src import xla_bridge
 from jax._src.interpreters import mlir
+from jax._src.lib import xla_client
 
 jax.config.parse_flags_with_absl()
 
@@ -103,6 +104,21 @@ class MlirRegisterLoweringTest(jtu.JaxTestCase):
         "Registering an MLIR lowering rule for primitive .+ for an unknown "
         "platform foo. Known platforms are: .+."):
       mlir.register_lowering(prim=None, rule=None, platform="foo")
+
+
+class ShardingTest(jtu.JaxTestCase):
+  def test_hlo_sharding_roundtrip(self):
+    proto = xla_client.OpSharding()
+    hlo_sharding = xla_client.HloSharding.from_proto(proto)
+    serialized_proto = jex.sharding.get_serialized_proto_from_hlo_sharding(
+        hlo_sharding
+    )
+    self.assertIsInstance(serialized_proto, bytes)
+    deserialized_hlo_sharding = jex.sharding.get_hlo_sharding_from_serialized_proto(
+        serialized_proto
+    )
+    self.assertIsInstance(deserialized_hlo_sharding, xla_client.HloSharding)
+    self.assertEqual(hlo_sharding, deserialized_hlo_sharding)
 
 
 if __name__ == "__main__":
