5a521247ea8c20fdaab1316de5b558c58dbbc054
Michael Hudgins <michaelhudgins@google.com>
2025-07-22 06:27:19 -0700

    Bump libtpu version before release.
    PiperOrigin-RevId: 785829885

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ _current_jaxlib_version = '0.6.2'
 # The following should be updated after each new jaxlib release.
 _latest_jaxlib_version_on_pypi = '0.6.2'
 
-_libtpu_version = '0.0.17.*'
+_libtpu_version = '0.0.19.*'
 
 def load_version_module(pkg_path):
   spec = importlib.util.spec_from_file_location(
