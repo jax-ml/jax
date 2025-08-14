4dbe827bf4a7e14c0c8582ffe195dfcff66e9399
Chase Riley Roberts <chaser@nvidia.com>
2025-08-13 17:06:11 -0700

    Cleanup and comments

--- jax/experimental/timeline/collective_matmul_benchmarks.py
+++ jax/experimental/timeline/collective_matmul_benchmarks.py
@@ -32,14 +32,18 @@ def gemm(a, b):
 @partial(jax.shard_map, mesh=mesh,
          in_specs=(P('data', None), P('data', None)),
          out_specs=P('data', None))
-def check_sync(a, b):
-    with timeline.Timeline() as t:
-        tc = t.launch(compute_on("gpu_stream:1")(gemm)(a, b))
-        ta = t.launch(ring(a))
-        a = t.finish(ta)
-        c = t.finish(tc)
-        return a, c
-        
+def collective_matmul_normal(a, b):
+  c = jnp.zeros((num_devices, a.shape[0], b.shape[1]))
+  idx = jax.lax.axis_index('data')
+  for i in range(num_devices):
+    c_part = compute_on(f"gpu_stream:{(i%2) + 1}")(gemm)(a, b)
+    if i != num_devices - 1:
+      b = ring(b)
+    c = update_slice(c, c_part, (idx + i - 1) % num_devices)
+  # Final reshape turns it back into a matrix
+  return c.reshape((-1, c.shape[-1]))
+
+
 
 @jax.jit
 @partial(jax.shard_map, mesh=mesh,
@@ -69,18 +73,33 @@ def collective_matmul(a, b):
   c_part_fut = None
   c = jnp.zeros((num_devices, a.shape[0], b.shape[1]))
   idx = jax.lax.axis_index('data')
+  
+  # Setup a control dependency timeline.
   with timeline.Timeline() as t:
+      
     for i in range(num_devices):
-      c_part_fut_tmp = t.launch(compute_on(f"gpu_stream:{(i%2) + 1}")(gemm)(a, b))
+      # Launch the communication first
       if i != num_devices - 1:
         b_fut = t.launch(ring(b))
+      
+      # Launch the gemm.
+      c_part_fut_tmp = t.launch(
+          compute_on(f"gpu_stream:{(i%2) + 1}")(gemm)(a, b))
+
+      # If we have a part form the last iteration, 
+      # use that and update the final c.
       if c_part_fut:
         c_part = t.finish(c_part_fut)
         c = c.at[(idx + i - 1) % num_devices].set(c_part)
+
+      # Use the tmp c_part from earlier in the ext iteration.
       c_part_fut = c_part_fut_tmp
       if i != num_devices - 1:
         b = t.finish(b_fut)
+
+    # Use the last piece of c before exiting the timeline.
     c = c.at[(idx - 1) % num_devices].set(t.finish(c_part_fut))
+  
   # Final reshape turns it back into a matrix
   return c.reshape((-1, c.shape[-1]))
 
@@ -90,11 +109,17 @@ a = jax.device_put(a, s)
 b = jnp.ones((2048 * 8 * 4, 2048 * 4), dtype=jnp.bfloat16)
 b = jax.device_put(b, s)
 
-print("Running timeline model")
+print("Running scheduling group model")
 for _ in range(10):
-  collective_matmul(a, b).block_until_ready()
+  collective_matmul_normal(a, b).block_until_ready()
 
 print("Running scheduling group model")
 for _ in range(10):
   collective_matmul_groups(a, b).block_until_ready()
+
+
+print("Running timeline model")
+for _ in range(10):
+  collective_matmul(a, b).block_until_ready()
+
 
