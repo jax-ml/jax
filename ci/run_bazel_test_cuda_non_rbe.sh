6db891b4a02ff965aded1fdf1f376c13fe252893
Yulia Baturina <ybaturina@google.com>
2025-08-14 15:24:07 -0700

    Add wheel build and tests in JAX presubmit.
    The jobs are needed to replicate postsubmit Github Workflow actions and catch the majority of the issues in the presubmit.
    
    The tests are executed if PR has label `CI Optional Wheels Presubmit`.
    
    PiperOrigin-RevId: 795222280

--- ci/run_bazel_test_cuda_non_rbe.sh
+++ ci/run_bazel_test_cuda_non_rbe.sh
@@ -92,6 +92,11 @@ if [[ "$driver_major_version" -lt "580" ]]; then
   TEST_CONFIG="$TEST_CONFIG --@cuda_driver//:enable_forward_compatibility=true"
 fi
 
+if [[ "$JAXCI_BUILD_JAXLIB" != "true" ]]; then
+  cuda_libs_flag="--config=cuda_libraries_from_stubs"
+else
+  cuda_libs_flag="--@local_config_cuda//cuda:override_include_cuda_libs=true"
+fi
 
 # Don't abort the script if one command fails to ensure we run both test
 # commands below.
@@ -106,6 +111,7 @@ bazel test --config=$TEST_CONFIG \
       --config=use_tar_archive_files \
       --repo_env=HERMETIC_PYTHON_VERSION="$JAXCI_HERMETIC_PYTHON_VERSION" \
       --@rules_python//python/config_settings:py_freethreaded="$FREETHREADED_FLAG_VALUE" \
+      --override_repository=xla="${JAXCI_XLA_GIT_DIR}" \
       --//jax:build_jaxlib=$JAXCI_BUILD_JAXLIB \
       --//jax:build_jax=$JAXCI_BUILD_JAX \
       --test_env=XLA_PYTHON_CLIENT_ALLOCATOR=platform \
@@ -122,9 +128,10 @@ bazel test --config=$TEST_CONFIG \
       --action_env=JAX_ENABLE_X64="$JAXCI_ENABLE_X64" \
       --action_env=NCCL_DEBUG=WARN \
       --color=yes \
-      --config=cuda_libraries_from_stubs \
+      $cuda_libs_flag \
       //tests:gpu_tests //tests:backend_independent_tests \
       //tests/pallas:gpu_tests //tests/pallas:backend_independent_tests
+
 
 # Store the return value of the first bazel command.
 first_bazel_cmd_retval=$?
@@ -136,6 +143,7 @@ bazel test --config=$TEST_CONFIG \
       --config=use_tar_archive_files \
       --repo_env=HERMETIC_PYTHON_VERSION="$JAXCI_HERMETIC_PYTHON_VERSION" \
       --@rules_python//python/config_settings:py_freethreaded="$FREETHREADED_FLAG_VALUE" \
+      --override_repository=xla="${JAXCI_XLA_GIT_DIR}" \
       --//jax:build_jaxlib=$JAXCI_BUILD_JAXLIB \
       --//jax:build_jax=$JAXCI_BUILD_JAX \
       --test_env=XLA_PYTHON_CLIENT_ALLOCATOR=platform \
@@ -148,7 +156,7 @@ bazel test --config=$TEST_CONFIG \
       --action_env=JAX_ENABLE_X64="$JAXCI_ENABLE_X64" \
       --action_env=NCCL_DEBUG=WARN \
       --color=yes \
-      --config=cuda_libraries_from_stubs \
+      $cuda_libs_flag \
       //tests:gpu_tests //tests/pallas:gpu_tests
 
 # Store the return value of the second bazel command.
