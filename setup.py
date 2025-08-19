5712de44e97c455faed1fd45532e821ca66d025a
Jake Harmon <jakeharmon@google.com>
2025-08-19 16:20:05 +0000

    Prepare for JAX release 0.7.1

--- setup.py
+++ setup.py
@@ -19,11 +19,11 @@ from setuptools import setup, find_packages
 
 project_name = 'jax'
 
-_current_jaxlib_version = '0.7.0'
+_current_jaxlib_version = '0.7.1'
 # The following should be updated after each new jaxlib release.
 _latest_jaxlib_version_on_pypi = '0.7.0'
 
-_libtpu_version = '0.0.19.*'
+_libtpu_version = '0.0.20.*'
 
 def load_version_module(pkg_path):
   spec = importlib.util.spec_from_file_location(
