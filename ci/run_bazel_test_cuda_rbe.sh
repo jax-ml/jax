91817ed1daf41e8024ed23d99579d64a7e737fc4
jax authors <google-ml-automation@google.com>
2025-08-27 13:54:23 -0700

    Replace Bazel CUDA presubmit for `jaxlib=head, python=3.11, x64=0` with `jaxlib=pypi_latest, python=3.11, x64=0`.
    PiperOrigin-RevId: 800161933

--- ci/run_bazel_test_cuda_rbe.sh
+++ ci/run_bazel_test_cuda_rbe.sh
@@ -49,6 +49,7 @@ bazel test --config=rbe_linux_x86_64_cuda \
       --action_env=JAX_ENABLE_X64="$JAXCI_ENABLE_X64" \
       --color=yes \
       --@local_config_cuda//cuda:override_include_cuda_libs=true \
+      --//jax:build_jaxlib=$JAXCI_BUILD_JAXLIB \
       //tests:gpu_tests //tests:backend_independent_tests \
       //tests/pallas:gpu_tests //tests/pallas:backend_independent_tests \
       //jaxlib/tools:jax_cuda_plugin_wheel_size_test \
