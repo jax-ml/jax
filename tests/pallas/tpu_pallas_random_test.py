604c4ecfdbb21a63032f53a60af9a457d721d235
jax authors <google-ml-automation@google.com>
2025-09-08 03:48:01 -0700

    Revert [Pallas TPU] Support lowering extended dtypes
    Reverts e0cc40204afdc065659225897a69afe06991af99
    
    PiperOrigin-RevId: 804357229

--- tests/pallas/tpu_pallas_random_test.py
+++ tests/pallas/tpu_pallas_random_test.py
@@ -255,17 +255,18 @@ class ThreefryTest(parameterized.TestCase):
   def test_pallas_matches_jax_threefry(self, shape, generator_and_dtype):
     generator, dtype = generator_and_dtype
     def body(key_ref, o_ref):
-      key = key_ref[...]
+      key = jax.random.wrap_key_data(key_ref[...], impl='threefry2x32')
       o_ref[...] = generator(key, shape=o_ref[...].shape)
 
     threefry_key = jax_random.key(0, impl="threefry2x32")
     o_shape = jax.ShapeDtypeStruct(shape, dtype)
     with jax.threefry_partitionable(True):
+      # TODO(justinfu): support passing keys into VMEM.
       result = pl.pallas_call(
           body,
           in_specs=[pl.BlockSpec(memory_space=pltpu.VMEM)],
           out_shape=o_shape,
-      )(threefry_key)
+      )(jax.random.key_data(threefry_key))
       jax_result = generator(threefry_key, shape=o_shape.shape)
     np.testing.assert_array_equal(result, jax_result)
 
