d55d5f7dfc16531a1d2dc6f9d0a3d8c1745cd45a
Daniel Suo <dsuo@google.com>
2025-09-12 12:28:35 -0700

    [multihost_utils] Add PRNGKeyArray support to `host_local_array_to_global_array` and `global_array_to_host_local_array`.
    PiperOrigin-RevId: 806385578

--- jax/experimental/multihost_utils.py
+++ jax/experimental/multihost_utils.py
@@ -31,6 +31,7 @@ from jax._src import array
 from jax._src import sharding_impls
 from jax._src.interpreters import pxla
 from jax._src import pjit as pjit_lib
+from jax._src import prng
 from jax.sharding import PartitionSpec as P
 from jax._src import distributed
 from jax._src.util import safe_zip
@@ -241,6 +242,10 @@ def host_local_array_to_global_array_impl(
   if isinstance(arr, array.ArrayImpl) and isinstance(
       arr.sharding, jax.sharding.PmapSharding):
     arr = np.array(arr)
+  is_prng_key_array = isinstance(arr, prng.PRNGKeyArray)
+  dtype = arr.dtype
+  if is_prng_key_array:
+    arr = arr._base_array
 
   local_sharding = jax.sharding.NamedSharding(global_mesh.local_mesh, pspec)
 
@@ -259,9 +264,12 @@ def host_local_array_to_global_array_impl(
   global_aval = _local_to_global_aval(
       core.ShapedArray(arr.shape, arr.dtype), global_mesh, pspec)
 
-  return pxla.batched_device_put(
+  out = pxla.batched_device_put(
       global_aval, jax.sharding.NamedSharding(global_mesh, pspec),
       arrays, list(global_mesh.local_mesh.devices.flat))
+  if is_prng_key_array:
+    return prng.PRNGKeyArray(dtype._impl, out)
+  return out
 
 
 def host_local_array_to_global_array(
@@ -392,6 +400,10 @@ def global_array_to_host_local_array_impl(
   # If the Array is already fully addressable i.e. host local, return it.
   if isinstance(arr, array.ArrayImpl) and arr.is_fully_addressable:
     return arr
+  is_prng_key_array = isinstance(arr, prng.PRNGKeyArray)
+  dtype = arr.dtype
+  if is_prng_key_array:
+    arr = arr._base_array
 
   global_sharding = jax.sharding.NamedSharding(global_mesh, pspec)
   local_sharding = jax.sharding.NamedSharding(global_mesh.local_mesh, pspec)
@@ -404,7 +416,10 @@ def global_array_to_host_local_array_impl(
     else:
       resharded_array = jax.device_put(arr, global_sharding)
       arrays = resharded_array._arrays
-    return array.ArrayImpl(local_aval, local_sharding, arrays, committed=True)
+    out = array.ArrayImpl(local_aval, local_sharding, arrays, committed=True)
+    if is_prng_key_array:
+      return prng.PRNGKeyArray(dtype._impl, out)
+    return out
   else:
     # numpy array can show up here during AD.
     arr = dtypes.canonicalize_value(arr)
