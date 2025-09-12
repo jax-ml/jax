eeb4318e9d2a10824b36450d37ff7862963f7224
Adam Paszke <apaszke@google.com>
2025-09-12 09:40:26 -0700

    [Pallas:MGPU] Allow changing the number of arrivals on cluster barriers
    PiperOrigin-RevId: 806321210

--- jax/_src/pallas/mosaic_gpu/lowering.py
+++ jax/_src/pallas/mosaic_gpu/lowering.py
@@ -310,7 +310,7 @@ def _run_scoped_resource_estimator(
       )
       rs += Resources(
           barrier_counts=collections.Counter(
-              [mgpu.ClusterBarrier(collective_dims, *aval.shape)]
+              [mgpu.ClusterBarrier(collective_dims, aval.dtype.num_arrivals, *aval.shape)]
           )
       )
       continue
@@ -2622,7 +2622,7 @@ def _run_scoped_lowering_rule(
         )
         barrier_ref = alloc_stack.enter_context(
             ctx.module_ctx.reserve_barrier(
-                mgpu.ClusterBarrier(collective_dims, *aval.shape)
+                mgpu.ClusterBarrier(collective_dims, aval.dtype.num_arrivals, *aval.shape)
             )
         )
         input_refs.append(barrier_ref)
