d100afdf5f0adea797dd07682be9fb04a7c11c9e
RJ Skerry-Ryan <rjryan@google.com>
2025-09-24 23:31:21 -0700

    Reverts 4d8b701068d25c84c123481751a0ddc3be08d6ab
    PiperOrigin-RevId: 811200924

--- jax/_src/interpreters/mlir.py
+++ jax/_src/interpreters/mlir.py
@@ -1438,33 +1438,6 @@ def _set_up_aliases(input_output_aliases, avals_in, avals_out,
           xla_donated_args = [False] * len(avals_in)
         xla_donated_args[input_id] = True
 
-  aliased_output_ids = {i for i in input_output_aliases if i is not None}
-
-  results_not_matched = collections.defaultdict(collections.deque)
-  for i, (aval, rm) in enumerate(zip(avals_out, result_memory_kinds)):
-    if i not in aliased_output_ids and aval is not core.abstract_token:
-      results_not_matched[(aval.size, rm)].append(i)
-
-  # For each donated argument that hasn't been aliased or donated to XLA, try to
-  # find an output array with matching size ignoring shapes. If a matching
-  # output array is found, then the argument is donated to XLA.
-  # Similar to the aliasing logic above, an argument is donated to XLA even if
-  # its layout and the output's layout don't match. This is being done to
-  # provide more opportunities for XLA to reuse the donated arguments.
-  for input_idx in range(len(out_donated_args)):
-    # If the argument is not a token and hasn't been aliased or donated to XLA,
-    # then try to find an output array with matching size.
-    if (out_donated_args[input_idx]
-        and avals_in[input_idx] is not core.abstract_token):
-      key = (avals_in[input_idx].size, arg_memory_kinds[input_idx])
-      if results_not_matched.get(key, ()):
-        # XLA donate the argument because there's a matching output array.
-        results_not_matched[key].popleft()
-        out_donated_args[input_idx] = False
-        if xla_donated_args is None:
-          xla_donated_args = [False] * len(avals_in)
-        xla_donated_args[input_idx] = True
-
   return input_output_aliases, out_donated_args, xla_donated_args
 
 Token = ir.Value
