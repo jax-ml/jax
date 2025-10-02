470ea124ffd2b2dc1fbbd76faa7c563ddf4a4930
Adam Paszke <apaszke@google.com>
2025-10-02 07:21:04 -0700

    [Mosaic TPU] Add limited support for third/second minor transposes of packed types
    PiperOrigin-RevId: 814223129

--- jaxlib/mosaic/dialect/tpu/transforms/infer_vector_layout.cc
+++ jaxlib/mosaic/dialect/tpu/transforms/infer_vector_layout.cc
@@ -1826,9 +1826,14 @@ class VectorLayoutInferer {
       }
     }
     Layout required_layout = some_layout;
+    // We have a special limited implementation for packed untiled/tiled swaps.
+    if (untiled_tiled_swap && layout.packing() > 1) {
+      required_layout = VectorLayout(layout.bitwidth(), LayoutOffsets{0, 0},
+                                     {layout.packing(), target_shape_[1]},
+                                     ImplicitDim::kNone);
     // Require native tiling if we're going to use the XLU, or doing a
-    // major/minor permute.
-    if (untiled_tiled_swap ||
+    // major/minor permute on 32-bit data.
+    } else if (untiled_tiled_swap ||
         permutation[permutation.size() - 1] == permutation.size() - 2) {
       auto native_tiling = nativeTiling(layout.bitwidth());
       required_layout = VectorLayout(layout.bitwidth(), LayoutOffsets{0, 0},
