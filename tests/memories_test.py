Fix out_memory_spaces_flat calculation. It was using in_tree before but should have used out_tree instead.
PiperOrigin-RevId: 814487834

tests/memories_test.py
@@ -1055,8 +1055,8 @@ class ComputeOffload(jtu.BufferDonationTestCase):
     np_inp = np.arange(16).reshape(8, 2)
     arr = jax.device_put(np_inp, s)
 
-    @compute_on('device_host')
-    @jax.jit
+    @compute_on2(compute_type='device_host',
+                 out_memory_spaces=jax.memory.Space.Device)
     def g(x, y):
       return x * y
 