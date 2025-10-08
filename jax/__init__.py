870952c659768a1a245320ddd6938c351295f63a
Matthew Johnson <mattjj@google.com>
2025-10-08 04:34:32 +0000

    [mutable-arrays] add jax.freeze, alias for jax.ref.freeze

--- jax/__init__.py
+++ jax/__init__.py
@@ -140,6 +140,7 @@ from jax._src.shard_map import shard_map as shard_map
 from jax._src.shard_map import smap as smap
 
 from jax.ref import new_ref as new_ref
+from jax.ref import freeze as freeze
 from jax.ref import Ref as Ref
 
 # Force import, allowing jax.interpreters.* to be used after import jax.
