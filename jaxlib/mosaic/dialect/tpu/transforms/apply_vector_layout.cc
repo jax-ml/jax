dd5f7a9b7e23de50931797134a59b21dd1e831da
jax authors <google-ml-automation@google.com>
2025-10-07 17:16:56 -0700

    Automated Code Change
    PiperOrigin-RevId: 816443549

--- jaxlib/mosaic/dialect/tpu/transforms/apply_vector_layout.cc
+++ jaxlib/mosaic/dialect/tpu/transforms/apply_vector_layout.cc
@@ -30,8 +30,6 @@ limitations under the License.
 
 #include "absl/algorithm/container.h"
 #include "absl/log/check.h"
-#include "absl/log/log.h"
-#include "absl/status/status.h"
 #include "absl/types/span.h"
 #include "llvm/ADT/APInt.h"
 #include "llvm/ADT/ArrayRef.h"
