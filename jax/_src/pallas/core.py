73f10b97d1d269d9209efaffce51a7a168686f20
Peter Hawkins <phawkins@google.com>
2025-10-08 11:39:45 -0700

    Use a single fixed channel ID for all collectives emitted by JAX.
    PiperOrigin-RevId: 816795241

--- jax/_src/pallas/core.py
+++ jax/_src/pallas/core.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Dummy change to trigger gemax tests.
+
 """Module for pallas-core functionality."""
 from __future__ import annotations
 
