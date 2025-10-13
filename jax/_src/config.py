18f96c9009624d6bec0cade1f514b8f2601f4d6a
Praveen Narayanan <pravnar@google.com>
2025-10-13 16:59:01 -0700

    Set the default value of `jax_ragged_dot_use_ragged_dot_instruction` to True. And lower to chlo.ragged_dot even in the presence of collectives.
    PiperOrigin-RevId: 818893228

--- jax/_src/config.py
+++ jax/_src/config.py
@@ -2286,8 +2286,7 @@ jax_dump_ir_modes = string_flag(
 
 jax_ragged_dot_use_ragged_dot_instruction = bool_state(
     name='jax_ragged_dot_use_ragged_dot_instruction',
-    default=False,
-    upgrade=True,
+    default=True if jaxlib_extension_version >= 381 else False,
     help=(
         '(TPU only) If True, use chlo.ragged_dot instruction for ragged_dot()'
         ' lowering. Otherwise, rely on the rollout logic in lowering rule for'
