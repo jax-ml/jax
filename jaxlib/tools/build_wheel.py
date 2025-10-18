6b101736175e704e7d19634687e3338393368ae6
Rachel Han <hanrach@google.com>
2025-10-17 17:40:16 -0700

    Move xla literal out of xla_compiler
    PiperOrigin-RevId: 820886673

--- jaxlib/tools/build_wheel.py
+++ jaxlib/tools/build_wheel.py
@@ -214,6 +214,7 @@ def prepare_wheel(wheel_sources_path: pathlib.Path, *, cpu, wheel_sources):
           f"{source_file_prefix}jaxlib/_sdy_mpmd.{pyext}",
           f"{source_file_prefix}jaxlib/_pathways.{pyext}",
           f"{source_file_prefix}jaxlib/_profiler.{pyext}",
+          f"{source_file_prefix}jaxlib/_xla.{pyext}",
           f"{source_file_prefix}jaxlib/_profile_data.{pyext}",
       ],
   )
