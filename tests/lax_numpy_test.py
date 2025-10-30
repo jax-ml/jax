075494cd61b1d5363acdfede9bf16dd789faed04
Peter Hawkins <phawkins@google.com>
2025-10-30 14:26:55 -0700

    Catch overflow if very large 'k' are passed to jnp.tri.
    PiperOrigin-RevId: 826187586

--- tests/lax_numpy_test.py
+++ tests/lax_numpy_test.py
@@ -2085,14 +2085,20 @@ class LaxBackedNumpyTests(jtu.JaxTestCase):
     dtype=default_dtypes,
     n=[0, 4],
     m=[None, 0, 1, 3, 4],
-    k=range(-4, 4),
+    k=[*range(-4, 4), -2**33, 2**33],
   )
   def testTri(self, m, n, k, dtype):
-    np_fun = lambda k: np.tri(n, M=m, dtype=dtype)
-    jnp_fun = lambda k: jnp.tri(n, M=m, dtype=dtype)
+    np_fun = lambda k: np.tri(n, M=m, k=k, dtype=dtype)
+    jnp_fun = lambda k: jnp.tri(n, M=m, k=k, dtype=dtype)
     args_maker = lambda: [k]
-    self._CheckAgainstNumpy(np_fun, jnp_fun, args_maker)
-    self._CompileAndCheck(jnp_fun, args_maker)
+    if not config.enable_x64.value and (
+        k < np.iinfo(np.int32).min or k > np.iinfo(np.int32).max
+    ):
+      with self.assertRaises(OverflowError):
+        jnp_fun(k)
+    else:
+      self._CheckAgainstNumpy(np_fun, jnp_fun, args_maker)
+      self._CompileAndCheck(jnp_fun, args_maker)
 
   def test_tri_bug_22751(self):
     with self.assertRaisesRegex(
