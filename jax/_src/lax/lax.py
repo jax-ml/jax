Fix reduce_sum_transpose_rule which does a broadcast_in_dim to set out_sharding=operand.aval.to_cotangent_aval().sharding instead of operand.aval.sharding. THis is because if operand is reduced, then on bwd pass, we want the cotangent type to become unreduced.
PiperOrigin-RevId: 834511302

jax/_src/lax/lax.py
@@ -7824,8 +7824,9 @@ def _reduce_sum_transpose_rule(cotangent, operand, *, axes, out_sharding):
   assert ad.is_undefined_primal(operand)
   input_shape = operand.aval.shape
   broadcast_dimensions = tuple(np.delete(np.arange(len(input_shape)), axes))
-  result = broadcast_in_dim(cotangent, input_shape, broadcast_dimensions,
-                            out_sharding=operand.aval.sharding)
+  result = broadcast_in_dim(
+      cotangent, input_shape, broadcast_dimensions,
+      out_sharding=operand.aval.to_cotangent_aval().sharding)
   assert result.shape == input_shape
   return [result]
 