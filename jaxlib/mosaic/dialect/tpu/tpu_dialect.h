9e97164b65ae896081a0f8698f5529a4565f25e6
Adam Paszke <apaszke@google.com>
2025-11-28 06:46:06 -0800

    [Mosaic TPU] Remove infer vector layout from tpu.td
    PiperOrigin-RevId: 837831820

--- jaxlib/mosaic/dialect/tpu/tpu_dialect.h
+++ jaxlib/mosaic/dialect/tpu/tpu_dialect.h
@@ -73,12 +73,6 @@ std::unique_ptr<OperationPass<func::FuncOp>> createInferMemRefLayoutPass(
     int hardware_generation, std::array<int64_t, 2> target_shape,
     const TpuTilingFlags& tpu_tiling_flags, bool align = true);
 
-std::unique_ptr<OperationPass<func::FuncOp>> createInferVectorLayoutPass(
-    int hardware_generation = -1,
-    std::array<int64_t, 2> target_shape = {8, 128},
-    const TpuTilingFlags& tpu_tiling_flags = {},
-    bool shape_invariant_numerics = true);
-
 std::unique_ptr<OperationPass<func::FuncOp>> createApplyVectorLayoutPass(
     const ApplyVectorLayoutContext &ctx = ApplyVectorLayoutContext{});
 
