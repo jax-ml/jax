c0fcb0b200ccabf7690c9df2f0703a2fbc598ea3
Matthew Johnson <mattjj@google.com>
2025-12-03 22:42:56 +0000

    don't warn on complex->real cast in dot transpose
    fixes #33521

--- jax/_src/lax/lax.py
+++ jax/_src/lax/lax.py
@@ -5530,7 +5530,8 @@ def _dot_general_transpose_lhs(g, x, y, *, dimension_numbers, precision,
                                 out_sharding=ds)
   x_bar = transpose(dot_general_out, tuple(out_axes))
   if x_bar.dtype != x.aval.dtype:
-    x_bar = _convert_element_type(x_bar, x.aval.dtype, x.aval.weak_type)
+    x_bar = _convert_element_type(x_bar, x.aval.dtype, x.aval.weak_type,
+                                  warn_on_complex_to_real_cast=False)
   return x_bar
 
 def _dot_general_transpose_rhs(g, x, y, *, dimension_numbers, precision,
