a7c6dbba6ffa3b445f90bfeac7b2091ead5e1fe5
Sharad Vikram <sharadmv@google.com>
2025-12-15 17:15:52 -0800

    Add state discharge rule for closed call/call primitives
    PiperOrigin-RevId: 844981830

--- jax/_src/state/discharge.py
+++ jax/_src/state/discharge.py
@@ -155,6 +155,7 @@ _discharge_rules: dict[core.Primitive, DischargeRule] = {}
 def register_discharge_rule(prim: core.Primitive):
   def register(f: DischargeRule):
     _discharge_rules[prim] = f
+    return f
 
   return register
 
@@ -629,6 +630,27 @@ def _closed_call_discharge_rule(
   sentinel = object()
   assert next(ref_vals_iter, sentinel) is sentinel
   return new_invals, out_vals
+
+@register_discharge_rule(core.call_p)
+def _call_discharge_rule(
+    in_avals: Sequence[core.AbstractValue], _,*args,
+    call_jaxpr: core.Jaxpr):
+  closed_call_jaxpr = core.ClosedJaxpr(call_jaxpr, ())
+  discharged_closed_jaxpr, num_outs, fun = _cached_closed_jaxpr_discharge(
+      closed_call_jaxpr)
+  discharged_call_jaxpr = discharged_closed_jaxpr.jaxpr
+  discharged_consts = discharged_closed_jaxpr.consts
+  discharged_call_jaxpr = pe.convert_constvars_jaxpr(discharged_call_jaxpr)
+  out_and_ref_vals = core.closed_call_p.bind(fun, *discharged_consts, *args,
+                                             call_jaxpr=discharged_call_jaxpr)
+  out_vals, ref_vals = split_list(out_and_ref_vals, [num_outs])
+  ref_vals_iter = iter(ref_vals)
+  new_invals = tuple(next(ref_vals_iter) if isinstance(aval, AbstractRef)
+                     else None for aval in in_avals)
+  sentinel = object()
+  assert next(ref_vals_iter, sentinel) is sentinel
+  return new_invals, out_vals
+
 
 # # `run_state`
 
