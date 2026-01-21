63aef87ea7fbb622b104a0af19bd2ebfa7522304
Sam Anklesaria <sanklesaria@openteams.com>
2026-01-21 17:37:00 -0600

    Add dict comparison

--- benchmarks/benchmark_object_registration.py
+++ benchmarks/benchmark_object_registration.py
@@ -101,8 +101,11 @@ def make_tree(cls, depth, num_data, num_static):
   if depth == 0:
     return cls(data_children=[], static_children=[])
   data_children = [make_tree(cls, depth - 1, num_data, num_static) for _ in range(num_data)]
-  static_children = [make_tree(cls, depth - 1, num_data, num_static) for _ in range(num_static)]
+  static_children = ["hello world" for _ in range(num_static)]
   return cls(data_children=data_children, static_children=static_children)
+
+def make_dict(data_children, static_children):
+  return {f'data{i+1}': d for i, d in enumerate(data_children)}
 
 def make_dataclass_tree(cls, depth, num_data, num_static):
   if depth == 0:
@@ -113,7 +116,7 @@ def make_dataclass_tree(cls, depth, num_data, num_static):
       kwargs[f'static{i+1}'] = None
     return cls(**kwargs)
   data_children = [make_dataclass_tree(cls, depth - 1, num_data, num_static) for _ in range(num_data)]
-  static_children = [make_dataclass_tree(cls, depth - 1, num_data, num_static) for _ in range(num_static)]
+  static_children = ["hello world" for _ in range(num_static)]
   kwargs = {}
   for i, child in enumerate(data_children):
     kwargs[f'data{i+1}'] = child
@@ -134,7 +137,7 @@ def ci(a):
 
 if __name__ == '__main__':
   width = 10
-  depth = 4
+  depth = 5
   num_data = width // 2
   num_static = width - num_data
 
@@ -142,8 +145,8 @@ if __name__ == '__main__':
   pytree_registered = make_tree(PyTreeNodeRegistered, depth, num_data, num_static)
   DataclassRegistered = make_dataclass_registered(num_data, num_static)
   dataclass_registered = make_dataclass_tree(DataclassRegistered, depth, num_data, num_static)
-  print(type(dataclass_registered))
   pyobj_registered = make_tree(PyObjectRegistered, depth, num_data, num_static)
+  dict_registered = make_tree(make_dict, depth, num_data, num_static)
 
   print("Benchmarking object...")
   time_obj = [benchmark_roundtrip(obj_registered) for _ in range(3)]
@@ -153,11 +156,14 @@ if __name__ == '__main__':
   time_dataclass = [benchmark_roundtrip(dataclass_registered) for _ in range(3)]
   print("Benchmarking pyobject")
   time_pyobject = [benchmark_roundtrip(pyobj_registered) for _ in range(3)]
+  print("Benchmarking dict")
+  time_dict = [benchmark_roundtrip(dict_registered) for _ in range(3)]
 
   print(f"register_object: {ci(time_obj)}")
   print(f"register_pytree_node: {ci(time_pytree)}")
   print(f"register_dataclass: {ci(time_dataclass)}")
   print(f"register_pyobject: {ci(time_pyobject)}")
+  print(f"register_dict: {ci(time_dict)}")
 
   print(f"Improvement : {np.mean(time_obj) / np.mean(time_pyobject):.4f}")
 
