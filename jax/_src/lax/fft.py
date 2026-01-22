182eab58d85c49b700884758399d2ae58f85348c
Peter Hawkins <phawkins@google.com>
2026-01-22 11:44:40 -0800

    Use irfft to transpose rfft.
    See discussion #34553, credit to @mariogeiger
    
    PiperOrigin-RevId: 859706541

--- jax/_src/lax/fft.py
+++ jax/_src/lax/fft.py
@@ -16,19 +16,19 @@ from __future__ import annotations
 
 from collections.abc import Sequence
 import enum
-from functools import partial
 import math
 
 import numpy as np
 
 from jax._src import dispatch
 from jax._src import dtypes
-from jax._src.api import jit, linear_transpose, ShapeDtypeStruct
+from jax._src.api import jit
 from jax._src.core import Primitive, is_constant_shape
 from jax._src.interpreters import ad
 from jax._src.interpreters import batching
 from jax._src.interpreters import mlir
 from jax._src.lax import lax
+from jax._src.lax import slicing
 from jax._src.lib.mlir.dialects import hlo
 
 __all__ = [
@@ -142,17 +142,26 @@ def _naive_rfft(x, fft_lengths):
 
 @jit(static_argnums=1)
 def _rfft_transpose(t, fft_lengths):
-  # The transpose of RFFT can't be expressed only in terms of irfft. Instead of
-  # manually building up larger twiddle matrices (which would increase the
-  # asymptotic complexity and is also rather complicated), we rely JAX to
-  # transpose a naive RFFT implementation.
-  dummy_shape = t.shape[:-len(fft_lengths)] + fft_lengths
-  dummy_primal = ShapeDtypeStruct(dummy_shape, _real_dtype(t.dtype))
-  transpose = linear_transpose(
-      partial(_naive_rfft, fft_lengths=fft_lengths), dummy_primal)
-  result, = transpose(t)
-  assert result.dtype == _real_dtype(t.dtype), (result.dtype, t.dtype)
-  return result
+  # The transpose can be computed directly using irfft with a mask to account
+  # for Hermitian redundancy. Mask values are 1 for DC and Nyquist, 2 for others.
+  n = fft_lengths[-1]
+  is_odd = n % 2
+  m = t.shape[-1]
+  mask = lax.full_like(t, 2.0, shape=(m,))
+  mask = slicing.dynamic_update_slice(
+      mask, lax.full_like(t, 1.0, shape=(1,)), (0,)
+  )
+  if not is_odd:
+    mask = slicing.dynamic_update_slice(
+        mask, lax.full_like(t, 1.0, shape=(1,)), (m - 1,)
+    )
+
+  N = math.prod(fft_lengths)
+  # The mask is along the last dimension.
+  mask = lax.expand_dims(mask, range(t.ndim - 1))
+  out = N * fft(lax.conj(t) / mask, FftType.IRFFT, fft_lengths)
+  assert out.dtype == _real_dtype(t.dtype), (out.dtype, t.dtype)
+  return out
 
 def _irfft_transpose(t, fft_lengths):
   # The transpose of IRFFT is the RFFT of the cotangent times a scaling
@@ -162,12 +171,15 @@ def _irfft_transpose(t, fft_lengths):
   x = fft(t, FftType.RFFT, fft_lengths)
   n = x.shape[-1]
   is_odd = fft_lengths[-1] % 2
-  full = partial(lax.full_like, t, dtype=x.dtype)
-  mask = lax.concatenate(
-      [full(1.0, shape=(1,)),
-       full(2.0, shape=(n - 2 + is_odd,)),
-       full(1.0, shape=(1 - is_odd,))],
-      dimension=0)
+  mask = lax.full_like(t, 2.0, shape=(n,), dtype=x.dtype)
+  mask = slicing.dynamic_update_slice(
+      mask, lax.full_like(t, 1.0, shape=(1,), dtype=x.dtype), (0,)
+  )
+  if not is_odd:
+    mask = slicing.dynamic_update_slice(
+        mask, lax.full_like(t, 1.0, shape=(1,), dtype=x.dtype), (n - 1,)
+    )
+
   scale = 1 / math.prod(fft_lengths)
   out = scale * lax.expand_dims(mask, range(x.ndim - 1)) * x
   assert out.dtype == _complex_dtype(t.dtype), (out.dtype, t.dtype)
