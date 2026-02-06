753298d32971e3ec1ee2f98fb44c2c3f479d9de0
jax authors <google-ml-automation@google.com>
2026-02-06 05:03:13 -0800

    Add collective metadata version of collective matmul.
    PiperOrigin-RevId: 866408949

--- tests/pallas/mgpu_collective_matmul_test.py
+++ tests/pallas/mgpu_collective_matmul_test.py
@@ -17,12 +17,14 @@
 import functools
 import os
 
+from absl.testing import absltest
 from absl.testing import parameterized  # pylint: disable=g-multiple-import
 import jax
 from jax import lax
 from jax import random
 from jax._src import test_multiprocess as jt_multiprocess
 from jax._src import test_util as jtu
+from jax._src.config import config as jax_config
 from jax._src.pallas import pallas_call
 from jax.experimental.mosaic import gpu as mgpu
 from jax.experimental.pallas.ops.gpu import collective_matmul_mgpu
@@ -31,6 +33,13 @@ import numpy as np
 
 
 P = jax.sharding.PartitionSpec
+
+
+def is_nvshmem_used() -> bool:
+  return (
+      "XLA_FLAGS" in os.environ
+      and "--xla_gpu_experimental_enable_nvshmem" in os.environ["XLA_FLAGS"]
+  )
 
 
 @jtu.with_config(jax_traceback_filtering="off")
@@ -47,8 +56,12 @@ class CollectiveMatmulTestCase(jtu.JaxTestCase):
       if "FAIL_ON_NVSHMEM_UNAVAILABLE" in os.environ:
         raise ValueError("NVSHMEM library unavailable.")
       else:
-        self.skipTest("NVSHMEM library unavailable.")
-    if jax.process_count() == 1:
+        self.skipTest(
+            "Skip test since cross-device collectives are not supported"
+            " (either NVSHMEM is not available in multi-process mode, or"
+            "several processes with several devices per process are used)."
+        )
+    if is_nvshmem_used() and jax.process_count() == 1:
       self.skipTest("Test requires multiple processes.")
     if os.environ.get("XLA_PYTHON_CLIENT_ALLOCATOR", "") == "platform":
       self.skipTest("NVSHMEM doesn't work with the platform allocator.")
@@ -148,4 +161,8 @@ if __name__ == "__main__":
   os.environ["XLA_FLAGS"] = (
       os.environ.get("XLA_FLAGS", "") + " --xla_gpu_autotune_level=0"
   )
-  jt_multiprocess.main()
+  if is_nvshmem_used():
+    jt_multiprocess.main()
+  else:
+    jax_config.config_with_absl()
+    absltest.main(testLoader=jtu.JaxTestLoader())
