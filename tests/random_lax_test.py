3334de9a84a8811813dd97d155bf5c82b1a9a63e
Benjamin Chetioui <bchetioui@google.com>
2026-02-11 03:27:47 -0800

    [NFC] Remove unnecessary comment from `testMultivariateNormalSingularCovariance`.
    PiperOrigin-RevId: 868589810

--- tests/random_lax_test.py
+++ tests/random_lax_test.py
@@ -963,7 +963,7 @@ class DistributionsTest(RandomTestBase):
                         check_dtypes=False)
 
   @jtu.sample_product(method=['cholesky', 'eigh', 'svd'])
-  @jtu.skip_on_devices('cuda', 'tpu')  # Some NaNs on accelerators. Passing on ROCm
+  @jtu.skip_on_devices('cuda', 'tpu')  # Some NaNs on accelerators.
   def testMultivariateNormalSingularCovariance(self, method):
     # Singular covariance matrix https://github.com/jax-ml/jax/discussions/13293
     mu = jnp.zeros((2,))
