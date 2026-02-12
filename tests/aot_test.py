ed3d354d91aabed55a3919896418c1cb9c0cce22
Gulsum Gudukbay Akbulut <gulsum.gudukbayakbulut@amd.com>
2026-02-12 16:07:47 -0600

    Enable test_deviceless_aot_compile on ROCm
    Unskip test_deviceless_aot_compile for ROCm support.

--- tests/aot_test.py
+++ tests/aot_test.py
@@ -264,10 +264,6 @@ class JaxAotTest(jtu.JaxTestCase):
 
   @jtu.run_on_devices('gpu')
   def test_deviceless_aot_compile(self):
-    # Skip test on ROCm due to topology not implemented error. Issue #34711
-    # TODO(GulsumGudukbay): Unskip once fixed.
-    if jtu.is_device_rocm():
-      self.skipTest("Skipped on ROCm due to topology not implemented error.")
     target_config = xc.get_topology_for_devices(jax.devices()).target_config
     with jtu.global_config_context(jax_platforms="cpu"):
       topology = topologies.get_topology_desc(
