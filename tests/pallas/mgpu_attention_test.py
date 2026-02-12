7979be45a9493ab5bb798e2a161948a2de03c321
Justin Fu <justinjfu@gmail.com>
2026-02-12 19:06:11 +0000

    Add MGPU splash attention implementation

--- tests/pallas/mgpu_attention_test.py
+++ tests/pallas/mgpu_attention_test.py
@@ -14,7 +14,9 @@
 # ==============================================================================
 """Test different parameterizations of FlashAttention."""
 
+import math
 import os
+from typing import Literal
 
 import numpy as np
 from absl.testing import absltest, parameterized
@@ -30,13 +32,21 @@ try:
   import jax.experimental.mosaic.gpu  # noqa: F401
 except ImportError:
   attention_mgpu = None
+  attention_mask = None
+  hopper_splash_attention = None
 else:
   from jax.experimental.pallas.ops.gpu import attention_mgpu
-
+  from jax.experimental.pallas.ops.gpu.splash_attention import attention_mask
+  from jax.experimental.pallas.ops.gpu.splash_attention import hopper_splash_attention
 
 config.parse_flags_with_absl()
 os.environ["XLA_FLAGS"] = (
     os.environ.get("XLA_FLAGS", "") + " --xla_gpu_autotune_level=0")
+
+MaskType = Literal[
+    "full", "zeros", "causal", "random", "multidoc", "sliding_window", "zeroed_partial_blocks", "realistic"
+]
+
 
 
 @jtu.with_config(jax_traceback_filtering="off")
@@ -176,6 +186,441 @@ class FlashAttentionTestCase(jtu.JaxTestCase):
     except ValueError as e:
       if "exceeds available shared memory" in e.args[0]:
         self.skipTest("Not enough SMEM for this configuration.")
+
+
+@jtu.with_config(jax_traceback_filtering="off")
+class AttentionMaskTest(jtu.JaxTestCase):
+
+  def setUp(self):
+    super().setUp()
+    if attention_mgpu is None:
+      self.skipTest("Mosaic GPU not available.")
+
+  def test_fwd_mask_2_tiling(self):
+    mask = jnp.array(
+        [
+            [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 1],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0, 0, 1, 0],
+            [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        ]
+    )
+    mask = mask.reshape((1, 1, mask.shape[0], mask.shape[1])).astype(bool)
+    mask_info = attention_mask.process_dynamic_mask(
+        mask=mask,
+        block_shape=(2, 2),
+        is_dkv=False,
+        data_tiling=2,
+    )
+    np.testing.assert_array_equal(
+        mask_info.block_mask[0, 0],
+        jnp.array(
+            [
+                [2, 1, 2, 0, 0],
+                [2, 2, 0, 0, 0],
+                [2, 1, 0, 1, 0],
+                [0, 0, 1, 0, 0],
+            ]
+        ),
+    )
+    np.testing.assert_array_equal(
+        mask_info.data_next[0, 0],
+        jnp.array(
+            [
+                [0, 1, 4, -1, -1],
+                [0, 1, 2, 4, -1],
+            ]
+        ),
+    )
+    np.testing.assert_array_equal(
+        mask_info.mask_next[0, 0],
+        jnp.array(
+            [
+                [-1, 1, -1, -1, -1],
+                [-1, -1, -1, -1, -1],
+                [-1, 11, -1, 14, -1],
+                [-1, -1, 17, -1, -1],
+            ]
+        ),
+    )
+
+  def test_dkv_mask_2_tiling(self):
+    mask = jnp.array(
+        [
+            [1, 1, 1, 1, 1, 1, 0, 0],
+            [1, 1, 1, 1, 1, 1, 0, 0],
+            [1, 1, 1, 1, 1, 1, 0, 0],
+            [0, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 1, 1, 0, 0],
+            [1, 1, 0, 0, 0, 1, 0, 0],
+        ]
+    )
+    mask = mask.reshape((1, 1, mask.shape[0], mask.shape[1])).astype(bool)
+    mask_info = attention_mask.process_dynamic_mask(
+        mask=mask,
+        block_shape=(2, 2),
+        is_dkv=True,
+        data_tiling=2,
+    )
+    np.testing.assert_array_equal(
+        mask_info.block_mask[0, 0],
+        jnp.array(
+            [
+                [0, 0, 0, 0],
+                [0, 0, 2, 0],
+                [2, 2, 1, 0],
+                [1, 2, 0, 1],
+                [2, 0, 1, 0],
+            ]
+        ),
+    )
+    np.testing.assert_array_equal(
+        mask_info.data_next[0, 0],
+        jnp.array(
+            [
+                [-1, -1],
+                [-1, 0],
+                [0, 1],
+                [1, 2],
+                [4, 4],
+            ]
+        ),
+    )
+    np.testing.assert_array_equal(
+        mask_info.mask_next[0, 0],
+        jnp.array(
+            [
+                [-1, -1, -1, -1],
+                [-1, -1, -1, -1],
+                [-1, -1, 6, -1],
+                [4, -1, -1, 11],
+                [-1, -1, 18, -1],
+            ]
+        ),
+    )
+
+  def test_compress(self):
+    x = jnp.array(
+        [
+            [0, 1, 2, -1, -1, 10, 6, 7, -1, -1, -1, 9, -1],
+            [-1, -1, -1, -1, 0, 1, 2, -1, -1, -1, -1, -1, -1],
+        ]
+    )
+    expected = jnp.array(
+        [
+            [0, 1, 2, 10, 6, 7, 9, -1, -1, -1, -1, -1, -1],
+            [0, 1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+        ]
+    )
+    np.testing.assert_array_equal(
+        attention_mask.compress_array(x, placeholder=-1)[0], expected)
+
+  def test_tiled_max(self):
+    x = jnp.array([[0, 1, 2, -1, -1, -1], [-1, -1, 2, 3, 4, -1],
+                  [-1, 1, 2, 3, -1, -1], [-1, -1, 2, 3, 4, -1]])
+    expected = jnp.array([[0, 1, 2, 3, 4, -1], [-1, 1, 2, 3, 4, -1]])
+    np.testing.assert_array_equal(
+        attention_mask.tiled_max(x, tile_size=2, axis=-2), expected)
+
+
+@jtu.with_config(jax_traceback_filtering="off")
+class SplashAttentionTest(jtu.JaxTestCase):
+
+  def setUp(self):
+    super().setUp()
+    if attention_mgpu is None:
+      self.skipTest("Mosaic GPU not available.")
+    if (not jtu.test_device_matches(["cuda"]) or
+            not jtu.is_cuda_compute_capability_equal("9.0")):
+      self.skipTest("Only works on GPU with capability sm90a")
+
+    self.tuning_config = hopper_splash_attention.TuningConfig(
+        block_q=64,
+        block_kv=64,
+        max_concurrent_steps=2,
+        block_q_dkv=64,
+        block_kv_dkv=64,
+        block_q_dq=64,
+        block_kv_dq=64,
+    )
+
+  def _generate_mask(self, mask_type: MaskType, batch_size: int, seq_len: int) -> jax.Array:
+    def build_mask(
+        *,
+        position_ids: jax.Array,
+        segment_ids: jax.Array,
+        kv_token_mask: jax.Array,
+        kv_position_ids: jax.Array,
+        kv_segment_ids: jax.Array,
+        causal: bool,
+        sliding_window_length: int | None = None,
+    ) -> jax.Array:
+      mask = jnp.ones((1, 1, 1, 1), dtype=bool)
+      if causal:
+        position_ids_ = position_ids[:, None, :, None]
+        kv_position_ids_ = kv_position_ids[:, None, None, :]
+        causal_mask = position_ids_ >= kv_position_ids_
+        mask = jnp.logical_and(mask, causal_mask)
+
+      if sliding_window_length:
+        if not causal:
+          raise NotImplementedError("Sliding window mask must also be causal")
+        position_ids_ = position_ids[:, None, :, None]
+        kv_position_ids_ = kv_position_ids[:, None, None, :]
+        local_window_mask = jnp.abs(
+            position_ids_ - kv_position_ids_) < sliding_window_length
+        mask = jnp.logical_and(mask, local_window_mask)
+
+      segment_ids = segment_ids[:, None, :, None]
+      kv_segment_ids = kv_segment_ids[:, None, None, :]
+      document_mask = segment_ids == kv_segment_ids
+      mask = jnp.logical_and(mask, document_mask)
+
+      kv_token_mask = kv_token_mask[:, None, None, :]
+      mask = jnp.logical_and(mask, kv_token_mask)
+      return mask
+
+    if mask_type == "full":
+      mask = jnp.ones((batch_size, 1, seq_len, seq_len), dtype=jnp.bool_)
+    elif mask_type == "zeros":
+      mask = jnp.zeros((batch_size, 1, seq_len, seq_len), dtype=jnp.bool_)
+    elif mask_type == "random":
+      mask = jax.random.bernoulli(jax.random.key(
+          0), 128.0 / seq_len, shape=(batch_size, 1, seq_len, seq_len))
+    elif mask_type == "causal":
+      position_ids = jnp.arange(seq_len).reshape((1, seq_len))
+      kv_position_ids = jnp.arange(seq_len).reshape((1, seq_len))
+      segment_ids = jnp.zeros((1, seq_len), dtype=jnp.int32)
+      kv_token_mask = jnp.ones((1, seq_len), dtype=jnp.bool_)
+      mask = build_mask(
+          position_ids=position_ids,
+          segment_ids=segment_ids,
+          kv_token_mask=kv_token_mask,
+          kv_position_ids=kv_position_ids,
+          kv_segment_ids=segment_ids,
+          causal=True,
+      )
+      mask = jnp.tile(mask, (batch_size, 1, 1, 1))
+    elif mask_type == "multidoc":
+      masks = []
+      for batch_idx in range(batch_size):
+        position_ids = jnp.arange(seq_len).reshape((1, seq_len))
+        kv_position_ids = jnp.arange(seq_len).reshape((1, seq_len))
+        num_docs = 2 ** (batch_idx + 1)
+        segment_ids = np.repeat(jnp.arange(num_docs), seq_len // num_docs)
+        segment_ids = jnp.reshape(segment_ids, (1, seq_len))
+        kv_token_mask = jnp.ones((1, seq_len), dtype=jnp.bool_)
+        mask = build_mask(
+            position_ids=position_ids,
+            segment_ids=segment_ids,
+            kv_token_mask=kv_token_mask,
+            kv_position_ids=kv_position_ids,
+            kv_segment_ids=segment_ids,
+            causal=True,
+        )
+        mask = jnp.tile(mask, (batch_size, 1, 1, 1))
+        masks.append(mask)
+      mask = jnp.concatenate(masks, axis=0)
+    elif mask_type == "sliding_window":
+      # Create sliding window mask
+      masks = []
+      for batch_idx in range(batch_size):
+        position_ids = jnp.arange(seq_len).reshape((1, seq_len))
+        kv_position_ids = position_ids
+        segment_ids = jnp.zeros((1, seq_len), dtype=jnp.int32)
+        kv_token_mask = jnp.ones((1, seq_len), dtype=jnp.bool_)
+
+        mask = build_mask(
+            position_ids=position_ids,
+            segment_ids=segment_ids,
+            kv_token_mask=kv_token_mask,
+            kv_position_ids=kv_position_ids,
+            kv_segment_ids=segment_ids,
+            causal=True,
+        )
+        position_ids_ = position_ids[:, None, :, None]
+        kv_position_ids_ = kv_position_ids[:, None, None, :]
+        sliding_window_mask = jnp.abs(
+            position_ids_ - kv_position_ids_) < (2 ** (6 + batch_idx))
+        mask = jnp.logical_and(mask, sliding_window_mask)
+        masks.append(mask)
+      mask = jnp.concatenate(masks, axis=0)
+    elif mask_type == "zeroed_partial_blocks":
+      mask = jnp.ones((1, 1, seq_len, seq_len), dtype=jnp.bool_)
+      mask = mask.at[:, :, 0:96, 0:96].set(False)
+      mask = jnp.tile(mask, (batch_size, 1, 1, 1))
+    elif mask_type == "realistic":
+      # Generate a "realistic" mask with multiple documents of varying sizes that are also "masked" with padding tokens
+      # at the end of each document.
+      masks = []
+      temp = 10.0
+      for batch in range(batch_size):
+        num_docs = 2 * (batch + 2)
+        probs_key, sample_key, pad_key = jax.random.split(
+            jax.random.key(batch), 3)
+        position_ids = jnp.arange(seq_len).reshape((1, seq_len))
+        kv_position_ids = jnp.arange(seq_len).reshape((1, seq_len))
+        probs = jax.random.dirichlet(probs_key, jnp.ones((num_docs,)) * temp)
+        doc_sizes = jax.random.multinomial(
+            sample_key, seq_len, probs).astype(jnp.int32)
+        segment_ids = jnp.concatenate([jnp.array([i] * int(n)) for (i, n) in enumerate(doc_sizes)]).reshape(
+            (1, seq_len)
+        )
+
+        n_keep = [
+            int(jax.random.binomial(jax.random.fold_in(pad_key, i), doc_sizes[i], 0.8)) for i in range(num_docs)
+        ]
+        kv_token_mask = jnp.concatenate(
+            [
+                jnp.concatenate([jnp.ones((n_keep[i],)), jnp.zeros(
+                    (int(doc_sizes[i]) - n_keep[i],))])
+                for i in range(num_docs)
+            ]
+        )
+        kv_token_mask = kv_token_mask.reshape((1, seq_len)).astype(jnp.bool_)
+        mask = build_mask(
+            position_ids=position_ids,
+            segment_ids=segment_ids,
+            kv_token_mask=kv_token_mask,
+            kv_position_ids=kv_position_ids,
+            kv_segment_ids=segment_ids,
+            causal=True,
+        )
+        masks.append(mask)
+      mask = jnp.concatenate(masks, axis=0)
+    assert mask.shape == (batch_size, 1, seq_len, seq_len)
+    return mask
+
+  def _generate_qkv(self, shape, seed=0, q_heads_per_kv_head=1):
+    q_rng, k_rng, v_rng = jax.random.split(jax.random.key(seed), 3)
+    q_shape = shape
+    if q_heads_per_kv_head != 1:
+      q_shape = (shape[0], shape[1], shape[2] * q_heads_per_kv_head, shape[3])
+    q = jax.random.normal(q_rng, q_shape, dtype=jnp.float32)
+    k = jax.random.normal(k_rng, shape, dtype=jnp.float32)
+    v = jax.random.normal(v_rng, shape, dtype=jnp.float32)
+    # RMS norm
+    q = q / jnp.sqrt(jnp.sum(q * q, axis=-1, keepdims=True) + 1e-6)
+    k = k / jnp.sqrt(jnp.sum(k * k, axis=-1, keepdims=True) + 1e-6)
+    return q, k, v
+
+  @parameterized.product(
+      batch_size=(1, 2),
+      q_heads_per_kv_head=(1, 2),
+      mask_type=("realistic", "full", "causal", "random",
+                 "sliding_window", "zeroed_partial_blocks")
+  )
+  def test_mgpu_masked_fwd(self, batch_size: int, q_heads_per_kv_head: int, mask_type: MaskType):
+    if not jtu.is_cuda_compute_capability_equal("9.0"):
+      self.skipTest("Test only runs on H100 GPUs.")
+    seq_len = 2 * 1024
+    q, k, v = self._generate_qkv(
+        (batch_size, seq_len, 1, 128), q_heads_per_kv_head=q_heads_per_kv_head)
+    q = q.astype(jnp.bfloat16)
+    k = k.astype(jnp.bfloat16)
+    v = v.astype(jnp.bfloat16)
+    scale = 1.0 / np.sqrt(q.shape[-1])
+    mask = self._generate_mask(mask_type, batch_size, seq_len)
+    mask_info = attention_mask.process_dynamic_mask(
+        mask=mask,
+        block_shape=(self.tuning_config.block_q, self.tuning_config.block_kv),
+        is_dkv=False,
+        data_tiling=2,
+    )
+
+    ref_result, ref_lse = jax.nn.dot_product_attention(
+        q.astype(jnp.float32),
+        k.astype(jnp.float32),
+        v.astype(jnp.float32),
+        mask=mask,
+        scale=scale,
+        return_residual=True,
+    )
+    ref_lse = ref_lse.swapaxes(-1, -2)  # Swap from BTN -> BNT
+    # MGPU scales lse by log2(e) but JAX does not.
+    ref_lse *= math.log2(math.e)
+    mgpu_result, (lse,) = hopper_splash_attention.attention(
+        q, k, v, mask_info=mask_info, config=self.tuning_config, scale=scale, save_residuals=True
+    )
+    np.testing.assert_allclose(mgpu_result, ref_result, atol=1e-2)
+    np.testing.assert_allclose(lse, ref_lse, atol=5e-4)
+
+  @parameterized.product(
+      batch_size=(1, 2),
+      q_heads_per_kv_head=(1, 2),
+      mask_type=("realistic", "full", "causal", "random",
+                 "sliding_window", "zeroed_partial_blocks")
+  )
+  def test_mgpu_masked_bwd(self, batch_size: int, q_heads_per_kv_head: int, mask_type: MaskType):
+    if not jtu.is_cuda_compute_capability_equal("9.0"):
+      self.skipTest("Test only runs on H100 GPUs.")
+    seq_len = 2 * 1024
+    q, k, v = self._generate_qkv(
+        (batch_size, seq_len, 1, 128), q_heads_per_kv_head=q_heads_per_kv_head)
+    q = q.astype(jnp.bfloat16)
+    k = k.astype(jnp.bfloat16)
+    v = v.astype(jnp.bfloat16)
+    scale = 1.0 / np.sqrt(q.shape[-1])
+    mask = self._generate_mask(mask_type, batch_size, seq_len)
+    mask_info = attention_mask.process_dynamic_mask(
+        mask=mask,
+        block_shape=(self.tuning_config.block_q, self.tuning_config.block_kv),
+        is_dkv=False,
+        data_tiling=2,
+    )
+    mask_info_dkv = attention_mask.process_dynamic_mask(
+        mask=mask,
+        block_shape=(self.tuning_config.block_q, self.tuning_config.block_kv),
+        is_dkv=True,
+        data_tiling=2,
+    )
+
+    def run_mgpu(q, k, v):
+      result = hopper_splash_attention.attention(
+          q,
+          k,
+          v,
+          mask_info=mask_info,
+          mask_info_dkv=mask_info_dkv,
+          config=self.tuning_config,
+          scale=scale,
+          save_residuals=False,
+      )
+      return jnp.sum(result)
+
+    @jax.jit
+    def run_reference(q, k, v):
+      result = jax.nn.dot_product_attention(
+          q.astype(jnp.float32),
+          k.astype(jnp.float32),
+          v.astype(jnp.float32),
+          mask=mask,
+          scale=scale,
+          return_residual=False,
+      )
+      return jnp.sum(result)
+
+    result_grad = jax.grad(run_mgpu, argnums=[0, 1, 2])(q, k, v)
+    ref_grad = jax.grad(run_reference, argnums=[0, 1, 2])(q, k, v)
+
+    np.testing.assert_allclose(result_grad[0], ref_grad[0], atol=2e-3)  # dq
+    np.testing.assert_allclose(result_grad[1], ref_grad[1], atol=2e-3)  # dk
+    if mask_type == "zeros":
+      # For zeros mask, reference outputs 1 but the kernel outputs 0, so
+      # we manually check against zero.
+      np.testing.assert_array_equal(result_grad[2], 0)
+    else:
+      np.testing.assert_allclose(
+          result_grad[2], ref_grad[2], atol=5e-2, rtol=5e-2)  # dv
+
 
 if __name__ == "__main__":
   absltest.main(testLoader=jtu.JaxTestLoader())
