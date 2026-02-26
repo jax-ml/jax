48c5eb4710447c4efe43530fb9fc7a84eda5af29
jax authors <google-ml-automation@google.com>
2026-02-25 20:38:03 -0800

    Reverts 6869e28a4bde0f6d3a06a7c39858fe40cc44ff60
    PiperOrigin-RevId: 875482217

--- jaxlib/mosaic/dialect/tpu/tpu_ops.cc
+++ jaxlib/mosaic/dialect/tpu/tpu_ops.cc
@@ -1602,6 +1602,15 @@ LogicalResult EnqueueIndirectDMAOp::fold(FoldAdaptor,
   return memref::foldMemRefCast(*this);
 }
 
+// TODO(b/395630795): Remove after 2025-08-10.
+LogicalResult WaitDMAOp::verify() {
+  auto sem_type = getMemRefType(getSemaphore());
+  if (sem_type.getRank() != 0) {
+    return emitOpError("DMA wait semaphore must be rank 0");
+  }
+  return success();
+}
+
 void WaitDMA2Op::build(OpBuilder &builder, OperationState &state,
                        Value semaphore, Value src, Value dst) {
   build(builder, state, semaphore, src, dst, /*device_id=*/nullptr,
