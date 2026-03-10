78126876ceaaaffec54aec76e1b9309b41a18a8d
jax authors <google-ml-automation@google.com>
2026-03-10 09:51:34 -0700

    Expose register_extra_dialect in mosaic to support loading custom dialect attributes for backward compatability.
    PiperOrigin-RevId: 881485498

--- jax/experimental/mosaic/__init__.py
+++ jax/experimental/mosaic/__init__.py
@@ -16,4 +16,5 @@
 
 from jax._src.tpu_custom_call import as_tpu_kernel as as_tpu_kernel
 from jax._src.tpu_custom_call import lower_module_to_custom_call as lower_module_to_custom_call
+from jax._src.tpu_custom_call import register_extra_dialect as _register_extra_dialect
 from jax._src.tpu_custom_call import Tiling as Tiling
