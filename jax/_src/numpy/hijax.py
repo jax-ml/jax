more fixes for sharded computation

jax/_src/numpy/hijax.py
@@ -169,6 +169,7 @@ def _searchsorted_impl(sorted_arr: ArrayLike, query: ArrayLike, *, dimension: in
                        batch_dims: int, side: str, dtype: np.dtype, method: str):
   """Main implementation of searchsorted primitive."""
   sorted_arr = jnp.moveaxis(sorted_arr, dimension, -1)
+  dtype = dtypes._maybe_canonicalize_explicit_dtype(dtype, "searchsorted")
 
   if method == "scan":
     impl: Callable[..., Array] = functools.partial(_searchsorted_scan_impl, unrolled=False)
@@ -196,6 +197,10 @@ def _searchsorted_scan_impl(
   """Scan-based implementation of searchsorted."""
   assert sorted_arr.ndim == 1
   assert side in ["left", "right"]
+  assert dtypes.issubdtype(dtype, np.integer)
+  (n,) = sorted_arr.shape
+  if n > dtypes.iinfo(dtype).max:
+    raise OverflowError(f"Python integer {n} out of bounds for {dtype}")
   if sorted_arr.shape[0] == 0:
     return lax.full(query.shape, fill_value=0, dtype=dtype)
   if query.ndim > 0:
@@ -205,21 +210,18 @@ def _searchsorted_scan_impl(
     )(sorted_arr, query)
 
   op = lax._sort_le_comparator if side == "left" else lax._sort_lt_comparator  # pylint: disable=protected-access
-  unsigned_dtype = np.uint64 if dtype == np.int64 else np.uint32
+  unsigned_dtype = np.uint64 if dtypes.iinfo(dtype).bits == 64 else np.uint32
   def body_fun(state, _):
     low, high = state
-    mid = low.astype(unsigned_dtype) + high.astype(unsigned_dtype)
-    mid = lax.div(mid, jnp.array(2, dtype=unsigned_dtype)).astype(dtype)
+    mid = low + (high - low) // 2  # use this form to avoid overflow
     go_left = op(query, sorted_arr[mid])
-    return (jnp.where(go_left, low, mid), jnp.where(go_left, mid, high)), ()
-  (n,) = sorted_arr.shape
+    return (lax.select(go_left, low, mid), lax.select(go_left, mid, high)), ()
   n_levels = int(np.ceil(np.log2(n + 1)))
   vma = tuple(core.typeof(sorted_arr).vma)
-  init = (core.pvary(jnp.array(0, dtype), vma),
-          core.pvary(jnp.array(n, dtype), vma))
+  init = (core.pvary(unsigned_dtype(0), vma), core.pvary(unsigned_dtype(n), vma))
   carry, _ = control_flow.scan(body_fun, init, (), length=n_levels,
                                unroll=n_levels if unrolled else 1)
-  return carry[1]
+  return carry[1].astype(dtype)
 
 
 @functools.partial(api.jit, static_argnames=["side", "dtype"])