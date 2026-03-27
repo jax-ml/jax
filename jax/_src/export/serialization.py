70820be000848b68b80a0e531fc5f618053be88a
Peter Hawkins <phawkins@google.com>
2026-03-27 09:50:57 -0700

    Remove code to handle DisabledSafetyCheckKind.shape_assertions.
    PiperOrigin-RevId: 890504529

--- jax/_src/export/serialization.py
+++ jax/_src/export/serialization.py
@@ -728,11 +728,5 @@ def _deserialize_disabled_safety_check(
     )
   if kind == ser_flatbuf.DisabledSafetyCheckKind.platform:
     return _export.DisabledSafetyCheck.platform()
-  if kind == ser_flatbuf.DisabledSafetyCheckKind.shape_assertions:
-    # shape_assertions has been deprecated in June 2024 (turned into a no-op),
-    # and removed in November 2024. We deserialize it to a DisabledSafetyCheck
-    # that has no effect.
-    # TODO(necula): remove this after June 2025, when we should not have any
-    # more serialized artifacts with shape_assertions.
-    return _export.DisabledSafetyCheck.custom_call("no op")
+
   raise ValueError(f"Cannot deserialize DisabledSafetyCheck with unknown kind: {kind}")
