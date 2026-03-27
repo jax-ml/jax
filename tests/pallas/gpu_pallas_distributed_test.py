657ed708aa0d5f543edb3b0308d1812663f80d7e
Gleb Pobudzey <pobudzey@google.com>
2026-03-27 05:58:05 -0700

    [Pallas MGPU] Adding a test that checks semaphore values after a multimem reduction.
    PiperOrigin-RevId: 890406256

--- tests/pallas/gpu_pallas_distributed_test.py
+++ tests/pallas/gpu_pallas_distributed_test.py
@@ -595,28 +595,41 @@ class PallasCallRemoteDMATest(TestCase):
       self.monkey_patched_api_was_used = True
       return  # Only 2 processes needed.
 
-    def kernel(y_ref, sem):
+    def kernel(y_ref, sem_out_ref, sem):
       plgpu.semaphore_signal_multicast(sem.at[0], collective_axes='x')
       # Wait for the multicast signal (each device gets signaled by all devices)
       pl.semaphore_wait(sem.at[0], 2)  # Wait for signals from both devices
       y_ref[...] = jnp.ones_like(y_ref)
+      sem_out_ref[0] = pl.semaphore_read(sem.at[0])
+      sem_out_ref[1] = pl.semaphore_read(sem.at[1])
 
     kernel_call = self.pallas_call(
         kernel,
-        out_specs=pl.BlockSpec(memory_space=plgpu.GMEM),
-        out_shape=jax.ShapeDtypeStruct((8, 128), jnp.float32),
+        out_specs=[pl.BlockSpec(memory_space=plgpu.GMEM)] * 2,
+        out_shape=(
+            jax.ShapeDtypeStruct((8, 128), jnp.float32),
+            jax.ShapeDtypeStruct((2,), jnp.int32),
+        ),
         scratch_shapes=[plgpu.SemaphoreType.REGULAR((2,))],
         compiler_params=plgpu.CompilerParams(),
     )
 
     devices = jax.devices()[:2]
     mesh = jax.sharding.Mesh(devices, ['x'])
-    y = jax.jit(
+    y, sem_out = jax.jit(
         jax.shard_map(
             kernel_call, mesh=mesh, in_specs=(), out_specs=P(None), check_vma=False,
         )
     )()
     np.testing.assert_allclose(y, jnp.ones_like(y))
+
+    try:
+      np.testing.assert_allclose(sem_out, jnp.zeros_like(sem_out))
+    except Exception:
+      # On some CUDA versions there is a compiler bug where the predicate
+      # on the multimem reduction is not respected.
+      if cuda_versions.cuda_runtime_get_version() not in [12080, 12090, 13000]:
+        raise
 
   def test_permuted_mesh(self):
     def kernel(y_ref, sem):
