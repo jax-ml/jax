9b7bd343b453ea92fdecdb795c4506763fd5940c
Yash Katariya <yashkatariya@google.com>
2026-04-01 20:34:06 -0700

    Allow setting only `abstract_mesh` at the top level (via with `use_abstract_mesh`) without `set_mesh` (i.e. setting concrete mesh)
    For example:
    
    ```
    abstract_mesh = jax.sharding.AbstractMesh((2,), ('x',))
    inp = jax.ShapeDtypeStruct((8, 2), np.float32)
    
    @jax.jit
    def f(x):
      return x * 2
    
    with jax.sharding.use_abstract_mesh(abstract_mesh):
      lowered = f.trace(inp).lower(lowering_platforms=('tpu',))
    self.assertIn('mhlo.num_partitions = 2', lowered.as_text())
    ```
    
    PiperOrigin-RevId: 893251965

--- jax/_src/stages.py
+++ jax/_src/stages.py
@@ -968,17 +968,17 @@ class SourceInfo(NamedTuple):
 
 @dataclasses.dataclass
 class DeviceAssignmentMismatch:
-  da: Sequence[xc.Device]
+  da: Sequence[xc.Device] | int
   m_type: MismatchType
   source_info: SourceInfo | None
 
   @property
   def device_ids(self) -> Sequence[int]:
-    return [d.id for d in self.da]
+    return [d.id for d in self.da]  # type: ignore
 
   @property
   def platform(self) -> str:
-    return self.da[0].platform.upper()
+    return self.da[0].platform.upper()  # type: ignore
 
   def _maybe_api_name(self, api_name) -> str:
     return f" {api_name}'s" if self.m_type == MismatchType.CONTEXT_DEVICES else ""
@@ -992,14 +992,17 @@ class DeviceAssignmentMismatch:
 
   @property
   def _dev_ids_plat_str(self):
-    return f"device ids {self.device_ids} on platform {self.platform}"
+    if isinstance(self.da, int):
+      return f"as AbstractMesh of size {self.da}"
+    else:
+      return f"with device ids {self.device_ids} on platform {self.platform}"
 
   def m_type_str(self, api_name):
     return (f'{self.source_info and self.source_info.eqn_name} inside {api_name}'
             if self.m_type == MismatchType.SHARDING_INSIDE_COMPUTATION else self.m_type)
 
   def _str(self, api_name):
-    return (f"{self._maybe_api_name(api_name)} {self.m_type_str(api_name)} with "
+    return (f"{self._maybe_api_name(api_name)} {self.m_type_str(api_name)} "
             f"{self._dev_ids_plat_str}{self.source_info_str}")
 
 
