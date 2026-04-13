4e8d615ca031a9921d006d11843ee1926d295b2c
Jake VanderPlas <jakevdp@google.com>
2026-04-13 10:41:04 -0700

    Deprecate a few dozen jax.core APIs for v0.10.0

--- jax/extend/core/__init__.py
+++ jax/extend/core/__init__.py
@@ -21,21 +21,42 @@ from jax._src.abstract_arrays import (
 
 from jax._src.core import (
   AbstractToken as AbstractToken,
-  call_impl as call_impl,
+  CallPrimitive as CallPrimitive,
   ClosedJaxpr as ClosedJaxpr,
+  DebugInfo as DebugInfo,
+  DropVar as DropVar,
+  Effect as Effect,
+  Effects as Effects,
+  InconclusiveDimensionOperation as InconclusiveDimensionOperation,
   Jaxpr as Jaxpr,
   JaxprEqn as JaxprEqn,
-  jaxpr_as_fun as jaxpr_as_fun,
-  mapped_aval as mapped_aval,
+  JaxprTypeError as JaxprTypeError,
   Literal as Literal,
   Primitive as Primitive,
+  Token as Token,
+  TraceTag as TraceTag,
+  Var as Var,
+  call_impl as call_impl,
+  check_jaxpr as check_jaxpr,
+  concrete_or_error as concrete_or_error,
+  find_top_trace as find_top_trace,
+  gensym as gensym,
+  get_opaque_trace_state as get_opaque_trace_state,
+  jaxpr_as_fun as jaxpr_as_fun,
+  jaxprs_in_params as jaxprs_in_params,
+  mapped_aval as mapped_aval,
+  new_jaxpr_eqn as new_jaxpr_eqn,
+  no_effects as no_effects,
+  nonempty_axis_env as nonempty_axis_env_DO_NOT_USE,  # noqa: F401
+  primal_dtype_to_tangent_dtype as primal_dtype_to_tangent_dtype,
   set_current_trace as set_current_trace,
   subjaxprs as subjaxprs,
   take_current_trace as take_current_trace,
-  Token as Token,
-  TraceTag as TraceTag,
   unmapped_aval as unmapped_aval,
-  Var as Var,
+  unsafe_am_i_under_a_jit as unsafe_am_i_under_a_jit_DO_NOT_USE,  # noqa: F401
+  unsafe_am_i_under_a_vmap as unsafe_am_i_under_a_vmap_DO_NOT_USE,  # noqa: F401
+  unsafe_get_axis_names as unsafe_get_axis_names_DO_NOT_USE,  # noqa: F401
+  valid_jaxtype as valid_jaxtype,
 )
 
 from . import primitives as primitives
