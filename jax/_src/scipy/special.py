852d2d32f60dc9d0143d08a7f47c1a5754203c1f
Jah-yee <jydu_seven@outlook.com>
2026-04-19 00:24:33 +0800

    Add jax.scipy.special.erfcx (scaled complementary error function)

--- jax/_src/scipy/special.py
+++ jax/_src/scipy/special.py
@@ -554,6 +554,29 @@ def erfc(x: ArrayLike) -> Array:
   return lax.erfc(x)
 
 
+def erfcx(x: ArrayLike) -> Array:
+  r"""The scaled complementary error function
+
+  JAX implementation of :obj:`scipy.special.erfcx`.
+
+  .. math::
+
+     \mathrm{erfcx}(x) = e^{x^2} \mathrm{erfc}(x)
+
+  Args:
+    x: arraylike, real-valued.
+
+  Returns:
+    array containing values of the scaled complementary error function.
+
+  See also:
+    - :func:`jax.scipy.special.erfc`
+    - :func:`jax.scipy.special.erf`
+  """
+  x, = promote_args_inexact("erfcx", x)
+  return lax.exp(lax.square(x)) * lax.erfc(x)
+
+
 def erfinv(x: ArrayLike) -> Array:
   """The inverse of the error function
 
