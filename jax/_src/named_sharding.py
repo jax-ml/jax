jax-ml/jax

commit 21c3eb047faefbc0a7741c3750740dac167a338d

Make SdyArray immutable and cache SdyArray to sharding attribute conversion.
PiperOrigin-RevId: 902899761

jax/_src/named_sharding.py
@@ -41,8 +41,7 @@ class AUTO:
     self.mesh = mesh
 
   def _to_sdy_sharding(self, ndim: int) -> SdyArray:
-    dim_shardings = [SdyDim(axes=[], is_open=True)
-                     for _ in range(ndim)]
+    dim_shardings = (SdyDim(axes=(), is_open=True),) * ndim
     return SdyArray(mesh_shape=self.mesh.shape_tuple,
                     dim_shardings=dim_shardings)
 
@@ -267,19 +266,20 @@ class NamedSharding(jsharding.Sharding):
     return named_sharding_to_xla_hlo_sharding(self, num_dimensions)
 
   def _to_sdy_sharding(self, num_dimensions: int) -> SdyArray:
-    dim_shardings = [SdyDim(axes=[], is_open=False)
-                     for _ in range(num_dimensions)]
-    for i, dim_spec in enumerate(self.spec):
+    dim_shardings = []
+    for dim_spec in self.spec:
       if dim_spec is PartitionSpec.UNCONSTRAINED:
-        dim_shardings[i].is_open = True
+        dim_shardings.append(SdyDim(axes=(), is_open=True))
       elif dim_spec is None:
         # Already empty and closed sharding.
-        pass
+        dim_shardings.append(SdyDim(axes=(), is_open=False))
       else:
         dim_spec = dim_spec if isinstance(dim_spec, tuple) else (dim_spec,)
-        dim_shardings[i].axes = dim_spec
+        dim_shardings.append(SdyDim(axes=dim_spec, is_open=False))
+    dim_shardings.extend(
+      [SdyDim(axes=(), is_open=False)] * (num_dimensions - len(self.spec)))
     return SdyArray(mesh_shape=self.mesh.shape_tuple,
-                    dim_shardings=dim_shardings,
+                    dim_shardings=tuple(dim_shardings),
                     logical_device_ids=self._logical_device_ids,
                     unreduced_axes=self.spec.unreduced)
 
@@ -306,9 +306,9 @@ def get_array_mapping(axis_resources):
       d[axis] = i
   return d
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class SdyDim:
-  axes: Sequence[str]
+  axes: tuple[str, ...]
   is_open: bool
 
   def build(self) -> sdy.DimensionShardingAttr:
@@ -334,15 +334,19 @@ def _get_axes(axes, mesh_shape):
   # McJAX.
   return tuple(n for n, _ in mesh_shape if n in axes)
 
-@dataclasses.dataclass(kw_only=True)
+@dataclasses.dataclass(kw_only=True, frozen=True)
 class SdyArray:
   mesh_shape: tuple[tuple[str, int], ...] | None
-  dim_shardings: Sequence[SdyDim]
+  dim_shardings: tuple[SdyDim, ...]
   logical_device_ids: tuple[int, ...] | None = None
   replicated_axes: tuple[str, ...] = ()
   unreduced_axes: frozenset[str] = frozenset()
 
-  def build(self) -> sdy.TensorShardingAttr:
+  def build(self, cache: dict[SdyArray, sdy.TensorShardingAttr]) -> sdy.TensorShardingAttr:
+    attr = cache.get(self)
+    if attr is not None:
+      return attr
+
     if self.mesh_shape is None:
       mesh_attr = sdy.MeshAttr.get([])
     else:
@@ -354,11 +358,13 @@ class SdyArray:
 
     replicated_axes = _get_axes(self.replicated_axes, self.mesh_shape)
     unreduced_axes = _get_axes(self.unreduced_axes, self.mesh_shape)
-    return sdy.TensorShardingAttr.get(
+    attr = sdy.TensorShardingAttr.get(
         mesh_attr,
         [dim_sharding.build() for dim_sharding in self.dim_shardings],
         replicated_axes=[sdy.AxisRefAttr.get(axis) for axis in replicated_axes],
         unreduced_axes=[sdy.AxisRefAttr.get(axis) for axis in unreduced_axes])
+    cache[self] = attr
+    return attr
 
   def __repr__(self):
     dim_sharding_repr = ', '.join(
@@ -382,7 +388,7 @@ def modify_sdy_sharding_wrt_axis_types(sdy_sharding: SdyArray, mesh):
     replicated_axes = tuple(r for r in remaining_axes
                             if mesh._name_to_type[r] == mesh_lib.AxisType.Explicit)
     return SdyArray(mesh_shape=sdy_sharding.mesh_shape,
-                    dim_shardings=dim_shardings,
+                    dim_shardings=tuple(dim_shardings),
                     logical_device_ids=sdy_sharding.logical_device_ids,
                     replicated_axes=replicated_axes)
   return sdy_sharding

jax/_src/callback.py
@@ -142,7 +142,7 @@ def _get_sdy_array_list_for_callbacks(avals: Sequence[core.ShapedArray]) -> SdyA
   return SdyArrayList([
       SdyArray(
           mesh_shape=(),
-          dim_shardings=[SdyDim(axes=[], is_open=False)] * ndim,
+          dim_shardings=(SdyDim(axes=(), is_open=False),) * ndim,
           logical_device_ids=()) for ndim in ndims])
 
 
@@ -205,7 +205,7 @@ def _callback_op_sharding(
       op_sharding = SdyArrayList(num_sdy_shardings * [
           SdyArray(
               mesh_shape=(),
-              dim_shardings=[],
+              dim_shardings=(),
               logical_device_ids=(device_index,))])
     else:
       op_sharding = xc.OpSharding()
@@ -593,6 +593,7 @@ _XLA_HOST_TRANSFER_PJRT_RENDEZVOUS_HANDLER_NAME = "pjrt_rendezvous"
 
 
 def send_to_host(
+    ctx: mlir.ModuleContext,
     channel: int,
     token: ir.Value[hlo.TokenType],
     operand: Any,
@@ -622,13 +623,14 @@ def send_to_host(
       assert len(sharding.shardings) >= 1
       sharding = SdyArrayList([
           SdyArray(
-              mesh_shape=(), dim_shardings=[],
+              mesh_shape=(), dim_shardings=(),
               logical_device_ids=sharding.shardings[0].logical_device_ids)])
-    mlir.set_sharding(send_op, sharding)
+    mlir.set_sharding(ctx, send_op, sharding)
   return send_op.result
 
 
 def receive_from_host(
+    ctx: mlir.ModuleContext,
     channel: int,
     token: ir.Value[hlo.TokenType],
     out_aval: core.ShapedArray,
@@ -662,9 +664,9 @@ def receive_from_host(
       sharding = SdyArrayList([
           sharding.shardings[0],
           SdyArray(
-              mesh_shape=(), dim_shardings=[],
+              mesh_shape=(), dim_shardings=(),
               logical_device_ids=sharding.shardings[0].logical_device_ids)])
-    mlir.set_sharding(recv_op, sharding)
+    mlir.set_sharding(ctx, recv_op, sharding)
   # Token should be at the end of the results
   result, token = recv_op.results
   return token, result
@@ -719,13 +721,13 @@ def _emit_tpu_python_callback(
     dummy_send_aval = core.ShapedArray((1,), np.float32)
     dummy_send_val = mlir.ir_constant(np.zeros(1, np.float32))
     operand_shapes = [*operand_shapes, _aval_to_xla_shape(dummy_send_aval)]
-    token = send_to_host(send_channel, token, dummy_send_val,
+    token = send_to_host(ctx.module_context, send_channel, token, dummy_send_val,
                          sharding=sharding)
     send_channels.append(send_channel)
   else:
     for operand in operands:
       channel = ctx.module_context.new_channel()
-      token = send_to_host(channel, token, operand, sharding=sharding)
+      token = send_to_host(ctx.module_context, channel, token, operand, sharding=sharding)
       send_channels.append(channel)
 
   recv_channels = []
@@ -742,7 +744,7 @@ def _emit_tpu_python_callback(
     result_shapes = [_aval_to_xla_shape(dummy_recv_aval)]
     channel = ctx.module_context.new_channel()
     token, _ = receive_from_host(
-        channel, token, dummy_recv_aval, sharding=sharding
+        ctx.module_context, channel, token, dummy_recv_aval, sharding=sharding
     )
     recv_channels.append(channel)
   else:
@@ -750,7 +752,7 @@ def _emit_tpu_python_callback(
       channel = ctx.module_context.new_channel()
       assert isinstance(result_aval, core.ShapedArray)
       token, out = receive_from_host(
-          channel, token, result_aval, sharding=sharding
+          ctx.module_context, channel, token, result_aval, sharding=sharding
       )
       outputs.append(out)
       recv_channels.append(channel)
@@ -879,7 +881,7 @@ def emit_python_callback(
       sharding = SdyArrayList([
           SdyArray(
               mesh_shape=(),
-              dim_shardings=[],
+              dim_shardings=(),
               logical_device_ids=()),
           *sharding.shardings])
     ctx = dataclasses.replace(
@@ -899,7 +901,7 @@ def emit_python_callback(
   )(ctx, *operands, index=np.uint64(index))
 
   if sharding is not None:
-    mlir.set_sharding(result, sharding)
+    mlir.set_sharding(ctx.module_context, result, sharding)
 
   results = result.results
 

jax/_src/debugging.py
@@ -186,7 +186,7 @@ def debug_callback_lowering(ctx, *args, effect, partitioned, callback, **params)
     if config.use_shardy_partitioner.value:
       sharding = sharding_impls.SdyArrayList([
           sharding_impls.SdyArray(
-              mesh_shape=(), dim_shardings=[], logical_device_ids=(0,))])
+              mesh_shape=(), dim_shardings=(), logical_device_ids=(0,))])
     else:
       sharding = xc.OpSharding()
       sharding.type = xc.OpSharding.Type.MAXIMAL

jax/_src/interpreters/mlir.py
@@ -53,6 +53,7 @@ from jax._src.lib import jaxlib_extension_version
 from jax._src.lib import xla_client as xc
 from jax._src.lib.mlir import dialects, ir, passmanager
 from jax._src.lib.mlir.dialects import func as func_dialect, hlo
+from jax._src.lib.mlir.dialects import sdy
 from jax._src.mesh import AxisType
 from jax._src.partition_spec import PartitionSpec
 from jax._src.sharding import Sharding as JSharding
@@ -773,6 +774,7 @@ class ModuleContext:
   # Cached primitive lowerings.
   lowering_cache: dict[LoweringCacheKey, LoweringCacheValue]
   cached_primitive_lowerings: dict[Any, func_dialect.FuncOp]
+  sharding_attr_cache: dict[SdyArray, sdy.TensorShardingAttr]
 
   # Cached traceback information.
   traceback_caches: TracebackCaches
@@ -801,7 +803,8 @@ class ModuleContext:
       cached_primitive_lowerings: None | dict[Any, func_dialect.FuncOp] = None,
       traceback_caches: None | TracebackCaches = None,
       shape_poly_state = None,
-      all_default_mem_kind: bool = True):
+      all_default_mem_kind: bool = True,
+      sharding_attr_cache: None | dict[SdyArray, sdy.TensorShardingAttr] = None):
 
     self.context = context or make_ir_context()
     self.module = module or ir.Module.create(loc=ir.Location.unknown(self.context))
@@ -823,6 +826,7 @@ class ModuleContext:
       shape_poly_state or ShapePolyLoweringState((), tuple(platforms)))
     self.all_default_mem_kind = all_default_mem_kind
     self.lowering_parameters = lowering_parameters
+    self.sharding_attr_cache = ({} if sharding_attr_cache is None else sharding_attr_cache)
 
   def get_backend(self, optional: bool = False) -> xc.Client | None:
     if len(self.platforms) > 1:
@@ -1787,9 +1791,9 @@ def lower_jaxpr_to_fun(
       for attrs, sharding in zip(arg_attrs, ir_arg_shardings):
         if sharding is not None:
           if config.use_shardy_partitioner.value:
-            attrs["sdy.sharding"] = get_sharding_attr(sharding)
+            attrs["sdy.sharding"] = get_sharding_attr(ctx, sharding)
           else:
-            attrs["mhlo.sharding"] = get_sharding_attr(sharding)
+            attrs["mhlo.sharding"] = get_sharding_attr(ctx, sharding)
 
     if ir_arg_memory_kinds is not None:
       for attrs, memory_kind in zip(arg_attrs, ir_arg_memory_kinds):
@@ -1861,9 +1865,9 @@ def lower_jaxpr_to_fun(
                                    unconstrained_variants):  # pyrefly: ignore[bad-argument-type]
       if sharding is not None and not uv.contains_unconstrained:
         if config.use_shardy_partitioner.value:
-          attrs["sdy.sharding"] = get_sharding_attr(sharding)
+          attrs["sdy.sharding"] = get_sharding_attr(ctx, sharding)
         else:
-          attrs["mhlo.sharding"] = get_sharding_attr(sharding)
+          attrs["mhlo.sharding"] = get_sharding_attr(ctx, sharding)
 
   if ir_result_memory_kinds is not None:
     for attrs, mem_kind in zip(result_attrs, ir_result_memory_kinds):
@@ -2033,10 +2037,10 @@ def replicate_trailing_dims(ctx, val: ir.Value, aval) -> ir.Value:
     physical_ndim = core.physical_aval(aval).ndim
     s = SdyArray(
         mesh_shape=None,
-        dim_shardings=[
-            sharding_impls.SdyDim(axes=[], is_open=i < aval.ndim)
+        dim_shardings=tuple(
+            sharding_impls.SdyDim(axes=(), is_open=i < aval.ndim)
             for i in range(physical_ndim)
-        ])
+        ))
     return wrap_with_sharding_op(ctx, val, aval, s)
   else:
     return wrap_with_sharding_op(
@@ -2997,7 +3001,7 @@ def _wrap_with_spmd_op(name: str,
                        has_side_effect: bool = False,
                        allow_shardy_lowering: bool = False):
   if config.use_shardy_partitioner.value and allow_shardy_lowering:
-    return dialects.sdy.ShardingConstraintOp(x, sharding.build()).result  # pyrefly: ignore[missing-attribute]
+    return dialects.sdy.ShardingConstraintOp(x, sharding.build(ctx.module_context.sharding_attr_cache)).result  # pyrefly: ignore[missing-attribute]
 
   # unspecified_dims indicate dimensions whose shardings are not specified and
   # XLA sharding propagation can change them.
@@ -3018,7 +3022,7 @@ def _wrap_with_spmd_op(name: str,
                    api_version=1,
                    result_shapes=result_shapes,
                    has_side_effect=has_side_effect)
-  set_sharding(op, sharding)
+  set_sharding(ctx.module_context, op, sharding)
   return op.result
 
 
@@ -3052,18 +3056,20 @@ def lower_with_sharding_in_types(ctx, op, aval):
     return wrap_with_sharding_op(ctx, op, aval, proto, unspecified_dims)
 
 
-def set_sharding(op, sharding: xc.OpSharding | SdyArray | SdyArrayList):
+def set_sharding(ctx: ModuleContext, op,
+                 sharding: xc.OpSharding | SdyArray | SdyArrayList):
   if isinstance(sharding, (SdyArray, SdyArrayList)):
-    op.attributes["sdy.sharding"] = get_sharding_attr(sharding)
+    op.attributes["sdy.sharding"] = get_sharding_attr(ctx, sharding)
   else:
-    op.attributes["mhlo.sharding"] = get_sharding_attr(sharding)
+    op.attributes["mhlo.sharding"] = get_sharding_attr(ctx, sharding)
 
 
 def get_sharding_attr(
+    ctx: ModuleContext,
     sharding: xc.OpSharding | SdyArray | SdyArrayList
 ) -> ir.Attribute:
   if isinstance(sharding, (SdyArray, SdyArrayList)):
-    return sharding.build()
+    return sharding.build(ctx.sharding_attr_cache)
   else:
     # If there are very large numbers of devices, use the proto representation.
     # The MHLO to HLO conversion supports both, and the proto representation is

jax/_src/lax/parallel.py
@@ -1186,7 +1186,7 @@ def _psend_lowering_gpu(ctx, x, *, axis_name, perm):
 
   sharding = xc.OpSharding()
   sharding.type = xc.OpSharding.Type.MANUAL
-  mlir.set_sharding(send_op, sharding)
+  mlir.set_sharding(ctx.module_context, send_op, sharding)
   return send_op.results
 
 
@@ -1230,7 +1230,7 @@ def _precv_lowering_gpu(ctx, token, *, out_shape, axis_name, perm):
 
   sharding = xc.OpSharding()
   sharding.type = xc.OpSharding.Type.MANUAL
-  mlir.set_sharding(recv_op, sharding)
+  mlir.set_sharding(ctx.module_context, recv_op, sharding)
 
   # recv_op should return an array of [RankedTensorType, StableHlo.token]; we
   # only need the tensor.

jax/_src/shard_map.py
@@ -14,6 +14,7 @@
 from __future__ import annotations
 
 from collections.abc import Callable, Hashable, Sequence, Set
+import dataclasses
 import enum
 from functools import partial
 import inspect
@@ -914,8 +915,11 @@ def _shardy_shard_map_sharding(
     aval_in = core.physical_aval(aval_in)
   sdy_sharding = ns._to_sdy_sharding(aval_in.ndim)
   if len(manual_axes) < len(mesh.axis_names):
-    for dim_sharding in sdy_sharding.dim_shardings:
-      dim_sharding.is_open = True
+    new_dim_shardings = tuple(
+        dataclasses.replace(d, is_open=True) for d in sdy_sharding.dim_shardings
+    )
+    sdy_sharding = dataclasses.replace(
+      sdy_sharding, dim_shardings=new_dim_shardings)
   return sdy_sharding
 
 
@@ -984,14 +988,16 @@ def _shard_map_lowering_shardy(
   in_shardings = (
       [_get_token_sharding(ctx, mesh)] * (num_tokens + num_dim_vars) +
       const_args_shardings + in_shardings)
-  in_shardings = sharding_impls.SdyArrayList(in_shardings).build()
+  in_shardings = sharding_impls.SdyArrayList(in_shardings).build(
+    ctx.module_context.sharding_attr_cache)
 
   out_shardings = list(
       map(partial(_shardy_shard_map_sharding, ctx, mesh, manual_axes),
           out_specs, ctx.avals_out))
   out_shardings = [
       _get_token_sharding(ctx, mesh)] * num_tokens + out_shardings
-  out_shardings = sharding_impls.SdyArrayList(out_shardings).build()
+  out_shardings = sharding_impls.SdyArrayList(out_shardings).build(
+    ctx.module_context.sharding_attr_cache)
 
   output_types = ([hlo.TokenType.get()] * num_tokens +
                   mlir.flatten_ir_types(map(mlir.aval_to_ir_types, ctx.avals_out)))

jax/_src/sharding_impls.py
@@ -90,9 +90,9 @@ def device_replica_id_map(sharding, global_shape: Shape) -> Mapping[Device, int]
 class SdyArrayList:
   shardings: Sequence[SdyArray]
 
-  def build(self) -> sdy.TensorShardingPerValueAttr:
+  def build(self, cache: dict[SdyArray, sdy.TensorShardingAttr]) -> sdy.TensorShardingPerValueAttr:
     return sdy.TensorShardingPerValueAttr.get(
-        [sharding.build() for sharding in self.shardings])
+        [sharding.build(cache) for sharding in self.shardings])
 
 
 replicated_hlo_sharding = xc.HloSharding.replicate()
@@ -169,8 +169,7 @@ class SingleDeviceSharding(jsharding.Sharding):
     return replicated_hlo_sharding
 
   def _to_sdy_sharding(self, num_dimensions: int) -> SdyArray:
-    sdy_dim_sharding = [SdyDim(axes=[], is_open=False)
-                        for _ in range(num_dimensions)]
+    sdy_dim_sharding = (SdyDim(axes=(), is_open=False),) * num_dimensions
     return SdyArray(mesh_shape=None, dim_shardings=sdy_dim_sharding)
 
   @property

commit 1e6d1ddaa652a0c6eab4ce85a5dc522ce1cb2670

Don't create too many dim_shardings in named_sharding_to_sdy_sharding.
Fixes some breakage from https://github.com/jax-ml/jax/pull/37007

PiperOrigin-RevId: 903218366

jax/_src/named_sharding.py
@@ -476,7 +476,9 @@ def named_sharding_to_xla_hlo_sharding(
 def named_sharding_to_sdy_sharding(self, num_dimensions: int,
                                    modify_wrt_axis_types: bool) -> SdyArray:
   dim_shardings = []
-  for dim_spec in self.spec:
+  for i, dim_spec in enumerate(self.spec):
+    if i >= num_dimensions:
+      break
     if dim_spec is PartitionSpec.UNCONSTRAINED:
       dim_shardings.append(SdyDim(axes=(), is_open=True))
     elif dim_spec is None:
@@ -486,7 +488,7 @@ def named_sharding_to_sdy_sharding(self, num_dimensions: int,
       dim_spec = dim_spec if isinstance(dim_spec, tuple) else (dim_spec,)
       dim_shardings.append(SdyDim(axes=dim_spec, is_open=False))
   dim_shardings.extend(
-      [SdyDim(axes=(), is_open=False)] * (num_dimensions - len(self.spec)))
+      [SdyDim(axes=(), is_open=False)] * (num_dimensions - len(dim_shardings)))
 
   explicit_replicated_axes = frozenset()
   if modify_wrt_axis_types and self.mesh._any_axis_auto: