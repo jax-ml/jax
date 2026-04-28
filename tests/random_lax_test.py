4488d838f2fc38f2f9e11bc1a0a3e8b3b75131cb
Sam Anklesaria <sanklesaria@openteams.com>
2026-04-28 11:32:20 -0500

    [random] Add out_sharding and correct shape and dtype to jax.random.double_sided_maxwell

--- tests/random_lax_test.py
+++ tests/random_lax_test.py
@@ -223,6 +223,7 @@ _OUT_SHARDING_CASES = [
     ('cauchy', lambda key, n, s: random.cauchy(key, shape=(n,), out_sharding=s)),
     ('chisquare', lambda key, n, s: random.chisquare(key, df=1.0, shape=(n,), out_sharding=s)),
     ('dirichlet', lambda key, n, s: random.dirichlet(key, jnp.ones(3), shape=(n,), out_sharding=s)),
+    ('double_sided_maxwell', lambda key, n, s: random.double_sided_maxwell(key, loc=0., scale=1., shape=(n,), out_sharding=s)),
     ('exponential', lambda key, n, s: random.exponential(key, shape=(n,), out_sharding=s)),
     ('f', lambda key, n, s: random.f(key, dfnum=2.0, dfden=3.0, shape=(n,), out_sharding=s)),
     ('gumbel', lambda key, n, s: random.gumbel(key, shape=(n,), out_sharding=s)),
