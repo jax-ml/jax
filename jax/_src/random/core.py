e556ec310cb21bcbbeebc98ea933df6bb952ab25
Yash Katariya <yashkatariya@google.com>
2026-05-19 13:57:41 -0700

    Update out_sharding in _maxwell to match the new shape and explicitly mark the added dimension as replicated
    PiperOrigin-RevId: 918013478

--- jax/_src/random/core.py
+++ jax/_src/random/core.py
@@ -2535,6 +2535,10 @@ def maxwell(key: ArrayLike,
 @jit(static_argnums=(1, 2, 3))
 def _maxwell(key, shape, dtype, out_sharding) -> Array:
   shape = shape + (3,)
+  if out_sharding is not None:
+    new_partitions = (*out_sharding.spec, None)
+    out_sharding = out_sharding.update(
+        spec=out_sharding.spec.update(partitions=new_partitions))
   norm_rvs = normal(key=key, shape=shape, dtype=dtype, out_sharding=out_sharding)
   return jnp_linalg.norm(norm_rvs, axis=-1)
 
