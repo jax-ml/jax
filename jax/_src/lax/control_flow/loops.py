8e4f9e621f57559786501821f3722317363dc865
Junwhan Ahn <junwhan@google.com>
2026-06-08 18:15:09 -0700

    Reverts c3da028b6bd1e791c44e1d9d36b5771e1304cb46
    PiperOrigin-RevId: 928873667

--- jax/_src/lax/control_flow/loops.py
+++ jax/_src/lax/control_flow/loops.py
@@ -715,13 +715,20 @@ def _split_leading(sz, x):
 def _concat(a, b): return lax.concatenate([a, b], 0)
 
 def _empty_array(prefix, length_spec, aval):
-  out_sharding = aval.sharding.update(spec=aval.sharding.spec.update(
+  sharding = aval.sharding.update(spec=aval.sharding.spec.update(
       partitions=(*length_spec, *aval.sharding.spec.partitions)))
-  with use_abstract_mesh(out_sharding.mesh):
-    empty = lax.empty(
-        (*prefix, *aval.shape), aval.dtype, out_sharding=out_sharding,
-        out_memory_space=aval.memory_space)
-  return core.pvary(empty, tuple(aval.mat.varying))
+  # TODO(yashkatariya): Replace `lax.empty2` with `lax.empty` once
+  # AllocateBuffer issues are fixed. Also delete `empty2` after this usage is
+  # removed. Basically uncomment the following 2 lines.
+  # lax.empty will also need to take a memory_space argument.
+  # empty = lax.empty((*prefix, *aval.shape), aval.dtype, out_sharding=sharding,
+  #                   memory_space=aval.memory_space)
+  # return core.pvary(empty, tuple(aval.mat.varying))
+  empty = core.pvary(lax.empty2(aval.dtype, memory_space=aval.memory_space),
+                     tuple(aval.mat.varying))
+  with use_abstract_mesh(sharding.mesh):
+    out = lax.broadcast(empty, (*prefix, *aval.shape), out_sharding=sharding)
+  return out
 
 
 def _scan_abstract_eval(*args, reverse, length, num_consts, num_carry, jaxpr,
