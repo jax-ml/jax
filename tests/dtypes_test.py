19b97ed1c93bdf3f9f5818962e16c191f593bd3a
Pedro Gonnet <gonnet@google.com>
2026-06-16 07:31:15 -0700

    Make sure every numerical type has it's own type.
    This allows static type checking, e.g.
    ```py
    @overload
    def GetThing(dtype: type[jnp.int8] | type[jnp.int16] | ...) -> IntThing:
      ...
    
    @overload
    def GetThing(dtype: type[jnp.float16] | type[jnp.float32] | ...) -> FloatThing:
      ...
    
    @overload
    def GetThing(dtype: jnp.dtype) -> Thing:
      ...
    ```
    
    PiperOrigin-RevId: 933079826

--- tests/dtypes_test.py
+++ tests/dtypes_test.py
@@ -17,24 +17,24 @@ import dataclasses
 import enum
 import functools
 from functools import partial
+import inspect
 import itertools
 import operator
+import re
 import types
 
 from absl.testing import absltest
 from absl.testing import parameterized
-
-import numpy as np
-
 import jax
 from jax import numpy as jnp
-from jax._src import earray
 from jax._src import config
 from jax._src import core
 from jax._src import dtypes
+from jax._src import earray
 from jax._src import literals
 from jax._src import test_util as jtu
 from jax._src.lax import lax as lax_internal
+import numpy as np
 
 config.parse_flags_with_absl()
 
@@ -122,6 +122,14 @@ UINT_DTYPES = {
   64: np.uint64,
 }
 
+jnp_dtypes = frozenset(
+    t
+    for n, t in inspect.getmembers(jnp)
+    if re.fullmatch(
+        r'(u?int\d+|b?float\d+(_e\d+m\d+(b\d+)?f?n?u?z?)?|complex\d+)', n
+    )
+)
+
 def identity(x):
   """A named identity function for use in tests"""
   return x
@@ -132,6 +140,21 @@ TypedComplex = literals.TypedComplex
 TypedNdArray = literals.TypedNdArray
 
 class DtypesTest(jtu.JaxTestCase):
+
+  @parameterized.named_parameters(
+      (f'{a.dtype.name}_{b.dtype.name}', a, b)
+      for a, b in itertools.product(
+          jnp_dtypes,
+          jnp_dtypes
+      )
+  )
+  def test_all_scalar_dtype_have_distinct_types(
+      self, a, b
+  ):
+    if a == b:
+      self.assertIsInstance(a, type(b))
+    else:
+      self.assertNotIsInstance(a, type(b))
 
   def test_canonicalize_type(self):
     expected = {
