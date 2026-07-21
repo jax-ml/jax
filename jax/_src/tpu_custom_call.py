d375708d1da23ed6acb138bc9d45901c6c4bc7ce
David Pizzuto <pizzud@google.com>
2026-07-21 09:35:07 -0700

    mosaic: Consistently serialize between as_tpu_kernel and lowered_as_tpu_kernel.
    lowered_as_tpu_kernel takes a module that has been lowered already, so the
    Mosaic mangling didn't happen. Let's add that mangling. Notably, we need to
    ensure two things for successful IR downgrading that are done by as_tpu_kernel
    today:
    
    - trigger mangling so that deserialization works at all
    
    - the serialization_format flag is set on the FuncOp so that demangling happens
      properly
    
    PiperOrigin-RevId: 951537248

--- jax/_src/tpu_custom_call.py
+++ jax/_src/tpu_custom_call.py
@@ -24,7 +24,7 @@ import enum
 import io
 import json
 import logging
-from typing import Any, TypedDict, cast
+from typing import Any, TypedDict
 
 from jax._src import api
 from jax._src import config
@@ -966,7 +966,7 @@ def lowered_as_tpu_kernel(
     flags: dict[str, bool | int | float] | None = None,
     allow_input_fusion: Sequence[bool] | None = None,
     input_output_aliases: tuple[tuple[int, int], ...] = (),
-    serialization_format: int | None = None,
+    serialization_format: int | None = 1,
     internal_scratch_in_bytes: int | None = None,
     disable_bounds_checks: bool = False,
     disable_semaphore_checks: bool = False,
@@ -976,10 +976,28 @@ def lowered_as_tpu_kernel(
     opt_level: OptLevel | None = None,
 ) -> Callable[..., Any]:
   device_type = _get_device_type(lowered_module)
-  lowered_module_asm = cast(
-      bytes,
-      lowered_module.operation.get_asm(binary=True, enable_debug_info=True),
-  )
+  ctx = lowered_module.context
+  with ctx, lowered_module.operation.location as _:
+    module_op = lowered_module.operation.clone(ip=False)
+    # Temporarily allow unregistered dialects for serialization.
+    prev_allow_unregistered_dialects = ctx.allow_unregistered_dialects
+    ctx.allow_unregistered_dialects = True
+    # We hardcode a specific version both here and below, since this path is
+    # only used by some internal tests that don't need serialization, but we do
+    # need a concrete version on the module.
+    current_ir_version = 15
+    try:
+      pipeline = PassManager.parse(
+          "builtin.module(mosaic-serde{serialize=true"
+          f" target-version={current_ir_version}}})"
+      )
+      pipeline.run(module_op)
+    finally:
+      ctx.allow_unregistered_dialects = prev_allow_unregistered_dialects
+    bytecode_buffer = io.BytesIO()
+    module_op.write_bytecode(bytecode_buffer, desired_version=0)
+    lowered_module_asm = bytecode_buffer.getvalue()
+
   if isinstance(has_side_effects, bool):
     has_side_effects = (
         TpuSideEffectType.PURE
@@ -988,7 +1006,7 @@ def lowered_as_tpu_kernel(
     )
   config = _lowered_to_custom_call_config(
       lowered_module_asm,
-      lowered_module_asm_version=None,
+      lowered_module_asm_version=current_ir_version,
       vmem_limit_bytes=vmem_limit_bytes,
       cost_estimate=cost_estimate,
       flags=flags,
