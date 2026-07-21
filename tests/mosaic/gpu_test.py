b9fc424ff97eaab045412be9c9503ff443aebc20
Adam Paszke <apaszke@google.com>
2026-07-21 06:27:53 -0700

    [Mosaic GPU] Add support for row-major ldmatrix/stmatrix for all dtypes
    We only require that vector_length * bitwidth == 32.
    
    PiperOrigin-RevId: 951453235

--- tests/mosaic/gpu_test.py
+++ tests/mosaic/gpu_test.py
@@ -3711,17 +3711,29 @@ class Sm80Test(TestCase):
     np.testing.assert_array_equal(y, np.array(1, dtype=np.int32))
 
   @parameterized.product(
-      dtype=(jnp.int16,),
+      dtype=(jnp.int8, jnp.int16, jnp.int32),
       m_mult=(1, 2, 3, 4, 6, 7, 9),
       k_mult=(1, 2, 3, 4, 6, 7, 9),
   )
   @jtu.thread_unsafe_test()
-  def test_ldmatrix(self, dtype, m_mult, k_mult):
+  def test_ldmatrix_basic(self, dtype, m_mult, k_mult, transpose=False):
     m, k = 32 * m_mult, 8 * k_mult
     dtype = jnp.dtype(dtype)
-    swizzle = next(s for s in (128, 64, 32, 16) if (k * 2) % s == 0)
+    in_shape = (k, m) if transpose else (m, k)
+    bitwidth = np.iinfo(dtype).bits
+    try:
+      swizzle = next(
+          s for s in (128, 64, 32, 16)
+          if (in_shape[1] * bitwidth // 8) % s == 0
+          # For async copy.
+          and (((in_shape[0] // 8) * ((in_shape[1] * bitwidth // 8) // s)) % 4 == 0)
+      )
+    except StopIteration:
+      self.skipTest("No suitable swizzle found.")
+    elems_per_reg = 32 // bitwidth
+    swizzle_elems = swizzle * 8 // bitwidth
     layout = fa.TiledLayout(
-        fa.Tiling(((32, 8), (8, 8), (2,))),
+        fa.Tiling(((32, 4 * elems_per_reg), (8, 4 * elems_per_reg), (elems_per_reg,))),
         warp_dims=(-5,),
         lane_dims=(-3, -2),
         vector_dim=-1,
@@ -3732,29 +3744,47 @@ class Sm80Test(TestCase):
           src_ref=a,
           dst_ref=a_smem,
           swizzle=swizzle,
-          gmem_transform=mgpu.TileTransform((8, swizzle // 2)),
+          gmem_transform=mgpu.TileTransform((8, swizzle_elems)),
           barrier=barrier,
           implementation=mgpu.AsyncCopyImplementation.CP_ASYNC,
       )
       barrier.wait()
+      if transpose:
+        a_smem = mgpu.memref_transpose(a_smem, (1, 0, 3, 2))
       a_fa = mgpu.FragmentedArray.load_tiled(
           a_smem, layout=layout, swizzle=swizzle, is_signed=True
       )
       a_fa.store_untiled(out, optimized=False)
 
-    x = self.prng.integers(-10000, 10000, (m, k)).astype(dtype)
+    x = self.prng.integers(-10000, 10000, in_shape).astype(dtype)
+    out_shape = jax.ShapeDtypeStruct((m, k), dtype)
     with jtu.set_env(MOSAIC_GPU_DUMP_PTX="1"), self.capture_stdout() as ptx:
       y = mgpu.as_gpu_kernel(
-          kernel, (1, 1, 1), (128, 1, 1), x, x,
+          kernel, (1, 1, 1), (128, 1, 1), x, out_shape,
           (
-              jax.ShapeDtypeStruct(tile_shape(x.shape, (8, swizzle // 2)), dtype),
+              jax.ShapeDtypeStruct(tile_shape(in_shape, (8, swizzle_elems)), dtype),
               mgpu.TMABarrier(1)
           ),
       )(x)
-      np.testing.assert_array_equal(y, x)
-    num = next(n for n in (4, 2, 1) if (m_mult * k_mult) % n == 0)
-    self.assertIn(f"ldmatrix.sync.aligned.m8n8.x{num}.shared", ptx())
+      expected = x.T if transpose else x
+      np.testing.assert_array_equal(y, expected)
+    num_per_tile = 8 / (4 * elems_per_reg)
+    num = next(n for n in (4, 2, 1) if int(num_per_tile * m_mult * k_mult) % n == 0)
+    expected_instr = (
+        f"ldmatrix.sync.aligned.m8n8.x{num}.trans.shared"
+        if transpose
+        else f"ldmatrix.sync.aligned.m8n8.x{num}.shared"
+    )
+    self.assertIn(expected_instr, ptx())
     self.assertNotIn("ld.shared", ptx())
+
+  @parameterized.product(
+      m_mult=(1, 2, 3, 4, 6, 7, 9),
+      k_mult=(1, 2, 3, 4, 6, 7, 9),
+  )
+  @jtu.thread_unsafe_test()
+  def test_ldmatrix_transpose(self, m_mult, k_mult):
+    return self.test_ldmatrix(jnp.int16, m_mult, k_mult, transpose=False)
 
 
 class BarrierTest(TestCase):
@@ -6086,27 +6116,40 @@ class FragmentedArrayTest(TestCase):
 
   @parameterized.product(
       dtype=(jnp.int16,),
+      transpose=(False, True),
   )
   @jtu.thread_unsafe_test()
-  def test_stmatrix(self, dtype):
+  def test_stmatrix(self, dtype, transpose):
     m, k = 128, 128
     dtype = jnp.dtype(dtype)
     swizzle = 128
     def kernel(ctx, x, out, x_smem):
       mma_layouts = mgpu.MMALayouts(utils.dtype_to_ir_type(dtype))
-      x_fa = mgpu.FragmentedArray.load_untiled(x, layout=mma_layouts.lhs, is_signed=True, optimized=False)
-      x_fa.store_tiled(x_smem, swizzle=swizzle)
+      x_fa = mgpu.FragmentedArray.load_untiled(
+          x, layout=mma_layouts.lhs, is_signed=True, optimized=False
+      )
+      x_smem_store = x_smem
+      if transpose:
+        x_smem_store = mgpu.memref_transpose(x_smem, (1, 0, 3, 2))
+      x_fa.store_tiled(x_smem_store, swizzle=swizzle)
       mgpu.warpgroup_barrier()
       copy(x_smem, mgpu.TileTransform((8, swizzle // 2)).apply(out), swizzle)
 
     x = self.prng.integers(-10000, 10000, (m, k)).astype(dtype)
+    out_shape = (k, m) if transpose else (m, k)
     with jtu.set_env(MOSAIC_GPU_DUMP_PTX="1"), self.capture_stdout() as ptx:
       y = mgpu.as_gpu_kernel(
-          kernel, (1, 1, 1), (128, 1, 1), x, x,
-          jax.ShapeDtypeStruct(tile_shape(x.shape, (8, swizzle // 2)), dtype),
+          kernel, (1, 1, 1), (128, 1, 1), x, out_shape,
+          jax.ShapeDtypeStruct(tile_shape(out_shape, (8, swizzle // 2)), dtype),
       )(x)
-      np.testing.assert_array_equal(y, x)
-    self.assertIn("stmatrix.sync.aligned.m8n8.x4.shared", ptx())
+      expected = x.T if transpose else x
+      np.testing.assert_array_equal(y, expected)
+    expected_instr = (
+        "stmatrix.sync.aligned.m8n8.x4.trans.shared"
+        if transpose
+        else "stmatrix.sync.aligned.m8n8.x4.shared"
+    )
+    self.assertIn(expected_instr, ptx())
     self.assertNotIn("st.shared", ptx())
 
 
