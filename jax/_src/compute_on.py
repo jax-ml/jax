dd430b64ac123b07ec978b4a6013581f700923c4
Yash Katariya <yashkatariya@google.com>
2026-07-29 21:08:03 -0700

    Don't call `update` on tokens in compute_on_abstract_eval
    PiperOrigin-RevId: 956251363

--- jax/_src/compute_on.py
+++ jax/_src/compute_on.py
@@ -123,8 +123,8 @@ dispatch.simple_impl(compute_on_p)
 
 def _compute_on_abstract_eval(*in_avals, jaxpr, compute_type, out_memory_spaces,
                               compiler_options_json):
-  out_avals = [a.update(memory_space=s)
-               for a, s in zip(jaxpr.out_avals, out_memory_spaces)]
+  out_avals = [a.update(memory_space=s) if isinstance(a, core.ShapedArray)
+               else a for a, s in zip(jaxpr.out_avals, out_memory_spaces)]
   return out_avals, core.positional_effects(jaxpr)
 compute_on_p.def_effectful_abstract_eval(_compute_on_abstract_eval)
 
