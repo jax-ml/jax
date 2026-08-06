b2f83130e628cde642edc8467f50ae0038a235dc
vfdev-5 <vfdev.5@gmail.com>
2026-08-06 09:05:50 +0200

    WIP

--- tests/pallas/mgpu_torch_test.py
+++ tests/pallas/mgpu_torch_test.py
@@ -16,6 +16,7 @@
 import functools
 
 from absl.testing import absltest
+from absl.testing import parameterized
 
 import jax
 from jax._src import config
@@ -196,6 +197,170 @@ class TorchTest(jtu.JaxTestCase):
         x.cpu(), torch.ones((128,), dtype=torch.float32, device="cpu")
     )
 
+  @parameterized.parameters(
+      plgpu.LoweringSemantics.Lane,
+      plgpu.LoweringSemantics.Warpgroup,
+  )
+  def test_simple_matmul(self, lowering_semantics):
+    # TODO: put CC version constraints: CC >= 12.0
+    m, n, k = 128, 128, 64
+    acc_dtype = jnp.float32
+    dtype = jnp.bfloat16
+
+    to_torch_dtypes = {
+      jnp.float32: torch.float32,
+      jnp.float16: torch.float16,
+      jnp.bfloat16: torch.bfloat16,
+    }
+
+    @plgpu.kernel(
+        out_type=jax.ShapeDtypeStruct((m, n), acc_dtype),
+        compiler_params=plgpu.CompilerParams(lowering_semantics=lowering_semantics),
+    )
+    def kernel(x_ref, y_ref, o_ref):
+      acc = plgpu.layout_cast(
+          jnp.zeros((m, n), acc_dtype), plgpu.Layout.MMA_ACC(dtype)
+      )
+      x = plgpu.load(
+          x_ref, layout=plgpu.Layout.MMA_LHS(dtype), optimized=False
+      )
+      y = plgpu.load(
+          y_ref.T, layout=plgpu.Layout.MMA_RHS(dtype), optimized=False
+      )
+      o_ref[...] = plgpu.mma(acc, x, y)
+
+    gen = torch.Generator(device="cuda").manual_seed(123)
+    x = torch.rand(m, k, generator=gen, dtype=to_torch_dtypes[dtype], device="cuda")
+    y = torch.rand(n, k, generator=gen, dtype=to_torch_dtypes[dtype], device="cuda")
+    out = plgpu.as_torch_kernel(kernel)(x, y)
+    expected = (x.to(to_torch_dtypes[acc_dtype]) @ y.T.to(to_torch_dtypes[acc_dtype]))
+    np.testing.assert_allclose(out.cpu(), expected.cpu(), atol=1e-2, rtol=1e-2)
+
+  @parameterized.parameters(
+      plgpu.LoweringSemantics.Lane,
+      plgpu.LoweringSemantics.Warpgroup,
+  )
+  def test_matmul_with_smem(self, lowering_semantics):
+    # TODO: put CC version constraints: CC >= 12.0
+    m, n, k = 128, 128, 64
+    acc_dtype = jnp.float32
+    dtype = jnp.bfloat16
+
+    to_torch_dtypes = {
+      jnp.float32: torch.float32,
+      jnp.float16: torch.float16,
+      jnp.bfloat16: torch.bfloat16,
+    }
+
+    @plgpu.kernel(
+        out_type=jax.ShapeDtypeStruct((m, n), acc_dtype),
+        compiler_params=plgpu.CompilerParams(lowering_semantics=lowering_semantics),
+        scratch_types=[
+            plgpu.SMEM((m, k), dtype),
+            plgpu.SMEM((k, n), dtype),
+            plgpu.Barrier(num_arrivals=2),
+        ],
+    )
+    def kernel(x_ref, y_ref, o_ref, x_smem, y_smem, barrier_ref):
+      acc = plgpu.layout_cast(
+          jnp.zeros((m, n), acc_dtype), plgpu.Layout.MMA_ACC(dtype)
+      )
+
+      plgpu.copy_gmem_to_smem(x_ref, x_smem, barrier_ref)
+      plgpu.copy_gmem_to_smem(y_ref, y_smem, barrier_ref)
+      plgpu.barrier_wait(barrier_ref)
+
+      x = plgpu.load(
+          x_smem, layout=plgpu.Layout.MMA_LHS(dtype), optimized=False
+      )
+      y = plgpu.load(
+          y_smem, layout=plgpu.Layout.MMA_RHS(dtype), optimized=False
+      )
+      o_ref[...] = plgpu.mma(acc, x, y)
+
+    gen = torch.Generator(device="cuda").manual_seed(123)
+    x = torch.rand(m, k, generator=gen, dtype=to_torch_dtypes[dtype], device="cuda")
+    y = torch.rand(k, n, generator=gen, dtype=to_torch_dtypes[dtype], device="cuda")
+    out = plgpu.as_torch_kernel(kernel)(x, y)
+    expected = (x.to(to_torch_dtypes[acc_dtype]) @ y.to(to_torch_dtypes[acc_dtype]))
+    np.testing.assert_allclose(out.cpu(), expected.cpu(), atol=1e-2, rtol=1e-2)
+
+  @parameterized.parameters([
+      plgpu.LoweringSemantics.Lane,
+      plgpu.LoweringSemantics.Warpgroup,
+  ])
+  def test_matmul_with_pipeline(self, lowering_semantics):
+    # TODO: put CC version constraints: CC >= 12.0
+    m, n, k = 512, 512, 256
+    tile_m = tile_n = 64
+    tile_k = 64
+    out_dtype = jnp.float32
+    dtype = jnp.bfloat16
+
+    to_torch_dtypes = {
+      jnp.float32: torch.float32,
+      jnp.float16: torch.float16,
+      jnp.bfloat16: torch.bfloat16,
+    }
+
+    @plgpu.kernel(
+        out_type=jax.ShapeDtypeStruct((m, n), out_dtype),
+        compiler_params=plgpu.CompilerParams(lowering_semantics=lowering_semantics),
+        grid=(m // tile_m, n // tile_n),
+        grid_names=('m', 'n'),
+        scratch_types=[
+            plgpu.SMEM((tile_m, tile_n), out_dtype),  # output
+        ],
+    )
+    def kernel(x_gmem, y_gmem, o_gmem, o_smem):
+      pid_m = jax.lax.axis_index('m')
+      pid_n = jax.lax.axis_index('n')
+
+      acc = plgpu.layout_cast(
+          jnp.zeros((tile_m, tile_n), out_dtype), plgpu.Layout.MMA_ACC(dtype)
+      )
+
+      def body(_, x_smem, y_smem, carry):
+        x = plgpu.load(
+            x_smem, layout=plgpu.Layout.MMA_LHS(dtype), optimized=False
+        )
+        y = plgpu.load(
+            y_smem, layout=plgpu.Layout.MMA_RHS(dtype), optimized=False
+        )
+        return plgpu.mma(carry, x, y)
+
+      acc = plgpu.emit_pipeline(
+        body,
+        grid=(k // tile_k,),
+        in_specs=[
+            plgpu.BlockSpec(
+                (tile_m, tile_k), lambda ki: (pid_m, ki), delay_release=1
+            ),
+            plgpu.BlockSpec(
+                (tile_k, tile_n), lambda ki: (ki, pid_n), delay_release=1
+            ),
+        ],
+        max_concurrent_steps=2,
+        init_carry=acc,
+      )(x_gmem, y_gmem)
+
+      o_smem[...] = acc.astype(out_dtype)
+      plgpu.commit_smem()
+      plgpu.copy_smem_to_gmem(
+          o_smem,
+          o_gmem.at[
+              pl.ds(pid_m * tile_m, tile_m),
+              pl.ds(pid_n * tile_n, tile_n)
+          ],
+      )
+      plgpu.wait_smem_to_gmem(0)  # Wait for all copies to finish.
+
+    gen = torch.Generator(device="cuda").manual_seed(123)
+    x = torch.rand(m, k, generator=gen, dtype=to_torch_dtypes[dtype], device="cuda")
+    y = torch.rand(k, n, generator=gen, dtype=to_torch_dtypes[dtype], device="cuda")
+    out = plgpu.as_torch_kernel(kernel)(x, y)
+    expected = (x.to(to_torch_dtypes[out_dtype]) @ y.to(to_torch_dtypes[out_dtype]))
+    np.testing.assert_allclose(out.cpu(), expected.cpu(), atol=1e-2, rtol=1e-2)
 
 if __name__ == "__main__":
   absltest.main(testLoader=jtu.JaxTestLoader())
