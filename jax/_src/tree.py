e273139446ad63f3254cab20469aac9c87da2cfb
Jake VanderPlas <jakevdp@google.com>
2026-08-12 15:51:46 -0700

    [typing] use implicit typevars in lax, numpy, tree, and tree_util

--- jax/_src/tree.py
+++ jax/_src/tree.py
@@ -15,11 +15,9 @@ from __future__ import annotations
 
 from collections.abc import Callable, Iterable
 import dataclasses
-from typing import Any, TypeVar, TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
 
 from jax._src import tree_util
-
-T = TypeVar("T")
 
 
 def all(tree: Any, *, is_leaf: Callable[[Any], bool] | None = None) -> bool:
@@ -156,10 +154,10 @@ def map(f: Callable[..., Any],
   return tree_util.tree_map(f, tree, *rest, is_leaf=is_leaf)
 
 
-def reduce(function: Callable[[T, Any], T],
-           tree: Any,
-           initializer: T | tree_util.Unspecified = tree_util.Unspecified(),
-           is_leaf: Callable[[Any], bool] | None = None) -> T:
+def reduce[T](function: Callable[[T, Any], T],
+              tree: Any,
+              initializer: T | tree_util.Unspecified = tree_util.Unspecified(),
+              is_leaf: Callable[[Any], bool] | None = None) -> T:
   """Call reduce() over the leaves of a tree.
 
   Args:
@@ -193,7 +191,7 @@ def reduce(function: Callable[[T, Any], T],
   return tree_util.tree_reduce(function, tree, initializer, is_leaf=is_leaf)
 
 
-def reduce_associative(
+def reduce_associative[T](
     operation: Callable[[T, T], T],
     tree: Any,
     *,
