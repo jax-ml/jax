4fa096c614163d3b90da01f24d6ab92ca132db1c
Alex Y. Chan <alechan@nvidia.com>
2026-08-13 12:57:35 +0100

    [Pallas][Triton] Handle unnamed NVIDIA device in GpuInfo

--- jax/_src/pallas/triton/gpu_info.py
+++ jax/_src/pallas/triton/gpu_info.py
@@ -162,6 +162,16 @@ def get_gpu_info() -> GpuInfo:
     return GpuInfo(
         gpu_version=None, arch_name=device_kind, compute_capability=0)
 
+  # generic NVIDIA GPU device without GpuVersion entry
+  if get_device_platform() == "gpu" and device_kind.startswith("NVIDIA"):
+    gpu_arch_name = _get_device_arch_name(device_kind)
+    gpu_compute_capability = int(gpu_arch_name.replace(".", ""))
+    return GpuInfo(
+        gpu_version=None,
+        arch_name=gpu_arch_name,
+        compute_capability=gpu_compute_capability,
+    )
+
   if device_kind in registry:
     return registry[device_kind]()
   raise ValueError(f"Unsupported GPU device kind: {device_kind}")
@@ -178,6 +188,15 @@ def get_gpu_info_from_version(
   """
   return _get_gpu_info_impl(gpu_version)
 
+def _get_device_arch_name(device_kind: str) -> str:
+  concrete_device = pxla.get_default_device()
+
+  if device_kind != concrete_device.device_kind:
+    raise ValueError(
+        f"Cannot infer architecture of an unknown abstract GPU {device_kind!r}"
+    )
+
+  return concrete_device.compute_capability
 
 def get_device_kind() -> str:
   device = pxla.get_default_device()
