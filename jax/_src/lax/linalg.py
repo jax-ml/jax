Fix eigh gradients for large eigenvalues

jax/_src/lax/linalg.py
@@ -1357,7 +1357,8 @@ def _eigh_jvp_rule(
   eye_n = lax._eye(a.dtype, (n, n))
   # carefully build reciprocal delta-eigenvalue matrix, avoiding NaNs.
   with config.numpy_rank_promotion("allow"):
-    Fmat = lax.integer_pow(eye_n + w[..., np.newaxis, :] - w[..., np.newaxis], -1) - eye_n
+    delta_w = w[..., np.newaxis, :] - w[..., np.newaxis]
+    Fmat = lax.integer_pow(delta_w + eye_n, -1) - eye_n
   # eigh impl doesn't support batch dims, but future-proof the grad.
   dot = partial(lax.dot if a.ndim == 2 else lax.batch_matmul,
                 precision=lax.Precision.HIGHEST)