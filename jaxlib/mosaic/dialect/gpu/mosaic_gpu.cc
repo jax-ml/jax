ff478f46263fb3123ac747aa068acd30b9da3667
Allan Renucci <allanrenucci@google.com>
2026-08-31 02:31:59 -0700

    [NFC] Convert `BroadcastInDimOp` canonicalization pattern to a folder
    From https://mlir.llvm.org/docs/Canonicalization/#when-to-use-the-fold-method-vs-rewriterpatterns-for-canonicalizations:
    
    > A canonicalization should always be implemented as a `fold` method if it can be
    
    PiperOrigin-RevId: 973760058

--- jaxlib/mosaic/dialect/gpu/mosaic_gpu.cc
+++ jaxlib/mosaic/dialect/gpu/mosaic_gpu.cc
@@ -1376,39 +1376,24 @@ void ReinterpretCastOp::getCanonicalizationPatterns(
   patterns.add<FoldMGPUReinterpretCastOfSliceSMEM>(context);
 }
 
-namespace {
-
-struct FuseChainedBroadcastInDims
-    : public mlir::OpRewritePattern<BroadcastInDimOp> {
-  using OpRewritePattern::OpRewritePattern;
-
-  mlir::LogicalResult matchAndRewrite(
-      BroadcastInDimOp op, mlir::PatternRewriter& rewriter) const override {
-    auto parent_bcast = op.getOperand().getDefiningOp<BroadcastInDimOp>();
-    if (!parent_bcast) {
-      return mlir::failure();
-    }
-
-    llvm::ArrayRef<int64_t> dims1 = parent_bcast.getBroadcastDimensions();
-    llvm::ArrayRef<int64_t> dims2 = op.getBroadcastDimensions();
-
-    llvm::SmallVector<int64_t> new_dims;
-    new_dims.reserve(dims1.size());
-    for (int64_t dim : dims1) {
-      new_dims.push_back(dims2[dim]);
-    }
-
-    rewriter.replaceOpWithNewOp<BroadcastInDimOp>(
-        op, op.getType(), parent_bcast.getOperand(), new_dims);
-    return mlir::success();
+mlir::OpFoldResult BroadcastInDimOp::fold(FoldAdaptor) {
+  // Fold chains: broadcast_in_dim(broadcast_in_dim(x)) -> broadcast_in_dim(x).
+  auto parent_bcast = getOperand().getDefiningOp<BroadcastInDimOp>();
+  if (!parent_bcast) {
+    return {};
   }
-};
+  llvm::ArrayRef<int64_t> dims1 = parent_bcast.getBroadcastDimensions();
+  llvm::ArrayRef<int64_t> dims2 = getBroadcastDimensions();
 
-}  // namespace
+  llvm::SmallVector<int64_t> new_dims;
+  new_dims.reserve(dims1.size());
+  for (int64_t dim : dims1) {
+    new_dims.push_back(dims2[dim]);
+  }
 
-void BroadcastInDimOp::getCanonicalizationPatterns(
-    mlir::RewritePatternSet& patterns, mlir::MLIRContext* context) {
-  patterns.add<FuseChainedBroadcastInDims>(context);
+  getOperandMutable().assign(parent_bcast.getOperand());
+  setBroadcastDimensions(new_dims);
+  return getResult();
 }
 
 namespace {
