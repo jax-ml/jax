4ef69732588f4e65f6739a618285e3b2dfbd2e5e
David Pizzuto <pizzud@google.com>
2026-09-03 14:58:34 -0700

    pjrt_c_api_client: Remove "PJRT C API" from the platform version string.
    This allows easier alignment with internal codepaths that don't go through this
    particular case.
    
    PiperOrigin-RevId: 975941153

--- jaxlib/py_client.cc
+++ jaxlib/py_client.cc
@@ -857,6 +857,18 @@ PyType_Slot PyClient::slots_[] = {
             return 0;
           })
       .def_prop_ro("runtime_type", &PyClient::runtime_type)
+      .def_prop_ro(
+          "is_c_api",
+          [](PyClient& self) -> bool {
+            if (auto* pjrt_comp =
+                    xla::ifrt::dyn_cast_or_null<ifrt::PjRtCompatibleClient>(
+                        self.ifrt_client())) {
+              if (auto* pjrt_client = pjrt_comp->pjrt_client()) {
+                return pjrt_client->IsCApi();
+              }
+            }
+            return false;
+          })
       .def("device_count", &PyClient::device_count)
       .def("local_device_count", &PyClient::addressable_device_count)
       .def("devices", &PyClient::Devices)
