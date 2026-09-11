acf13732519a355928d9a6a4062aad65fc2b1279
Peter Hawkins <phawkins@google.com>
2026-09-10 17:14:27 -0700

    [tests] Set complex64 tolerance for lax.tan in tests.
    The tolerance for float32 was already relaxed to 3e-5, but complex64 was omitted and fell back to the 1e-6 default, causing testOpAgainstNumpy to fail on Windows.
    
    PiperOrigin-RevId: 979479874

--- jax/_src/internal_test_util/lax_test_util.py
+++ jax/_src/internal_test_util/lax_test_util.py
@@ -257,7 +257,7 @@ def lax_ops():
           1,
           float_dtypes + complex_dtypes,
           test_util.rand_default,
-          {np.float32: 3e-5},
+          {np.float32: 3e-5, np.complex64: 3e-5},
       ),
       op_record(
           "asin",
