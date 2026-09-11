5570f3d6fef9e44f44f3b06096674298a7e89ed3
Peter Hawkins <phawkins@google.com>
2026-09-10 23:25:56 -0700

    Skip SparsecoreOffloadTest tests that require libtpu >= 0.0.48.
    PiperOrigin-RevId: 979618875

--- tests/memories_test.py
+++ tests/memories_test.py
@@ -2013,6 +2013,8 @@ class SparsecoreOffloadTest(jtu.JaxTestCase):
         or jtu.is_device_tpu_at_least(6)
     ):
       self.skipTest("Does not have a sparsecore present")
+    if not jtu.is_libtpu_at_least("0.0.48"):
+      self.skipTest("Requires libtpu >= 0.0.48")
 
     arr1 = jax.device_put(np.arange(64 * 128).reshape(64, 128), P("x", None))
     arr2 = jax.device_put(np.arange(128 * 128).reshape(128, 128), P("x", None))
@@ -2089,6 +2091,8 @@ class SparsecoreOffloadTest(jtu.JaxTestCase):
         or jtu.is_device_tpu_at_least(6)
     ):
       self.skipTest("Does not have a sparsecore present")
+    if not jtu.is_libtpu_at_least("0.0.48"):
+      self.skipTest("Requires libtpu >= 0.0.48")
 
     arr1 = jax.device_put(np.arange(64 * 128).reshape(64, 128), P("x", None))
     arr2 = jax.device_put(np.arange(128 * 128).reshape(128, 128), P("x", None))
@@ -2199,6 +2203,8 @@ class SparsecoreOffloadTest(jtu.JaxTestCase):
   def test_sparsecore_two_rss(self):
     if not jtu.is_device_tpu_at_least(7):
       self.skipTest("Requires device with SC support and queuing enabled.")
+    if not jtu.is_libtpu_at_least("0.0.48"):
+      self.skipTest("Requires libtpu >= 0.0.48")
 
     mesh = jtu.create_mesh((8,), "x")
     arr = jnp.arange(512 * 256, dtype=np.float32).reshape(512, 256)
@@ -2234,6 +2240,8 @@ class SparsecoreOffloadTest(jtu.JaxTestCase):
   def test_sparsecore_two_ars(self):
     if not jtu.is_device_tpu_at_least(7):
       self.skipTest("Requires device with SC support and queuing enabled.")
+    if not jtu.is_libtpu_at_least("0.0.48"):
+      self.skipTest("Requires libtpu >= 0.0.48")
 
     mesh = jtu.create_mesh((8,), "x")
     arr = jnp.arange(512 * 256, dtype=np.float32).reshape(512, 256)
