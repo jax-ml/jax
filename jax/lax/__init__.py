acdd65b76c1f4441a59848647621ac31000109fd
Yash Katariya <yashkatariya@google.com>
2026-09-22 19:31:45 -0700

    Expose psum_scatter_start from jax.lax. But this is experimental and the API is subject to change.
    PiperOrigin-RevId: 986404398

--- jax/lax/__init__.py
+++ jax/lax/__init__.py
@@ -393,6 +393,7 @@ from jax._src.lax.parallel import (
   psum as psum,
   psum_p as psum_p,
   psum_scatter as psum_scatter,
+  psum_scatter_start as psum_scatter_start,
   pswapaxes as pswapaxes,
   ragged_all_to_all as ragged_all_to_all,
   ragged_all_to_all_p as ragged_all_to_all_p,
