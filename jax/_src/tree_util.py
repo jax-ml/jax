4ed249d532f0c43b56ed1c67b4963fc092b11557
Jake VanderPlas <vanderplas@google.com>
2026-09-25 14:20:41 -0700

    [cleanup] delete dead code: `jax._src.tree_util._ensure_inbounds`
    PiperOrigin-RevId: 988492102

--- jax/_src/tree_util.py
+++ jax/_src/tree_util.py
@@ -1375,17 +1375,3 @@ def _prefix_error(
      f"{prefix_tree_keys} and {full_tree_keys}")
   for k, t1, t2 in zip(prefix_tree_keys, prefix_tree_children, full_tree_children):
     yield from _prefix_error((*key_path, k), t1, t2)
-
-def _ensure_inbounds(allow_invalid: bool, num_args: int, argnums: Sequence[int]
-                     ) -> tuple[int, ...]:
-  """Ensure argnum is within bounds. Also resolves negative argnums."""
-  result = []
-  for i in argnums:
-    if i >= num_args and allow_invalid: continue
-    if not -num_args <= i < num_args:
-      raise ValueError(
-          "Positional argument indices, e.g. for `static_argnums`, must have "
-          "value greater than or equal to -len(args) and less than len(args), "
-          f"but got value {i} for len(args) == {num_args}.")
-    result.append(i % num_args)  # Resolve negative
-  return tuple(result)
